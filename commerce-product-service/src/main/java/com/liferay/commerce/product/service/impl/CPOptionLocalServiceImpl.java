7445854ef6e86fa1b5c5827a805b4ac5655ce351
Rodrigo Guedes de Souza <rodrigo.guedes@liferay.com>
2018-07-09 13:38:29 -0300

    COMMERCE-API Add upsert to CPOptionService

--- commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPOptionLocalServiceImpl.java
+++ commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPOptionLocalServiceImpl.java
@@ -18,6 +18,7 @@ import com.liferay.commerce.product.exception.CPOptionKeyException;
 import com.liferay.commerce.product.model.CPOption;
 import com.liferay.commerce.product.search.CPOptionIndexer;
 import com.liferay.commerce.product.service.base.CPOptionLocalServiceBaseImpl;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.exception.SystemException;
 import com.liferay.portal.kernel.model.SystemEventConstants;
@@ -61,6 +62,20 @@ public class CPOptionLocalServiceImpl extends CPOptionLocalServiceBaseImpl {
 			Map<Locale, String> nameMap, Map<Locale, String> descriptionMap,
 			String ddmFormFieldTypeName, boolean facetable, boolean required,
 			boolean skuContributor, String key, ServiceContext serviceContext)
+			throws PortalException {
+
+		return cpOptionLocalService.addCPOption(nameMap, descriptionMap, ddmFormFieldTypeName,
+				facetable, required, skuContributor, key, StringPool.BLANK,
+				serviceContext);
+	}
+
+	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public CPOption addCPOption(
+			Map<Locale, String> nameMap, Map<Locale, String> descriptionMap,
+			String ddmFormFieldTypeName, boolean facetable, boolean required,
+			boolean skuContributor, String key, String externalReferenceCode,
+			ServiceContext serviceContext)
 		throws PortalException {
 
 		User user = userLocalService.getUser(serviceContext.getUserId());
@@ -87,6 +102,7 @@ public class CPOptionLocalServiceImpl extends CPOptionLocalServiceBaseImpl {
 		cpOption.setSkuContributor(skuContributor);
 		cpOption.setKey(key);
 		cpOption.setExpandoBridgeAttributes(serviceContext);
+		cpOption.setExternalReferenceCode(externalReferenceCode);
 
 		cpOptionPersistence.update(cpOption);
 
@@ -135,6 +151,14 @@ public class CPOptionLocalServiceImpl extends CPOptionLocalServiceBaseImpl {
 		throws PortalException {
 
 		return cpOptionPersistence.fetchByG_K(groupId, key);
+	}
+
+	@Override
+	public CPOption fetchByExternalReferenceCode(
+			String externalReferenceCode) {
+
+		return cpOptionPersistence.fetchByExternalReferenceCode(
+				externalReferenceCode);
 	}
 
 	@Override
@@ -252,6 +276,31 @@ public class CPOptionLocalServiceImpl extends CPOptionLocalServiceBaseImpl {
 		cpOption.setExpandoBridgeAttributes(serviceContext);
 
 		cpOptionPersistence.update(cpOption);
+
+		return cpOption;
+	}
+
+	@Override
+	public CPOption upsertCPOption(
+			Map<Locale, String> nameMap, Map<Locale, String> descriptionMap,
+			String ddmFormFieldTypeName, boolean facetable, boolean required,
+			boolean skuContributor, String key, String externalReferenceCode,
+			ServiceContext serviceContext)
+			throws PortalException {
+
+		CPOption cpOption = cpOptionPersistence.fetchByExternalReferenceCode(
+				externalReferenceCode);
+
+		if (cpOption == null) {
+			cpOption = addCPOption(nameMap, descriptionMap, ddmFormFieldTypeName,
+					facetable, required, skuContributor, key, externalReferenceCode,
+					serviceContext);
+		}
+		else {
+			cpOption = updateCPOption(cpOption.getCPOptionId(), nameMap, descriptionMap,
+					ddmFormFieldTypeName, facetable, required, skuContributor, key,
+					serviceContext);
+		}
 
 		return cpOption;
 	}
