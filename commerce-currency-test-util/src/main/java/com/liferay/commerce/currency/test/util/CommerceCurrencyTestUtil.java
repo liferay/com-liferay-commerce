080f715ad00cfd341185557c6728b64894d735fe
Luca Pellizzon <luca.pellizzon@liferay.com>
2018-07-10 18:19:36 +0200

    COMMERCE-397 commerce currency test util module

---
+++ commerce-currency-test-util/src/main/java/com/liferay/commerce/currency/test/util/CommerceCurrencyTestUtil.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.currency.test.util;
+
+import com.liferay.commerce.currency.model.CommerceCurrency;
+import com.liferay.commerce.currency.model.CommerceCurrencyConstants;
+import com.liferay.commerce.currency.service.CommerceCurrencyLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
+
+import java.math.BigDecimal;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+/**
+ * @author Luca Pellizzon
+ */
+public class CommerceCurrencyTestUtil {
+
+	public static CommerceCurrency addCommerceCurrency(long groupId)
+		throws PortalException {
+
+		Map<Locale, String> formatPatternMap = new HashMap();
+
+		formatPatternMap.put(
+			Locale.US, CommerceCurrencyConstants.DEFAULT_FORMAT_PATTERN);
+
+		ServiceContext serviceContext =
+			ServiceContextTestUtil.getServiceContext(groupId);
+
+		return CommerceCurrencyLocalServiceUtil.addCommerceCurrency(
+			RandomTestUtil.randomString(),
+			RandomTestUtil.randomLocaleStringMap(), BigDecimal.ONE,
+			formatPatternMap, 2, 2, "", false, RandomTestUtil.randomDouble(),
+			true, serviceContext);
+	}
+
+}
