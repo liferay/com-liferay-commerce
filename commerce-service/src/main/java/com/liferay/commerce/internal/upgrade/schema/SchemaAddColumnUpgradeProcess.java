344ed8c6889e571eb6fad6f4edd579e0f4bd8b66
Rodrigo Guedes de Souza <rodrigo.guedes@liferay.com>
2018-08-22 14:50:57 -0300

    COMMERCE-API Add upgrade process to CommerceService

---
+++ commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/schema/SchemaAddColumnUpgradeProcess.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.internal.upgrade.schema;
+
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+/**
+ * @author Rodrigo Guedes de Souza
+ */
+public class SchemaAddColumnUpgradeProcess extends UpgradeProcess {
+
+	public SchemaAddColumnUpgradeProcess(
+		Class entityClass, String tableName, String columnName,
+		String columnType) {
+
+		_entityClass = entityClass;
+		_tableName = tableName;
+		_columnName = columnName;
+		_columnType = columnType;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		_addColumn();
+	}
+
+	private void _addColumn() throws Exception {
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				String.format(
+					"Adding column %s to table %s", _columnName, _tableName));
+		}
+
+		if (!hasColumn(_tableName, _columnName)) {
+			alter(
+				_entityClass,
+				new AlterTableAddColumn(
+					_columnName + StringPool.SPACE + _columnType));
+		}
+		else {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Column %s already exists on table %s", _columnName,
+						_tableName));
+			}
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		SchemaAddColumnUpgradeProcess.class);
+
+	private final String _columnName;
+	private final String _columnType;
+	private final Class _entityClass;
+	private final String _tableName;
+
+}
