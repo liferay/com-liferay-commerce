a481cea4810be72f11619d23b88b64905071cb64
Marco Leo <marco.leo@liferay.com>
2018-08-29 00:22:26 +0200

    COMMERCE-587 Add auth servlet filter

---
+++ commerce-headless-graphql/src/main/java/com/liferay/commerce/headless/graphql/internal/servlet/filter/CommerceGraphQLAuthVerifierFilter.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.headless.graphql.internal.servlet.filter;
+
+import com.liferay.portal.servlet.filters.authverifier.AuthVerifierFilter;
+
+import javax.servlet.Filter;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author MArco Leo
+ */
+@Component(
+	immediate = true,
+	property = {
+		"filter.init.auth.verifier.PortalSessionAuthVerifier.urls.includes=/test/*",
+		"osgi.http.whiteboard.filter.name=com.liferay.commerce.headless.graphql.internal.servlet.filter.CommerceGraphQLAuthVerifierFilter",
+		"osgi.http.whiteboard.servlet.pattern=/test/*"
+	},
+	service = Filter.class
+)
+public class CommerceGraphQLAuthVerifierFilter extends AuthVerifierFilter {
+}
