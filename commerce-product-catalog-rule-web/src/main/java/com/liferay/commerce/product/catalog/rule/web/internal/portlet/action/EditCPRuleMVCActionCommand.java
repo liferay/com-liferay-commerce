51fb445953cc867059fc0c599104da5af7d9ee68
Ethan Bustad <ethan.bustad@liferay.com>
2018-08-30 15:46:11 -0700

    COMMERCE-552 Move asset category rule-type logic to the CPRuleTypeImpl

--- commerce-product-catalog-rule-web/src/main/java/com/liferay/commerce/product/catalog/rule/web/internal/portlet/action/EditCPRuleMVCActionCommand.java
+++ commerce-product-catalog-rule-web/src/main/java/com/liferay/commerce/product/catalog/rule/web/internal/portlet/action/EditCPRuleMVCActionCommand.java
@@ -18,10 +18,8 @@ import com.liferay.commerce.product.constants.CPPortletKeys;
 import com.liferay.commerce.product.exception.CPRuleTypeException;
 import com.liferay.commerce.product.exception.NoSuchCPRuleException;
 import com.liferay.commerce.product.model.CPRule;
-import com.liferay.commerce.product.model.CPRuleAssetCategoryRel;
 import com.liferay.commerce.product.service.CPRuleAssetCategoryRelService;
 import com.liferay.commerce.product.service.CPRuleService;
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
@@ -34,14 +32,11 @@ import com.liferay.portal.kernel.servlet.SessionErrors;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.TransactionConfig;
 import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
-import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.Constants;
-import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.ParamUtil;
 import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.util.StringUtil;
 
-import java.util.List;
 import java.util.concurrent.Callable;
 
 import javax.portlet.ActionRequest;
@@ -174,61 +169,7 @@ public class EditCPRuleMVCActionCommand extends BaseMVCActionCommand {
 				cpRuleId, name, active, type, serviceContext);
 		}
 
-		if (cpRule != null) {
-			updateCPRuleAssetCategoryRels(actionRequest, cpRule);
-		}
-
 		return cpRule;
-	}
-
-	protected void updateCPRuleAssetCategoryRels(
-			ActionRequest actionRequest, CPRule cpRule)
-		throws PortalException {
-
-		String assetCategoryIds = ParamUtil.getString(
-			actionRequest, "assetCategoryIds");
-
-		ServiceContext serviceContext = ServiceContextFactory.getInstance(
-			CPRuleAssetCategoryRel.class.getName(), actionRequest);
-
-		String[] assetCategoryIdsArray = StringUtil.split(
-			assetCategoryIds, StringPool.COMMA);
-
-		List<CPRuleAssetCategoryRel> cpRuleAssetCategoryRels =
-			_cpRuleAssetCategoryRelService.getCPRuleAssetCategoryRels(
-				cpRule.getCPRuleId());
-
-		for (CPRuleAssetCategoryRel cpRuleAssetCategoryRel :
-				cpRuleAssetCategoryRels) {
-
-			if (ArrayUtil.contains(
-					assetCategoryIdsArray,
-					String.valueOf(
-						cpRuleAssetCategoryRel.getAssetCategoryId()))) {
-
-				continue;
-			}
-
-			_cpRuleAssetCategoryRelService.deleteCPRuleAssetCategoryRel(
-				cpRuleAssetCategoryRel.getCPRuleAssetCategoryRelId());
-		}
-
-		long[] cpRuleAssetCategoryIds =
-			_cpRuleAssetCategoryRelService.getAssetCategoryIds(
-				cpRule.getCPRuleId());
-
-		for (String newAssetCategoryId : assetCategoryIdsArray) {
-			long assetCategoryId = GetterUtil.getLong(newAssetCategoryId);
-
-			if (ArrayUtil.contains(cpRuleAssetCategoryIds, assetCategoryId)) {
-				continue;
-			}
-
-			if (assetCategoryId > 0) {
-				_cpRuleAssetCategoryRelService.addCPRuleAssetCategoryRel(
-					cpRule.getCPRuleId(), assetCategoryId, serviceContext);
-			}
-		}
 	}
 
 	private static final Log _log = LogFactoryUtil.getLog(
