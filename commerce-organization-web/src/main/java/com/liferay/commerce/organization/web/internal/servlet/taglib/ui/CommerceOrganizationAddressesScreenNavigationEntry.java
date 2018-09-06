e539d5e5c0ac27da57b5d0dde4588a1718436e19
Alessio Rendina <asilcomando@gmail.com>
2018-09-06 14:07:36 +0200

    COMMERCE-429 Fixed labels

--- commerce-organization-web/src/main/java/com/liferay/commerce/organization/web/internal/servlet/taglib/ui/CommerceOrganizationAddressesScreenNavigationEntry.java
+++ commerce-organization-web/src/main/java/com/liferay/commerce/organization/web/internal/servlet/taglib/ui/CommerceOrganizationAddressesScreenNavigationEntry.java
@@ -25,13 +25,11 @@ import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.model.Organization;
 import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.kernel.util.ResourceBundleUtil;
 import com.liferay.portal.kernel.util.WebKeys;
 
 import java.io.IOException;
 
 import java.util.Locale;
-import java.util.ResourceBundle;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -62,10 +60,7 @@ public class CommerceOrganizationAddressesScreenNavigationEntry
 
 	@Override
 	public String getLabel(Locale locale) {
-		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
-			"content.Language", locale, getClass());
-
-		return LanguageUtil.get(resourceBundle, "addresses");
+		return LanguageUtil.get(locale, "addresses");
 	}
 
 	@Override
