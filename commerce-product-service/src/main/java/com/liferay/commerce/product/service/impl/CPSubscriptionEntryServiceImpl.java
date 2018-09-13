74653d7817e371d67cd300c7f74546345f746103
Alessio Rendina <asilcomando@gmail.com>
2018-09-13 14:06:00 +0200

    COMMERCE-621 Added new methods to local and remote service implementation

--- commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPSubscriptionEntryServiceImpl.java
+++ commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPSubscriptionEntryServiceImpl.java
@@ -14,11 +14,53 @@
 
 package com.liferay.commerce.product.service.impl;
 
+import com.liferay.commerce.product.constants.CPActionKeys;
+import com.liferay.commerce.product.constants.CPConstants;
+import com.liferay.commerce.product.model.CPSubscriptionEntry;
 import com.liferay.commerce.product.service.base.CPSubscriptionEntryServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermissionFactory;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
 
 /**
  * @author Alessio Antonio Rendina
  */
 public class CPSubscriptionEntryServiceImpl
 	extends CPSubscriptionEntryServiceBaseImpl {
+
+	@Override
+	public List<CPSubscriptionEntry> getCPSubscriptionEntries(
+			long groupId, long userId, int start, int end,
+			OrderByComparator<CPSubscriptionEntry> orderByComparator)
+		throws PortalException {
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), groupId,
+			CPActionKeys.MANAGE_COMMERCE_PRODUCT_SUBSCRIPTIONS);
+
+		return cpSubscriptionEntryLocalService.getCPSubscriptionEntries(
+			groupId, userId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCPSubscriptionEntriesCount(long groupId, long userId)
+		throws PortalException {
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), groupId,
+			CPActionKeys.MANAGE_COMMERCE_PRODUCT_SUBSCRIPTIONS);
+
+		return cpSubscriptionEntryLocalService.getCPSubscriptionEntriesCount(
+			groupId, userId);
+	}
+
+	private static volatile PortletResourcePermission
+		_portletResourcePermission =
+			PortletResourcePermissionFactory.getInstance(
+				CPSubscriptionEntryServiceImpl.class,
+				"_portletResourcePermission", CPConstants.RESOURCE_NAME);
+
 }
