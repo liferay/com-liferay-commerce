4658695a5da2ce345833c6b71aed90698ae75cd8
Luca Pellizzon <luca.pellizzon@liferay.com>
2018-09-13 15:07:35 +0200

    COMMERCE-620 product subscriptioncustom query

---
+++ commerce-product-service/src/main/java/com/liferay/commerce/product/service/persistence/impl/CPSubscriptionEntryFinderImpl.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.service.persistence.impl;
+
+import com.liferay.commerce.product.model.CPSubscriptionEntry;
+import com.liferay.commerce.product.model.impl.CPSubscriptionEntryImpl;
+import com.liferay.commerce.product.service.persistence.CPSubscriptionEntryFinder;
+import com.liferay.portal.dao.orm.custom.sql.CustomSQL;
+import com.liferay.portal.kernel.dao.orm.QueryPos;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.dao.orm.SQLQuery;
+import com.liferay.portal.kernel.dao.orm.Session;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.spring.extender.service.ServiceReference;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * @author Luca Pellizzon
+ */
+public class CPSubscriptionEntryFinderImpl
+	extends CPSubscriptionEntryFinderBaseImpl
+	implements CPSubscriptionEntryFinder {
+
+	public static final String FIND_BY_NEXT_ITERATION_DATE =
+		CPSubscriptionEntryFinder.class.getName() + ".findByNextIterationDate";
+
+	@Override
+	public List<CPSubscriptionEntry> findByNextIterationDate(
+		Date nextIterationDate) {
+
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = _customSQL.get(
+				getClass(), FIND_BY_NEXT_ITERATION_DATE);
+
+			SQLQuery q = session.createSynchronizedSQLQuery(sql);
+
+			q.addEntity(
+				CPSubscriptionEntryImpl.TABLE_NAME,
+				CPSubscriptionEntryImpl.class);
+
+			QueryPos qPos = QueryPos.getInstance(q);
+
+			if (nextIterationDate != null) {
+				qPos.add(nextIterationDate);
+			}
+
+			return (List<CPSubscriptionEntry>)QueryUtil.list(
+				q, getDialect(), QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
+
+	@ServiceReference(type = CustomSQL.class)
+	private CustomSQL _customSQL;
+
+}
