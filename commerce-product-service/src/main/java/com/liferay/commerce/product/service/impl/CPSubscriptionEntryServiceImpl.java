370ac8d4d6349bc070f471ad725622d8b8f8d7f8
Luca Pellizzon <luca.pellizzon@liferay.com>
2018-09-13 15:08:51 +0200

    COMMERCE-620 CPSubscriptionEntry services impl

--- commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPSubscriptionEntryServiceImpl.java
+++ commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CPSubscriptionEntryServiceImpl.java
@@ -19,8 +19,12 @@ import com.liferay.commerce.product.constants.CPConstants;
 import com.liferay.commerce.product.model.CPSubscriptionEntry;
 import com.liferay.commerce.product.service.base.CPSubscriptionEntryServiceBaseImpl;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.search.BaseModelSearchResult;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermissionFactory;
+import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.OrderByComparator;
 
 import java.util.List;
@@ -30,6 +34,40 @@ import java.util.List;
  */
 public class CPSubscriptionEntryServiceImpl
 	extends CPSubscriptionEntryServiceBaseImpl {
+
+	@Override
+	public void deleteCPSubscriptionEntry(long cpSubscriptionEntryId)
+		throws PortalException {
+
+		CPSubscriptionEntry cpSubscriptionEntry =
+			cpSubscriptionEntryLocalService.getCPSubscriptionEntry(
+				cpSubscriptionEntryId);
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), cpSubscriptionEntry.getGroupId(),
+			CPActionKeys.MANAGE_COMMERCE_PRODUCT_SUBSCRIPTIONS);
+
+		cpSubscriptionEntryLocalService.deleteCPSubscriptionEntry(
+			cpSubscriptionEntryId);
+	}
+
+	@Override
+	public CPSubscriptionEntry fetchCPSubscriptionEntry(
+			long cpSubscriptionEntryId)
+		throws PrincipalException {
+
+		CPSubscriptionEntry cpSubscriptionEntry =
+			cpSubscriptionEntryLocalService.fetchCPSubscriptionEntry(
+				cpSubscriptionEntryId);
+
+		if (cpSubscriptionEntry != null) {
+			_portletResourcePermission.check(
+				getPermissionChecker(), cpSubscriptionEntry.getGroupId(),
+				CPActionKeys.MANAGE_COMMERCE_PRODUCT_SUBSCRIPTIONS);
+		}
+
+		return cpSubscriptionEntry;
+	}
 
 	@Override
 	public List<CPSubscriptionEntry> getCPSubscriptionEntries(
@@ -55,6 +93,37 @@ public class CPSubscriptionEntryServiceImpl
 
 		return cpSubscriptionEntryLocalService.getCPSubscriptionEntriesCount(
 			groupId, userId);
+	}
+
+	@Override
+	public BaseModelSearchResult<CPSubscriptionEntry>
+			searchCPSubscriptionEntries(
+				long companyId, long groupId, Boolean active, String keywords,
+				int start, int end, Sort sort)
+		throws PrincipalException {
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), groupId,
+			CPActionKeys.MANAGE_COMMERCE_PRODUCT_SUBSCRIPTIONS);
+
+		return cpSubscriptionEntryLocalService.searchCPSubscriptionEntries(
+			companyId, groupId, active, keywords, start, end, sort);
+	}
+
+	@Override
+	public CPSubscriptionEntry updateCommercePriceEntry(
+			long cpSubscriptionEntryId, long subscriptionCycleLength,
+			String subscriptionCyclePeriod, long maxSubscriptionCyclesNumber,
+			boolean active, ServiceContext serviceContext)
+		throws PrincipalException {
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), serviceContext.getScopeGroupId(),
+			CPActionKeys.MANAGE_COMMERCE_PRODUCT_SUBSCRIPTIONS);
+
+		return cpSubscriptionEntryLocalService.updateCPSubscriptionEntry(
+			cpSubscriptionEntryId, subscriptionCycleLength,
+			subscriptionCyclePeriod, maxSubscriptionCyclesNumber, active);
 	}
 
 	private static volatile PortletResourcePermission
