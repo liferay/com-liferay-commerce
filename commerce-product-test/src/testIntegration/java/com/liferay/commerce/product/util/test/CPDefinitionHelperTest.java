74535bffb1a365d86a3d2cb101f13dca2e38103e
Luca Pellizzon <luca.pellizzon@liferay.com>
2018-09-17 15:39:37 +0200

    COMMERCE-583 moved getSearchContext method to util class

--- commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/util/test/CPDefinitionHelperTest.java
+++ commerce-product-test/src/testIntegration/java/com/liferay/commerce/product/util/test/CPDefinitionHelperTest.java
@@ -23,37 +23,28 @@ import com.liferay.commerce.product.model.CPDefinition;
 import com.liferay.commerce.product.model.CPInstance;
 import com.liferay.commerce.product.test.util.CPTestUtil;
 import com.liferay.commerce.product.util.CPDefinitionHelper;
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.kernel.search.SearchContext;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.ListUtil;
-import com.liferay.portal.kernel.util.Validator;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
 import com.liferay.portal.test.rule.Inject;
 import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 import com.liferay.portal.test.rule.PermissionCheckerTestRule;
 
-import java.io.Serializable;
-
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.frutilla.FrutillaRule;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -76,7 +67,6 @@ public class CPDefinitionHelperTest {
 		_group = GroupTestUtil.addGroup();
 	}
 
-	@Ignore
 	@Test
 	public void testSearchCategory() throws PortalException {
 		frutillaRule.scenario(
@@ -116,8 +106,8 @@ public class CPDefinitionHelperTest {
 		AssetCategory assetCategory = CPTestUtil.addCategoryToCPDefinitions(
 			_group.getGroupId(), cpDefinitionIds);
 
-		SearchContext searchContext = _getSearchContext(
-			null, WorkflowConstants.STATUS_APPROVED);
+		SearchContext searchContext = CPTestUtil.getSearchContext(
+			null, WorkflowConstants.STATUS_APPROVED, _group);
 
 		CPQuery cpQuery = new CPQuery();
 
@@ -150,38 +140,6 @@ public class CPDefinitionHelperTest {
 	}
 
 	@Test
-	public void testSearchDraft() throws PortalException {
-		frutillaRule.scenario(
-			"Search for CPDefinitions by 'draft' status"
-		).given(
-			"A collection of CPDefinitions"
-		).when(
-			"I search for CPDefinitions given the status as a search filter"
-		).then(
-			"The results will contain only those product in draft status. In " +
-				"this specific case 0"
-		);
-
-		_addCPInstances(_group.getGroupId(), _CP_INSTANCES_COUNT);
-
-		SearchContext searchContext = _getSearchContext(
-			null, WorkflowConstants.STATUS_DRAFT);
-
-		CPQuery cpQuery = new CPQuery();
-
-		CPDataSourceResult cpDataSourceResult = _cpDefinitionHelper.search(
-			_group.getGroupId(), searchContext, cpQuery, QueryUtil.ALL_POS,
-			QueryUtil.ALL_POS);
-
-		List<CPCatalogEntry> cpCatalogEntries =
-			cpDataSourceResult.getCPCatalogEntries();
-
-		Assert.assertEquals(
-			cpCatalogEntries.toString(), 0, cpCatalogEntries.size());
-	}
-
-	@Ignore
-	@Test
 	public void testSearchName() throws PortalException {
 		frutillaRule.scenario(
 			"Search for CPDefinitions by name"
@@ -203,8 +161,8 @@ public class CPDefinitionHelperTest {
 
 		CPDefinition cpDefinition = randomCPInstance.getCPDefinition();
 
-		SearchContext searchContext = _getSearchContext(
-			cpDefinition.getName(), WorkflowConstants.STATUS_APPROVED);
+		SearchContext searchContext = CPTestUtil.getSearchContext(
+			cpDefinition.getName(), WorkflowConstants.STATUS_APPROVED, _group);
 
 		CPQuery cpQuery = new CPQuery();
 
@@ -225,7 +183,6 @@ public class CPDefinitionHelperTest {
 			cpCatalogEntry.getCPDefinitionId());
 	}
 
-	@Ignore
 	@Test
 	public void testSearchStar() throws PortalException {
 		frutillaRule.scenario(
@@ -241,8 +198,8 @@ public class CPDefinitionHelperTest {
 		CPInstance[] cpInstances = _addCPInstances(
 			_group.getGroupId(), _CP_INSTANCES_COUNT);
 
-		SearchContext searchContext = _getSearchContext(
-			null, WorkflowConstants.STATUS_APPROVED);
+		SearchContext searchContext = CPTestUtil.getSearchContext(
+			null, WorkflowConstants.STATUS_APPROVED, _group);
 
 		CPQuery cpQuery = new CPQuery();
 
@@ -283,38 +240,6 @@ public class CPDefinitionHelperTest {
 		}
 
 		return cpInstances;
-	}
-
-	private SearchContext _getSearchContext(String keywords, int status) {
-		SearchContext searchContext = new SearchContext();
-
-		LinkedHashMap<String, Object> params = new LinkedHashMap<>();
-
-		if (Validator.isNotNull(keywords)) {
-			params.put("keywords", keywords);
-		}
-		else {
-			params.put("keywords", StringPool.STAR);
-		}
-
-		Map<String, Serializable> attributes = new HashMap<>();
-
-		attributes.put(Field.STATUS, status);
-		attributes.put("params", params);
-
-		searchContext.setAttributes(attributes);
-
-		searchContext.setCompanyId(_group.getCompanyId());
-		searchContext.setGroupIds(new long[] {_group.getGroupId()});
-
-		if (Validator.isNotNull(keywords)) {
-			searchContext.setKeywords(keywords);
-		}
-		else {
-			searchContext.setKeywords(StringPool.STAR);
-		}
-
-		return searchContext;
 	}
 
 	private static final int _CP_INSTANCES_COUNT = 10;
