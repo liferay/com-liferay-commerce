51270522fd45216d5929dd119337c3c7026d16cd
Ethan Bustad <ethan.bustad@liferay.com>
2018-09-18 15:56:17 -0700

    COMMERCE-627 Remove @Ignore annotation so tests are actually run

--- commerce-price-list-test/src/testIntegration/java/com/liferay/commerce/price/list/service/test/CommercePriceEntryLocalServiceTest.java
+++ commerce-price-list-test/src/testIntegration/java/com/liferay/commerce/price/list/service/test/CommercePriceEntryLocalServiceTest.java
@@ -45,7 +45,6 @@ import org.frutilla.FrutillaRule;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -54,7 +53,6 @@ import org.junit.runner.RunWith;
  * @author Zoltán Takács
  * @author Ethan Bustad
  */
-@Ignore
 @RunWith(Arquillian.class)
 public class CommercePriceEntryLocalServiceTest {
 
