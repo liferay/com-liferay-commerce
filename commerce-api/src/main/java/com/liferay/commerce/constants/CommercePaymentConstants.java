7ab4e062d1c284ca41b942fe44815e77a712eddd
Luca Pellizzon <luca.pellizzon@liferay.com>
2018-09-21 09:05:27 +0200

    COMMERCE-600 constants class for payment engine and methods
    COMMERCE-600 constants class for payment engine and methods

---
+++ commerce-api/src/main/java/com/liferay/commerce/constants/CommercePaymentConstants.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.constants;
+
+/**
+ * @author Luca Pellizzon
+ */
+public class CommercePaymentConstants {
+
+	public static final int COMMERCE_PAYMENT_METHOD_TYPE_OFFLINE = 2;
+
+	public static final int COMMERCE_PAYMENT_METHOD_TYPE_ONLINE_REDIRECT = 1;
+
+	public static final int COMMERCE_PAYMENT_METHOD_TYPE_ONLINE_STANDARD = 0;
+
+}
