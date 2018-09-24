900839ee760a965ee8f13f0629d19f145531611f
Marco Leo <marco.leo@liferay.com>
2018-09-24 16:55:27 +0200

    COMMERCE-619 Remove not used reference

--- commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/portlet/action/EditCPInstanceMVCActionCommand.java
+++ commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/portlet/action/EditCPInstanceMVCActionCommand.java
@@ -22,7 +22,6 @@ import com.liferay.commerce.product.exception.NoSuchSkuContributorCPDefinitionOp
 import com.liferay.commerce.product.model.CPDefinition;
 import com.liferay.commerce.product.model.CPInstance;
 import com.liferay.commerce.product.service.CPInstanceService;
-import com.liferay.commerce.product.util.CPInstanceHelper;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.portlet.PortletProvider;
 import com.liferay.portal.kernel.portlet.PortletProviderUtil;
@@ -289,8 +288,6 @@ public class EditCPInstanceMVCActionCommand extends BaseMVCActionCommand {
 			cpInstanceId, width, height, depth, weight, serviceContext);
 	}
 
-	@Reference
-	private CPInstanceHelper _cpInstanceHelper;
 	protected void updateSubscriptionInfo(ActionRequest actionRequest)
 		throws PortalException {
 
