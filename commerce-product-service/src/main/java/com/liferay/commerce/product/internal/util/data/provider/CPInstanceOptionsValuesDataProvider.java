71335d9890b84d0ace92355d174990b87a8faacb
Alessandro Iovane <iovaneale@gmail.com>
2018-09-25 12:43:08 +0200

    COMMERCE-634 Auto SF

--- commerce-product-service/src/main/java/com/liferay/commerce/product/internal/util/data/provider/CPInstanceOptionsValuesDataProvider.java
+++ commerce-product-service/src/main/java/com/liferay/commerce/product/internal/util/data/provider/CPInstanceOptionsValuesDataProvider.java
@@ -79,7 +79,7 @@ public class CPInstanceOptionsValuesDataProvider implements DDMDataProvider {
 		}
 
 		try {
-			/*
+			/**
 			 * Extract the filters and the outputs based on fields that were
 			 * filled or not in the data provider request
 			 * Ex:
