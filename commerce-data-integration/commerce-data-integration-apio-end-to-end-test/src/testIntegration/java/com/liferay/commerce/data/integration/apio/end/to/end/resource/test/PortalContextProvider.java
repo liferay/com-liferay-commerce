057a9d2b829e6a58fad25571762f25c41b02f263
Zoltán Takács <zoltan.takacs@liferay.com>
2018-10-09 11:46:51 +0200

    COMMERCE-628 Fix package names

--- commerce-data-integration/commerce-data-integration-apio-end-to-end-test/src/testIntegration/java/com/liferay/commerce/data/integration/apio/end/to/end/resource/test/PortalContextProvider.java
+++ commerce-data-integration/commerce-data-integration-apio-end-to-end-test/src/testIntegration/java/com/liferay/commerce/data/integration/apio/end/to/end/resource/test/PortalContextProvider.java
@@ -1,18 +1,18 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
  *
- * The contents of this file are subject to the terms of the Liferay Enterprise
- * Subscription License ("License"). You may not use this file except in
- * compliance with the License. You can obtain a copy of the License by
- * contacting Liferay, Inc. See the License for the specific language governing
- * permissions and limitations under the License, including but not limited to
- * distribution rights of the Software.
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
  *
- *
- *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
  */
 
-package com.liferay.commerce.data.integration.apio.internal.resource.test;
+package com.liferay.commerce.data.integration.apio.end.to.end.resource.test;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -30,11 +30,10 @@ import com.liferay.commerce.apio.jsonld.representation.util.constants.SchemaOrgC
 import com.liferay.commerce.apio.jsonld.representation.util.form.Property;
 import com.liferay.commerce.apio.jsonld.representation.util.operation.Method;
 import com.liferay.commerce.apio.jsonld.representation.util.operation.Operation;
-import com.liferay.commerce.data.integration.apio.internal.client.RESTClient;
+import com.liferay.commerce.data.integration.apio.end.to.end.client.RESTClient;
 import com.liferay.external.reference.service.EROrganizationLocalService;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.service.OrganizationLocalService;
