f8b3f9dcc6065cf97e44cbb014820cc8106e5fe9
Marco Leo <marco.leo@liferay.com>
2018-10-10 12:48:16 +0200

    COMMERCE-620 Update Order Item if the product is of type subscription

--- commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceOrderItemLocalServiceImpl.java
+++ commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceOrderItemLocalServiceImpl.java
@@ -125,6 +125,14 @@ public class CommerceOrderItemLocalServiceImpl
 		_setCommerceOrderItemDiscountValue(
 			commerceOrderItem, commerceProductPrice.getDiscountValue());
 
+		boolean subscription = cpDefinition.isSubscriptionEnabled();
+
+		if (cpInstance.isOverrideSubscriptionInfo()) {
+			subscription = cpInstance.isSubscriptionEnabled();
+		}
+
+		commerceOrderItem.setSubscription(subscription);
+
 		commerceOrderItemPersistence.update(commerceOrderItem);
 
 		return commerceOrderItem;
