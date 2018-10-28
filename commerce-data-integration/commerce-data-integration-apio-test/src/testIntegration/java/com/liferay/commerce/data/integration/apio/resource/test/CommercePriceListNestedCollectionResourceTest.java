45921e8dc421537cfd6c8fd8fe0edfdd8cd22ad6
Zoltán Takács <zoltan.takacs@liferay.com>
2018-10-28 12:47:39 +0100

    COMMERCE-675 Add site activator for the integration tests

---
+++ commerce-data-integration/commerce-data-integration-apio-test/src/testIntegration/java/com/liferay/commerce/data/integration/apio/resource/test/CommercePriceListNestedCollectionResourceTest.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.data.integration.apio.resource.test;
+
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+import com.liferay.commerce.data.integration.apio.resource.test.utils.CommerceTestSiteActivator;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.test.rule.AggregateTestRule;
+import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
+import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Zoltán Takács
+ */
+@RunWith(Arquillian.class)
+public class CommercePriceListNestedCollectionResourceTest
+	extends PortalContextProvider {
+
+	@ClassRule
+	@Rule
+	public static final AggregateTestRule aggregateTestRule =
+		new LiferayIntegrationTestRule();
+
+	@Before
+	public void setUp() throws Exception {
+		_group = GroupTestUtil.addGroup();
+
+		CommerceTestSiteActivator.initialize(_group.getGroupId());
+	}
+
+	@Test
+	public void testA() {
+		Assert.assertTrue(true);
+	}
+
+	@DeleteAfterTestRun
+	private Group _group;
+
+}
