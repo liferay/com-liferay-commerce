cdc6c55c8ca190978c70705a804515e4d1f46b49
Alessio Rendina <asilcomando@gmail.com>
2018-10-29 17:02:05 +0100

    COMMERCE-697 Refactor commerce subscription entry local and remote service implementation

--- commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceSubscriptionEntryServiceImpl.java
+++ commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceSubscriptionEntryServiceImpl.java
@@ -24,6 +24,7 @@ import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermissionFactory;
 import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.UnicodeProperties;
 
 import java.util.List;
 
@@ -82,7 +83,7 @@ public class CommerceSubscriptionEntryServiceImpl
 	@Override
 	public BaseModelSearchResult<CommerceSubscriptionEntry>
 			getCommerceSubscriptionEntries(
-				long companyId, long groupId, Long maxSubscriptionCyclesNumber,
+				long companyId, long groupId, Long maxSubscriptionCycles,
 				Boolean active, String keywords, int start, int end, Sort sort)
 		throws PortalException {
 
@@ -92,8 +93,8 @@ public class CommerceSubscriptionEntryServiceImpl
 
 		return commerceSubscriptionEntryLocalService.
 			getCommerceSubscriptionEntries(
-				companyId, groupId, maxSubscriptionCyclesNumber, active,
-				keywords, start, end, sort);
+				companyId, groupId, maxSubscriptionCycles, active, keywords,
+				start, end, sort);
 	}
 
 	@Override
@@ -122,10 +123,15 @@ public class CommerceSubscriptionEntryServiceImpl
 	}
 
 	@Override
-	public CommerceSubscriptionEntry updateCommercePriceEntry(
-			long commerceSubscriptionEntryId, long subscriptionCycleLength,
-			String subscriptionCyclePeriod, long maxSubscriptionCyclesNumber,
-			boolean active)
+	public CommerceSubscriptionEntry updateCommerceSubscriptionEntry(
+			long commerceSubscriptionEntryId, int subscriptionLength,
+			String subscriptionType,
+			UnicodeProperties subscriptionTypeSettingsProperties,
+			long maxSubscriptionCycles, boolean active, int startDateMonth,
+			int startDateDay, int startDateYear, int startDateHour,
+			int startDateMinute, int nextInterationDateMonth,
+			int nextInterationDateDay, int nextInterationDateYear,
+			int nextInterationDateHour, int nextInterationDateMinute)
 		throws PortalException {
 
 		CommerceSubscriptionEntry commerceSubscriptionEntry =
@@ -138,8 +144,13 @@ public class CommerceSubscriptionEntryServiceImpl
 
 		return commerceSubscriptionEntryLocalService.
 			updateCommerceSubscriptionEntry(
-				commerceSubscriptionEntryId, subscriptionCycleLength,
-				subscriptionCyclePeriod, maxSubscriptionCyclesNumber, active);
+				commerceSubscriptionEntryId, subscriptionLength,
+				subscriptionType, subscriptionTypeSettingsProperties,
+				maxSubscriptionCycles, active, startDateMonth, startDateDay,
+				startDateYear, startDateHour, startDateMinute,
+				nextInterationDateMonth, nextInterationDateDay,
+				nextInterationDateYear, nextInterationDateHour,
+				nextInterationDateMinute);
 	}
 
 	private static volatile PortletResourcePermission
