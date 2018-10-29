273832028af01942fa8f590b4fbde0883e2a6068
Alessio Rendina <asilcomando@gmail.com>
2018-10-29 17:16:34 +0100

    COMMERCE-697 Added commerce product subscription type service wrapper order comparator

---
+++ commerce-product-service/src/main/java/com/liferay/commerce/product/internal/util/comparator/CPSubscriptionTypeServiceWrapperOrderComparator.java
@@ -0,0 +1,66 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.internal.util.comparator;
+
+import com.liferay.commerce.product.util.CPSubscriptionType;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerCustomizerFactory.ServiceWrapper;
+import com.liferay.portal.kernel.util.MapUtil;
+
+import java.io.Serializable;
+
+import java.util.Comparator;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CPSubscriptionTypeServiceWrapperOrderComparator
+	implements Comparator<ServiceWrapper<CPSubscriptionType>>, Serializable {
+
+	public CPSubscriptionTypeServiceWrapperOrderComparator() {
+		this(true);
+	}
+
+	public CPSubscriptionTypeServiceWrapperOrderComparator(boolean ascending) {
+		_ascending = ascending;
+	}
+
+	@Override
+	public int compare(
+		ServiceWrapper<CPSubscriptionType> serviceWrapper1,
+		ServiceWrapper<CPSubscriptionType> serviceWrapper2) {
+
+		int displayOrder1 = MapUtil.getInteger(
+			serviceWrapper1.getProperties(),
+			"commerce.product.subscription.type.order", Integer.MAX_VALUE);
+		int displayOrder2 = MapUtil.getInteger(
+			serviceWrapper2.getProperties(),
+			"commerce.product.subscription.type.order", Integer.MAX_VALUE);
+
+		int value = Integer.compare(displayOrder1, displayOrder2);
+
+		if (_ascending) {
+			return value;
+		}
+
+		return Math.negateExact(value);
+	}
+
+	public boolean isAscending() {
+		return _ascending;
+	}
+
+	private final boolean _ascending;
+
+}
