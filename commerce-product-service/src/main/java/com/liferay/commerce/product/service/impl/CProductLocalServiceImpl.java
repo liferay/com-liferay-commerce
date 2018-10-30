5e98378f860a74ffcb67ca9317ad3cad489dfb15
Ethan Bustad <ethan.bustad@liferay.com>
2018-10-30 16:13:47 -0700

    COMMERCE-704 Fix generated files

--- commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CProductLocalServiceImpl.java
+++ commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CProductLocalServiceImpl.java
@@ -17,23 +17,7 @@ package com.liferay.commerce.product.service.impl;
 import com.liferay.commerce.product.service.base.CProductLocalServiceBaseImpl;
 
 /**
- * The implementation of the c product local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.commerce.product.service.CProductLocalService} interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
- * @author Marco Leo
- * @see CProductLocalServiceBaseImpl
- * @see com.liferay.commerce.product.service.CProductLocalServiceUtil
+ * @author Ethan Bustad
  */
 public class CProductLocalServiceImpl extends CProductLocalServiceBaseImpl {
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Always use {@link com.liferay.commerce.product.service.CProductLocalServiceUtil} to access the c product local service.
-	 */
 }
