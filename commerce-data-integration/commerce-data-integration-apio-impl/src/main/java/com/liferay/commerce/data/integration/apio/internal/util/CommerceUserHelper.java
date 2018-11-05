885bc75d53f4c4bd3f445bb2e6dc2dbf247d57d4
Zoltán Takács <zoltan.takacs@liferay.com>
2018-11-05 15:17:40 +0100

    COMMERCE-628 Add missing updateUser operation

--- commerce-data-integration/commerce-data-integration-apio-impl/src/main/java/com/liferay/commerce/data/integration/apio/internal/util/CommerceUserHelper.java
+++ commerce-data-integration/commerce-data-integration-apio-impl/src/main/java/com/liferay/commerce/data/integration/apio/internal/util/CommerceUserHelper.java
@@ -14,6 +14,7 @@
 
 package com.liferay.commerce.data.integration.apio.internal.util;
 
+import com.liferay.apio.architect.functional.Try;
 import com.liferay.commerce.data.integration.apio.identifier.ClassPKExternalReferenceCode;
 import com.liferay.commerce.data.integration.apio.internal.form.CommerceUserUpserterForm;
 import com.liferay.commerce.data.integration.apio.internal.model.UserWrapper;
@@ -22,9 +23,14 @@ import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Contact;
+import com.liferay.portal.kernel.model.ListTypeConstants;
+import com.liferay.portal.kernel.model.ListTypeModel;
 import com.liferay.portal.kernel.model.Organization;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.service.ListTypeService;
 import com.liferay.portal.kernel.service.OrganizationLocalService;
 import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
@@ -34,6 +40,9 @@ import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.Validator;
 
+import java.util.Date;
+import java.util.Optional;
+
 import javax.ws.rs.BadRequestException;
 
 import org.osgi.service.component.annotations.Component;
@@ -41,26 +50,22 @@ import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Eduardo V. Bruno
+ * @author Zoltán Takács
  */
 @Component(immediate = true, service = CommerceUserHelper.class)
 public class CommerceUserHelper {
 
 	public void deleteUser(
-			ClassPKExternalReferenceCode classPKExternalReferenceCode,
-			Company company)
+			ClassPKExternalReferenceCode commerceUserCPKERC, Company company)
 		throws PortalException {
 
-		User user = getUser(
-			classPKExternalReferenceCode, company.getCompanyId());
+		User user = getUser(commerceUserCPKERC, company.getCompanyId());
 
 		if (user == null) {
 			if (_log.isInfoEnabled()) {
 				_log.info(
-					String.format(
-						"User does not exist with identifier %d : %s",
-						classPKExternalReferenceCode.getClassPK(),
-						classPKExternalReferenceCode.
-							getExternalReferenceCode()));
+					"User does not exist with identifier: " +
+						commerceUserCPKERC);
 			}
 		}
 		else {
@@ -68,24 +73,80 @@ public class CommerceUserHelper {
 		}
 	}
 
-	public User getUser(
-			ClassPKExternalReferenceCode classPKExternalReferenceCode,
-			long companyId)
+	public User getUser(ClassPKExternalReferenceCode commerceUserCPKERC)
 		throws PortalException {
 
-		long classPK = classPKExternalReferenceCode.getClassPK();
+		return getUser(commerceUserCPKERC, CompanyThreadLocal.getCompanyId());
+	}
+
+	public User getUser(
+			ClassPKExternalReferenceCode commerceUserCPKERC, long companyId)
+		throws PortalException {
+
+		long classPK = commerceUserCPKERC.getClassPK();
 
 		if (classPK > 0) {
 			return _userLocalService.getUserById(classPK);
 		}
 
 		return _userLocalService.fetchUserByReferenceCode(
-			companyId, classPKExternalReferenceCode.getExternalReferenceCode());
+			companyId, commerceUserCPKERC.getExternalReferenceCode());
+	}
+
+	public UserWrapper updateUser(
+			ClassPKExternalReferenceCode commerceUserCPKERC,
+			CommerceUserUpserterForm commerceUserUpserterForm,
+			ThemeDisplay themeDisplay)
+		throws PortalException {
+
+		User user = getUser(commerceUserCPKERC, themeDisplay.getCompanyId());
+
+		Contact contact = user.getContact();
+
+		long prefixId = _getPrefixId(
+			commerceUserUpserterForm.getHonorificPrefix(),
+			ListTypeConstants.CONTACT_PREFIX, contact.getPrefixId());
+		long suffixId = _getPrefixId(
+			commerceUserUpserterForm.getHonorificSuffix(),
+			ListTypeConstants.CONTACT_SUFFIX, contact.getSuffixId());
+
+		String alternateName = _getAlternateName(
+			commerceUserUpserterForm, user);
+
+		Date birthdayDate = user.getBirthday();
+
+		user = _userLocalService.updateUser(
+			user.getUserId(), user.getPassword(),
+			commerceUserUpserterForm.getPassword(),
+			commerceUserUpserterForm.getPassword(), false,
+			user.getReminderQueryQuestion(), user.getReminderQueryAnswer(),
+			alternateName, commerceUserUpserterForm.getEmail(),
+			user.getFacebookId(), user.getOpenId(), false, null,
+			user.getLanguageId(), user.getTimeZoneId(), user.getGreeting(),
+			user.getComments(), commerceUserUpserterForm.getGivenName(),
+			user.getMiddleName(), commerceUserUpserterForm.getFamilyName(),
+			prefixId, suffixId, _isMale(commerceUserUpserterForm, user),
+			_getDefaultValue(
+				commerceUserUpserterForm.getBirthdayMonthOptional(),
+				birthdayDate.getMonth()),
+			_getDefaultValue(
+				commerceUserUpserterForm.getBirthdayDayOptional(),
+				birthdayDate.getDate()),
+			_getDefaultValue(
+				commerceUserUpserterForm.getBirthdayYearOptional(),
+				birthdayDate.getYear()),
+			contact.getSmsSn(), contact.getFacebookSn(), contact.getJabberSn(),
+			contact.getSkypeSn(), contact.getTwitterSn(),
+			commerceUserUpserterForm.getJobTitle(), user.getGroupIds(),
+			user.getOrganizationIds(), user.getRoleIds(), null,
+			user.getUserGroupIds(), new ServiceContext());
+
+		return new UserWrapper(user, themeDisplay);
 	}
 
 	public UserWrapper upsert(
-			ThemeDisplay themeDisplay,
-			CommerceUserUpserterForm commerceUserUpserterForm)
+			CommerceUserUpserterForm commerceUserUpserterForm,
+			ThemeDisplay themeDisplay)
 		throws PortalException {
 
 		long[] commerceAccountIds = _extractCommerceAccountIds(
@@ -94,18 +155,21 @@ public class CommerceUserHelper {
 		long[] roleIds = _getRoleNames(
 			commerceUserUpserterForm, themeDisplay.getCompanyId());
 
+		String password = commerceUserUpserterForm.getPassword();
+
 		String screenName = _getScreenName(
 			themeDisplay.getCompanyId(), commerceUserUpserterForm);
 
 		User user = _erUserLocalService.addOrUpdateUser(
 			commerceUserUpserterForm.getExternalReferenceCode(),
-			themeDisplay.getUserId(), themeDisplay.getCompanyId(), true, null,
-			null, screenName == null, screenName,
-			commerceUserUpserterForm.getEmail(), LocaleUtil.getDefault(),
-			commerceUserUpserterForm.getGivenName(), null,
-			commerceUserUpserterForm.getFamilyName(), 0, 0, true, 1, 1, 1970,
-			commerceUserUpserterForm.getJobTitle(), null, commerceAccountIds,
-			roleIds, null, null, true, new ServiceContext());
+			themeDisplay.getUserId(), themeDisplay.getCompanyId(),
+			password == null, password, password, screenName == null,
+			screenName, commerceUserUpserterForm.getEmail(),
+			LocaleUtil.getDefault(), commerceUserUpserterForm.getGivenName(),
+			null, commerceUserUpserterForm.getFamilyName(), 0, 0, true, 1, 1,
+			1970, commerceUserUpserterForm.getJobTitle(), null,
+			commerceAccountIds, roleIds, null, null, true,
+			new ServiceContext());
 
 		return new UserWrapper(user, themeDisplay);
 	}
@@ -149,10 +213,38 @@ public class CommerceUserHelper {
 		return commerceAccountIds;
 	}
 
-	private long[] _getRoleNames(
-		CommerceUserUpserterForm form, long companyId) {
+	private String _getAlternateName(
+		CommerceUserUpserterForm commerceUserUpserterForm, User user) {
 
-		String[] roleNames = form.getRoleNames();
+		if (commerceUserUpserterForm.getAlternateName() == null) {
+			return user.getScreenName();
+		}
+
+		return commerceUserUpserterForm.getAlternateName();
+	}
+
+	private Integer _getDefaultValue(
+		Optional<Integer> optional, int defaultValue) {
+
+		return optional.orElse(defaultValue);
+	}
+
+	private long _getPrefixId(
+		String honorificTitle, String className, long defaultTitle) {
+
+		return Try.fromFallible(
+			() -> _listTypeService.getListType(honorificTitle, className)
+		).map(
+			ListTypeModel::getListTypeId
+		).orElse(
+			defaultTitle
+		);
+	}
+
+	private long[] _getRoleNames(
+		CommerceUserUpserterForm commerceUserUpserterForm, long companyId) {
+
+		String[] roleNames = commerceUserUpserterForm.getRoleNames();
 
 		if (ArrayUtil.isEmpty(roleNames)) {
 			return new long[0];
@@ -187,11 +279,23 @@ public class CommerceUserHelper {
 		return user.getScreenName();
 	}
 
+	private Boolean _isMale(
+			CommerceUserUpserterForm commerceUserUpserterForm, User user)
+		throws PortalException {
+
+		Optional<Boolean> optional = commerceUserUpserterForm.isMaleOptional();
+
+		return optional.orElse(user.isMale());
+	}
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		CommerceUserHelper.class);
 
 	@Reference
 	private ERUserLocalService _erUserLocalService;
+
+	@Reference
+	private ListTypeService _listTypeService;
 
 	@Reference
 	private OrganizationLocalService _organizationLocalService;
