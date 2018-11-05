6ed96c79bdd2fce72305d6d58384e5ed84442e56
Luca Pellizzon <luca.pellizzon@liferay.com>
2018-11-05 18:11:59 +0100

    COMMERCE-600 new checkout step for payment management

---
+++ commerce-checkout-web/src/main/java/com/liferay/commerce/checkout/web/internal/util/PaymentProcessCommerceCheckoutStep.java
@@ -0,0 +1,118 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.checkout.web.internal.util;
+
+import com.liferay.commerce.checkout.web.constants.CommerceCheckoutWebKeys;
+import com.liferay.commerce.checkout.web.internal.display.context.PaymentProcessCheckoutStepDisplayContext;
+import com.liferay.commerce.checkout.web.util.BaseCommerceCheckoutStep;
+import com.liferay.commerce.checkout.web.util.CommerceCheckoutStep;
+import com.liferay.commerce.checkout.web.util.CommerceCheckoutStepServicesTracker;
+import com.liferay.commerce.payment.engine.CommercePaymentEngine;
+import com.liferay.commerce.service.CommerceOrderService;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.portal.kernel.util.Portal;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luca Pellizzon
+ */
+@Component(
+	immediate = true,
+	property = {
+		"commerce.checkout.step.name=" + PaymentProcessCommerceCheckoutStep.NAME,
+		"commerce.checkout.step.order:Integer=" + (Integer.MAX_VALUE - 90)
+	},
+	service = CommerceCheckoutStep.class
+)
+public class PaymentProcessCommerceCheckoutStep
+	extends BaseCommerceCheckoutStep {
+
+	public static final String NAME = "payment-process";
+
+	@Override
+	public String getName() {
+		return NAME;
+	}
+
+	@Override
+	public boolean isVisible(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+
+		return false;
+	}
+
+	@Override
+	public void processAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+	}
+
+	@Override
+	public void render(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws Exception {
+
+		PaymentProcessCheckoutStepDisplayContext
+			paymentProcessCheckoutStepDisplayContext =
+				new PaymentProcessCheckoutStepDisplayContext(
+					httpServletRequest, _commerceOrderService,
+					_commercePaymentEngine,
+					_commerceCheckoutStepServicesTracker, _portal);
+
+		httpServletRequest.setAttribute(
+			CommerceCheckoutWebKeys.COMMERCE_CHECKOUT_STEP_DISPLAY_CONTEXT,
+			paymentProcessCheckoutStepDisplayContext);
+
+		_jspRenderer.renderJSP(
+			httpServletRequest, httpServletResponse,
+			"/checkout_step/payment_process.jsp");
+	}
+
+	@Override
+	public boolean showControls(
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse) {
+
+		return false;
+	}
+
+	@Reference
+	private CommerceCheckoutStepServicesTracker
+		_commerceCheckoutStepServicesTracker;
+
+	@Reference
+	private CommerceOrderService _commerceOrderService;
+
+	@Reference
+	private CommercePaymentEngine _commercePaymentEngine;
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
+	@Reference
+	private Portal _portal;
+
+}
