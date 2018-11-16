e055f727aec4a73439aa6cc526f81b7e0b81f1e3
Alessio Rendina <asilcomando@gmail.com>
2018-11-16 17:52:58 +0100

    COMMERCE-622 Refactor commerce order validator

--- commerce-service/src/main/java/com/liferay/commerce/internal/order/AvailabilityCommerceOrderValidatorImpl.java
+++ commerce-service/src/main/java/com/liferay/commerce/internal/order/AvailabilityCommerceOrderValidatorImpl.java
@@ -18,6 +18,7 @@ import com.liferay.commerce.constants.CommerceOrderConstants;
 import com.liferay.commerce.inventory.CPDefinitionInventoryEngine;
 import com.liferay.commerce.inventory.CPDefinitionInventoryEngineRegistry;
 import com.liferay.commerce.model.CPDefinitionInventory;
+import com.liferay.commerce.model.CommerceOrder;
 import com.liferay.commerce.model.CommerceOrderItem;
 import com.liferay.commerce.order.CommerceOrderValidator;
 import com.liferay.commerce.order.CommerceOrderValidatorResult;
@@ -26,6 +27,13 @@ import com.liferay.commerce.product.model.CPInstance;
 import com.liferay.commerce.service.CPDefinitionInventoryLocalService;
 import com.liferay.commerce.service.CommerceOrderItemLocalService;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -53,59 +61,13 @@ public class AvailabilityCommerceOrderValidatorImpl
 
 	@Override
 	public CommerceOrderValidatorResult validate(
-			CommerceOrderItem commerceOrderItem)
-		throws PortalException {
-
-		CPInstance cpInstance = commerceOrderItem.getCPInstance();
-
-		CPDefinition cpDefinition = cpInstance.getCPDefinition();
-
-		if (!cpDefinition.isApproved() || !cpInstance.isApproved() ||
-			!cpInstance.isPublished() || !cpInstance.isPurchasable()) {
-
-			return new CommerceOrderValidatorResult(
-				commerceOrderItem.getCommerceOrderItemId(), false,
-				"the-product-is-no-longer-available");
-		}
-
-		CPDefinitionInventory cpDefinitionInventory =
-			_cpDefinitionInventoryLocalService.
-				fetchCPDefinitionInventoryByCPDefinitionId(
-					cpDefinition.getCPDefinitionId());
-
-		CPDefinitionInventoryEngine cpDefinitionInventoryEngine =
-			_cpDefinitionInventoryEngineRegistry.getCPDefinitionInventoryEngine(
-				cpDefinitionInventory);
-
-		if (cpDefinitionInventoryEngine.isBackOrderAllowed(cpInstance)) {
-			return new CommerceOrderValidatorResult(true);
-		}
-
-		int availableQuantity = cpDefinitionInventoryEngine.getStockQuantity(
-			cpInstance);
-
-		int orderQuantity =
-			_commerceOrderItemLocalService.getCPInstanceQuantity(
-				commerceOrderItem.getCPInstanceId(),
-				CommerceOrderConstants.ORDER_STATUS_OPEN);
-
-		if (orderQuantity > availableQuantity) {
-			return new CommerceOrderValidatorResult(
-				commerceOrderItem.getCommerceOrderItemId(), false,
-				"that-quantity-is-unavailable");
-		}
-
-		return new CommerceOrderValidatorResult(true);
-	}
-
-	@Override
-	public CommerceOrderValidatorResult validate(
-			CPInstance cpInstance, int quantity)
+			CommerceOrder commerceOrder, CPInstance cpInstance, int quantity)
 		throws PortalException {
 
 		if (cpInstance == null) {
 			return new CommerceOrderValidatorResult(
-				false, "please-select-a-valid-product");
+				false, "please-select-a-valid-product",
+				_getResourceBundle(commerceOrder));
 		}
 
 		CPDefinition cpDefinition = cpInstance.getCPDefinition();
@@ -114,7 +76,8 @@ public class AvailabilityCommerceOrderValidatorImpl
 			!cpInstance.isPublished() || !cpInstance.isPurchasable()) {
 
 			return new CommerceOrderValidatorResult(
-				false, "the-product-is-no-longer-available");
+				false, "the-product-is-no-longer-available",
+				_getResourceBundle(commerceOrder));
 		}
 
 		CPDefinitionInventory cpDefinitionInventory =
@@ -142,10 +105,76 @@ public class AvailabilityCommerceOrderValidatorImpl
 
 		if (orderQuantity > availableQuantity) {
 			return new CommerceOrderValidatorResult(
-				false, "that-quantity-is-unavailable");
+				false, "that-quantity-unavailable",
+				_getResourceBundle(commerceOrder));
 		}
 
 		return new CommerceOrderValidatorResult(true);
+	}
+
+	@Override
+	public CommerceOrderValidatorResult validate(
+			CommerceOrderItem commerceOrderItem)
+		throws PortalException {
+
+		CPInstance cpInstance = commerceOrderItem.getCPInstance();
+
+		CPDefinition cpDefinition = cpInstance.getCPDefinition();
+
+		if (!cpDefinition.isApproved() || !cpInstance.isApproved() ||
+			!cpInstance.isPublished() || !cpInstance.isPurchasable()) {
+
+			return new CommerceOrderValidatorResult(
+				commerceOrderItem.getCommerceOrderItemId(), false,
+				"the-product-is-no-longer-available",
+				_getResourceBundle(commerceOrderItem.getCommerceOrder()));
+		}
+
+		CPDefinitionInventory cpDefinitionInventory =
+			_cpDefinitionInventoryLocalService.
+				fetchCPDefinitionInventoryByCPDefinitionId(
+					cpDefinition.getCPDefinitionId());
+
+		CPDefinitionInventoryEngine cpDefinitionInventoryEngine =
+			_cpDefinitionInventoryEngineRegistry.getCPDefinitionInventoryEngine(
+				cpDefinitionInventory);
+
+		if (cpDefinitionInventoryEngine.isBackOrderAllowed(cpInstance)) {
+			return new CommerceOrderValidatorResult(true);
+		}
+
+		int availableQuantity = cpDefinitionInventoryEngine.getStockQuantity(
+			cpInstance);
+
+		int orderQuantity =
+			_commerceOrderItemLocalService.getCPInstanceQuantity(
+				commerceOrderItem.getCPInstanceId(),
+				CommerceOrderConstants.ORDER_STATUS_OPEN);
+
+		if (orderQuantity > availableQuantity) {
+			return new CommerceOrderValidatorResult(
+				commerceOrderItem.getCommerceOrderItemId(), false,
+				"that-quantity-unavailable",
+				_getResourceBundle(commerceOrderItem.getCommerceOrder()));
+		}
+
+		return new CommerceOrderValidatorResult(true);
+	}
+
+	private ResourceBundle _getResourceBundle(CommerceOrder commerceOrder)
+		throws PortalException {
+
+		Locale locale = _portal.getSiteDefaultLocale(
+			commerceOrder.getSiteGroupId());
+
+		User user = _userLocalService.fetchUser(commerceOrder.getUserId());
+
+		if (user != null) {
+			locale = user.getLocale();
+		}
+
+		return ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
 	}
 
 	@Reference
@@ -158,5 +187,11 @@ public class AvailabilityCommerceOrderValidatorImpl
 	@Reference
 	private CPDefinitionInventoryLocalService
 		_cpDefinitionInventoryLocalService;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
--- commerce-service/src/main/java/com/liferay/commerce/internal/order/DefaultCommerceOrderValidatorImpl.java
+++ commerce-service/src/main/java/com/liferay/commerce/internal/order/DefaultCommerceOrderValidatorImpl.java
@@ -17,13 +17,21 @@ package com.liferay.commerce.internal.order;
 import com.liferay.commerce.inventory.CPDefinitionInventoryEngine;
 import com.liferay.commerce.inventory.CPDefinitionInventoryEngineRegistry;
 import com.liferay.commerce.model.CPDefinitionInventory;
+import com.liferay.commerce.model.CommerceOrder;
 import com.liferay.commerce.model.CommerceOrderItem;
 import com.liferay.commerce.order.CommerceOrderValidator;
 import com.liferay.commerce.order.CommerceOrderValidatorResult;
 import com.liferay.commerce.product.model.CPInstance;
 import com.liferay.commerce.service.CPDefinitionInventoryLocalService;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -51,59 +59,7 @@ public class DefaultCommerceOrderValidatorImpl
 
 	@Override
 	public CommerceOrderValidatorResult validate(
-			CommerceOrderItem commerceOrderItem)
-		throws PortalException {
-
-		CPInstance cpInstance = commerceOrderItem.getCPInstance();
-
-		CPDefinitionInventory cpDefinitionInventory =
-			_cpDefinitionInventoryLocalService.
-				fetchCPDefinitionInventoryByCPDefinitionId(
-					cpInstance.getCPDefinitionId());
-
-		CPDefinitionInventoryEngine cpDefinitionInventoryEngine =
-			_cpDefinitionInventoryEngineRegistry.getCPDefinitionInventoryEngine(
-				cpDefinitionInventory);
-
-		int minOrderQuantity = cpDefinitionInventoryEngine.getMinOrderQuantity(
-			cpInstance);
-		int maxOrderQuantity = cpDefinitionInventoryEngine.getMaxOrderQuantity(
-			cpInstance);
-		String[] allowedOrderQuantities =
-			cpDefinitionInventoryEngine.getAllowedOrderQuantities(cpInstance);
-
-		if ((minOrderQuantity > 0) &&
-			(commerceOrderItem.getQuantity() < minOrderQuantity)) {
-
-			return new CommerceOrderValidatorResult(
-				commerceOrderItem.getCommerceOrderItemId(), false,
-				"the-minimum-quantity-is-x", String.valueOf(minOrderQuantity));
-		}
-
-		if ((maxOrderQuantity > 0) &&
-			(commerceOrderItem.getQuantity() > maxOrderQuantity)) {
-
-			return new CommerceOrderValidatorResult(
-				commerceOrderItem.getCommerceOrderItemId(), false,
-				"the-maximum-quantity-is-x", String.valueOf(maxOrderQuantity));
-		}
-
-		if ((allowedOrderQuantities.length > 0) &&
-			!ArrayUtil.contains(
-				allowedOrderQuantities,
-				String.valueOf(commerceOrderItem.getQuantity()))) {
-
-			return new CommerceOrderValidatorResult(
-				commerceOrderItem.getCommerceOrderItemId(), false,
-				"that-quantity-is-not-allowed");
-		}
-
-		return new CommerceOrderValidatorResult(true);
-	}
-
-	@Override
-	public CommerceOrderValidatorResult validate(
-			CPInstance cpInstance, int quantity)
+			CommerceOrder commerceOrder, CPInstance cpInstance, int quantity)
 		throws PortalException {
 
 		if (cpInstance == null) {
@@ -126,16 +82,24 @@ public class DefaultCommerceOrderValidatorImpl
 		String[] allowedOrderQuantities =
 			cpDefinitionInventoryEngine.getAllowedOrderQuantities(cpInstance);
 
+		if (cpDefinitionInventoryEngine.isBackOrderAllowed(cpInstance) &&
+			(quantity >= minOrderQuantity) && (quantity <= maxOrderQuantity)) {
+
+			return new CommerceOrderValidatorResult(true);
+		}
+
 		if ((minOrderQuantity > 0) && (quantity < minOrderQuantity)) {
 			return new CommerceOrderValidatorResult(
 				false, "the-minimum-quantity-is-x",
-				String.valueOf(minOrderQuantity));
+				String.valueOf(minOrderQuantity),
+				_getResourceBundle(commerceOrder));
 		}
 
 		if ((maxOrderQuantity > 0) && (quantity > maxOrderQuantity)) {
 			return new CommerceOrderValidatorResult(
 				false, "the-maximum-quantity-is-x",
-				String.valueOf(maxOrderQuantity));
+				String.valueOf(maxOrderQuantity),
+				_getResourceBundle(commerceOrder));
 		}
 
 		if ((allowedOrderQuantities.length > 0) &&
@@ -143,10 +107,86 @@ public class DefaultCommerceOrderValidatorImpl
 				allowedOrderQuantities, String.valueOf(quantity))) {
 
 			return new CommerceOrderValidatorResult(
-				false, "that-quantity-is-not-allowed");
+				false, "that-quantity-is-not-allowed",
+				_getResourceBundle(commerceOrder));
 		}
 
 		return new CommerceOrderValidatorResult(true);
+	}
+
+	@Override
+	public CommerceOrderValidatorResult validate(
+			CommerceOrderItem commerceOrderItem)
+		throws PortalException {
+
+		CPInstance cpInstance = commerceOrderItem.getCPInstance();
+
+		CPDefinitionInventory cpDefinitionInventory =
+			_cpDefinitionInventoryLocalService.
+				fetchCPDefinitionInventoryByCPDefinitionId(
+					cpInstance.getCPDefinitionId());
+
+		CPDefinitionInventoryEngine cpDefinitionInventoryEngine =
+			_cpDefinitionInventoryEngineRegistry.getCPDefinitionInventoryEngine(
+				cpDefinitionInventory);
+
+		if (cpDefinitionInventoryEngine.isBackOrderAllowed(cpInstance)) {
+			return new CommerceOrderValidatorResult(true);
+		}
+
+		int minOrderQuantity = cpDefinitionInventoryEngine.getMinOrderQuantity(
+			cpInstance);
+		int maxOrderQuantity = cpDefinitionInventoryEngine.getMaxOrderQuantity(
+			cpInstance);
+		String[] allowedOrderQuantities =
+			cpDefinitionInventoryEngine.getAllowedOrderQuantities(cpInstance);
+
+		if ((minOrderQuantity > 0) &&
+			(commerceOrderItem.getQuantity() < minOrderQuantity)) {
+			
+			return new CommerceOrderValidatorResult(
+				commerceOrderItem.getCommerceOrderItemId(), false,
+				"the-minimum-quantity-is-x", String.valueOf(minOrderQuantity),
+				_getResourceBundle(commerceOrderItem.getCommerceOrder()));
+		}
+
+		if ((maxOrderQuantity > 0) &&
+			(commerceOrderItem.getQuantity() > maxOrderQuantity)) {
+
+			return new CommerceOrderValidatorResult(
+				commerceOrderItem.getCommerceOrderItemId(), false,
+				"the-maximum-quantity-is-x", String.valueOf(maxOrderQuantity),
+				_getResourceBundle(commerceOrderItem.getCommerceOrder()));
+		}
+
+		if ((allowedOrderQuantities.length > 0) &&
+			!ArrayUtil.contains(
+				allowedOrderQuantities,
+				String.valueOf(commerceOrderItem.getQuantity()))) {
+
+			return new CommerceOrderValidatorResult(
+				commerceOrderItem.getCommerceOrderItemId(), false,
+				"that-quantity-is-not-allowed",
+				_getResourceBundle(commerceOrderItem.getCommerceOrder()));
+		}
+
+		return new CommerceOrderValidatorResult(true);
+	}
+
+	private ResourceBundle _getResourceBundle(CommerceOrder commerceOrder)
+		throws PortalException {
+
+		Locale locale = _portal.getSiteDefaultLocale(
+			commerceOrder.getSiteGroupId());
+
+		User user = _userLocalService.fetchUser(commerceOrder.getUserId());
+
+		if (user != null) {
+			locale = user.getLocale();
+		}
+
+		return ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
 	}
 
 	@Reference
@@ -156,5 +196,11 @@ public class DefaultCommerceOrderValidatorImpl
 	@Reference
 	private CPDefinitionInventoryLocalService
 		_cpDefinitionInventoryLocalService;
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 }
