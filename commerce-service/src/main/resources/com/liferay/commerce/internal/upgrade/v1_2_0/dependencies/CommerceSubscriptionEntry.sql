5a420cc6a99fd9ac8443224c99f4838f90759efc
Alessio Rendina <asilcomando@gmail.com>
2018-11-19 11:29:47 +0100

    COMMERCE-622 Use subscription status instead of active column

--- commerce-service/src/main/resources/com/liferay/commerce/internal/upgrade/v1_2_0/dependencies/CommerceSubscriptionEntry.sql
+++ commerce-service/src/main/resources/com/liferay/commerce/internal/upgrade/v1_2_0/dependencies/CommerceSubscriptionEntry.sql
@@ -13,14 +13,14 @@ create table CommerceSubscriptionEntry (
 	subscriptionType VARCHAR(75) null,
 	subscriptionTypeSettings TEXT null,
 	maxSubscriptionCycles LONG,
-	active_ BOOLEAN,
+	subscriptionStatus INTEGER,
 	lastIterationDate DATE null,
 	nextIterationDate DATE null,
 	startDate DATE null
 );
 
-create index IX_68301629 on CommerceSubscriptionEntry (active_);
 create index IX_6D080A04 on CommerceSubscriptionEntry (groupId, userId);
+create index IX_B496E103 on CommerceSubscriptionEntry (subscriptionStatus);
 create index IX_4363DED4 on CommerceSubscriptionEntry (uuid_[$COLUMN_LENGTH:75$], companyId);
 create unique index IX_943E0A56 on CommerceSubscriptionEntry (uuid_[$COLUMN_LENGTH:75$], groupId);
 
