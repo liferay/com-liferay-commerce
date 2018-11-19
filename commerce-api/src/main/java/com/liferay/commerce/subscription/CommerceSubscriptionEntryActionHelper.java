821a0cdc256d14ff30f1e17b2033a08d366670c9
Alessio Rendina <asilcomando@gmail.com>
2018-11-19 12:31:42 +0100

    COMMERCE-622 Added commerce subscription entry action helper

---
+++ commerce-api/src/main/java/com/liferay/commerce/subscription/CommerceSubscriptionEntryActionHelper.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.subscription;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@ProviderType
+public interface CommerceSubscriptionEntryActionHelper {
+
+	public void activateCommerceSubscriptionEntry(
+			long commerceSubscriptionEntryId)
+		throws PortalException;
+
+	public void cancelCommerceSubscriptionEntry(
+			long commerceSubscriptionEntryId)
+		throws PortalException;
+
+	public void suspendCommerceSubscriptionEntry(
+			long commerceSubscriptionEntryId)
+		throws PortalException;
+
+}
