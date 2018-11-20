de3a90d69b8163deccb78e0338c2abf5ca0ee081
Ethan Bustad <ethan.bustad@liferay.com>
2018-11-20 14:30:18 +0700

    COMMERCE-737 Source formatting

--- commerce-data-integration/commerce-data-integration-apio-impl/src/main/java/com/liferay/commerce/data/integration/apio/internal/resource/WebSiteCollectionResource.java
+++ commerce-data-integration/commerce-data-integration-apio-impl/src/main/java/com/liferay/commerce/data/integration/apio/internal/resource/WebSiteCollectionResource.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.commerce.data.integration.apio.internal.resource;
 
 import com.liferay.apio.architect.pagination.PageItems;
@@ -29,9 +43,18 @@ import org.osgi.service.component.annotations.Reference;
  * @author Victor Oliveira
  * @author Alejandro Hernández
  */
-@Component(immediate = true)
+@Component(immediate = true, service = CollectionResource.class)
 public class WebSiteCollectionResource
 	implements CollectionResource<Group, Long, WebSiteIdentifier> {
+
+	@Override
+	public CollectionRoutes<Group, Long> collectionRoutes(
+		CollectionRoutes.Builder<Group, Long> builder) {
+
+		return builder.addGetter(
+			this::_getPageItems, Company.class
+		).build();
+	}
 
 	@Override
 	public String getName() {
@@ -92,11 +115,4 @@ public class WebSiteCollectionResource
 	@Reference
 	private GroupLocalService _groupLocalService;
 
-	@Override
-	public CollectionRoutes<Group, Long> collectionRoutes(
-		CollectionRoutes.Builder<Group, Long> builder) {
-		return builder.addGetter(
-			this::_getPageItems, Company.class
-		).build();
-	}
 }
