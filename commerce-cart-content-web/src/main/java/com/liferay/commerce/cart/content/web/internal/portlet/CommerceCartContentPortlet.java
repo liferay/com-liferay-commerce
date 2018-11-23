8ffb78f3af14a0a3bf5a6efac838b90b6a739864
Alessio Rendina <asilcomando@gmail.com>
2018-11-23 17:19:28 +0100

    COMMERCE-662 Added css to commerce cart content portlet

--- commerce-cart-content-web/src/main/java/com/liferay/commerce/cart/content/web/internal/portlet/CommerceCartContentPortlet.java
+++ commerce-cart-content-web/src/main/java/com/liferay/commerce/cart/content/web/internal/portlet/CommerceCartContentPortlet.java
@@ -53,6 +53,7 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.css-class-wrapper=portlet-commerce-cart-content",
 		"com.liferay.portlet.display-category=commerce",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.instanceable=true",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.private-request-attributes=false",
