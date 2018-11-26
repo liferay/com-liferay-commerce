c1d043fbd26a2091797a895c3fea5cf4ab01e446
Marco Leo <marco.leo88@gmail.com>
2018-11-26 11:17:04 +0100

    Revert "COMMERCE-677 Removed useless portlet property"
    This reverts commit 73be49bdfb61ced51393ed7dc7cd755a95dad56f.

--- commerce-subscription-web/src/main/java/com/liferay/commerce/subscription/web/internal/portlet/CommerceSubscriptionEntryPortlet.java
+++ commerce-subscription-web/src/main/java/com/liferay/commerce/subscription/web/internal/portlet/CommerceSubscriptionEntryPortlet.java
@@ -44,6 +44,7 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.add-default-resource=true",
 		"com.liferay.portlet.css-class-wrapper=portlet-commerce-product-subscription",
 		"com.liferay.portlet.display-category=category.hidden",
+		"com.liferay.portlet.header-portlet-css=/css/main.css",
 		"com.liferay.portlet.layout-cacheable=true",
 		"com.liferay.portlet.preferences-owned-by-group=true",
 		"com.liferay.portlet.preferences-unique-per-layout=false",
