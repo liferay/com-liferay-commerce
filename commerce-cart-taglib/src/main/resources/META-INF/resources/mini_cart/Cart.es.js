4167b93d8f3eb0085432e867d4b4263b99f107d0
Marco Leo <marco.leo@liferay.com>
2018-11-26 17:09:51 +0100

    COMMERCE-686 Use temporary a local function for debounce

--- commerce-cart-taglib/src/main/resources/META-INF/resources/mini_cart/Cart.es.js
+++ commerce-cart-taglib/src/main/resources/META-INF/resources/mini_cart/Cart.es.js
@@ -1,6 +1,4 @@
 import 'clay-icon';
-
-import debounce from 'debounce';
 import Component from 'metal-component';
 import Soy from 'metal-soy';
 
@@ -10,6 +8,26 @@ import './CartProduct.es';
 import './Summary.es';
 
 class Cart extends Component {
+
+	// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+	debounce(func, wait, immediate) {
+		var timeout;
+		return function() {
+			var context = this, args = arguments;
+			var later = function() {
+				timeout = null;
+				if (!immediate) func.apply(context, args);
+			};
+			var callNow = immediate && !timeout;
+			clearTimeout(timeout);
+			timeout = setTimeout(later, wait);
+			if (callNow) func.apply(context, args);
+		};
+	};
+
 	toggleCart() {
 		return (this.isOpen = !this.isOpen);
 	}
@@ -28,11 +46,11 @@ class Cart extends Component {
 		const normalizedProducts = rawProducts.map(productData => {
 			return Object.assign(
 				{
-					sendUpdateRequest: debounce(
+					sendUpdateRequest: this.debounce(
 						() => this.sendUpdateRequest(productData.id),
 						500
 					),
-					sendDeleteRequest: debounce(
+					sendDeleteRequest: this.debounce(
 						() => this.sendDeleteRequest(productData.id),
 						500
 					)
