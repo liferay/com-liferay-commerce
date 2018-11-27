f42f9ce8a9ee65f029d6ef85e65fe2ddae32ad9c
Alec Sloan <alec.sloan@liferay.com>
2018-11-27 14:09:06 -0800

    COMMERCE-704 commerce-service Upgrade process

--- commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/CommerceUpgradeStepRegistrator.java
+++ commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/CommerceUpgradeStepRegistrator.java
@@ -19,14 +19,21 @@ import com.liferay.commerce.internal.upgrade.v1_1_0.CommerceOrderNoteUpgradeProc
 import com.liferay.commerce.internal.upgrade.v1_1_0.CommerceOrderUpgradeProcess;
 import com.liferay.commerce.internal.upgrade.v1_2_0.CommerceSubscriptionUpgradeProcess;
 import com.liferay.commerce.internal.upgrade.v2_0_0.CommercePaymentMethodUpgradeProcess;
+import com.liferay.commerce.internal.upgrade.v2_1_0.CPDAvailabilityEstimateUpgradeProcess;
+import com.liferay.commerce.internal.upgrade.v2_1_0.CPDefinitionInventoryUpgradeProcess;
+import com.liferay.commerce.internal.upgrade.v2_1_0.CommerceWarehouseItemUpgradeProcess;
+import com.liferay.commerce.product.service.CPDefinitionLocalService;
+import com.liferay.commerce.product.service.CPInstanceLocalService;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Rodrigo Guedes de Souza
+ * @author Alec Sloan
  */
 @Component(immediate = true, service = UpgradeStepRegistrator.class)
 public class CommerceUpgradeStepRegistrator implements UpgradeStepRegistrator {
@@ -51,6 +58,16 @@ public class CommerceUpgradeStepRegistrator implements UpgradeStepRegistrator {
 			_SCHEMA_VERSION_1_2_0, _SCHEMA_VERSION_2_0_0,
 			new CommercePaymentMethodUpgradeProcess());
 
+		registry.register(
+			_SCHEMA_VERSION_2_0_0, _SCHEMA_VERSION_2_1_0,
+			new CommerceWarehouseItemUpgradeProcess(
+				_cpDefinitionLocalService, _cpInstanceLocalService),
+			new com.liferay.commerce.internal.upgrade.v2_1_0.
+				CommerceOrderItemUpgradeProcess(
+					_cpDefinitionLocalService, _cpInstanceLocalService),
+			new CPDAvailabilityEstimateUpgradeProcess(),
+			new CPDefinitionInventoryUpgradeProcess());
+
 		if (_log.isInfoEnabled()) {
 			_log.info("COMMERCE UPGRADE STEP REGISTRATOR FINISHED");
 		}
@@ -64,7 +81,15 @@ public class CommerceUpgradeStepRegistrator implements UpgradeStepRegistrator {
 
 	private static final String _SCHEMA_VERSION_2_0_0 = "2.0.0";
 
+	private static final String _SCHEMA_VERSION_2_1_0 = "2.1.0";
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		CommerceUpgradeStepRegistrator.class);
+
+	@Reference
+	private CPDefinitionLocalService _cpDefinitionLocalService;
+
+	@Reference
+	private CPInstanceLocalService _cpInstanceLocalService;
 
 }
---
+++ commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v2_1_0/CommerceOrderItemUpgradeProcess.java
@@ -0,0 +1,181 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.internal.upgrade.v2_1_0;
+
+import com.liferay.commerce.model.impl.CommerceOrderItemModelImpl;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CPInstance;
+import com.liferay.commerce.product.service.CPDefinitionLocalService;
+import com.liferay.commerce.product.service.CPInstanceLocalService;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.dao.db.IndexMetadata;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.ObjectValuePair;
+
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Alec Sloan
+ */
+public class CommerceOrderItemUpgradeProcess extends UpgradeProcess {
+
+	public CommerceOrderItemUpgradeProcess(
+		CPDefinitionLocalService cpDefinitionLocalService,
+		CPInstanceLocalService cpInstanceLocalService) {
+
+		_cpDefinitionLocalService = cpDefinitionLocalService;
+		_cpInstanceLocalService = cpInstanceLocalService;
+	}
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		_addColumn(
+			CommerceOrderItemModelImpl.class,
+			CommerceOrderItemModelImpl.TABLE_NAME, "CProductId", "LONG");
+
+		_addIndexes(CommerceOrderItemModelImpl.TABLE_NAME);
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Statement s = null;
+
+		try {
+			ps = connection.prepareStatement(
+				"update CommerceOrderItem set CProductId = ? where " +
+					"CPInstanceId = ?");
+
+			s = connection.createStatement();
+
+			rs = s.executeQuery("select * from CommerceOrderItem");
+
+			while (rs.next()) {
+				long instanceId = rs.getLong("CPInstanceId");
+
+				CPInstance cpInstance = _cpInstanceLocalService.getCPInstance(
+					instanceId);
+
+				CPDefinition cpDefinition =
+					_cpDefinitionLocalService.getCPDefinition(
+						cpInstance.getCPDefinitionId());
+
+				ps.setLong(1, cpDefinition.getCProductId());
+
+				ps.setLong(2, instanceId);
+
+				ps.execute();
+			}
+		}
+		finally {
+			DataAccess.cleanUp(ps);
+			DataAccess.cleanUp(s, rs);
+		}
+	}
+
+	private void _addColumn(
+			Class<?> entityClass, String tableName, String columnName,
+			String columnType)
+		throws Exception {
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				String.format(
+					"Adding column %s to table %s", columnName, tableName));
+		}
+
+		if (!hasColumn(tableName, columnName)) {
+			alter(
+				entityClass,
+				new AlterTableAddColumn(
+					columnName + StringPool.SPACE + columnType));
+		}
+		else {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Column %s already exists on table %s", columnName,
+						tableName));
+			}
+		}
+	}
+
+	private void _addIndexes(String tableName) throws Exception {
+		Class<?> clazz = getClass();
+
+		List<ObjectValuePair<String, IndexMetadata>> indexesSQL = getIndexesSQL(
+			clazz.getClassLoader(), tableName);
+
+		for (ObjectValuePair<String, IndexMetadata> indexSQL : indexesSQL) {
+			IndexMetadata indexMetadata = indexSQL.getValue();
+
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Adding index %s to table %s",
+						indexMetadata.getIndexName(), tableName));
+			}
+
+			if (!_tableHasIndex(tableName, indexMetadata.getIndexName())) {
+				runSQL(indexMetadata.getCreateSQL(null));
+			}
+			else if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Index %s already exists on table %s",
+						indexMetadata.getIndexName(), tableName));
+			}
+		}
+	}
+
+	private boolean _tableHasIndex(String tableName, String indexName)
+		throws Exception {
+
+		ResultSet rs = null;
+
+		try {
+			DatabaseMetaData metadata = connection.getMetaData();
+
+			rs = metadata.getIndexInfo(null, null, tableName, false, false);
+
+			while (rs.next()) {
+				String curIndexName = rs.getString("index_name");
+
+				if (Objects.equals(indexName, curIndexName)) {
+					return true;
+				}
+			}
+		}
+		finally {
+			DataAccess.cleanUp(rs);
+		}
+
+		return false;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CommerceOrderItemUpgradeProcess.class);
+
+	private final CPDefinitionLocalService _cpDefinitionLocalService;
+	private final CPInstanceLocalService _cpInstanceLocalService;
+
+}
