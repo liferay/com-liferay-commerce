51df27eb77680fd50f5cd20005eeb1ede4aa76d8
Alessio Rendina <asilcomando@gmail.com>
2018-12-03 12:15:53 +0100

    COMMERCE-699 Refactor commerce order local service implementation method

--- commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceOrderLocalServiceImpl.java
+++ commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceOrderLocalServiceImpl.java
@@ -1185,6 +1185,7 @@ public class CommerceOrderLocalServiceImpl
 
 		commerceOrder.setUserId(user.getUserId());
 		commerceOrder.setUserName(user.getFullName());
+		commerceOrder.setOrderUserId(user.getUserId());
 
 		return commerceOrderPersistence.update(commerceOrder);
 	}
