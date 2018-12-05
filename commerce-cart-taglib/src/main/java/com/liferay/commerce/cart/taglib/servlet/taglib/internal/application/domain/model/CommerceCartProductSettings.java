42baa94da7b64b4f37bfdcd3408ff9e27e715988
Alessio Rendina <asilcomando@gmail.com>
2018-12-05 11:38:37 +0100

    COMMERCE-686 Added commerce cart domain models

---
+++ commerce-cart-taglib/src/main/java/com/liferay/commerce/cart/taglib/servlet/taglib/internal/application/domain/model/CommerceCartProductSettings.java
@@ -0,0 +1,62 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.cart.taglib.servlet.taglib.internal.application.domain.model;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CommerceCartProductSettings {
+
+	public CommerceCartProductSettings() {
+	}
+
+	public int[] getAllowedQuantities() {
+		return _allowedQuantities;
+	}
+
+	public int getMaxQuantity() {
+		return _maxQuantity;
+	}
+
+	public int getMinQuantity() {
+		return _minQuantity;
+	}
+
+	public int getMultipleQuantity() {
+		return _multipleQuantity;
+	}
+
+	public void setAllowedQuantities(int[] allowedQuantities) {
+		_allowedQuantities = allowedQuantities;
+	}
+
+	public void setMaxQuantity(int maxQuantity) {
+		_maxQuantity = maxQuantity;
+	}
+
+	public void setMinQuantity(int minQuantity) {
+		_minQuantity = minQuantity;
+	}
+
+	public void setMultipleQuantity(int multipleQuantity) {
+		_multipleQuantity = multipleQuantity;
+	}
+
+	private int[] _allowedQuantities;
+	private int _maxQuantity;
+	private int _minQuantity;
+	private int _multipleQuantity;
+
+}
