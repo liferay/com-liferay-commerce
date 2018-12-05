48cacaf86ffdfd8c4942f7ce95fd0aea9e7b828d
Luca Pellizzon <luca.pellizzon@liferay.com>
2018-12-05 15:41:27 +0100

    COMMERCE-600 paypal payment method configuration

---
+++ commerce-payment-method-paypal/src/main/java/com/liferay/commerce/payment/method/paypal/internal/portlet/action/EditPayPalCommercePaymentMethodConfigurationMVCActionCommand.java
@@ -0,0 +1,101 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.payment.method.paypal.internal.portlet.action;
+
+import com.liferay.commerce.admin.constants.CommerceAdminPortletKeys;
+import com.liferay.commerce.payment.method.paypal.internal.constants.PayPalCommercePaymentMethodConstants;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.settings.GroupServiceSettingsLocator;
+import com.liferay.portal.kernel.settings.ModifiableSettings;
+import com.liferay.portal.kernel.settings.Settings;
+import com.liferay.portal.kernel.settings.SettingsFactory;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.Constants;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luca Pellizzon
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + CommerceAdminPortletKeys.COMMERCE_ADMIN,
+		"mvc.command.name=editPayPalCommercePaymentMethodConfiguration"
+	},
+	service = MVCActionCommand.class
+)
+public class EditPayPalCommercePaymentMethodConfigurationMVCActionCommand
+	extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		String cmd = ParamUtil.getString(actionRequest, Constants.CMD);
+
+		if (cmd.equals(Constants.UPDATE)) {
+			_updateCommercePaymentMethod(actionRequest);
+		}
+	}
+
+	private void _updateCommercePaymentMethod(ActionRequest actionRequest)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)actionRequest.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		String clientId = ParamUtil.getString(
+			actionRequest, "settings--clientId--");
+		String clientSecret = ParamUtil.getString(
+			actionRequest, "settings--clientSecret--");
+		String mode = ParamUtil.getString(actionRequest, "settings--mode--");
+
+		Map<String, String> parameterMap = new HashMap<>();
+
+		parameterMap.put("clientId", clientId);
+		parameterMap.put("clientSecret", clientSecret);
+		parameterMap.put("mode", mode);
+
+		Settings settings = _settingsFactory.getSettings(
+			new GroupServiceSettingsLocator(
+				themeDisplay.getScopeGroupId(),
+				PayPalCommercePaymentMethodConstants.SERVICE_NAME));
+
+		ModifiableSettings modifiableSettings =
+			settings.getModifiableSettings();
+
+		for (Map.Entry<String, String> entry : parameterMap.entrySet()) {
+			modifiableSettings.setValue(entry.getKey(), entry.getValue());
+		}
+
+		modifiableSettings.store();
+	}
+
+	@Reference
+	private SettingsFactory _settingsFactory;
+
+}
