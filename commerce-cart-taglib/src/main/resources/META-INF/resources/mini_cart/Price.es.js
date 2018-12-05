89bf6dae0fb237b13c129dfb818c22b8c598c067
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2018-12-05 17:11:03 +0100

    COMMERCE-686 Soy Config added on the Cart

--- commerce-cart-taglib/src/main/resources/META-INF/resources/mini_cart/Price.es.js
+++ commerce-cart-taglib/src/main/resources/META-INF/resources/mini_cart/Price.es.js
@@ -1,25 +1,15 @@
 import template from './Price.soy';
 import Component from 'metal-component';
-import Soy from 'metal-soy';
+import Soy, { Config } from 'metal-soy';
 
 class Price extends Component {}
 
 Price.STATE = {
-	formattedPrice: {
-		value: null
-	},
-	formattedPromoPrice: {
-		value: null
-	},
-	additionalDiscountedClasses: {
-		value: ''
-	},
-	additionalOldPriceClasses: {
-		value: ''
-	},
-	additionalPriceClasses: {
-		value: ''
-	}
+	formattedPrice: Config.string(),
+	formattedPromoPrice: Config.string(),
+	additionalDiscountedClasses: Config.string().value(''),
+	additionalOldPriceClasses: Config.string().value(''),
+	additionalPriceClasses: Config.string().value('')
 };
 
 Soy.register(Price, template);
