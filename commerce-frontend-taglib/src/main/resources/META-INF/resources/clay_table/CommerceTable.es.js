ae875a7bbe3e6248ff8722e6ebac5c6f0993e9d2
Marco Leo <marco.leo@liferay.com>
2018-12-08 00:02:40 +0100

    COMMERCE-755 Add Tagblib to show table

---
+++ commerce-frontend-taglib/src/main/resources/META-INF/resources/clay_table/CommerceTable.es.js
@@ -0,0 +1,19 @@
+import 'clay-icon';
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+
+import template from './CommerceTable.soy';
+
+class CommerceTable extends Component {
+
+
+}
+
+Soy.register(CommerceTable, template);
+
+CommerceTable.STATE = {
+
+};
+
+export {CommerceTable};
+export default CommerceTable;
