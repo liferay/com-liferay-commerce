b5e78a5e10bd4e647a4f30a417ed1d965aece998
Marco Leo <marco.leo@liferay.com>
2018-12-07 23:31:47 +0100

    COMMERCE-755 Add classes to manage clay table

---
+++ commerce-frontend-taglib/src/main/java/com/liferay/commerce/frontend/taglib/table/ClayTableSchemaBuilder.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.frontend.taglib.table;
+
+/**
+ * @author Marco Leo
+ */
+public interface ClayTableSchemaBuilder {
+
+	public void addField(ClayTableSchemaField clayTableSchemaField);
+
+	public ClayTableSchemaField addField(String fieldName);
+
+	public ClayTableSchemaField addField(String fieldName, String label);
+
+	public void addInputNamesMap(String inputName, Object object);
+
+	public void addProperties(String name, Object value);
+
+	public ClayTableSchema build();
+
+	public void setInputNameField(String inputNameField);
+
+	public void setInputValueField(String inputValueField);
+
+}
