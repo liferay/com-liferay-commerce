7a895f546cd6cfd402b9ea6f45a25b2c849d5fe0
Marco Leo <marco.leo@liferay.com>
2018-12-07 23:27:58 +0100

    COMMERCE-686 SF

--- commerce-cart-rest/src/main/java/com/liferay/commerce/cart/rest/internal/model/CartItemUpdate.java
+++ commerce-cart-rest/src/main/java/com/liferay/commerce/cart/rest/internal/model/CartItemUpdate.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- * <p>
+ *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * <p>
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -13,8 +13,6 @@
  */
 
 package com.liferay.commerce.cart.rest.internal.model;
-
-import com.fasterxml.jackson.annotation.JsonUnwrapped;
 
 import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
@@ -25,14 +23,15 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement(name = "cartItem")
 public class CartItemUpdate {
 
-	public void setQuantity(int quantity){
-		_quantity = quantity;
-	}
-
 	@XmlElement
-	public int getQuantity(){
+	public int getQuantity() {
 		return _quantity;
 	}
 
+	public void setQuantity(int quantity) {
+		_quantity = quantity;
+	}
+
 	private int _quantity;
+
 }
