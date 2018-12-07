e588b28c2a7fbaf33fc29143438a3fc10c05552d
Marco Leo <marco.leo@liferay.com>
2018-12-07 23:31:47 +0100

    COMMERCE-755 Add classes to manage clay table

---
+++ commerce-frontend-taglib/src/main/java/com/liferay/commerce/frontend/taglib/internal/table/ClayTableActionProviderRegistryImpl.java
@@ -0,0 +1,92 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.frontend.taglib.internal.table;
+
+import com.liferay.commerce.frontend.taglib.table.ClayTableActionProvider;
+import com.liferay.commerce.frontend.taglib.table.ClayTableActionProviderRegistry;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerCustomizerFactory;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerCustomizerFactory.ServiceWrapper;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Marco Leo
+ */
+@Component(immediate = true, service = ClayTableActionProviderRegistry.class)
+public class ClayTableActionProviderRegistryImpl
+	implements ClayTableActionProviderRegistry {
+
+	@Override
+	public List<ClayTableActionProvider> getClayTableActionProviders(
+		String key) {
+
+		List<ClayTableActionProvider> clayTableActionProviders =
+			new ArrayList<>();
+
+		List<ServiceWrapper<ClayTableActionProvider>>
+			clayTableActionProviderServiceWrappers =
+				_serviceTrackerMap.getService(key);
+
+		if (clayTableActionProviderServiceWrappers == null) {
+			if (_log.isDebugEnabled()) {
+				_log.debug(
+					"No ClayTableActionProvider registered with key " + key);
+			}
+
+			return null;
+		}
+
+		for (ServiceWrapper<ClayTableActionProvider>
+				tableActionProviderServiceWrapper :
+					clayTableActionProviderServiceWrappers) {
+
+			clayTableActionProviders.add(
+				tableActionProviderServiceWrapper.getService());
+		}
+
+		return clayTableActionProviders;
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerMap = ServiceTrackerMapFactory.openMultiValueMap(
+			bundleContext, ClayTableActionProvider.class, "commerce.table.name",
+			ServiceTrackerCustomizerFactory.
+				<ClayTableActionProvider>serviceWrapper(bundleContext));
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		ClayTableActionProviderRegistryImpl.class);
+
+	private ServiceTrackerMap
+		<String, List<ServiceWrapper<ClayTableActionProvider>>>
+			_serviceTrackerMap;
+
+}
