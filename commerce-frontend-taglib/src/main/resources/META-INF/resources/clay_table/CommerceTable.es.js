cc214c006c962ffae540e655e7a564b28c83446c
Marco Leo <marco.leo@liferay.com>
2018-12-10 14:16:31 +0100

    COMMERCE-755 Add base implementation for Clay Table

--- commerce-frontend-taglib/src/main/resources/META-INF/resources/clay_table/CommerceTable.es.js
+++ commerce-frontend-taglib/src/main/resources/META-INF/resources/clay_table/CommerceTable.es.js
@@ -1,4 +1,5 @@
-import 'clay-icon';
+import 'clay-table';
+import 'clay-pagination-bar';
 import Component from 'metal-component';
 import Soy from 'metal-soy';
 
@@ -6,6 +7,15 @@ import template from './CommerceTable.soy';
 
 class CommerceTable extends Component {
 
+	/**
+	 * Toggles the selection of an item and adds or removes it from selected items
+	 * list.
+	 * @param {!Event} event
+	 * @private
+	 */
+	_handleItemToggled(event) {
+
+	}
 
 }
 
