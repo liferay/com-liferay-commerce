557cda0bf468f4110609e531d270064039a8cd52
Ethan Bustad <ethan.bustad@liferay.com>
2018-12-13 12:55:11 -0800

    COMMERCE-704 CPDefinitionLink should now link a defn with a product

---
+++ commerce-product-service/src/main/java/com/liferay/commerce/product/internal/upgrade/v1_3_0/CPDefinitionLinkUpgradeProcess.java
@@ -0,0 +1,258 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.internal.upgrade.v1_3_0;
+
+import com.liferay.commerce.product.model.impl.CPDefinitionLinkModelImpl;
+import com.liferay.portal.kernel.dao.db.IndexMetadata;
+import com.liferay.portal.kernel.dao.jdbc.DataAccess;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+import com.liferay.portal.kernel.util.ObjectValuePair;
+
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+
+import java.util.List;
+import java.util.Objects;
+
+/**
+ * @author Ethan Bustad
+ */
+public class CPDefinitionLinkUpgradeProcess extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		_renameColumn(
+			CPDefinitionLink.class, CPDefinitionLinkModelImpl.TABLE_NAME,
+			"CPDefinitionId1", "CPDefinitionId LONG");
+
+		_addColumn(
+			CPDefinitionGroupedEntry.class,
+			CPDefinitionLinkModelImpl.TABLE_NAME, "entryCProductId", "long");
+
+		_addIndexes(CPDefinitionLinkModelImpl.TABLE_NAME);
+
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		Statement s = null;
+
+		try {
+			ps = connection.prepareStatement(
+				"update CPDefinitionLink set CProductId = ? where " +
+					"CPDefinitionId2 = ?");
+
+			s = connection.createStatement();
+
+			rs = s.executeQuery("select * from CPDefinitionLink");
+
+			while (rs.next()) {
+				long cpDefinitionId2 = rs.getLong("CPDefinitionId2");
+
+				long cProductId = _getCProductId(cpDefinitionId2);
+
+				ps.setLong(1, cProductId);
+
+				ps.setLong(2, cpDefinitionId2);
+
+				ps.execute();
+			}
+		}
+		finally {
+			DataAccess.cleanUp(ps);
+			DataAccess.cleanUp(s, rs);
+		}
+
+		_dropIndex(CPDefinitionLinkModelImpl.TABLE_NAME, "IX_31ED1AF");
+		_dropIndex(CPDefinitionLinkModelImpl.TABLE_NAME, "IX_EA724334");
+		_dropIndex(CPDefinitionLinkModelImpl.TABLE_NAME, "IX_F76842CE");
+
+		_dropColumn(CPDefinitionLinkModelImpl.TABLE_NAME, "CPDefinitionId2");
+	}
+
+	private void _addColumn(
+			Class<?> entityClass, String tableName, String columnName,
+			String columnType)
+		throws Exception {
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				String.format(
+					"Adding column %s to table %s", columnName, tableName));
+		}
+
+		if (!hasColumn(tableName, columnName)) {
+			alter(
+				entityClass,
+				new AlterTableAddColumn(
+					columnName + StringPool.SPACE + columnType));
+		}
+		else {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Column %s already exists on table %s", columnName,
+						tableName));
+			}
+		}
+	}
+
+	private void _addIndexes(String tableName) throws Exception {
+		Class<?> clazz = getClass();
+
+		List<ObjectValuePair<String, IndexMetadata>> indexesSQL = getIndexesSQL(
+			clazz.getClassLoader(), tableName);
+
+		for (ObjectValuePair<String, IndexMetadata> indexSQL : indexesSQL) {
+			IndexMetadata indexMetadata = indexSQL.getValue();
+
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Adding index %s to table %s",
+						indexMetadata.getIndexName(), tableName));
+			}
+
+			if (!_tableHasIndex(tableName, indexMetadata.getIndexName())) {
+				runSQL(indexMetadata.getCreateSQL(null));
+			}
+			else if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Index %s already exists on table %s",
+						indexMetadata.getIndexName(), tableName));
+			}
+		}
+	}
+
+	private void _dropColumn(String tableName, String columnName) {
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				String.format(
+					"Dropping column %s from table %s", columnName, tableName));
+		}
+
+		if (hasColumn(tableName, columnName)) {
+			runSQL(
+				StringBundler.concat(
+					"alter table ", tableName, " drop column ", columnName));
+		}
+		else {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Column %s already does not exist on table %s",
+						columnName, tableName));
+			}
+		}
+	}
+
+	private void _dropIndex(String tableName, String indexName) {
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				String.format(
+					"Dropping index %s from table %s", indexName, tableName));
+		}
+
+		if (_tableHasIndex(tableName, indexName)) {
+			runSQL(
+				StringBundler.concat(
+					"drop index ", indexName, " on ", tableName));
+		}
+		else {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Index %s already does not exist on table %s",
+						indexName, tableName));
+			}
+		}
+	}
+
+	private void _getCProductId(long cpDefinitionId) throws Exception {
+		Statement s = null;
+		ResultSet rs = null;
+
+		try {
+			s = connection.createStatement();
+
+			rs = s.executeQuery(
+				"select CProductId from CPDefinition where CPDefinitionId = " +
+					cpDefinitionId);
+
+			if (rs.next()) {
+				return rs.getLong("CProductId");
+			}
+		}
+		finally {
+			DataAccess.cleanUp(s, rs);
+		}
+	}
+
+	private void _renameColumn(
+			Class<?> tableClass, String tableName, String oldColumnName,
+			String newColumnName)
+		throws Exception {
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				String.format(
+					"Renaming column %s to %s in table %s", oldColumnName,
+					newColumnName, tableName));
+		}
+
+		if (!hasColumn(tableName, newColumnName)) {
+			alter(
+				tableClass, new AlterColumnName(oldColumnName, newColumnName));
+		}
+		else {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Column %s already exists on table %s", newColumnName,
+						tableName));
+			}
+		}
+	}
+
+	private boolean _tableHasIndex(String tableName, String indexName)
+		throws Exception {
+
+		ResultSet rs = null;
+
+		try {
+			DatabaseMetaData metadata = connection.getMetaData();
+
+			rs = metadata.getIndexInfo(null, null, tableName, false, false);
+
+			while (rs.next()) {
+				String curIndexName = rs.getString("index_name");
+
+				if (Objects.equals(indexName, curIndexName)) {
+					return true;
+				}
+			}
+		}
+		finally {
+			DataAccess.cleanUp(rs);
+		}
+
+		return false;
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CPDefinitionLinkUpgradeProcess.class);
+
+}
