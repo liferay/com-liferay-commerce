e1bd69b09538bf5c94818f7e8731deb27e24532a
Luca Pellizzon <luca.pellizzon@liferay.com>
2018-12-21 12:50:31 +0100

    COMMERCE-600 fix typo Commerce_CommercePaymentMethod

--- commerce-payment-service/src/main/resources/META-INF/sql/tables.sql
+++ commerce-payment-service/src/main/resources/META-INF/sql/tables.sql
@@ -13,19 +13,3 @@ create table CommercePaymentMethodGroupRel (
 	priority DOUBLE,
 	active_ BOOLEAN
 );
-
-create table Commerce_CommercePaymentMethod (
-	commercePaymentMethodId LONG not null primary key,
-	groupId LONG,
-	companyId LONG,
-	userId LONG,
-	userName VARCHAR(75) null,
-	createDate DATE null,
-	modifiedDate DATE null,
-	name STRING null,
-	description STRING null,
-	imageId LONG,
-	engineKey VARCHAR(75) null,
-	priority DOUBLE,
-	active_ BOOLEAN
-);
