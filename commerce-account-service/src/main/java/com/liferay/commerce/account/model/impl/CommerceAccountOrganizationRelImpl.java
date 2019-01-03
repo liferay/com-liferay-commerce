db9e2dfe117c66ad93c67895513e50fd457d4d62
Alessio Rendina <asilcomando@gmail.com>
2019-01-03 20:06:38 +0100

    COMMERCE-774 Refactor commerce account organization rel implementation

--- commerce-account-service/src/main/java/com/liferay/commerce/account/model/impl/CommerceAccountOrganizationRelImpl.java
+++ commerce-account-service/src/main/java/com/liferay/commerce/account/model/impl/CommerceAccountOrganizationRelImpl.java
@@ -16,25 +16,25 @@ package com.liferay.commerce.account.model.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.service.OrganizationLocalServiceUtil;
+
 /**
- * The extended model implementation for the CommerceAccountOrganizationRel service. Represents a row in the &quot;CommerceAccountOrganizationRel&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.commerce.account.model.CommerceAccountOrganizationRel} interface.
- * </p>
- *
  * @author Marco Leo
+ * @author Alessio Antonio Rendina
  */
 @ProviderType
 public class CommerceAccountOrganizationRelImpl
 	extends CommerceAccountOrganizationRelBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a commerce account organization rel model instance should use the {@link com.liferay.commerce.account.model.CommerceAccountOrganizationRel} interface instead.
-	 */
 	public CommerceAccountOrganizationRelImpl() {
+	}
+
+	@Override
+	public Organization getOrganization() throws PortalException {
+		return OrganizationLocalServiceUtil.getOrganization(
+			getOrganizationId());
 	}
 
 }
