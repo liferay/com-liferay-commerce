12d5c78e70e74ab8f55b77a3960e69785f7ac5b2
Alessio Rendina <asilcomando@gmail.com>
2019-01-03 11:07:39 +0100

    COMMERCE-774 Added user and organization model listeners

---
+++ commerce-account-service/src/main/java/com/liferay/commerce/account/internal/model/listener/UserModelListener.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.account.internal.model.listener;
+
+import com.liferay.commerce.account.service.CommerceAccountUserRelLocalService;
+import com.liferay.portal.kernel.model.BaseModelListener;
+import com.liferay.portal.kernel.model.ModelListener;
+import com.liferay.portal.kernel.model.User;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(immediate = true, service = ModelListener.class)
+public class UserModelListener extends BaseModelListener<User> {
+
+	@Override
+	public void onBeforeRemove(User user) {
+		_commerceAccountUserRelLocalService.
+			deleteCommerceAccountUserRelsByCommerceAccountUserId(
+				user.getUserId());
+	}
+
+	@Reference
+	private CommerceAccountUserRelLocalService
+		_commerceAccountUserRelLocalService;
+
+}
