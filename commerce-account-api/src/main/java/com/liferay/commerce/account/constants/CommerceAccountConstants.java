e3bc7a561f795361cba387d1ed45317621e088ba
Alessio Rendina <asilcomando@gmail.com>
2019-01-03 10:55:14 +0100

    COMMERCE-774 Added new constant

--- commerce-account-api/src/main/java/com/liferay/commerce/account/constants/CommerceAccountConstants.java
+++ commerce-account-api/src/main/java/com/liferay/commerce/account/constants/CommerceAccountConstants.java
@@ -1,11 +1,11 @@
 /**
  * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
- * <p>
+ *
  * This library is free software; you can redistribute it and/or modify it under
  * the terms of the GNU Lesser General Public License as published by the Free
  * Software Foundation; either version 2.1 of the License, or (at your option)
  * any later version.
- * <p>
+ *
  * This library is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
  * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
@@ -16,8 +16,12 @@ package com.liferay.commerce.account.constants;
 
 /**
  * @author Marco Leo
+ * @author Alessio Antonio Rendina
  */
 public class CommerceAccountConstants {
 
-	public static final int DEFAULT_PARENT_ACCOUNT_ID = 0;
+	public static final long DEFAULT_PARENT_ACCOUNT_ID = 0;
+
+	public static final String RESOURCE_NAME = "com.liferay.commerce.account";
+
 }
