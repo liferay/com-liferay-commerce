e7b677c530c85c454314cff3083baf7667071ea8
Alessio Rendina <asilcomando@gmail.com>
2019-01-05 17:38:32 +0100

    COMMERCE-774 Use new helper instead

--- commerce-health-status-web/src/main/java/com/liferay/commerce/health/status/web/internal/util/AccountsCommerceHealthStatus.java
+++ commerce-health-status-web/src/main/java/com/liferay/commerce/health/status/web/internal/util/AccountsCommerceHealthStatus.java
@@ -14,25 +14,17 @@
 
 package com.liferay.commerce.health.status.web.internal.util;
 
-import com.liferay.commerce.account.constants.CommerceAccountActionKeys;
 import com.liferay.commerce.account.constants.CommerceAccountConstants;
-import com.liferay.commerce.account.model.CommerceAccount;
-import com.liferay.commerce.constants.CommerceOrderActionKeys;
-import com.liferay.commerce.constants.CommerceOrderConstants;
+import com.liferay.commerce.account.util.CommerceAccountRoleHelper;
 import com.liferay.commerce.health.status.CommerceHealthStatus;
 import com.liferay.commerce.health.status.web.internal.constants.CommerceHealthStatusConstants;
-import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.GroupConstants;
-import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.Role;
-import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.service.GroupLocalService;
-import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
 import com.liferay.portal.kernel.service.RoleService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.ServiceContextFactory;
@@ -41,11 +33,8 @@ import com.liferay.portal.kernel.transaction.TransactionConfig;
 import com.liferay.portal.kernel.transaction.TransactionInvokerUtil;
 import com.liferay.portal.kernel.util.ResourceBundleUtil;
 
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 import java.util.ResourceBundle;
 import java.util.concurrent.Callable;
 
@@ -118,107 +107,11 @@ public class AccountsCommerceHealthStatus implements CommerceHealthStatus {
 		Group group = _groupLocalService.getGroup(groupId);
 
 		List<Role> roles = _roleService.getRoles(
-			group.getCompanyId(), new int[] {_ROLE_TYPE});
+			group.getCompanyId(),
+			new int[] {CommerceAccountConstants.ACCOUNT_ROLE_TYPE});
 
 		return !roles.isEmpty();
 	}
-
-	// TODO creare costante
-
-	private void _addRole(String name, ServiceContext serviceContext)
-		throws PortalException {
-
-		Role role = _roleService.addRole(
-			null, 0, name,
-			Collections.singletonMap(serviceContext.getLocale(), name),
-			Collections.emptyMap(), _ROLE_TYPE, StringPool.BLANK,
-			serviceContext);
-
-		_setRolePermissions(role, serviceContext);
-	}
-
-	private void _setRolePermissions(
-			Role role, Map<String, String[]> resourceActionIds,
-			ServiceContext serviceContext)
-		throws PortalException {
-
-		for (Map.Entry<String, String[]> entry : resourceActionIds.entrySet()) {
-			for (String actionId : entry.getValue()) {
-				_resourcePermissionLocalService.addResourcePermission(
-					serviceContext.getCompanyId(), entry.getKey(),
-					ResourceConstants.SCOPE_GROUP_TEMPLATE,
-					String.valueOf(GroupConstants.DEFAULT_PARENT_GROUP_ID),
-					role.getRoleId(), actionId);
-			}
-		}
-	}
-
-	private void _setRolePermissions(Role role, ServiceContext serviceContext)
-		throws PortalException {
-
-		Map<String, String[]> resourceActionIds = new HashMap<>();
-
-		String name = role.getName();
-
-		if (name.equals("Account Administrator")) {
-			resourceActionIds.put(
-				CommerceAccountConstants.RESOURCE_NAME,
-				new String[] {CommerceAccountActionKeys.MANAGE_ACCOUNTS});
-			resourceActionIds.put(
-				CommerceOrderConstants.RESOURCE_NAME,
-				new String[] {
-					CommerceOrderActionKeys.ADD_COMMERCE_ORDER,
-					CommerceOrderActionKeys.APPROVE_OPEN_COMMERCE_ORDERS,
-					CommerceOrderActionKeys.CHECKOUT_OPEN_COMMERCE_ORDERS,
-					CommerceOrderActionKeys.DELETE_COMMERCE_ORDERS,
-					CommerceOrderActionKeys.MANAGE_COMMERCE_ORDERS,
-					CommerceOrderActionKeys.VIEW_COMMERCE_ORDERS,
-					CommerceOrderActionKeys.VIEW_OPEN_COMMERCE_ORDERS
-				});
-			resourceActionIds.put(
-				CommerceAccount.class.getName(),
-				new String[] {
-					ActionKeys.DELETE, ActionKeys.UPDATE, ActionKeys.VIEW
-				});
-		}
-		else if (name.equals("Account Manager")) {
-			resourceActionIds.put(
-				CommerceAccountConstants.RESOURCE_NAME,
-				new String[] {CommerceAccountActionKeys.MANAGE_ACCOUNTS});
-			resourceActionIds.put(
-				CommerceAccount.class.getName(),
-				new String[] {
-					ActionKeys.DELETE, ActionKeys.UPDATE, ActionKeys.VIEW
-				});
-		}
-		else if (name.equals("Buyer")) {
-			resourceActionIds.put(
-				CommerceOrderConstants.RESOURCE_NAME,
-				new String[] {
-					CommerceOrderActionKeys.ADD_COMMERCE_ORDER,
-					CommerceOrderActionKeys.CHECKOUT_OPEN_COMMERCE_ORDERS,
-					CommerceOrderActionKeys.VIEW_COMMERCE_ORDERS,
-					CommerceOrderActionKeys.VIEW_OPEN_COMMERCE_ORDERS
-				});
-		}
-		else if (name.equals("Order Manager")) {
-			resourceActionIds.put(
-				CommerceOrderConstants.RESOURCE_NAME,
-				new String[] {
-					CommerceOrderActionKeys.ADD_COMMERCE_ORDER,
-					CommerceOrderActionKeys.APPROVE_OPEN_COMMERCE_ORDERS,
-					CommerceOrderActionKeys.CHECKOUT_OPEN_COMMERCE_ORDERS,
-					CommerceOrderActionKeys.DELETE_COMMERCE_ORDERS,
-					CommerceOrderActionKeys.MANAGE_COMMERCE_ORDERS,
-					CommerceOrderActionKeys.VIEW_COMMERCE_ORDERS,
-					CommerceOrderActionKeys.VIEW_OPEN_COMMERCE_ORDERS
-				});
-		}
-
-		_setRolePermissions(role, resourceActionIds, serviceContext);
-	}
-
-	private static final int _ROLE_TYPE = 4;
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		AccountsCommerceHealthStatus.class);
@@ -228,10 +121,10 @@ public class AccountsCommerceHealthStatus implements CommerceHealthStatus {
 			Propagation.REQUIRED, new Class<?>[] {Exception.class});
 
 	@Reference
-	private GroupLocalService _groupLocalService;
+	private CommerceAccountRoleHelper _commerceAccountRoleHelper;
 
 	@Reference
-	private ResourcePermissionLocalService _resourcePermissionLocalService;
+	private GroupLocalService _groupLocalService;
 
 	@Reference
 	private RoleService _roleService;
@@ -240,10 +133,8 @@ public class AccountsCommerceHealthStatus implements CommerceHealthStatus {
 
 		@Override
 		public Object call() throws Exception {
-			_addRole("Account Administrator", _serviceContext);
-			_addRole("Account Manager", _serviceContext);
-			_addRole("Buyer", _serviceContext);
-			_addRole("Order Manager", _serviceContext);
+			_commerceAccountRoleHelper.checkCommerceAccountRoles(
+				_serviceContext);
 
 			return null;
 		}
