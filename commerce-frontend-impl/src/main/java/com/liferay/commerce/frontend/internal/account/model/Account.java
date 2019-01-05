da23a5ae8da2fe51008b393b3cc2f9d915ae806f
Alessio Rendina <asilcomando@gmail.com>
2019-01-05 17:41:00 +0100

    COMMERCE-774 Added new commerce account resource and models

---
+++ commerce-frontend-impl/src/main/java/com/liferay/commerce/frontend/internal/account/model/Account.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.frontend.internal.account.model;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class Account {
+
+	public Account(long accountId, String name, String thumbnail) {
+		_accountId = accountId;
+		_name = name;
+		_success = true;
+		_thumbnail = thumbnail;
+	}
+
+	public Account(String[] errorMessages) {
+		_errorMessages = errorMessages;
+		_success = false;
+	}
+
+	public String[] getErrorMessages() {
+		return _errorMessages;
+	}
+
+	public long getId() {
+		return _accountId;
+	}
+
+	public String getName() {
+		return _name;
+	}
+
+	public boolean getSuccess() {
+		return _success;
+	}
+
+	public String getThumbnail() {
+		return _thumbnail;
+	}
+
+	public void setErrorMessages(String[] errorMessages) {
+		_errorMessages = errorMessages;
+	}
+
+	public void setSuccess(boolean success) {
+		_success = success;
+	}
+
+	private long _accountId;
+	private String[] _errorMessages;
+	private String _name;
+	private boolean _success;
+	private String _thumbnail;
+
+}
