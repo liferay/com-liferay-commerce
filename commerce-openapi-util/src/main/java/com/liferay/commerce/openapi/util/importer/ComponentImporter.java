b8715b566fda5136dfd5ecfc20a7a4590e59182c
Igor Beslic <igor.beslic@liferay.com>
2019-01-08 10:34:34 +0100

    COMMERCE-766 commerce-openapi-util - importer classes - access swagger API endpoint, fetches openapi specification, creates Definition object that represents openapi object model

---
+++ commerce-openapi-util/src/main/java/com/liferay/commerce/openapi/util/importer/ComponentImporter.java
@@ -0,0 +1,208 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.openapi.util.importer;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+import com.liferay.commerce.openapi.util.ComponentDefinition;
+import com.liferay.commerce.openapi.util.PropertyDefinition;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Consumer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Igor Beslic
+ */
+public class ComponentImporter {
+
+	public List<ComponentDefinition> getComponents(
+		JsonNode componentsJSONNode) {
+
+		List<ComponentDefinition> components = new ArrayList<>();
+
+		if (componentsJSONNode.has("schemas")) {
+			components.addAll(_getSchemas(componentsJSONNode));
+		}
+
+		if (componentsJSONNode.has("parameters")) {
+			components.addAll(_getParameters(componentsJSONNode));
+		}
+
+		return components;
+	}
+
+	private List<ComponentDefinition> _getParameters(
+		JsonNode componentsJSONNode) {
+
+		ParameterImporter parameterImporter = new ParameterImporter();
+
+		List<ComponentDefinition> components = new ArrayList<>();
+
+		JsonNode parametersJSONNode = componentsJSONNode.get("parameters");
+
+		Iterator<Map.Entry<String, JsonNode>> fields =
+			parametersJSONNode.fields();
+
+		while (fields.hasNext()) {
+			Map.Entry<String, JsonNode> parameterField = fields.next();
+
+			ComponentDefinition componentDefinition = new ComponentDefinition(
+				parameterField.getKey(),
+				parameterImporter.getParameter(parameterField.getValue()));
+
+			components.add(componentDefinition);
+
+			_logger.debug("Resolved parameter {}", componentDefinition);
+		}
+
+		return components;
+	}
+
+	private List<PropertyDefinition> _getPropertyDefinitions(
+		JsonNode componentJSONNode) {
+
+		List<PropertyDefinition> propertyDefinitions = new ArrayList<>();
+
+		List<String> requiredProperties = new ArrayList<>();
+
+		if (componentJSONNode.has("required")) {
+			JsonNode requiredJSONNode = componentJSONNode.get("required");
+
+			if (requiredJSONNode.isArray() && (requiredJSONNode.size() > 0)) {
+				for (int i = 0; i < requiredJSONNode.size(); i++) {
+					JsonNode jsonNode = requiredJSONNode.get(i);
+
+					requiredProperties.add(jsonNode.asText());
+				}
+			}
+		}
+
+		if (componentJSONNode.has("properties")) {
+			JsonNode propertiesJSONNode = componentJSONNode.get("properties");
+
+			Iterator<Map.Entry<String, JsonNode>> fields =
+				propertiesJSONNode.fields();
+
+			while (fields.hasNext()) {
+				Map.Entry<String, JsonNode> propertyEntry = fields.next();
+
+				JsonNode propertyJSONNode = propertyEntry.getValue();
+
+				JsonNode propertyTypeJSONNode = propertyJSONNode.get("type");
+
+				String propertyType = propertyTypeJSONNode.asText();
+
+				PropertyDefinition propertyDefinition = null;
+
+				if ("array".equals(propertyType)) {
+					JsonNode itemsJSONNode = propertyJSONNode.get("items");
+
+					JsonNode itemsTypeJSONNode = itemsJSONNode.get("type");
+
+					String itemType = itemsTypeJSONNode.asText();
+
+					String itemFormat = null;
+
+					if (itemsJSONNode.has("format")) {
+						JsonNode itemsFormatJSONNode = itemsJSONNode.get(
+							"format");
+
+						itemFormat = itemsFormatJSONNode.asText();
+					}
+
+					propertyDefinition = new PropertyDefinition(
+						propertyEntry.getKey(), propertyType, itemType,
+						itemFormat);
+				}
+				else {
+					String format = null;
+
+					if (propertyJSONNode.has("format")) {
+						JsonNode propertyFormatJSONNode = propertyJSONNode.get(
+							"format");
+
+						format = propertyFormatJSONNode.asText();
+					}
+
+					propertyDefinition = new PropertyDefinition(
+						propertyEntry.getKey(), propertyType, format);
+				}
+
+				_setIfHas(
+					propertyJSONNode, "example",
+					propertyDefinition :: setExample);
+
+				if (requiredProperties.contains(propertyDefinition.getName())) {
+					propertyDefinition.setRequired(true);
+				}
+
+				propertyDefinitions.add(propertyDefinition);
+			}
+		}
+
+		return propertyDefinitions;
+	}
+
+	private List<ComponentDefinition> _getSchemas(JsonNode componentsJSONNode) {
+		List<ComponentDefinition> components = new ArrayList<>();
+
+		JsonNode schemasJSONNode = componentsJSONNode.get("schemas");
+
+		Iterator<Map.Entry<String, JsonNode>> schemaFields =
+			schemasJSONNode.fields();
+
+		while (schemaFields.hasNext()) {
+			Map.Entry<String, JsonNode> schemaEntry = schemaFields.next();
+
+			JsonNode schemaJSONNode = schemaEntry.getValue();
+
+			JsonNode typeJSONNode = schemaJSONNode.get("type");
+
+			String type = typeJSONNode.asText();
+
+			ComponentDefinition componentDefinition = new ComponentDefinition(
+				schemaEntry.getKey(), _getPropertyDefinitions(schemaJSONNode),
+				type);
+
+			components.add(componentDefinition);
+
+			_logger.debug("Resolved schema {}", componentDefinition);
+		}
+
+		return components;
+	}
+
+	private void _setIfHas(
+		JsonNode jsonNode, String name, Consumer<String> setter) {
+
+		if (!jsonNode.has(name)) {
+			return;
+		}
+
+		JsonNode valueJSONNOde = jsonNode.get(name);
+
+		setter.accept(valueJSONNOde.asText());
+	}
+
+	private static final Logger _logger = LoggerFactory.getLogger(
+		ComponentImporter.class);
+
+}
---
+++ commerce-openapi-util/src/main/java/com/liferay/commerce/openapi/util/importer/OpenAPIImporter.java
@@ -0,0 +1,341 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.openapi.util.importer;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.liferay.commerce.openapi.util.ClassPropertiesFactory;
+import com.liferay.commerce.openapi.util.ComponentDefinition;
+import com.liferay.commerce.openapi.util.Content;
+import com.liferay.commerce.openapi.util.Definition;
+import com.liferay.commerce.openapi.util.Method;
+import com.liferay.commerce.openapi.util.Parameter;
+import com.liferay.commerce.openapi.util.Response;
+import com.liferay.commerce.openapi.util.Schema;
+import com.liferay.commerce.openapi.util.importer.exception.ImporterException;
+import com.liferay.petra.json.web.service.client.JSONWebServiceClient;
+import com.liferay.petra.json.web.service.client.JSONWebServiceException;
+import com.liferay.petra.json.web.service.client.internal.JSONWebServiceClientImpl;
+
+import java.io.IOException;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Igor Beslic
+ */
+public class OpenAPIImporter {
+
+	public static void main(String[] args) throws Exception {
+		OpenAPIImporter openAPIImporter = new OpenAPIImporter();
+
+		try {
+			Definition definition = openAPIImporter.getDefinition();
+
+			_logger.debug("Resolved definition {}", definition);
+
+			System.exit(0);
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+
+			System.exit(-1);
+		}
+	}
+
+	public OpenAPIImporter() throws IOException {
+		_properties = ClassPropertiesFactory.getPropertiesFor(getClass());
+
+		_apiDefinitionURL = _properties.getProperty("openapi.swagger.url");
+
+		_headers.add(
+			new BasicNameValuePair(
+				"Authorization",
+				_properties.getProperty("openapi.swagger.authorization.key")));
+
+		_jsonWebServiceClient = new JSONWebServiceClientImpl();
+
+		_jsonWebServiceClient.setHostName(
+			_properties.getProperty("openapi.swagger.host.name"));
+
+		_jsonWebServiceClient.setHostPort(
+			_getInteger(_properties.getProperty("openapi.swagger.host.port")));
+
+		_jsonWebServiceClient.setProtocol(
+			_properties.getProperty("openapi.swagger.protocol"));
+	}
+
+	public Definition getDefinition() {
+		if (_definition != null) {
+			return _definition;
+		}
+
+		try {
+			_openAPIDefinitionJSON = _jsonWebServiceClient.doGet(
+				_apiDefinitionURL, Collections.emptyList(), _headers);
+
+			_definition = _getDefinition(_openAPIDefinitionJSON);
+
+			return _definition;
+		}
+		catch (JSONWebServiceException jsonwse) {
+			throw new ImporterException(
+				"Unable to get open API definition", jsonwse);
+		}
+		catch (IOException ioe) {
+			throw new ImporterException(
+				"Unable to get open API definition", ioe);
+		}
+	}
+
+	private List<Content> _getContents(JsonNode contentJsonNode) {
+		List<Content> contents = new ArrayList<>();
+
+		Iterator<String> fieldNames = contentJsonNode.fieldNames();
+
+		fieldNames.forEachRemaining(
+			mimeType -> {
+				JsonNode mimeTypeJSONNode = contentJsonNode.get(mimeType);
+
+				Schema schema = null;
+
+				if (mimeTypeJSONNode.has("schema")) {
+					schema = ParameterImporter.getSchema(
+						mimeTypeJSONNode.get("schema"));
+				}
+
+				contents.add(new Content(mimeType, schema));
+			});
+
+		return contents;
+	}
+
+	private Definition _getDefinition(String json) throws IOException {
+		ObjectMapper mapper = new ObjectMapper();
+
+		JsonNode rootJSONNode = mapper.readTree(json);
+
+		JsonNode infoJSONNode = rootJSONNode.get("info");
+
+		JsonNode descriptionJSONNode = infoJSONNode.get("description");
+		JsonNode titleJSONNode = infoJSONNode.get("title");
+		JsonNode versionJSONNode = infoJSONNode.get("version");
+
+		Definition definition = new Definition(
+			versionJSONNode.asText(), descriptionJSONNode.asText(),
+			titleJSONNode.asText());
+
+		ComponentImporter componentImporter = new ComponentImporter();
+
+		List<ComponentDefinition> componentDefinitions =
+			componentImporter.getComponents(rootJSONNode.get("components"));
+
+		definition.setComponentDefinitions(componentDefinitions);
+
+		JsonNode pathsJSONNode = rootJSONNode.get("paths");
+
+		Iterator<String> iterator = pathsJSONNode.fieldNames();
+
+		while (iterator.hasNext()) {
+			String path = iterator.next();
+
+			List<Method> methods = _getMethods(
+				path, pathsJSONNode.get(path), componentDefinitions);
+
+			for (Method method : methods) {
+				definition.addMethod(method);
+			}
+		}
+
+		return definition;
+	}
+
+	private int _getInteger(String value) {
+		if (value == null) {
+			return 0;
+		}
+
+		try {
+			return Integer.parseInt(value);
+		}
+		catch (NumberFormatException nfe) {
+			_logger.error("Unable to parse value {}", value, nfe);
+		}
+
+		return 0;
+	}
+
+	private List<Method> _getMethods(
+		String path, JsonNode pathJSONNode,
+		List<ComponentDefinition> componentDefinitions) {
+
+		List<Method> methods = new ArrayList<>();
+
+		Iterator<String> iterator = pathJSONNode.fieldNames();
+
+		while (iterator.hasNext()) {
+			String httpMethodName = iterator.next();
+
+			JsonNode httpMethodJSONNode = pathJSONNode.get(httpMethodName);
+
+			String methodName = httpMethodName;
+
+			if (httpMethodJSONNode.has("operationId")) {
+				JsonNode operationIdJSONNode = httpMethodJSONNode.get(
+					"operationId");
+
+				methodName = operationIdJSONNode.asText();
+			}
+
+			List<Parameter> parameters = new ArrayList<>();
+
+			if (httpMethodJSONNode.has("parameters")) {
+				parameters.addAll(
+					_getParameters(
+						httpMethodJSONNode.get("parameters"),
+						componentDefinitions));
+			}
+
+			String methodAccepts = null;
+
+			if (httpMethodJSONNode.has("requestBody")) {
+				JsonNode requestBody = httpMethodJSONNode.get("requestBody");
+
+				List<Content> contents = _getContents(
+					requestBody.get("content"));
+
+				if (!contents.isEmpty()) {
+					Content content = contents.get(0);
+
+					Schema schema = content.getSchema();
+
+					parameters.add(
+						new Parameter(
+							schema.getReferencedModel(), "body", content,
+							true));
+
+					methodAccepts = content.getMimeType();
+				}
+			}
+
+			JsonNode responsesJSONNode = httpMethodJSONNode.get("responses");
+
+			List<Response> responses = new ArrayList<>();
+
+			Iterator<Map.Entry<String, JsonNode>> fields =
+				responsesJSONNode.fields();
+
+			fields.forEachRemaining(
+				entry -> {
+					JsonNode jsonNode = entry.getValue();
+
+					if (jsonNode.has("content")) {
+						List<Content> contents = _getContents(
+							jsonNode.get("content"));
+
+						for (Content content : contents) {
+							responses.add(
+								new Response(
+									_getInteger(entry.getKey()),
+									content.getMimeType()));
+						}
+					}
+					else {
+						responses.add(
+							new Response(_getInteger(entry.getKey()), null));
+					}
+				});
+
+			methods.add(
+				new Method(
+					methodName, methodAccepts, httpMethodName, path, parameters,
+					responses));
+		}
+
+		return methods;
+	}
+
+	private List<Parameter> _getParameters(
+		JsonNode parametersJSONNode,
+		List<ComponentDefinition> componentDefinitions) {
+
+		if (parametersJSONNode.isNull()) {
+			return Collections.emptyList();
+		}
+
+		List<Parameter> parameters = new ArrayList<>();
+
+		parametersJSONNode.forEach(
+			parameterJSONNode -> {
+				if (parameterJSONNode.has("$ref")) {
+					JsonNode parameterReferenceJSONNode = parameterJSONNode.get(
+						"$ref");
+
+					String parameterReference = Parameter.getParameterReference(
+						parameterReferenceJSONNode.asText());
+
+					_logger.debug(
+						"Parameter is reference to {}", parameterReference);
+
+					for (ComponentDefinition componentDefinition :
+							componentDefinitions) {
+
+						if (!componentDefinition.isParameter()) {
+							continue;
+						}
+
+						if (parameterReference.equals(
+								componentDefinition.getName())) {
+
+							parameters.add(componentDefinition.getParameter());
+
+							_logger.debug(
+								"Reference resolved to {}",
+								componentDefinition.getParameter());
+						}
+					}
+				}
+				else {
+					parameters.add(
+						ParameterImporter.getParameter(parameterJSONNode));
+				}
+			});
+
+		return parameters;
+	}
+
+	private static Logger _logger = LoggerFactory.getLogger(
+		OpenAPIImporter.class);
+
+	private final String _apiDefinitionURL;
+	private Definition _definition;
+	private final List<NameValuePair> _headers = new ArrayList<>();
+	private final JSONWebServiceClient _jsonWebServiceClient;
+	private String _openAPIDefinitionJSON;
+	private final Properties _properties;
+
+}
