e28f2fb2a3a89f75de7094d467c0ed524caf8ca4
Alessio Rendina <asilcomando@gmail.com>
2019-01-09 20:20:12 +0100

    COMMERCE-778 Refactor commerce order implementation

--- commerce-service/src/main/java/com/liferay/commerce/model/impl/CommerceOrderImpl.java
+++ commerce-service/src/main/java/com/liferay/commerce/model/impl/CommerceOrderImpl.java
@@ -16,6 +16,9 @@ package com.liferay.commerce.model.impl;
 
 import aQute.bnd.annotation.ProviderType;
 
+import com.liferay.commerce.account.constants.CommerceAccountConstants;
+import com.liferay.commerce.account.model.CommerceAccount;
+import com.liferay.commerce.account.service.CommerceAccountLocalServiceUtil;
 import com.liferay.commerce.constants.CommerceOrderConstants;
 import com.liferay.commerce.currency.model.CommerceCurrency;
 import com.liferay.commerce.currency.model.CommerceMoney;
@@ -25,19 +28,11 @@ import com.liferay.commerce.discount.CommerceDiscountValue;
 import com.liferay.commerce.model.CommerceAddress;
 import com.liferay.commerce.model.CommerceOrderItem;
 import com.liferay.commerce.model.CommerceShippingMethod;
-import com.liferay.commerce.organization.service.CommerceOrganizationLocalServiceUtil;
 import com.liferay.commerce.service.CommerceAddressLocalServiceUtil;
 import com.liferay.commerce.service.CommerceOrderItemLocalServiceUtil;
 import com.liferay.commerce.service.CommerceShippingMethodLocalServiceUtil;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
-import com.liferay.portal.kernel.model.Group;
-import com.liferay.portal.kernel.model.Organization;
-import com.liferay.portal.kernel.model.RoleConstants;
-import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.service.GroupLocalServiceUtil;
-import com.liferay.portal.kernel.service.OrganizationLocalServiceUtil;
-import com.liferay.portal.kernel.service.UserLocalServiceUtil;
 
 import java.math.BigDecimal;
 
@@ -65,34 +60,6 @@ public class CommerceOrderImpl extends CommerceOrderBaseImpl {
 		}
 
 		return null;
-	}
-
-	@Override
-	public String getClassName() throws PortalException {
-		Group group = GroupLocalServiceUtil.getGroup(getGroupId());
-
-		if (group.isOrganization() &&
-			CommerceOrganizationLocalServiceUtil.isB2BOrganization(
-				group.getOrganizationId())) {
-
-			return Organization.class.getName();
-		}
-
-		return User.class.getName();
-	}
-
-	@Override
-	public long getClassPK() throws PortalException {
-		Group group = GroupLocalServiceUtil.getGroup(getGroupId());
-
-		if (group.isOrganization() &&
-			CommerceOrganizationLocalServiceUtil.isB2BOrganization(
-				group.getOrganizationId())) {
-
-			return group.getOrganizationId();
-		}
-
-		return getOrderUserId();
 	}
 
 	@Override
@@ -129,54 +96,9 @@ public class CommerceOrderImpl extends CommerceOrderBaseImpl {
 	}
 
 	@Override
-	public long getCustomerId() {
-		long customerId = getOrderOrganizationId();
-
-		if (customerId <= 0) {
-			customerId = getOrderUserId();
-		}
-
-		return customerId;
-	}
-
-	@Override
-	public String getCustomerName() throws PortalException {
-		Organization organization = getOrderOrganization();
-
-		if (organization != null) {
-			return organization.getName();
-		}
-
-		User orderUser = getOrderUser();
-
-		if (orderUser.isDefaultUser()) {
-			return RoleConstants.GUEST;
-		}
-
-		return orderUser.getFullName();
-	}
-
-	@Override
-	public Organization getOrderOrganization() throws PortalException {
-		long orderOrganizationId = getOrderOrganizationId();
-
-		if (orderOrganizationId <= 0) {
-			return null;
-		}
-
-		return OrganizationLocalServiceUtil.getOrganization(
-			orderOrganizationId);
-	}
-
-	@Override
-	public User getOrderUser() throws PortalException {
-		long orderUserId = getOrderUserId();
-
-		if (orderUserId <= 0) {
-			return null;
-		}
-
-		return UserLocalServiceUtil.getUser(orderUserId);
+	public CommerceAccount getOrderAccount() throws PortalException {
+		return CommerceAccountLocalServiceUtil.getCommerceAccount(
+			getCommerceAccountId());
 	}
 
 	@Override
@@ -211,9 +133,11 @@ public class CommerceOrderImpl extends CommerceOrderBaseImpl {
 
 	@Override
 	public boolean isB2B() throws PortalException {
-		String className = getClassName();
+		CommerceAccount commerceAccount = getOrderAccount();
 
-		if (className.equals(Organization.class.getName())) {
+		if (commerceAccount.getType() ==
+				CommerceAccountConstants.ACCOUNT_TYPE_BUSINESS) {
+
 			return true;
 		}
 
@@ -233,9 +157,11 @@ public class CommerceOrderImpl extends CommerceOrderBaseImpl {
 
 	@Override
 	public boolean isGuestOrder() throws PortalException {
-		User orderUser = getOrderUser();
+		CommerceAccount commerceAccount = getOrderAccount();
 
-		if ((orderUser != null) && orderUser.isDefaultUser()) {
+		if (commerceAccount.getType() ==
+				CommerceAccountConstants.ACCOUNT_TYPE_GUEST) {
+
 			return true;
 		}
 
