6c7fdd9a65861707d07ef568d8e744db90e67f7a
Ethan Bustad <ethan.bustad@liferay.com>
2019-01-10 11:31:54 -0800

    COMMERCE-704 Associate friendly url with Product rather than Defn

--- commerce-product-service/src/main/java/com/liferay/commerce/product/internal/upgrade/v1_3_0/CPFriendlyURLEntryUpgradeProcess.java
+++ commerce-product-service/src/main/java/com/liferay/commerce/product/internal/upgrade/v1_3_0/CPFriendlyURLEntryUpgradeProcess.java
@@ -14,16 +14,23 @@
 
 package com.liferay.commerce.product.internal.upgrade.v1_3_0;
 
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CProduct;
 import com.liferay.commerce.product.model.impl.CPFriendlyURLEntryModelImpl;
 import com.liferay.portal.kernel.dao.db.IndexMetadata;
+import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
 import com.liferay.portal.kernel.dao.jdbc.DataAccess;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 import com.liferay.portal.kernel.util.ObjectValuePair;
+import com.liferay.portal.kernel.util.StringBundler;
 
 import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 import java.util.List;
 import java.util.Objects;
@@ -33,9 +40,51 @@ import java.util.Objects;
  */
 public class CPFriendlyURLEntryUpgradeProcess extends UpgradeProcess {
 
+	public CPFriendlyURLEntryUpgradeProcess(
+		ClassNameLocalService classNameLocalService) {
+
+		_classNameLocalService = classNameLocalService;
+	}
+
 	@Override
 	protected void doUpgrade() throws Exception {
 		_addIndexes(CPFriendlyURLEntryModelImpl.TABLE_NAME);
+
+		long cpDefinitionClassNameId = _classNameLocalService.getClassNameId(
+			CPDefinition.class);
+		long cProductClassNameId = _classNameLocalService.getClassNameId(
+			CProduct.class);
+
+		String updateCPFriendlyURLSQL =
+			"update CPFriendlyURLEntry set classNameId = ?, classPK = ? " +
+				"where classNameId = ? and classPK = ?";
+
+		String selectCPFriendlyURLEntrySQL = StringBundler.concat(
+			"select distinct classPK, CProductId from CPFriendlyURLEntry ",
+			"inner join CPDefinition on CPDefinition.CPDefinitionId = ",
+			"CPFriendlyURLEntry.classPK where classNameId = ",
+			String.valueOf(cpDefinitionClassNameId));
+
+		try (PreparedStatement ps =
+				AutoBatchPreparedStatementUtil.concurrentAutoBatch(
+					connection, updateCPFriendlyURLSQL);
+			Statement s = connection.createStatement();
+			ResultSet rs = s.executeQuery(selectCPFriendlyURLEntrySQL)) {
+
+			while (rs.next()) {
+				long classPK = rs.getLong("classPK");
+				long cProductId = rs.getLong("CProductId");
+
+				ps.setLong(1, cProductClassNameId);
+				ps.setLong(2, cProductId);
+				ps.setLong(3, cpDefinitionClassNameId);
+				ps.setLong(4, classPK);
+
+				ps.executeUpdate();
+			}
+
+			ps.executeBatch();
+		}
 	}
 
 	private void _addIndexes(String tableName) throws Exception {
@@ -93,5 +142,7 @@ public class CPFriendlyURLEntryUpgradeProcess extends UpgradeProcess {
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		CPFriendlyURLEntryUpgradeProcess.class);
+
+	private ClassNameLocalService _classNameLocalService;
 
 }
