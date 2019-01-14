9c118c637dde13e23eb33942fcfee797b8235c8d
Marco Leo <marco.leo@liferay.com>
2019-01-13 20:54:30 -0800

    COMMERCE-778 Replace logic inside segments with accounts

--- commerce-user-segment-service/src/main/java/com/liferay/commerce/user/segment/service/impl/CommerceUserSegmentEntryLocalServiceImpl.java
+++ commerce-user-segment-service/src/main/java/com/liferay/commerce/user/segment/service/impl/CommerceUserSegmentEntryLocalServiceImpl.java
@@ -14,6 +14,8 @@
 
 package com.liferay.commerce.user.segment.service.impl;
 
+import com.liferay.commerce.account.model.CommerceAccount;
+import com.liferay.commerce.account.service.CommerceAccountLocalService;
 import com.liferay.commerce.user.segment.exception.CommerceUserSegmentEntryKeyException;
 import com.liferay.commerce.user.segment.exception.CommerceUserSegmentEntryNameException;
 import com.liferay.commerce.user.segment.exception.CommerceUserSegmentEntrySystemException;
@@ -24,6 +26,7 @@ import com.liferay.commerce.user.segment.service.base.CommerceUserSegmentEntryLo
 import com.liferay.portal.kernel.cache.MultiVMPoolUtil;
 import com.liferay.portal.kernel.cache.PortalCache;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.RoleConstants;
@@ -49,6 +52,7 @@ import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.spring.extender.service.ServiceReference;
 
 import java.io.Serializable;
 
@@ -219,10 +223,12 @@ public class CommerceUserSegmentEntryLocalServiceImpl
 
 	@Override
 	public long[] getCommerceUserSegmentEntryIds(
-			long groupId, long organizationId, long userId)
+			long groupId, long commerceAccountId, long userId)
 		throws PortalException {
 
-		String cacheKey = userId + "_" + organizationId;
+		Group group = groupLocalService.getGroup(groupId);
+
+		String cacheKey = String.valueOf(commerceAccountId);
 
 		PortalCache<String, Serializable> portalCache =
 			MultiVMPoolUtil.getPortalCache("USER_SEGMENTS_" + groupId);
@@ -236,10 +242,8 @@ public class CommerceUserSegmentEntryLocalServiceImpl
 			return commerceUserSegmentEntryIds;
 		}
 
-		User user = userLocalService.getUser(userId);
-
 		SearchContext searchContext = buildSearchContext(
-			user.getCompanyId(), groupId, organizationId, userId);
+			group.getCompanyId(), groupId, commerceAccountId, userId);
 
 		Indexer<CommerceUserSegmentEntry> indexer =
 			IndexerRegistryUtil.nullSafeGetIndexer(
@@ -350,15 +354,15 @@ public class CommerceUserSegmentEntryLocalServiceImpl
 	}
 
 	protected SearchContext buildSearchContext(
-		long companyId, long groupId, long organizationId, long userId) {
+		long companyId, long groupId, long commerceAccountId, long userId) {
 
 		SearchContext searchContext = new SearchContext();
 
 		Map<String, Serializable> attributes = new HashMap<>();
 
 		attributes.put("active", true);
+		attributes.put("commerceAccountId", commerceAccountId);
 		attributes.put("criterionType", true);
-		attributes.put("organizationId", organizationId);
 
 		searchContext.setAttributes(attributes);
 
@@ -511,5 +515,8 @@ public class CommerceUserSegmentEntryLocalServiceImpl
 
 	private static final String[] _SELECTED_FIELD_NAMES =
 		{Field.ENTRY_CLASS_PK, Field.COMPANY_ID, Field.GROUP_ID, Field.UID};
+
+	@ServiceReference(type = CommerceAccountLocalService.class)
+	private CommerceAccountLocalService _commerceAccountLocalService;
 
 }
