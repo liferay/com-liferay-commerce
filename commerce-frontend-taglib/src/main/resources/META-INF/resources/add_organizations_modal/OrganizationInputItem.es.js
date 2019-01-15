7164be97a43856f3a98962e3e2e5114a14212cf5
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-01-15 18:12:14 +0100

    COMMERCE-686 add organization modal

---
+++ commerce-frontend-taglib/src/main/resources/META-INF/resources/add_organizations_modal/OrganizationInputItem.es.js
@@ -0,0 +1,34 @@
+'use strict';
+
+import template from './OrganizationInputItem.soy';
+import Component from 'metal-component';
+import Soy, {Config} from 'metal-soy';
+
+class OrganizationInputItem extends Component {
+	_handleRemoveItem(e) {
+		e.preventDefault();
+
+		return this.emit(
+			'removeItem',
+			{
+				id: this.id
+			}
+		);
+	}
+};
+
+Soy.register(OrganizationInputItem, template);
+
+OrganizationInputItem.STATE = {
+	id: Config.oneOfType(
+		[
+			Config.number(),
+			Config.string()
+		]
+	).required(),
+	name: Config.string().required(),
+	spritemap: Config.string()
+};
+
+export {OrganizationInputItem};
+export default OrganizationInputItem;
