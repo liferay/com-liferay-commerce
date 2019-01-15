5cd76eb387d72125606c01b000b5858940e30259
Alessio Rendina <asilcomando@gmail.com>
2019-01-15 19:05:24 +0100

    COMMERCE-778 Fixed commerce frontend resources

--- commerce-frontend-impl/src/main/java/com/liferay/commerce/frontend/internal/account/CommerceAccountResource.java
+++ commerce-frontend-impl/src/main/java/com/liferay/commerce/frontend/internal/account/CommerceAccountResource.java
@@ -393,7 +393,7 @@ public class CommerceAccountResource {
 			"commerceOrderId", String.valueOf(commerceOrderId));
 
 		String redirect = _portal.getCurrentURL(httpServletRequest);
-
+		
 		editURL.setParameter("redirect", redirect);
 
 		return editURL.toString();
