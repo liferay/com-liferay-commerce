ee80f49a9647f1cfaa519077c2ee8ec8b50b58c2
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-01-15 18:12:14 +0100

    COMMERCE-686 add organization modal

---
+++ commerce-frontend-taglib/src/main/resources/META-INF/resources/add_organizations_modal/OrganizationListItem.es.js
@@ -0,0 +1,63 @@
+'use strict';
+
+import template from './OrganizationListItem.soy';
+import Component from 'metal-component';
+import Soy, {Config} from 'metal-soy';
+
+import '../autocomplete_item/AutocompleteItem.es';
+
+class OrganizationListItem extends Component {
+
+	syncSelectedOrganizations() {
+		return this._selected = this.selectedOrganizations.reduce(
+			(hasItemBeenSelected, item) => 
+				hasItemBeenSelected || item.id === this.id, false
+		);
+	}
+
+	_handleToggleItem(e) {
+		e.preventDefault();
+
+		return this.emit(
+			'toggleItem',
+			{
+				id: this.id,
+				name: this.name
+			}
+		);
+	}
+
+};
+
+Soy.register(OrganizationListItem, template);
+
+OrganizationListItem.STATE = {
+	id: Config.oneOfType(
+		[
+			Config.string(),
+			Config.number()
+		]
+	),
+	name: Config.string().required(),
+	selectedOrganizations: Config.array(
+		Config.shapeOf(
+			{
+				id: Config.oneOfType(
+					[
+						Config.string(),
+						Config.number()
+					]
+				),
+				name: Config.string(),
+			}
+		)
+	).value(
+		[]
+	),
+	colorId: Config.number(),
+	query: Config.string(),
+	_selected: Config.bool().value(false),
+};
+
+export {OrganizationListItem};
+export default OrganizationListItem;
