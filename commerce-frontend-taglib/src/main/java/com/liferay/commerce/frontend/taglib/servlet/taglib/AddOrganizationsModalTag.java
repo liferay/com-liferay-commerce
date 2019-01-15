b67cc48d1c47741185f5397920edb10d50451c2f
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-01-15 18:12:14 +0100

    COMMERCE-686 add organization modal

---
+++ commerce-frontend-taglib/src/main/java/com/liferay/commerce/frontend/taglib/servlet/taglib/AddOrganizationsModalTag.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.frontend.taglib.servlet.taglib;
+
+import com.liferay.commerce.frontend.taglib.internal.js.loader.modules.extender.npm.NPMResolverProvider;
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
+import com.liferay.frontend.taglib.soy.servlet.taglib.ComponentRendererTag;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+/**
+ * @author Fabio Mastrorilli
+ */
+public class AddOrganizationsModalTag extends ComponentRendererTag {
+
+	@Override
+	public int doStartTag() {
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		putValue(
+			"organizationsAPI",
+			PortalUtil.getPortalURL(request) + "/o/commerce-ui/search-organizations");
+
+		putValue("query", "");
+
+		putValue(
+			"spritemap",
+			themeDisplay.getPathThemeImages() + "/commerce-icons.svg");
+
+		setTemplateNamespace("AddOrganizationsModal.render");
+
+		return super.doStartTag();
+	}
+
+	@Override
+	public String getModule() {
+		NPMResolver npmResolver = NPMResolverProvider.getNPMResolver();
+
+		if (npmResolver == null) {
+			return StringPool.BLANK;
+		}
+
+		return npmResolver.resolveModuleName(
+			"commerce-frontend-taglib/add_organizations_modal" +
+				"/AddOrganizationsModal.es");
+	}
+
+}
---
+++ commerce-frontend-taglib/src/main/resources/META-INF/resources/add_organizations_modal/AddOrganizationsModal.es.js
@@ -0,0 +1,188 @@
+'use strict';
+
+import {debounce} from 'metal-debounce';
+
+import template from './AddOrganizationsModal.soy';
+import Component from 'metal-component';
+import Soy, {Config} from 'metal-soy';
+
+import 'clay-modal';
+
+import './OrganizationInputItem.es';
+import './OrganizationListItem.es';
+
+const fakeData = [
+	{
+		id: 4,
+		name: 'Lorem Ipsum'
+	},
+	{
+		id: 5,
+		name: 'Dolor Sit'
+	},
+	{
+		id: 6,
+		name: 'Donec nunc elit'
+	},
+	{
+		id: 7,
+		name: 'Vestibulum ante ipsum'
+	},
+	{
+		id: 8,
+		name: 'Sed facilisis libero'
+	},
+	{
+		id: 9,
+		name: 'Phasellus finibut massa'
+	}
+];
+
+class AddOrganizationModal extends Component {
+
+	created() {
+		this._debouncedFetchOrganizations = debounce(this._fetchOrganizations.bind(this), 300);
+	}
+
+	attached() {
+		this._fetchOrganizations();
+	}
+
+	syncAddedOrganizations() {
+		const contentWrapper = this.element.querySelector('.autocomplete-input__content');
+		this.element.querySelector('.autocomplete-input__box').focus();
+		contentWrapper.scrollTo(0, contentWrapper.offsetHeight);
+		return true;
+	}
+
+	_handleCloseModal(e) {
+		e.preventDefault();
+		this._isVisible = false;
+	}
+
+	syncQuery() {
+		this._isLoading = true;
+		return this._debouncedFetchOrganizations();
+	}
+
+	_handleFormSubmit(evt) {
+		evt.preventDefault();
+
+		if (this.organizations.length) {
+			this._toggleItem(this.organizations[0])
+			this.query = '';
+			return true;
+		}
+		return false;
+	}
+
+	_handleInputBox(evt) {
+		if (evt.keyCode === 8 && !this.query.length) {
+			this.selectedOrganizations = this.selectedOrganizations.slice(0, -1);
+			return false;
+		}
+		return this.query = evt.target.value;
+	}
+
+	_toggleItem(organizationToBeToggled) {
+		if (!organizationToBeToggled.id) {
+			this.query = '';
+		}
+
+		const hasOrganizationAlreadyBeenAdded = this.selectedOrganizations.reduce(
+			(alreadyAdded, organization) => alreadyAdded || organization.id === organizationToBeToggled.id,
+			false
+		);
+
+		this.selectedOrganizations =
+			hasOrganizationAlreadyBeenAdded ?
+				this.selectedOrganizations.filter((organization) => organization.id !== organizationToBeToggled.id) :
+				[...this.selectedOrganizations, organizationToBeToggled];
+
+		return this.selectedOrganizations;
+	}
+
+	_fetchOrganizations() {
+		return fetch(
+			this.organizationsAPI + '?q=' + this.query,
+			{
+				method: 'GET'
+			}
+		)
+			.then(
+				response => response.json()
+			)
+			.then(
+				response => {
+					this._isLoading = false;
+
+					const fakeResult = fakeData.filter( org => org.name.toLowerCase().indexOf(this.query.toLowerCase()) > -1)
+					
+					return this.organizations = this.addColorToOrganizations(fakeResult);
+					return this.organizations = this.addColorToOrganizations(response.organizations);
+				}
+			);
+	}
+
+	addColorToOrganizations(organizations) {
+		return organizations.map( organization => {
+			return Object.assign(
+				{
+					colorId: Math.floor(Math.random() * 6) + 1
+				},
+				organization,
+			)
+		})
+	}
+
+	_addOrganizations() {
+		if (!this.selectedOrganizations.length) {
+			return false;
+		};
+
+		return this.emit(
+			'addOrganization',
+			this.selectedOrganizations
+		);
+	}
+
+	toggle() {
+		return this._isVisible = !this._isVisible;
+	}
+
+	open() {
+		return this._isVisible = true;
+	}
+
+	close() {
+		return this._isVisible = false;
+	}
+};
+
+Soy.register(AddOrganizationModal, template);
+
+const ORGANIZATION_SCHEMA = Config.shapeOf(
+	{
+		id: Config.oneOfType(
+			[
+				Config.number(),
+				Config.string()
+			]
+		).required(),
+		name: Config.string().required(),
+		colorId: Config.number()
+	}
+);
+
+AddOrganizationModal.STATE = {
+	organizationsAPI: Config.string().value(''),
+	query: Config.string().value(''),
+	spritemap: Config.string(),
+	organizations: Config.array(ORGANIZATION_SCHEMA).value([]),
+	selectedOrganizations: Config.array(ORGANIZATION_SCHEMA).value([]),
+	_isVisible: Config.bool().internal().value(false),
+	_isLoading: Config.bool().internal().value(false)
+};
+
+export {AddOrganizationModal};
+export default AddOrganizationModal;
