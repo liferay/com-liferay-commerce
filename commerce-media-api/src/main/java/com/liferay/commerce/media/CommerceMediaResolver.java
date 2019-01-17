120c12594c542480b9b4958a197774580996585b
Alec Sloan <Alec.sloan@Liferay.com>
2019-01-16 22:48:42 -0800

    COMMERCE-553 Add new commerce media api implementation

---
+++ commerce-media-api/src/main/java/com/liferay/commerce/media/CommerceMediaResolver.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.media;
+
+import aQute.bnd.annotation.ProviderType;
+
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author Alec Sloan
+ */
+@ProviderType
+public interface CommerceMediaResolver {
+
+	public String getDownloadUrl(
+			long cpAttachmentFileEntryId, long cpDefinitionId)
+		throws PortalException;
+
+	public byte[] getMediaBytes(HttpServletRequest httpServletRequest)
+		throws IOException, PortalException;
+
+	public String getThumbnailUrl(
+			long cpAttachmentFileEntryId, long cpDefinitionId)
+		throws PortalException;
+
+	public String getUrl(long cpAttachmentFileEntryId, long cpDefinitionId)
+		throws PortalException;
+
+	public String getUrl(
+			long cpAttachmentFileEntryId, long cpDefinitionId, boolean download,
+			boolean thumbnail)
+		throws PortalException;
+
+	public void sendMediaBytes(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException;
+
+	public void sendMediaBytes(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, String download)
+		throws IOException;
+
+}
