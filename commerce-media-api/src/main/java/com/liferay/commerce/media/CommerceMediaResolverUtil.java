ae136a977d417236f881319d1c3aabde45090c56
Alec Sloan <Alec.sloan@Liferay.com>
2019-01-16 22:48:42 -0800

    COMMERCE-553 Add new commerce media api implementation

---
+++ commerce-media-api/src/main/java/com/liferay/commerce/media/CommerceMediaResolverUtil.java
@@ -0,0 +1,103 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.media;
+
+import com.liferay.commerce.media.CommerceMediaResolver;
+import com.liferay.osgi.util.ServiceTrackerFactory;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.util.tracker.ServiceTracker;
+
+/**
+ * @author Alec Sloan
+ */
+public class CommerceMediaResolverUtil {
+
+	public static String getDownloadUrl(
+			long cpAttachmentFileEntryId, long cpDefinitionId)
+		throws PortalException {
+
+		CommerceMediaResolver commerceMediaResolver =
+			_serviceTracker.getService();
+
+		return commerceMediaResolver.getDownloadUrl(
+			cpAttachmentFileEntryId, cpDefinitionId);
+	}
+
+	public static byte[] getMediaBytes(HttpServletRequest httpServletRequest)
+		throws IOException, PortalException {
+
+		CommerceMediaResolver commerceMediaResolver =
+			_serviceTracker.getService();
+
+		return commerceMediaResolver.getMediaBytes(httpServletRequest);
+	}
+
+	public static String getThumbnailUrl(
+			long cpAttachmentFileEntryId, long cpDefinitionId)
+		throws PortalException {
+
+		CommerceMediaResolver commerceMediaResolver =
+			_serviceTracker.getService();
+
+		return commerceMediaResolver.getThumbnailUrl(
+			cpAttachmentFileEntryId, cpDefinitionId);
+	}
+
+	public static String getUrl(
+			long cpAttachmentFileEntryId, long cpDefinitionId)
+		throws PortalException {
+
+		CommerceMediaResolver commerceMediaResolver =
+			_serviceTracker.getService();
+
+		return commerceMediaResolver.getUrl(
+			cpAttachmentFileEntryId, cpDefinitionId);
+	}
+
+	public static void sendMediaBytes(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException, PortalException {
+
+		CommerceMediaResolver commerceMediaResolver =
+			_serviceTracker.getService();
+
+		commerceMediaResolver.sendMediaBytes(
+			httpServletRequest, httpServletResponse);
+	}
+
+	public static void sendMediaBytes(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, String download)
+		throws IOException, PortalException {
+
+		CommerceMediaResolver commerceMediaResolver =
+			_serviceTracker.getService();
+
+		commerceMediaResolver.sendMediaBytes(
+			httpServletRequest, httpServletResponse, download);
+	}
+
+	private static final ServiceTracker<?, CommerceMediaResolver>
+		_serviceTracker = ServiceTrackerFactory.open(
+			CommerceMediaResolver.class);
+
+}
