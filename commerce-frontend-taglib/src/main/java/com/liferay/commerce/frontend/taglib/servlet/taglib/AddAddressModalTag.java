56c602ab48d4f8f6ad84daf8c56f6b9380d30143
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-01-21 18:19:27 +0100

    COMMERCE-686 added add new address modal

---
+++ commerce-frontend-taglib/src/main/java/com/liferay/commerce/frontend/taglib/servlet/taglib/AddAddressModalTag.java
@@ -0,0 +1,64 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.frontend.taglib.servlet.taglib;
+
+import com.liferay.commerce.frontend.taglib.internal.js.loader.modules.extender.npm.NPMResolverProvider;
+import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
+import com.liferay.frontend.taglib.soy.servlet.taglib.ComponentRendererTag;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.PortalUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+/**
+ * @author Fabio Mastrorilli
+ */
+public class AddAddressModalTag extends ComponentRendererTag {
+
+	@Override
+	public int doStartTag() {
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		putValue(
+			"countriesAPI",
+			PortalUtil.getPortalURL(request) + "/api/jsonws/commerce.commercecountry/get-billing-commerce-countries");
+
+		putValue(
+			"regionsAPI",
+			PortalUtil.getPortalURL(request) + "/api/jsonws/commerce.commerceregion/get-commerce-regions");
+
+		putValue(
+			"spritemap",
+			themeDisplay.getPathThemeImages() + "/commerce-icons.svg");
+
+		setTemplateNamespace("AddAddressModal.render");
+
+		return super.doStartTag();
+	}
+
+	@Override
+	public String getModule() {
+		NPMResolver npmResolver = NPMResolverProvider.getNPMResolver();
+
+		if (npmResolver == null) {
+			return StringPool.BLANK;
+		}
+
+		return npmResolver.resolveModuleName(
+			"commerce-frontend-taglib/add_address_modal/AddAddressModal.es");
+	}
+
+}
