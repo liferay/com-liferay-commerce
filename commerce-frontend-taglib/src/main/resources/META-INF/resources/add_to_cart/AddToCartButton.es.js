47f856ca54f6499365147cc113cc2d0e405aba38
Marco Leo <marco.leo@liferay.com>
2019-01-21 00:05:37 -0800

    COMMERCE-686 Make Add to cart working

--- commerce-frontend-taglib/src/main/resources/META-INF/resources/add_to_cart/AddToCartButton.es.js
+++ commerce-frontend-taglib/src/main/resources/META-INF/resources/add_to_cart/AddToCartButton.es.js
@@ -5,6 +5,13 @@ import Soy, {Config} from 'metal-soy';
 import '../quantity_selector/QuantitySelector.es';
 
 class AddToCartButton extends Component {
+
+	created() {
+		if (!this.accountId) {
+			this.disabled = true;
+		}
+	}
+
 	updateQuantity(quantity) {
 		this.quantity = quantity;
 	}
@@ -15,15 +22,61 @@ class AddToCartButton extends Component {
 	}
 
 	handleBtnClick(e) {
-		if (!this.editMode && this.element === e.target) {
+		if (!this.editMode && this.element === e.target && !this.disabled) {
 			this.editMode = true;
 		}
 	}
 
 	handleSubmitClick() {
-		this.editMode = false;
-		this.emit('submitQuantity', this.productId, this.quantity);
+		const formData = new FormData();
+
+		formData.append('accountId', this.accountId);
+		formData.append('groupId', themeDisplay.getScopeGroupId());
+		formData.append('productId', this.productId);
+		formData.append('quantity', this.quantity);
+		formData.append('options', '[]');
+
+		if (this.orderId) {
+			formData.append('orderId', this.orderId);
+		}
+
+		fetch(
+			this.cartAPI,
+			{
+				body: formData,
+				method: 'POST'
+			}
+		)
+			.then(response => response.json())
+			.then(
+				(jsonresponse) => {
+					if (jsonresponse.success) {
+						Liferay.fire('updateCart', jsonresponse);
+
+						this.editMode = false;
+						this.emit('submitQuantity', this.productId, this.quantity);
+					}
+					else if (jsonresponse.errorMessages) {
+						this._showNotification(jsonresponse.errorMessages[0], 'danger');
+					}
+					else {
+						var validatorErrors = jsonresponse.validatorErrors;
+
+						if (validatorErrors) {
+							validatorErrors.forEach(
+								function(validatorError) {
+									this._showNotification(validatorError.message, 'danger');
+								}
+							);
+						}
+						else {
+							this._showNotification(jsonresponse.error, 'danger');
+						}
+					}
+				}
+			);
 	}
+
 
 	rendered() {
 		if (this.element.querySelector('input')) {
@@ -33,11 +86,47 @@ class AddToCartButton extends Component {
 			this.element.querySelector('select').focus();
 		}
 	}
+
+	_showNotification(message, type) {
+		AUI().use(
+			'liferay-notification',
+			() => {
+				new Liferay.Notification(
+					{
+						closeable: true,
+						delay: {
+							hide: 5000,
+							show: 0
+						},
+						duration: 500,
+						message: message,
+						render: true,
+						title: '',
+						type: type
+					}
+				);
+			}
+		);
+	}
 }
 
 Soy.register(AddToCartButton, template);
 
 AddToCartButton.STATE = {
+	accountId: Config.oneOfType(
+		[
+			Config.string(),
+			Config.number()
+		]
+	),
+	orderId: Config.oneOfType(
+		[
+			Config.string(),
+			Config.number()
+		]
+	),
+	cartAPI: Config.string().required(),
+	disabled: Config.bool().value(false),
 	editMode: Config.bool().value(false),
 	productId: Config.oneOfType(
 		[
