1c066df402d223ec113bde9a6f50395decdcf9d6
Marco Leo <marco.leo@liferay.com>
2019-01-21 16:56:12 -0800

    COMMERCE-686 Add new price tag

---
+++ commerce-frontend-taglib/src/main/java/com/liferay/commerce/frontend/taglib/internal/model/PriceModel.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.frontend.taglib.internal.model;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class PriceModel {
+
+	public PriceModel(String price) {
+		_price = price;
+	}
+
+	public String getDiscount() {
+		return _discount;
+	}
+
+	public String getPrice() {
+		return _price;
+	}
+
+	public String getPromoPrice() {
+		return _promoPrice;
+	}
+
+	public void setDiscount(String discount) {
+		_discount = discount;
+	}
+
+	public void setPrice(String price) {
+		_price = price;
+	}
+
+	public void setPromoPrice(String promoPrice) {
+		_promoPrice = promoPrice;
+	}
+
+	private String _discount;
+	private String _price;
+	private String _promoPrice;
+
+}
