fabeea242c042a5e78614c7cbe610963e82c0273
Ivica Čardić <ivica.cardic@liferay.com>
2019-01-22 14:14:41 -0800

    COMMERCE-798 commerce-openapi-admin - Define exception mappers

---
+++ commerce-openapi-admin/src/main/java/com/liferay/commerce/openapi/admin/internal/exception/NoSuchCPOptionExceptionMapper.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.openapi.admin.internal.exception;
+
+import com.liferay.commerce.openapi.core.exception.BaseExceptionMapper;
+import com.liferay.commerce.openapi.core.exception.RESTError;
+import com.liferay.commerce.product.exception.NoSuchCPOptionException;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
+
+/**
+ * @author Ivica Cardic
+ */
+@Component(
+	property = JaxrsWhiteboardConstants.JAX_RS_EXTENSION + "=true",
+	service = ExceptionMapper.class
+)
+@Provider
+public class NoSuchCPOptionExceptionMapper
+	extends BaseExceptionMapper<NoSuchCPOptionException> {
+
+	@Override
+	public int getErrorCode() {
+		return RESTError.NO_SUCH_PRODUCT_OPTION.getErrorCode();
+	}
+
+	@Override
+	public String getErrorDescription() {
+		return RESTError.NO_SUCH_PRODUCT_OPTION.getErrorDescription();
+	}
+
+	@Override
+	public Response.Status getStatus() {
+		return RESTError.NO_SUCH_PRODUCT_OPTION.getStatus();
+	}
+
+}
--- commerce-openapi-core-api/src/main/java/com/liferay/commerce/openapi/core/exception/RESTError.java
+++ commerce-openapi-core-api/src/main/java/com/liferay/commerce/openapi/core/exception/RESTError.java
@@ -24,6 +24,8 @@ public enum RESTError {
 
 	DUPLICATE_PRICE_ENTRY(
 		988, "Duplicated price entry", Response.Status.CONFLICT),
+	DUPLICATE_PRODUCT_OPTION_KEY(
+		993, "Duplicate product option key.", Response.Status.CONFLICT),
 	GENERAL_ERROR(999, "General error.", Response.Status.BAD_REQUEST),
 	INTERNAL_ERROR(
 		998, "Internal error. Please try again later.",
@@ -32,6 +34,8 @@ public enum RESTError {
 		987, "Unable to find product instance", Response.Status.NOT_FOUND),
 	NO_SUCH_PRICE_ENTRY(
 		989, "Unable to find price entry", Response.Status.NOT_FOUND),
+	NO_SUCH_PRODUCT_OPTION(
+		992, "Unable to find product option.", Response.Status.CONFLICT),
 	SEARCH_ERROR(
 		986, "Unable to fix the search index after 10 attempts",
 		Response.Status.INTERNAL_SERVER_ERROR);
