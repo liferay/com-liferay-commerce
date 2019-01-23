e7de81cdf38d8e7390811a96890f080302ddf867
Igor Beslic <igor.beslic@liferay.com>
2019-01-23 06:53:43 -0800

    COMMERCE-853 commerce-openapi-util - TEST - update resource generator test to test method signature syntax if extensions present

--- commerce-openapi-util/src/test/java/com/liferay/commerce/openapi/util/generator/ResourceGeneratorTest.java
+++ commerce-openapi-util/src/test/java/com/liferay/commerce/openapi/util/generator/ResourceGeneratorTest.java
@@ -16,6 +16,7 @@ package com.liferay.commerce.openapi.util.generator;
 
 import com.liferay.commerce.openapi.util.ComponentDefinition;
 import com.liferay.commerce.openapi.util.Content;
+import com.liferay.commerce.openapi.util.Extension;
 import com.liferay.commerce.openapi.util.Method;
 import com.liferay.commerce.openapi.util.Response;
 import com.liferay.commerce.openapi.util.Schema;
@@ -35,18 +36,68 @@ import org.junit.Test;
 public class ResourceGeneratorTest {
 
 	@Test
+	public void testGetReturnType() {
+		ResourceGenerator resourceGenerator = new ResourceGenerator(
+			"test", "test", "test", "test", "test", false, "test", "test");
+
+		Method getMethod = _getMethod(true);
+
+		Assert.assertEquals(
+			"Return type is collection DTO", "CollectionDTO<TestModel1DTO> ",
+			resourceGenerator.getReturnType(
+				getMethod, _getRandomComponentDefinitions(4, "TestModel")));
+
+		getMethod = _getMethod(false);
+
+		Assert.assertEquals(
+			"Return type is entity DTO", "TestModel1DTO ",
+			resourceGenerator.getReturnType(
+				getMethod, _getRandomComponentDefinitions(4, "TestModel")));
+	}
+
+	@Test
+	public void testToJavaxImports() {
+		ResourceGenerator resourceGenerator = new ResourceGenerator(
+			"test", "test", "test", "test", "test", false, "test", "test");
+
+		String imports = resourceGenerator.toJavaxImports(
+			_getMethods(true, "testModel", Collections.emptyList()),
+			_getRandomComponentDefinitions(4, "TestModel"));
+
+		Assert.assertTrue(
+			"Exactly one import statement expected",
+			_containsOnlyOne(imports, "import javax.ws.rs.GET;"));
+
+		imports = resourceGenerator.toJavaxImports(
+			_getMethods(true, "testModel", _getExtensions("locale", "company")),
+			_getRandomComponentDefinitions(4, "TestModel"));
+
+		Assert.assertTrue(
+			"Exactly one import statement expected",
+			_containsOnlyOne(imports, "import java.util.Locale;"));
+	}
+
+	@Test
 	public void testToResourceImplementationMethods() {
 		ResourceGenerator resourceGenerator = new ResourceGenerator(
 			"test", "test", "test", "test", "test", false, "test", "test");
 
 		String interfaceMethods =
 			resourceGenerator.toResourceImplementationMethods(
-				_getMethods("testModel"),
+				_getMethods(true, "testModel", Collections.emptyList()),
 				_getRandomComponentDefinitions(4, "TestModel"));
 
 		Assert.assertTrue(
 			"Pagination context expected",
 			interfaceMethods.contains("Pagination pagination"));
+
+		interfaceMethods = resourceGenerator.toResourceImplementationMethods(
+			_getMethods(true, "testModel", _getExtensions("locale")),
+			_getRandomComponentDefinitions(4, "TestModel"));
+
+		Assert.assertTrue(
+			"Context parameters with proper syntax expected",
+			interfaceMethods.contains("Locale locale, Pagination pagination"));
 	}
 
 	@Test
@@ -55,15 +106,75 @@ public class ResourceGeneratorTest {
 			"test", "test", "test", "test", "test", false, "test", "test");
 
 		String interfaceMethods = resourceGenerator.toResourceInterfaceMethods(
-			_getMethods("testModel"),
+			_getMethods(true, "testModel", Collections.emptyList()),
 			_getRandomComponentDefinitions(4, "TestModel"));
 
 		Assert.assertTrue(
 			"Pagination context expected",
 			interfaceMethods.contains("@Context Pagination pagination"));
+
+		interfaceMethods = resourceGenerator.toResourceInterfaceMethods(
+			_getMethods(true, "testModel", _getExtensions("locale")),
+			_getRandomComponentDefinitions(4, "TestModel"));
+
+		Assert.assertTrue(
+			"Context parameters with proper syntax expected",
+			interfaceMethods.contains(
+				"Locale locale, @Context Pagination pagination"));
 	}
 
-	private List<Method> _getMethods(String model) {
+	private boolean _containsOnlyOne(String source, String term) {
+		int count = 0;
+		int indexOf = -1;
+
+		do {
+			indexOf = source.indexOf(term, indexOf + 1);
+
+			if (indexOf >= 0) {
+				count++;
+			}
+		} while (indexOf != -1);
+
+		if (count == 1) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private List<Extension> _getExtensions(String... extensionOpenApiNames) {
+		List<Extension> extensions = new ArrayList<>();
+
+		if (extensionOpenApiNames == null) {
+			return extensions;
+		}
+
+		for (String extensionOpenApiName : extensionOpenApiNames) {
+			extensions.add(
+				new Extension(extensionOpenApiName, Collections.emptyList()));
+		}
+
+		return extensions;
+	}
+
+	private Method _getMethod(boolean responseContentArray) {
+		for (Method method :
+				_getMethods(
+					responseContentArray, "testModel",
+					Collections.emptyList())) {
+
+			if ("GET".equals(method.getHttpMethod())) {
+				return method;
+			}
+		}
+
+		return null;
+	}
+
+	private List<Method> _getMethods(
+		boolean responseContentArray, String model,
+		List<Extension> extensions) {
+
 		List<Method> methods = new ArrayList<>();
 
 		String absolutePath = String.format("/%s/{id}", model);
@@ -72,18 +183,20 @@ public class ResourceGeneratorTest {
 			new Method(
 				"update", _getRequestContents(), "PUT", absolutePath,
 				Collections.emptyList(),
-				_getResponses(201, 202, 400, 401, 404, 500),
-				Collections.emptyList()));
+				_getResponses(
+					responseContentArray, 201, 202, 400, 401, 404, 500),
+				extensions));
 		methods.add(
 			new Method(
 				"delete", Collections.emptyList(), "DELETE", absolutePath,
-				Collections.emptyList(), _getResponses(204, 401),
-				Collections.emptyList()));
+				Collections.emptyList(),
+				_getResponses(responseContentArray, 204, 401), extensions));
 		methods.add(
 			new Method(
 				"get", _getRequestContents(), "GET", absolutePath,
-				Collections.emptyList(), _getResponses(200, 400, 404, 500),
-				Collections.emptyList()));
+				Collections.emptyList(),
+				_getResponses(responseContentArray, 200, 400, 404, 500),
+				extensions));
 
 		return methods;
 	}
@@ -117,13 +230,21 @@ public class ResourceGeneratorTest {
 		return contents;
 	}
 
-	private List<Content> _getResponseContents() {
+	private List<Content> _getResponseContents(boolean responseContentArray) {
 		List<Content> contents = new ArrayList<>();
+
+		String responseContentSchemaType = "object";
+
+		if (responseContentArray) {
+			responseContentSchemaType = "array";
+		}
 
 		contents.add(
 			new Content(
 				"application/json",
-				new Schema("array", null, "#/components/schemas/TestModel1")));
+				new Schema(
+					responseContentSchemaType, null,
+					"#/components/schemas/TestModel1")));
 		contents.add(
 			new Content(
 				"application/json", new Schema("integer", "int32", null)));
@@ -135,12 +256,16 @@ public class ResourceGeneratorTest {
 		return contents;
 	}
 
-	private List<Response> _getResponses(Integer... statuses) {
+	private List<Response> _getResponses(
+		boolean responseContentArray, Integer... statuses) {
+
 		List<Response> responses = new ArrayList<>();
 
 		for (int status : statuses) {
 			if (status == 200) {
-				responses.add(new Response(status, _getResponseContents()));
+				responses.add(
+					new Response(
+						status, _getResponseContents(responseContentArray)));
 
 				continue;
 			}
