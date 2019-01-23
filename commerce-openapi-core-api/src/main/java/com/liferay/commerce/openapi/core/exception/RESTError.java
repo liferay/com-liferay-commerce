8c463e1d9b19c3e72c65b1979a24b983197b9dfb
Matija Petanjek <matija.petanjek@liferay.com>
2019-01-22 18:32:07 -0800

    COMMERCE-806 commerce-openapi - add exception mapper

--- commerce-openapi-core-api/src/main/java/com/liferay/commerce/openapi/core/exception/RESTError.java
+++ commerce-openapi-core-api/src/main/java/com/liferay/commerce/openapi/core/exception/RESTError.java
@@ -22,9 +22,18 @@ import javax.ws.rs.core.Response;
  */
 public enum RESTError {
 
+	DUPLICATE_PRICE_ENTRY(
+		988, "Duplicated price entry", Response.Status.CONFLICT),
 	GENERAL_ERROR(999, "General error.", Response.Status.BAD_REQUEST),
 	INTERNAL_ERROR(
 		998, "Internal error. Please try again later.",
+		Response.Status.INTERNAL_SERVER_ERROR),
+	NO_SUCH_CP_INSTANCE(
+		987, "Unable to find product instance", Response.Status.NOT_FOUND),
+	NO_SUCH_PRICE_ENTRY(
+		989, "Unable to find price entry", Response.Status.NOT_FOUND),
+	SEARCH_ERROR(
+		986, "Unable to fix the search index after 10 attempts",
 		Response.Status.INTERNAL_SERVER_ERROR);
 
 	public int getErrorCode() {
