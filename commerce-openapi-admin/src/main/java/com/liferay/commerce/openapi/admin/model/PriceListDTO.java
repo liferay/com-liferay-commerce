0da6f69bd0ca5a6aa5cca697981294f64460ecba
Ivica Čardić <ivica.cardic@liferay.com>
2019-01-30 16:12:12 -0800

    COMMERCE-876 commerce-openapi-admin - Regenerate

--- commerce-openapi-admin/src/main/java/com/liferay/commerce/openapi/admin/model/PriceListDTO.java
+++ commerce-openapi-admin/src/main/java/com/liferay/commerce/openapi/admin/model/PriceListDTO.java
@@ -14,17 +14,17 @@
 
 package com.liferay.commerce.openapi.admin.model;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
+
 import java.util.Date;
 
 import javax.annotation.Generated;
-
-import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * @author Igor Beslic
  */
 @Generated(value = "OSGiRESTModuleGenerator")
-@XmlRootElement(name = "PriceList")
+@JacksonXmlRootElement(localName = "PriceList")
 public class PriceListDTO {
 
 	public Long getCommercePriceListId() {
