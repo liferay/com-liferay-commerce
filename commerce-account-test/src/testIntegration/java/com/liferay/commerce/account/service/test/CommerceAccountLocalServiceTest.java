a493cc69c87d4b5ee1d1775b367a3e121ed79610
Alessio Rendina <asilcomando@gmail.com>
2019-02-03 00:51:58 +0100

    temp

--- commerce-account-test/src/testIntegration/java/com/liferay/commerce/account/service/test/CommerceAccountLocalServiceTest.java
+++ commerce-account-test/src/testIntegration/java/com/liferay/commerce/account/service/test/CommerceAccountLocalServiceTest.java
@@ -18,17 +18,24 @@ import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.commerce.account.constants.CommerceAccountConstants;
 import com.liferay.commerce.account.model.CommerceAccount;
 import com.liferay.commerce.account.service.CommerceAccountLocalService;
+import com.liferay.commerce.account.test.util.CommerceAccountTestUtil;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
+import com.liferay.portal.kernel.model.Organization;
+import com.liferay.portal.kernel.model.OrganizationConstants;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.OrganizationLocalService;
+import com.liferay.portal.kernel.service.OrganizationLocalServiceUtil;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.test.rule.AggregateTestRule;
 import com.liferay.portal.kernel.test.rule.DeleteAfterTestRun;
 import com.liferay.portal.kernel.test.util.CompanyTestUtil;
 import com.liferay.portal.kernel.test.util.GroupTestUtil;
+import com.liferay.portal.kernel.test.util.OrganizationTestUtil;
 import com.liferay.portal.kernel.test.util.ServiceContextTestUtil;
 import com.liferay.portal.kernel.test.util.UserTestUtil;
 import com.liferay.portal.kernel.workflow.WorkflowConstants;
@@ -80,21 +87,10 @@ public class CommerceAccountLocalServiceTest {
 			"That Commerce Account matches the one created before."
 		);
 
-		String name = "Business Account";
-		String email = "example@email.com";
-
-		Group group = GroupTestUtil.addGroup(
-			_company.getCompanyId(), _user.getUserId(),
-			GroupConstants.DEFAULT_PARENT_GROUP_ID);
-
-		ServiceContext serviceContext =
-			ServiceContextTestUtil.getServiceContext(group.getGroupId());
-
 		CommerceAccount businessCommerceAccount =
-			_commerceAccountLocalService.addBusinessCommerceAccount(
-				name, CommerceAccountConstants.DEFAULT_PARENT_ACCOUNT_ID, email,
-				StringPool.BLANK, true, StringPool.BLANK,
-				new long[] {_user.getUserId()}, null, serviceContext);
+			CommerceAccountTestUtil.addBusinessCommerceAccount(
+				_user.getUserId(), "Business Account", "example@email.com",
+				_getServiceContext());
 
 		List<CommerceAccount> commerceAccounts =
 			_commerceAccountLocalService.getUserCommerceAccounts(
@@ -117,6 +113,14 @@ public class CommerceAccountLocalServiceTest {
 			WorkflowConstants.STATUS_APPROVED, commerceAccount.getStatus());
 	}
 
+	private ServiceContext _getServiceContext() throws Exception {
+		Group group = GroupTestUtil.addGroup(
+			_user.getCompanyId(), _user.getUserId(),
+			GroupConstants.DEFAULT_PARENT_GROUP_ID);
+
+		return ServiceContextTestUtil.getServiceContext(group.getGroupId());
+	}
+
 	@Test
 	public void testAddPersonalCommerceAccount() throws Exception {
 		frutillaRule.scenario(
@@ -131,17 +135,9 @@ public class CommerceAccountLocalServiceTest {
 			"That Commerce Account matches the one created before."
 		);
 
-		Group group = GroupTestUtil.addGroup(
-			_company.getCompanyId(), _user.getUserId(),
-			GroupConstants.DEFAULT_PARENT_GROUP_ID);
-
-		ServiceContext serviceContext =
-			ServiceContextTestUtil.getServiceContext(group.getGroupId());
-
 		CommerceAccount personalCommerceAccount =
-			_commerceAccountLocalService.addPersonalCommerceAccount(
-				_user.getUserId(), StringPool.BLANK, StringPool.BLANK,
-				serviceContext);
+			CommerceAccountTestUtil.addPersonalCommerceAccount(
+				_user.getUserId(), _getServiceContext());
 
 		Assert.assertEquals(
 			personalCommerceAccount.getName(),
@@ -170,11 +166,163 @@ public class CommerceAccountLocalServiceTest {
 			WorkflowConstants.STATUS_APPROVED, commerceAccount.getStatus());
 	}
 
+	@Test
+	public void testBusinessOrganizationCommerceAccountsVisibility()
+		throws Exception {
+
+		frutillaRule.scenario(
+			"Adding new business Commerce Accounts"
+		).given(
+			"A company"
+		).when(
+			"The Commerce Accounts are created"
+		).and(
+			"Organizations are added to them"
+		).then(
+			"Check the visibility of that accounts for all the organizations"
+		);
+
+		ServiceContext serviceContext = _getServiceContext();
+
+		int count = 2;
+
+		for (int i = 1; i <= count; i++) {
+			Organization organization = OrganizationTestUtil.addOrganization(
+				OrganizationConstants.DEFAULT_PARENT_ORGANIZATION_ID,
+				"organization" + i, false);
+
+			User user = UserTestUtil.addUser(
+				"organizationUser" + i, serviceContext.getScopeGroupId());
+
+			OrganizationLocalServiceUtil.addUserOrganization(
+				user.getUserId(), organization);
+
+			CommerceAccountTestUtil.addBusinessCommerceAccount(
+				_user.getUserId(), "businessOrganizationAccount" + i,
+				"example@example.com", null,
+				new long[] {organization.getOrganizationId()}, serviceContext);
+		}
+
+		User organizationUser1 = _userLocalService.getUserByScreenName(
+			_company.getCompanyId(), "organizationUser1");
+		User organizationUser2 = _userLocalService.getUserByScreenName(
+			_company.getCompanyId(), "organizationUser2");
+
+		List<CommerceAccount> organizationUser1CommerceAccounts =
+			_commerceAccountLocalService.getUserCommerceAccounts(
+				organizationUser1.getUserId(),
+				CommerceAccountConstants.DEFAULT_PARENT_ACCOUNT_ID,
+				CommerceAccountConstants.SITE_TYPE_B2B, StringPool.BLANK,
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		Assert.assertEquals(
+			organizationUser1CommerceAccounts.toString(), 1,
+			organizationUser1CommerceAccounts.size());
+
+		CommerceAccount organizationUser1CommerceAccount =
+			organizationUser1CommerceAccounts.get(0);
+
+		Assert.assertEquals(
+			organizationUser1CommerceAccount.getName(),
+			"businessOrganizationAccount1");
+
+		List<CommerceAccount> organizationUser2CommerceAccounts =
+			_commerceAccountLocalService.getUserCommerceAccounts(
+				organizationUser1.getUserId(),
+				CommerceAccountConstants.DEFAULT_PARENT_ACCOUNT_ID,
+				CommerceAccountConstants.SITE_TYPE_B2B, StringPool.BLANK,
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		Assert.assertEquals(
+			organizationUser2CommerceAccounts.toString(), 1,
+			organizationUser2CommerceAccounts.size());
+
+		CommerceAccount organizationUser2CommerceAccount =
+			organizationUser2CommerceAccounts.get(0);
+
+		Assert.assertEquals(
+			organizationUser2CommerceAccount.getName(),
+			"businessOrganizationAccount1");
+	}
+
+	@Test
+	public void testBusinessUserCommerceAccountsVisibility()
+		throws Exception {
+
+		frutillaRule.scenario(
+			"Adding new business Commerce Accounts"
+		).given(
+			"A company"
+		).when(
+			"The Commerce Accounts are created"
+		).and(
+			"Users are added to them"
+		).then(
+			"Check the visibility of that accounts for all the users"
+		);
+
+		ServiceContext serviceContext = _getServiceContext();
+
+		int count = 2;
+
+		for (int i = 1; i <= count; i++) {
+			User user = UserTestUtil.addUser(
+				"businessUser" + i, serviceContext.getScopeGroupId());
+
+			CommerceAccountTestUtil.addBusinessCommerceAccount(
+				_user.getUserId(), "businessUserAccount" + i,
+				"example@example.com", new long[] {user.getUserId()}, null,
+				serviceContext);
+		}
+
+		User businessUser1 = _userLocalService.getUserByScreenName(
+			_company.getCompanyId(), "businessUser1");
+		User businessUser2 = _userLocalService.getUserByScreenName(
+			_company.getCompanyId(), "businessUser2");
+
+		List<CommerceAccount> businessUser1CommerceAccounts =
+			_commerceAccountLocalService.getUserCommerceAccounts(
+				businessUser1.getUserId(),
+				CommerceAccountConstants.DEFAULT_PARENT_ACCOUNT_ID,
+				CommerceAccountConstants.SITE_TYPE_B2B, StringPool.BLANK,
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		Assert.assertEquals(
+			businessUser1CommerceAccounts.toString(), 1,
+			businessUser1CommerceAccounts.size());
+
+		CommerceAccount businessUser1CommerceAccount =
+			businessUser1CommerceAccounts.get(0);
+
+		Assert.assertEquals(
+			businessUser1CommerceAccount.getName(), "businessUserAccount1");
+
+		List<CommerceAccount> businessUser2CommerceAccounts =
+			_commerceAccountLocalService.getUserCommerceAccounts(
+				businessUser2.getUserId(),
+				CommerceAccountConstants.DEFAULT_PARENT_ACCOUNT_ID,
+				CommerceAccountConstants.SITE_TYPE_B2B, StringPool.BLANK,
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS);
+
+		Assert.assertEquals(
+			businessUser2CommerceAccounts.toString(), 1,
+			businessUser2CommerceAccounts.size());
+
+		CommerceAccount businessUser2CommerceAccount =
+			businessUser2CommerceAccounts.get(0);
+
+		Assert.assertEquals(
+			businessUser2CommerceAccount.getName(), "businessUserAccount2");
+	}
+
 	@Rule
 	public final FrutillaRule frutillaRule = new FrutillaRule();
 
 	@Inject
 	private CommerceAccountLocalService _commerceAccountLocalService;
+
+	@Inject
+	private UserLocalService _userLocalService;
 
 	@DeleteAfterTestRun
 	private Company _company;
