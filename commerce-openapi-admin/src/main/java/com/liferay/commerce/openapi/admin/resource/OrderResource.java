35d82aaa382c9ef92f8007a5df549d55ea1f9604
Zoltán Takács <zoltan.takacs@liferay.com>
2019-02-05 12:53:20 +0100

    COMMERCE-906 Commits stubs for Order resource

---
+++ commerce-openapi-admin/src/main/java/com/liferay/commerce/openapi/admin/resource/OrderResource.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.openapi.admin.resource;
+
+import com.liferay.commerce.openapi.admin.model.OrderDTO;
+import com.liferay.commerce.openapi.core.context.Pagination;
+import com.liferay.commerce.openapi.core.model.CollectionDTO;
+
+import javax.annotation.Generated;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+
+/**
+ * @author Igor Beslic
+ */
+@Generated(value = "OSGiRESTModuleGenerator")
+@Path("/v1.0/order")
+public interface OrderResource {
+
+	@DELETE
+	@Path("/{id}")
+	public Response deleteOrder(@PathParam("id") String id) throws Exception;
+
+	@GET
+	@Produces("application/*")
+	public CollectionDTO<OrderDTO> getOrder(
+			@QueryParam("groupId") Long groupId, @Context String languageId,
+			@Context Pagination pagination)
+		throws Exception;
+
+	@GET
+	@Path("/{id}")
+	@Produces("application/*")
+	public OrderDTO getOrder(
+			@PathParam("id") String id, @Context String languageId)
+		throws Exception;
+
+	@Consumes("application/*")
+	@Path("/{id}")
+	@PUT
+	public Response updateOrder(
+			@PathParam("id") String id, OrderDTO orderDTO,
+			@Context String languageId)
+		throws Exception;
+
+}
