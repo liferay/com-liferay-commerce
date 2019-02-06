c2c9bb2fefe334a14cc2bfffc259a4491ad6ee1d
Zoltán Takács <zoltan.takacs@liferay.com>
2019-02-06 17:00:47 +0100

    COMMERCE-878 New stubs for v2.0

---
+++ commerce-openapi-admin/src/main/java/com/liferay/commerce/openapi/admin/internal/resource/v2_0/PriceEntryResourceImpl.java
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.openapi.admin.internal.resource.v2_0;
+
+import com.liferay.commerce.openapi.admin.model.v2_0.PriceEntryDTO;
+import com.liferay.commerce.openapi.admin.resource.v2_0.PriceEntryResource;
+import com.liferay.commerce.openapi.core.context.Pagination;
+import com.liferay.commerce.openapi.core.model.CollectionDTO;
+import com.liferay.oauth2.provider.scope.RequiresScope;
+
+import java.util.Collections;
+
+import javax.annotation.Generated;
+
+import javax.ws.rs.core.Response;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
+
+/**
+ * @author Igor Beslic
+ */
+@Component(
+	property = {
+		JaxrsWhiteboardConstants.JAX_RS_APPLICATION_SELECT + "=(osgi.jaxrs.name=CommerceOpenApiAdmin.Rest)",
+		JaxrsWhiteboardConstants.JAX_RS_RESOURCE + "=true", "api.version=v2.0"
+	},
+	scope = ServiceScope.PROTOTYPE, service = PriceEntryResource.class
+)
+@Generated(value = "OSGiRESTModuleGenerator")
+public class PriceEntryResourceImpl implements PriceEntryResource {
+
+	@Override
+	@RequiresScope("CommerceOpenApiAdmin.write")
+	public Response deletePriceEntry(String id) throws Exception {
+		Response.ResponseBuilder responseBuilder = Response.noContent();
+
+		return responseBuilder.build();
+	}
+
+	@Override
+	@RequiresScope("CommerceOpenApiAdmin.read")
+	public CollectionDTO<PriceEntryDTO> getPriceEntries(
+			Long groupId, Pagination pagination)
+		throws Exception {
+
+		return new CollectionDTO(Collections.emptyList(), 0);
+	}
+
+	@Override
+	@RequiresScope("CommerceOpenApiAdmin.read")
+	public PriceEntryDTO getPriceEntry(String id) throws Exception {
+		return new PriceEntryDTO();
+	}
+
+	@Override
+	@RequiresScope("CommerceOpenApiAdmin.write")
+	public Response updatePriceEntry(String id, PriceEntryDTO priceEntryDTO)
+		throws Exception {
+
+		Response.ResponseBuilder responseBuilder = Response.accepted();
+
+		return responseBuilder.build();
+	}
+
+	@Override
+	@RequiresScope("CommerceOpenApiAdmin.write")
+	public PriceEntryDTO upsertPriceEntry(
+			Long groupId, PriceEntryDTO priceEntryDTO)
+		throws Exception {
+
+		return new PriceEntryDTO();
+	}
+
+}
