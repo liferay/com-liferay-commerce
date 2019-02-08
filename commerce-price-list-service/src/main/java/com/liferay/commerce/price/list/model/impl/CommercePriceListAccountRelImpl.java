4435ba988df40c26f3beed1799db8041b97c5749
Ethan Bustad <ethan.bustad@liferay.com>
2019-02-08 15:23:33 -0800

    COMMERCE-943 Fix generated impls

--- commerce-price-list-service/src/main/java/com/liferay/commerce/price/list/model/impl/CommercePriceListAccountRelImpl.java
+++ commerce-price-list-service/src/main/java/com/liferay/commerce/price/list/model/impl/CommercePriceListAccountRelImpl.java
@@ -17,22 +17,13 @@ package com.liferay.commerce.price.list.model.impl;
 import aQute.bnd.annotation.ProviderType;
 
 /**
- * The extended model implementation for the CommercePriceListAccountRel service. Represents a row in the &quot;CommercePriceListAccountRel&quot; database table, with each column mapped to a property of this class.
- *
- * <p>
- * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.commerce.price.list.model.CommercePriceListAccountRel} interface.
- * </p>
- *
- * @author Alessio Antonio Rendina
+ * @author Ethan Bustad
  */
 @ProviderType
 public class CommercePriceListAccountRelImpl
 	extends CommercePriceListAccountRelBaseImpl {
-	/*
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. All methods that expect a commerce price list account rel model instance should use the {@link com.liferay.commerce.price.list.model.CommercePriceListAccountRel} interface instead.
-	 */
+
 	public CommercePriceListAccountRelImpl() {
 	}
+
 }
