ff9ed01091c67a9333386cdba3928b7b978d51f6
Igor Beslic <igor.beslic@liferay.com>
2019-02-11 22:48:08 +0100

    COMMERCE-924 commerce-openapi-util - Reorganize - convert OpenApiProperty into couple classes to make it more object oriented and to avoid IFs in code

---
+++ commerce-openapi-util/src/main/java/com/liferay/commerce/openapi/util/ReferenceArrayOpenApiProperty.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.openapi.util;
+
+import com.liferay.commerce.openapi.util.util.Provider;
+
+/**
+ * @author Igor Beslic
+ */
+public class ReferenceArrayOpenApiProperty extends OpenApiProperty {
+
+	public Provider getJavaTypeProvider() {
+		return _provider;
+	}
+
+	protected ReferenceArrayOpenApiProperty(
+		OpenApiPropertyBuilder openApiPropertyBuilder) {
+
+		super(openApiPropertyBuilder);
+
+		_provider = new Provider(super.getName(), super.getName());
+	}
+
+	private final Provider _provider;
+
+}
