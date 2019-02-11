91b0abd2cbda3a0eabba535dd589bdc55e999733
Igor Beslic <igor.beslic@liferay.com>
2019-02-11 22:48:08 +0100

    COMMERCE-924 commerce-openapi-util - Reorganize - convert OpenApiProperty into couple classes to make it more object oriented and to avoid IFs in code

---
+++ commerce-openapi-util/src/main/java/com/liferay/commerce/openapi/util/FreeFormDictionaryOpenApiProperty.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.openapi.util;
+
+import com.liferay.commerce.openapi.util.util.MapStringWildcardProvider;
+import com.liferay.commerce.openapi.util.util.Provider;
+
+/**
+ * @author Igor Beslic
+ */
+public class FreeFormDictionaryOpenApiProperty extends OpenApiProperty {
+
+	public Provider getJavaTypeProvider() {
+		return _provider;
+	}
+
+	protected FreeFormDictionaryOpenApiProperty(
+		OpenApiPropertyBuilder openApiPropertyBuilder) {
+
+		super(openApiPropertyBuilder);
+
+		OpenApiFormat dictionaryOpenApiFormat = OpenApiFormat.DICTIONARY;
+
+		_provider = new MapStringWildcardProvider(
+			dictionaryOpenApiFormat.getProvider());
+	}
+
+	private final Provider _provider;
+
+}
