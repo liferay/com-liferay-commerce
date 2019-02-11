c6c4e64a0c07b9afb35fc7cd1ca5c9e01e61e14b
Ivica Čardić <ivica.cardic@liferay.com>
2019-02-11 10:36:24 +0100

    COMMERCE-919 commerce-openapi-core-api - Add @Nested annottaion

---
+++ commerce-openapi-core-api/src/main/java/com/liferay/commerce/openapi/core/nested/Nested.java
@@ -0,0 +1,31 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.openapi.core.nested;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Ivica Cardic
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Nested {
+
+	String value();
+
+}
