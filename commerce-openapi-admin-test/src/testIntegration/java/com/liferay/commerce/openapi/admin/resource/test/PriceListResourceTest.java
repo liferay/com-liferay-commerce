ca42f28c695f79a671b2bdb6c79cae152c4c368f
Zoltán Takács <zoltan.takacs@liferay.com>
2019-02-13 00:41:25 +0100

    COMMERCE-882 Complete the test scenarios for PriceList operations

--- commerce-openapi-admin-test/src/testIntegration/java/com/liferay/commerce/openapi/admin/resource/test/PriceListResourceTest.java
+++ commerce-openapi-admin-test/src/testIntegration/java/com/liferay/commerce/openapi/admin/resource/test/PriceListResourceTest.java
@@ -18,7 +18,6 @@ import static org.hamcrest.CoreMatchers.equalTo;
 
 import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
 import com.liferay.commerce.openapi.admin.resource.test.utils.CommerceTestSiteActivator;
-import com.liferay.petra.string.StringBundler;
 import com.liferay.portal.kernel.json.JSONFactory;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.Group;
@@ -31,9 +30,15 @@ import com.liferay.portal.test.rule.LiferayIntegrationTestRule;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.ValidatableResponse;
+
+import java.net.URI;
 
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
 
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Rule;
@@ -62,13 +67,7 @@ public class PriceListResourceTest extends PortalContextProvider {
 
 	@Test
 	public void testAddResourceItem() {
-		JSONObject jsonObject = _jsonFactory.createJSONObject();
-
-		jsonObject.put("active", true);
-		jsonObject.put("currency", "USD");
-		jsonObject.put("name", RandomTestUtil.randomString());
-		jsonObject.put("neverExpire", true);
-		jsonObject.put("priority", 0);
+		JSONObject jsonObject = _getResourceDTO(_PRICE_LIST_NAME);
 
 		RestAssured.given(
 		).request(
@@ -96,17 +95,181 @@ public class PriceListResourceTest extends PortalContextProvider {
 		);
 	}
 
+	@Test
+	public void testDeleteResourceItem() {
+		testAddResourceItem();
+
+		ValidatableResponse resourceValidatableResponse =
+			_getValidatedResourceCollection();
+
+		resourceValidatableResponse.assertThat(
+		).body(
+			"totalItems", equalTo(1)
+		);
+
+		JsonPath jsonPath = resourceValidatableResponse.extract().jsonPath();
+
+		long resourceId = _getIdByName(jsonPath, _PRICE_LIST_NAME);
+
+		RestAssured.given(
+		).request(
+		).auth(
+		).preemptive(
+		).basic(
+			USER, PASSWORD
+		).accept(
+			ContentType.JSON
+		).when(
+		).delete(
+			_getPriceListResourceURL(resourceId)
+		).then(
+		).statusCode(
+			Response.Status.NO_CONTENT.getStatusCode()
+		);
+
+		resourceValidatableResponse = _getValidatedResourceCollection();
+
+		resourceValidatableResponse.assertThat(
+		).body(
+			"totalItems", equalTo(0)
+		);
+	}
+
+	@Test
+	public void testGetResourceItems() {
+		testAddResourceItem();
+
+		JsonPath jsonPath = _getResourceCollectionJsonPath();
+
+		String priceListName = jsonPath.param(
+			"priceListName", _PRICE_LIST_NAME
+		).get(
+			"items.find { item -> item.name == priceListName }.name"
+		);
+
+		Assert.assertEquals(_PRICE_LIST_NAME, priceListName);
+	}
+
+	@Test
+	public void testUpdateResourceItem() {
+		testAddResourceItem();
+		JsonPath jsonPath = _getResourceCollectionJsonPath();
+
+		long resourceId = _getIdByName(jsonPath, _PRICE_LIST_NAME);
+
+		JSONObject jsonObject = _getResourceDTO(_UPDATED_PRICE_LIST_NAME);
+
+		RestAssured.given(
+		).request(
+		).auth(
+		).preemptive(
+		).basic(
+			USER, PASSWORD
+		).accept(
+			ContentType.JSON
+		).contentType(
+			ContentType.JSON
+		).body(
+			jsonObject.toString()
+		).queryParam(
+			"groupId", _group.getGroupId()
+		).when(
+		).put(
+			_getPriceListResourceURL(resourceId)
+		).then(
+		).statusCode(
+			Response.Status.NO_CONTENT.getStatusCode()
+		);
+
+		// Verify if it's updated
+
+		jsonPath = _getResourceCollectionJsonPath();
+
+		String priceListName = jsonPath.param(
+			"priceListName", _UPDATED_PRICE_LIST_NAME
+		).get(
+			"items.find { item -> item.name == priceListName }.name"
+		);
+
+		Assert.assertEquals(_UPDATED_PRICE_LIST_NAME, priceListName);
+
+		long updatedItemId = _getIdByName(jsonPath, _UPDATED_PRICE_LIST_NAME);
+
+		Assert.assertEquals(resourceId, updatedItemId);
+	}
+
+	private long _getIdByName(JsonPath jsonPath, String priceListName) {
+		return jsonPath.param(
+			"priceListName", priceListName
+		).getLong(
+			"items.find { item -> item.name == priceListName }.id"
+		);
+	}
+
 	private String _getPriceListResourceURL() {
-		StringBundler sb = new StringBundler(3);
+		return _getPriceListResourceURL(null);
+	}
 
-		sb.append(getRootEndpointURL());
-		sb.append(_API_VERSION);
-		sb.append("priceList");
+	private String _getPriceListResourceURL(Long id) {
+		UriBuilder uriBuilder = UriBuilder.fromPath(getRootEndpointURL());
 
-		return sb.toString();
+		URI uri = uriBuilder.path(
+			_API_VERSION
+		).path(
+			"priceList"
+		).path(
+			(id == null) ? "" : String.valueOf(id)
+		).build();
+
+		return uri.toString();
+	}
+
+	private JsonPath _getResourceCollectionJsonPath() {
+		ValidatableResponse validatableResponse =
+			_getValidatedResourceCollection();
+
+		return validatableResponse.extract().jsonPath();
+	}
+
+	private JSONObject _getResourceDTO(String name) {
+		JSONObject jsonObject = _jsonFactory.createJSONObject();
+
+		jsonObject.put("active", true);
+		jsonObject.put("currency", "USD");
+		jsonObject.put("name", name);
+		jsonObject.put("neverExpire", true);
+		jsonObject.put("priority", 0);
+
+		return jsonObject;
+	}
+
+	private ValidatableResponse _getValidatedResourceCollection() {
+		return RestAssured.given(
+		).request(
+		).auth(
+		).preemptive(
+		).basic(
+			USER, PASSWORD
+		).accept(
+			ContentType.JSON
+		).queryParam(
+			"groupId", _group.getGroupId()
+		).when(
+		).get(
+			_getPriceListResourceURL()
+		).then(
+		).statusCode(
+			Response.Status.OK.getStatusCode()
+		);
 	}
 
 	private static final String _API_VERSION = "v1.0/";
+
+	private static final String _PRICE_LIST_NAME =
+		RandomTestUtil.randomString();
+
+	private static final String _UPDATED_PRICE_LIST_NAME =
+		RandomTestUtil.randomString();
 
 	@DeleteAfterTestRun
 	private Group _group;
