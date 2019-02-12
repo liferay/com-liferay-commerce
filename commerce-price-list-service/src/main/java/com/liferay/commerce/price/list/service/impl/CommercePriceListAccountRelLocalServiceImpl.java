94887ad2608420449a9ab6397e635e14adb966ea
Ethan Bustad <ethan.bustad@liferay.com>
2019-02-11 16:40:55 -0800

    COMMERCE-943 Make it work: action

--- commerce-price-list-service/src/main/java/com/liferay/commerce/price/list/service/impl/CommercePriceListAccountRelLocalServiceImpl.java
+++ commerce-price-list-service/src/main/java/com/liferay/commerce/price/list/service/impl/CommercePriceListAccountRelLocalServiceImpl.java
@@ -14,8 +14,14 @@
 
 package com.liferay.commerce.price.list.service.impl;
 
+import com.liferay.commerce.price.list.model.CommercePriceList;
 import com.liferay.commerce.price.list.model.CommercePriceListAccountRel;
 import com.liferay.commerce.price.list.service.base.CommercePriceListAccountRelLocalServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.service.ServiceContext;
 
 import java.util.List;
 
@@ -26,11 +32,107 @@ public class CommercePriceListAccountRelLocalServiceImpl
 	extends CommercePriceListAccountRelLocalServiceBaseImpl {
 
 	@Override
+	public CommercePriceListAccountRel addCommercePriceListAccountRel(
+			long commercePriceListId, long commerceAccountId, int order,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		User user = userLocalService.getUser(serviceContext.getUserId());
+		long groupId = serviceContext.getScopeGroupId();
+
+		CommercePriceListAccountRel commercePriceListAccountRel =
+			commercePriceListAccountRelPersistence.create(
+				counterLocalService.increment());
+
+		commercePriceListAccountRel.setGroupId(groupId);
+		commercePriceListAccountRel.setCompanyId(user.getCompanyId());
+		commercePriceListAccountRel.setUserId(user.getUserId());
+		commercePriceListAccountRel.setUserName(user.getFullName());
+		commercePriceListAccountRel.setCommerceAccountId(commerceAccountId);
+		commercePriceListAccountRel.setCommercePriceListId(commercePriceListId);
+		commercePriceListAccountRel.setOrder(order);
+		commercePriceListAccountRel.setExpandoBridgeAttributes(serviceContext);
+
+		// Commerce price list
+
+		reindexCommercePriceList(commercePriceListId);
+
+		// Cache
+
+		commercePriceListLocalService.cleanPriceListCache(groupId);
+
+		return commercePriceListAccountRelPersistence.update(
+			commercePriceListAccountRel);
+	}
+
+	@Override
+	public CommercePriceListAccountRel deleteCommercePriceListAccountRel(
+			CommercePriceListAccountRel commercePriceListAccountRel)
+		throws PortalException {
+
+		commercePriceListAccountRelPersistence.remove(
+			commercePriceListAccountRel);
+
+		// Expando
+
+		expandoRowLocalService.deleteRows(
+			commercePriceListAccountRel.getCommercePriceListAccountRelId());
+
+		// Commerce price list
+
+		reindexCommercePriceList(
+			commercePriceListAccountRel.getCommercePriceListId());
+
+		// Cache
+
+		commercePriceListLocalService.cleanPriceListCache(
+			commercePriceListAccountRel.getGroupId());
+
+		return commercePriceListAccountRel;
+	}
+
+	@Override
+	public CommercePriceListAccountRel deleteCommercePriceListAccountRel(
+			long commercePriceListAccountRelId)
+		throws PortalException {
+
+		CommercePriceListAccountRel commercePriceListAccountRel =
+			commercePriceListAccountRelPersistence.findByPrimaryKey(
+				commercePriceListAccountRelId);
+
+		return commercePriceListAccountRelLocalService.
+			deleteCommercePriceListAccountRel(commercePriceListAccountRel);
+	}
+
+	@Override
+	public void deleteCommercePriceListAccountRels(long commercePriceListId) {
+		commercePriceListAccountRelPersistence.removeByCommercePriceListId(
+			commercePriceListId);
+	}
+
+	@Override
+	public CommercePriceListAccountRel fetchCommercePriceListAccountRel(
+		long commercePriceListId, long commerceAccountId) {
+
+		return commercePriceListAccountRelPersistence.fetchByC_C(
+			commercePriceListId, commerceAccountId);
+	}
+
+	@Override
 	public List<CommercePriceListAccountRel> getCommercePriceListAccountRels(
 		long commercePriceListId) {
 
 		return commercePriceListAccountRelPersistence.findByCommercePriceListId(
 			commercePriceListId);
+	}
+
+	protected void reindexCommercePriceList(long commercePriceListId)
+		throws PortalException {
+
+		Indexer<CommercePriceList> indexer =
+			IndexerRegistryUtil.nullSafeGetIndexer(CommercePriceList.class);
+
+		indexer.reindex(CommercePriceList.class.getName(), commercePriceListId);
 	}
 
 }
