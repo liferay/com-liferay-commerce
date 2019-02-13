51eba2a355de08592b1eccd736d2e57d570360af
Matija Petanjek <matija.petanjek@liferay.com>
2019-02-13 19:40:32 +0100

    COMMERCE-960 commerce-openapi- extract EventsProcessorUtil to filter to eliminate future code duplication and trigger all *PreActions

---
+++ commerce-openapi-admin/src/main/java/com/liferay/commerce/openapi/admin/internal/filter/CommerceAdminEventsProcessorFilter.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.openapi.admin.internal.filter;
+
+import com.liferay.portal.events.EventsProcessorUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.PropsKeys;
+import com.liferay.portal.util.PropsValues;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.container.PreMatching;
+import javax.ws.rs.core.Context;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Matija Petanjek
+ */
+@Component(
+	immediate = true,
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=CommerceOpenApiAdmin.Rest)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Filter.CommerceAdminEventsProcessorFilter"
+	},
+	service = ContainerRequestFilter.class
+)
+@PreMatching
+public class CommerceAdminEventsProcessorFilter
+	implements ContainerRequestFilter {
+
+	@Override
+	public void filter(ContainerRequestContext requestContext) {
+		try {
+			EventsProcessorUtil.process(
+				PropsKeys.SERVLET_SERVICE_EVENTS_PRE,
+				PropsValues.SERVLET_SERVICE_EVENTS_PRE, _httpServletRequest,
+				_httpServletResponse);
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CommerceAdminEventsProcessorFilter.class);
+
+	@Context
+	private HttpServletRequest _httpServletRequest;
+
+	@Context
+	private HttpServletResponse _httpServletResponse;
+
+}
