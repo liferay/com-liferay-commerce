da24e4decc902524326bb81f418e75eff35ffe59
Igor Beslic <igor.beslic@liferay.com>
2019-02-13 15:37:46 +0100

    COMMERCE-974 commerce-openapi-core-api - Introduce Nullable annotation to help developer detect cases where null value should be checked (as we rely to AutoBoxing)

---
+++ commerce-openapi-core-api/src/main/java/com/liferay/commerce/openapi/core/annotation/Nullable.java
@@ -0,0 +1,28 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.openapi.core.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @author Igor Beslic
+ */
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.PARAMETER, ElementType.FIELD, ElementType.METHOD})
+public @interface Nullable {
+}
