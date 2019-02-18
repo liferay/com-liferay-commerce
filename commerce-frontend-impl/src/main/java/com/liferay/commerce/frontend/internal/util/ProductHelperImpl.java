e01ae8dd01b9c2a115691380843fe64750ab5e2d
Marco Leo <marco.leo@liferay.com>
2019-02-17 22:55:57 -0800

    COMMERCE-870 Implements util

---
+++ commerce-frontend-impl/src/main/java/com/liferay/commerce/frontend/internal/util/ProductHelperImpl.java
@@ -0,0 +1,143 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.frontend.internal.util;
+
+import com.liferay.commerce.constants.CPDefinitionInventoryConstants;
+import com.liferay.commerce.context.CommerceContext;
+import com.liferay.commerce.currency.model.CommerceMoney;
+import com.liferay.commerce.discount.CommerceDiscountValue;
+import com.liferay.commerce.frontend.model.PriceModel;
+import com.liferay.commerce.frontend.model.ProductSettingsModel;
+import com.liferay.commerce.frontend.util.ProductHelper;
+import com.liferay.commerce.model.CPDefinitionInventory;
+import com.liferay.commerce.price.CommerceProductPrice;
+import com.liferay.commerce.price.CommerceProductPriceCalculation;
+import com.liferay.commerce.product.model.CPInstance;
+import com.liferay.commerce.product.service.CPInstanceService;
+import com.liferay.commerce.service.CPDefinitionInventoryLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.math.BigDecimal;
+
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marco Leo
+ */
+@Component(service = ProductHelper.class)
+public class ProductHelperImpl implements ProductHelper {
+
+	public PriceModel getPrice(
+			long cpInstanceId, int quantity, CommerceContext commerceContext,
+			Locale locale)
+		throws PortalException {
+
+		CommerceProductPrice commerceProductPrice =
+			_commerceProductPriceCalculation.getCommerceProductPrice(
+				cpInstanceId, quantity, true, commerceContext);
+
+		if (commerceProductPrice == null) {
+			return null;
+		}
+
+		CommerceMoney unitPrice = commerceProductPrice.getUnitPrice();
+
+		PriceModel priceModel = new PriceModel(unitPrice.format(locale));
+
+		CommerceMoney unitPromoPrice = commerceProductPrice.getUnitPromoPrice();
+
+		BigDecimal promoPrice = unitPromoPrice.getPrice();
+
+		if ((promoPrice.compareTo(BigDecimal.ZERO) > 0) &&
+			(promoPrice.compareTo(unitPrice.getPrice()) < 0)) {
+
+			priceModel.setPromoPrice(unitPromoPrice.format(locale));
+		}
+
+		CommerceDiscountValue discountValue =
+			commerceProductPrice.getDiscountValue();
+
+		if (discountValue != null) {
+			CommerceMoney discountAmount = discountValue.getDiscountAmount();
+
+			priceModel.setDiscount(discountAmount.format(locale));
+		}
+
+		return priceModel;
+	}
+
+	public ProductSettingsModel getProductSettingsModel(long cpInstanceId)
+		throws PortalException {
+
+		ProductSettingsModel productSettingsModel = new ProductSettingsModel();
+
+		CPInstance cpInstance = _cpInstanceService.fetchCPInstance(
+			cpInstanceId);
+
+		int minOrderQuantity =
+			CPDefinitionInventoryConstants.DEFAULT_MIN_ORDER_QUANTITY;
+
+		int maxOrderQuantity =
+			CPDefinitionInventoryConstants.DEFAULT_MAX_ORDER_QUANTITY;
+
+		int multipleQuantity =
+			CPDefinitionInventoryConstants.DEFAULT_MULTIPLE_ORDER_QUANTITY;
+
+		CPDefinitionInventory cpDefinitionInventory = null;
+
+		if (cpInstance != null) {
+			cpDefinitionInventory =
+				_cpDefinitionInventoryLocalService.
+					fetchCPDefinitionInventoryByCPDefinitionId(
+						cpInstance.getCPDefinitionId());
+		}
+
+		if (cpDefinitionInventory != null) {
+			minOrderQuantity = cpDefinitionInventory.getMinOrderQuantity();
+			maxOrderQuantity = cpDefinitionInventory.getMaxOrderQuantity();
+			multipleQuantity = cpDefinitionInventory.getMultipleOrderQuantity();
+
+			int[] allowedOrderQuantitiesArray =
+				cpDefinitionInventory.getAllowedOrderQuantitiesArray();
+
+			if ((allowedOrderQuantitiesArray != null) &&
+				(allowedOrderQuantitiesArray.length > 0)) {
+
+				productSettingsModel.setAllowedQuantities(
+					allowedOrderQuantitiesArray);
+			}
+		}
+
+		productSettingsModel.setMinQuantity(minOrderQuantity);
+		productSettingsModel.setMaxQuantity(maxOrderQuantity);
+		productSettingsModel.setMultipleQuanty(multipleQuantity);
+
+		return productSettingsModel;
+	}
+
+	@Reference
+	private CommerceProductPriceCalculation _commerceProductPriceCalculation;
+
+	@Reference
+	private CPDefinitionInventoryLocalService
+		_cpDefinitionInventoryLocalService;
+
+	@Reference
+	private CPInstanceService _cpInstanceService;
+
+}
