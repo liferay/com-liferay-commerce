f263705d81002a32e947fb4f51bf707f25689484
Marco Leo <marco.leo@liferay.com>
2019-02-19 12:23:58 -0800

    COMMERCE-1034 Delete dummy data

--- commerce-frontend-taglib/src/main/java/com/liferay/commerce/frontend/taglib/servlet/taglib/CompareCheckboxTag.java
+++ commerce-frontend-taglib/src/main/java/com/liferay/commerce/frontend/taglib/servlet/taglib/CompareCheckboxTag.java
@@ -15,9 +15,18 @@
 package com.liferay.commerce.frontend.taglib.servlet.taglib;
 
 import com.liferay.commerce.frontend.taglib.internal.js.loader.modules.extender.npm.NPMResolverProvider;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.service.CPDefinitionServiceUtil;
+import com.liferay.commerce.product.util.CPCompareUtil;
 import com.liferay.frontend.js.loader.modules.extender.npm.NPMResolver;
 import com.liferay.frontend.taglib.soy.servlet.taglib.ComponentRendererTag;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author Fabio Diego Mastrorilli
@@ -30,11 +39,30 @@ public class CompareCheckboxTag extends ComponentRendererTag {
 		putValue("compareAvailable", true);
 		putValue("inCompare", false);
 		putValue("labelVisible", true);
-		putValue(
-			"pictureUrl",
-			"/o/commerce-media/products/45626/coil-spring-rear/45645" +
-				"/Minium_ProductImage_13.png?download=false");
-		putValue("productId", 45625);
+
+		Map<String, Object> context = getContext();
+
+		long cpDefinitionId = GetterUtil.getLong(context.get("productId"));
+
+		try {
+			CPDefinition cpDefinition = CPDefinitionServiceUtil.getCPDefinition(
+				cpDefinitionId);
+
+			putValue("pictureUrl", cpDefinition.getDefaultImageThumbnailSrc());
+
+			List<Long> cpDefinitionIds = CPCompareUtil.getCPDefinitionIds(
+				request);
+
+			if (cpDefinitionIds.contains(cpDefinitionId)) {
+				putValue("inCompare", true);
+			}
+			else {
+				putValue("inCompare", true);
+			}
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
 
 		setTemplateNamespace("CompareCheckbox.render");
 
@@ -52,5 +80,12 @@ public class CompareCheckboxTag extends ComponentRendererTag {
 		return npmResolver.resolveModuleName(
 			"commerce-frontend-taglib/compare_checkbox/CompareCheckbox.es");
 	}
+
+	public void setCPDefinitionId(long cpDefinitionId) {
+		putValue("productId", cpDefinitionId);
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CompareCheckboxTag.class);
 
 }
