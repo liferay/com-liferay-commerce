14027fc77245b757cd4bc54fff5aba2b9d1ab272
Igor Beslic <igor.beslic@liferay.com>
2019-02-20 23:35:50 +0100

    COMMERCE-942 commerce-openapi-util - Introduce open api reader - to separate read phase from parse (importer) phase

---
+++ commerce-openapi-util/src/main/java/com/liferay/commerce/openapi/util/importer/OpenApiReaderFactory.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.openapi.util.importer;
+
+import com.liferay.commerce.openapi.util.config.ConfigurationFactory;
+import com.liferay.commerce.openapi.util.importer.exception.ReaderException;
+
+import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Igor Beslic
+ */
+public class OpenApiReaderFactory {
+
+	public static OpenApiReader getOpenApiReader(Properties configuration) {
+		String hostName = configuration.getProperty(
+			"openapi.swagger.host.name");
+
+		if ((hostName != null) && (hostName.length() != 0)) {
+			return new URLOpenApiReader(configuration);
+		}
+
+		String openApiUrl = configuration.getProperty("openapi.swagger.url");
+
+		return new FileOpenApiReader(openApiUrl);
+	}
+
+	public static OpenApiReader getOpenApiReader(String externalReference) {
+		if (!isExternalReference(externalReference)) {
+			throw new ReaderException(
+				"No locator implementation for reference " + externalReference);
+		}
+
+		String[] referenceParts = externalReference.split("#");
+
+		String externalUrl = referenceParts[0];
+		String componentReference = referenceParts[1];
+
+		_logger.info(
+			"External reference detected - location: {} reference: {}",
+			externalUrl, componentReference);
+
+		if (!externalUrl.startsWith("http")) {
+			return new FileOpenApiReader(externalUrl);
+		}
+
+		Properties configuration = ConfigurationFactory.getConfigurationFor(
+			externalReference);
+
+		if (configuration == null) {
+			throw new ReaderException("Unable to create reader configuration");
+		}
+
+		return new URLOpenApiReader(configuration);
+	}
+
+	public static boolean isExternalReference(String reference) {
+		if (reference.startsWith("#")) {
+			return false;
+		}
+
+		return true;
+	}
+
+	private static final Logger _logger = LoggerFactory.getLogger(
+		OpenApiReaderFactory.class);
+
+}
