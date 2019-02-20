ab64b1cd947144874f68f713ecf798bee1f2d756
Luca Pellizzon <luca.pellizzon@liferay.com>
2019-02-20 10:07:32 +0100

    COMMERCE-1045 Mercanet payment request and payment request provider

---
+++ commerce-payment-method-mercanet/src/main/java/com/liferay/commerce/payment/method/mercanet/internal/MercanetCommercePaymentRequestProvider.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.payment.method.mercanet.internal;
+
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.commerce.payment.request.CommercePaymentRequest;
+import com.liferay.commerce.payment.request.CommercePaymentRequestProvider;
+import com.liferay.commerce.service.CommerceOrderLocalService;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.Locale;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Luca Pellizzon
+ */
+@Component(
+	immediate = true,
+	property = "commerce.payment.engine.method.key=" + MercanetCommercePaymentMethod.KEY,
+	service = CommercePaymentRequestProvider.class
+)
+public class MercanetCommercePaymentRequestProvider
+	implements CommercePaymentRequestProvider {
+
+	@Override
+	public CommercePaymentRequest getCommercePaymentRequest(
+			String cancelUrl, long commerceOrderId,
+			HttpServletRequest httpServletRequest, Locale locale,
+			String returnUrl, String transactionId)
+		throws PortalException {
+
+		CommerceOrder commerceOrder =
+			_commerceOrderLocalService.getCommerceOrder(commerceOrderId);
+
+		return new MercanetCommercePaymentRequest(
+			commerceOrder.getTotal(), cancelUrl, commerceOrderId, locale,
+			httpServletRequest, returnUrl, transactionId);
+	}
+
+	@Reference
+	private CommerceOrderLocalService _commerceOrderLocalService;
+
+}
