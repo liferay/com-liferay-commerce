824ca3087ba8545326be29f2bfe0b1ee66b53a1b
Luca Pellizzon <luca.pellizzon@liferay.com>
2019-02-20 09:58:51 +0100

    COMMERCE-600 redirection for parameter exchange is no longer used

--- commerce-payment-service/src/main/java/com/liferay/commerce/payment/servlet/CommercePaymentServlet.java
+++ commerce-payment-service/src/main/java/com/liferay/commerce/payment/servlet/CommercePaymentServlet.java
@@ -15,8 +15,11 @@
 package com.liferay.commerce.payment.servlet;
 
 import com.liferay.commerce.constants.CommercePaymentConstants;
+import com.liferay.commerce.model.CommerceOrder;
 import com.liferay.commerce.payment.engine.CommercePaymentEngine;
 import com.liferay.commerce.payment.result.CommercePaymentResult;
+import com.liferay.commerce.service.CommerceOrderService;
+import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.PermissionCheckerFactoryUtil;
@@ -27,6 +30,14 @@ import com.liferay.portal.kernel.util.Portal;
 
 import java.io.IOException;
 
+import java.net.URL;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.Servlet;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -71,17 +82,58 @@ public class CommercePaymentServlet extends HttpServlet {
 
 			PermissionThreadLocal.setPermissionChecker(permissionChecker);
 
-			_commerceOrderId = ParamUtil.getLong(
-				httpServletRequest, "commerceOrderId");
-			_confirmationStepUrl = ParamUtil.getString(
-				httpServletRequest, "confirmationStep");
+			long groupId = ParamUtil.getLong(httpServletRequest, "groupId");
+			String uuid = ParamUtil.getString(httpServletRequest, "uuid");
+
+			CommerceOrder commerceOrder =
+				_commerceOrderService.getCommerceOrderByUuidAndGroupId(
+					uuid, groupId);
+
+			_commerceOrderId = commerceOrder.getCommerceOrderId();
+
+			_nextUrl = ParamUtil.getString(httpServletRequest, "nextStep");
 
 			CommercePaymentResult commercePaymentResult = _startPayment(
 				httpServletRequest);
 
+			if (!commercePaymentResult.isSuccess()) {
+				httpServletResponse.sendRedirect(_nextUrl);
+
+				return;
+			}
+
 			if (commercePaymentResult.isOnlineRedirect()) {
-				httpServletResponse.sendRedirect(
+				String portalUrl = _portal.getPortalURL(httpServletRequest);
+
+				URL portalURL = new URL(portalUrl);
+
+				URL redirectUrl = new URL(
 					commercePaymentResult.getRedirectUrl());
+
+				if (Objects.equals(
+						portalURL.getHost(), redirectUrl.getHost())) {
+
+					Map<String, String> paramsMap = _getQueryMap(
+						redirectUrl.getQuery());
+
+					Set<Map.Entry<String, String>> entries =
+						paramsMap.entrySet();
+
+					for (Map.Entry<String, String> param : entries) {
+						httpServletRequest.setAttribute(
+							param.getKey(), param.getValue());
+					}
+
+					RequestDispatcher requestDispatcher =
+						httpServletRequest.getRequestDispatcher(
+							redirectUrl.getPath());
+
+					requestDispatcher.forward(
+						httpServletRequest, httpServletResponse);
+				}
+				else {
+					httpServletResponse.sendRedirect(redirectUrl.toString());
+				}
 			}
 
 			// Offline methods, payment complete
@@ -98,7 +150,7 @@ public class CommercePaymentServlet extends HttpServlet {
 				_commercePaymentEngine.completePayment(
 					_commerceOrderId, null, httpServletRequest);
 
-				httpServletResponse.sendRedirect(_confirmationStepUrl);
+				httpServletResponse.sendRedirect(_nextUrl);
 			}
 		}
 		catch (Exception e) {
@@ -106,20 +158,41 @@ public class CommercePaymentServlet extends HttpServlet {
 		}
 	}
 
+	private Map<String, String> _getQueryMap(String query)
+	{
+
+		String[] params = query.split(StringPool.AMPERSAND);
+
+		Map<String, String> map = new HashMap();
+
+		for (String param : params)
+		{
+			String name = param.split(StringPool.EQUAL)[0];
+			String value = param.split(StringPool.EQUAL)[1];
+
+			map.put(name, value);
+		}
+
+		return map;
+	}
+
 	private CommercePaymentResult _startPayment(
 			HttpServletRequest httpServletRequest)
 		throws Exception {
 
 		return _commercePaymentEngine.startPayment(
-			_commerceOrderId, _confirmationStepUrl, httpServletRequest);
+			_commerceOrderId, _nextUrl, httpServletRequest);
 	}
 
 	private long _commerceOrderId;
 
 	@Reference
+	private CommerceOrderService _commerceOrderService;
+
+	@Reference
 	private CommercePaymentEngine _commercePaymentEngine;
 
-	private String _confirmationStepUrl;
+	private String _nextUrl;
 
 	@Reference
 	private Portal _portal;
