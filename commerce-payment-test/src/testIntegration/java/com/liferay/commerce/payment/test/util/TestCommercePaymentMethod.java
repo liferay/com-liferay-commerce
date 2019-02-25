288a003b5340cba8db638ff89e4999aa8e65a263
Luca Pellizzon <luca.pellizzon@liferay.com>
2019-02-25 17:47:42 +0100

    COMMERCE-1060 payment method's mock for tests

---
+++ commerce-payment-test/src/testIntegration/java/com/liferay/commerce/payment/test/util/TestCommercePaymentMethod.java
@@ -0,0 +1,97 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.payment.test.util;
+
+import com.liferay.commerce.constants.CommerceOrderConstants;
+import com.liferay.commerce.constants.CommercePaymentConstants;
+import com.liferay.commerce.payment.method.CommercePaymentMethod;
+import com.liferay.commerce.payment.request.CommercePaymentRequest;
+import com.liferay.commerce.payment.result.CommercePaymentResult;
+
+import java.util.Collections;
+import java.util.Locale;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Luca Pellizzon
+ */
+@Component(
+	immediate = true,
+	property = "commerce.payment.engine.method.key=" + TestCommercePaymentMethod.KEY,
+	service = CommercePaymentMethod.class
+)
+public class TestCommercePaymentMethod implements CommercePaymentMethod {
+
+	public static final String KEY = "test-payment-method";
+
+	@Override
+	public CommercePaymentResult completePayment(
+			CommercePaymentRequest commercePaymentRequest)
+		throws Exception {
+
+		return new CommercePaymentResult(
+			null, commercePaymentRequest.getCommerceOrderId(),
+			CommerceOrderConstants.PAYMENT_STATUS_PAID, false, null, null,
+			Collections.emptyList(), true);
+	}
+
+	@Override
+	public String getDescription(Locale locale) {
+		return "TestMethod";
+	}
+
+	@Override
+	public String getKey() {
+		return KEY;
+	}
+
+	@Override
+	public String getName(Locale locale) {
+		return "TestMethod";
+	}
+
+	@Override
+	public int getPaymentType() {
+		return CommercePaymentConstants.COMMERCE_PAYMENT_METHOD_TYPE_OFFLINE;
+	}
+
+	@Override
+	public String getServletPath() {
+		return null;
+	}
+
+	@Override
+	public boolean isCompleteEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean isProcessPaymentEnabled() {
+		return true;
+	}
+
+	@Override
+	public CommercePaymentResult processPayment(
+			CommercePaymentRequest commercePaymentRequest)
+		throws Exception {
+
+		return new CommercePaymentResult(
+			null, commercePaymentRequest.getCommerceOrderId(),
+			CommerceOrderConstants.PAYMENT_STATUS_AUTHORIZED, false, null, null,
+			Collections.emptyList(), true);
+	}
+
+}
