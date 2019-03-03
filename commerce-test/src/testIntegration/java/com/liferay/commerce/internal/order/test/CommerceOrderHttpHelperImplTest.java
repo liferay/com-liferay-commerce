61ce498710696a9d2445fe2e8f9c7e11bdc922da
Marco Leo <marco.leo@liferay.com>
2019-03-03 15:14:23 -0800

    COMMERCE-1093 Fix tests

--- commerce-test/src/testIntegration/java/com/liferay/commerce/internal/order/test/CommerceOrderHttpHelperImplTest.java
+++ commerce-test/src/testIntegration/java/com/liferay/commerce/internal/order/test/CommerceOrderHttpHelperImplTest.java
@@ -98,7 +98,7 @@ public class CommerceOrderHttpHelperImplTest {
 				_user.getUserId());
 
 		CommerceContext commerceContext = new TestCommerceContext(
-			commerceCurrency, _user, _group, _commerceAccount, null, null);
+			commerceCurrency, _user, _group, _commerceAccount, null);
 
 		_httpServletRequest.setAttribute(WebKeys.THEME_DISPLAY, _themeDisplay);
 		_httpServletRequest.setAttribute(
