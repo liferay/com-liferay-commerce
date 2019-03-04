477911541221453698aaf5c83d73b21d324cac64
Ivica Cardic <ivica.cardic@liferay.com>
2019-03-04 22:20:24 +0100

    COMMERCE-1066 - commerce-batch-rest  - Add CommerceBatchApplication, ProcessResource and exception mappers

---
+++ commerce-batch/commerce-batch-rest/src/main/java/com/liferay/commerce/batch/rest/internal/application/CommerceBatchApplication.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.batch.rest.internal.application;
+
+import com.liferay.commerce.openapi.core.constants.OpenApiPropsKeys;
+
+import javax.ws.rs.core.Application;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
+
+/**
+ * @author Ivica Cardic
+ */
+@Component(
+	property = {
+		JaxrsWhiteboardConstants.JAX_RS_APPLICATION_BASE + "=/commerce-batch",
+		JaxrsWhiteboardConstants.JAX_RS_EXTENSION_SELECT + "=(osgi.jaxrs.name=Liferay.OAuth2)",
+		JaxrsWhiteboardConstants.JAX_RS_NAME + "=CommerceBatch.Rest",
+		OpenApiPropsKeys.EXCEPTION_MAPPERS_ENABLED,
+		"auth.verifier.auth.verifier.BasicAuthHeaderAuthVerifier.urls.includes=/*",
+		"auth.verifier.auth.verifier.OAuth2RestAuthVerifier.urls.includes=/*",
+		"auth.verifier.auth.verifier.PortalSessionAuthVerifier.urls.includes=/*",
+		"auth.verifier.guest.allowed=true",
+		"oauth2.scopechecker.type=annotations"
+	},
+	service = Application.class
+)
+public class CommerceBatchApplication extends Application {
+}
---
+++ commerce-batch/commerce-batch-rest/src/main/java/com/liferay/commerce/batch/rest/internal/resource/ProcessResource.java
@@ -0,0 +1,107 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.batch.rest.internal.resource;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import com.liferay.commerce.batch.engine.api.batch.BatchFileProcessor;
+import com.liferay.oauth2.provider.scope.RequiresScope;
+
+import java.util.Map;
+
+import javax.activation.DataHandler;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Response;
+
+import org.apache.cxf.jaxrs.ext.multipart.Attachment;
+import org.apache.cxf.jaxrs.ext.multipart.MultipartBody;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ServiceScope;
+import org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants;
+
+/**
+ * @author Ivica Cardic
+ */
+@Component(
+	property = {
+		JaxrsWhiteboardConstants.JAX_RS_APPLICATION_SELECT + "=(osgi.jaxrs.name=CommerceBatch.Rest)",
+		JaxrsWhiteboardConstants.JAX_RS_RESOURCE + "=true"
+	},
+	scope = ServiceScope.PROTOTYPE, service = ProcessResource.class
+)
+@Path("/process")
+public class ProcessResource {
+
+	@DELETE
+	@Path("")
+	@RequiresScope("CommerceBatch.write")
+	public Response delete(MultipartBody multipartBody) throws Exception {
+		return _process(multipartBody.getRootAttachment());
+	}
+
+	@GET
+	@Path("/{key}")
+	@RequiresScope("CommerceBatch.read")
+	public Response getBatchExecutionStatus(@PathParam("key") String key) {
+		Response.ResponseBuilder responseBuilder = Response.ok();
+
+		boolean active = _batchFileProcessor.getBatchExecutionStatus(key);
+
+		responseBuilder.entity(active);
+
+		return responseBuilder.build();
+	}
+
+	@Path("")
+	@POST
+	@RequiresScope("CommerceBatch.write")
+	public Response post(MultipartBody multipartBody) throws Exception {
+		return _process(multipartBody.getRootAttachment());
+	}
+
+	@Path("")
+	@PUT
+	@RequiresScope("CommerceBatch.write")
+	public Response put(MultipartBody multipartBody) throws Exception {
+		return _process(multipartBody.getRootAttachment());
+	}
+
+	private Response _process(Attachment attachment) throws Exception {
+		DataHandler dataHandler = attachment.getDataHandler();
+
+		Map<String, String> jobIdNameMap = _batchFileProcessor.process(
+			dataHandler.getName(), dataHandler.getInputStream());
+
+		Response.ResponseBuilder responseBuilder = Response.accepted();
+
+		responseBuilder.entity(_OBJECT_MAPPER.writeValueAsString(jobIdNameMap));
+
+		return responseBuilder.build();
+	}
+
+	private static final ObjectMapper _OBJECT_MAPPER = new ObjectMapper();
+
+	@Reference
+	private BatchFileProcessor _batchFileProcessor;
+
+}
