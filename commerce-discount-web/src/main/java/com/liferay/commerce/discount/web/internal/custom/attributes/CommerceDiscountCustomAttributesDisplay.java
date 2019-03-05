f6d9a870628e3c194cdd833e4df80cb428e8e36b
Ethan Bustad <ethan.bustad@liferay.com>
2019-03-05 09:52:51 -0800

    COMMERCE-1109 Add CustomAttributesDisplay classes for tables with expandos

---
+++ commerce-discount-web/src/main/java/com/liferay/commerce/discount/web/internal/custom/attributes/CommerceDiscountCustomAttributesDisplay.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.discount.web.internal.custom.attributes;
+
+import com.liferay.commerce.discount.constants.CommerceDiscountPortletKeys;
+import com.liferay.commerce.discount.model.CommerceDiscount;
+import com.liferay.expando.kernel.model.BaseCustomAttributesDisplay;
+import com.liferay.expando.kernel.model.CustomAttributesDisplay;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Ethan Bustad
+ */
+@Component(
+	immediate = true,
+	property = "javax.portlet.name=" + CommerceDiscountPortletKeys.COMMERCE_DISCOUNT,
+	service = CustomAttributesDisplay.class
+)
+public class CommerceDiscountCustomAttributesDisplay
+	extends BaseCustomAttributesDisplay {
+
+	@Override
+	public String getClassName() {
+		return CommerceDiscount.class.getName();
+	}
+
+}
