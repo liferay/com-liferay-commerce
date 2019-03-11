51eb796b9f24dcc9e432e3dc353c04e512b80b8e
Hugo Huijser <hugo.huijser@liferay.com>
2019-03-11 10:24:32 +0800

    LPS-91420 Avoid chaining

--- commerce-user-segment-test/src/testIntegration/java/com/liferay/commerce/user/segment/internal/search/test/CommerceUserSegmentEntryIndexerTest.java
+++ commerce-user-segment-test/src/testIntegration/java/com/liferay/commerce/user/segment/internal/search/test/CommerceUserSegmentEntryIndexerTest.java
@@ -94,11 +94,9 @@ public class CommerceUserSegmentEntryIndexerTest {
 
 		String name = document.get(Field.NAME);
 
-		String cleanName = name.replace(
-			"[", ""
-		).replace(
-			"]", ""
-		);
+		String cleanName = name.replace("[", "");
+
+		cleanName = cleanName.replace("]", "");
 
 		Assert.assertEquals(
 			commerceUserSegmentEntry.getName(Locale.ENGLISH), cleanName);
