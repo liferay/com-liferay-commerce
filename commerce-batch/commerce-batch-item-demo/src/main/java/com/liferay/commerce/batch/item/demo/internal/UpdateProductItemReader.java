7f679142497587097c32411fe732b5815e5c9b19
Ivica Cardic <ivica.cardic@liferay.com>
2019-03-12 10:40:32 +0100

    COMMERCE-1084 item demo module - Just for demoing purpose (drop this commit if not it's not needed)

---
+++ commerce-batch/commerce-batch-item-demo/src/main/java/com/liferay/commerce/batch/item/demo/internal/UpdateProductItemReader.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.batch.item.demo.internal;
+
+import com.liferay.commerce.batch.engine.api.item.BaseJSONItemReader;
+import com.liferay.commerce.batch.engine.api.item.ItemReader;
+import com.liferay.commerce.batch.item.demo.internal.model.ProductDTO;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+
+/**
+ * @author Ivica Cardic
+ */
+@Component(
+	property = {
+		"contentType=json", "operation=update", "type=product", "version=v1.0"
+	},
+	scope = ServiceScope.PROTOTYPE, service = ItemReader.class
+)
+public class UpdateProductItemReader extends BaseJSONItemReader<ProductDTO> {
+
+	@Override
+	public Class<ProductDTO> getType() {
+		return ProductDTO.class;
+	}
+
+}
---
+++ commerce-batch/commerce-batch-item-demo/src/main/java/com/liferay/commerce/batch/item/demo/internal/UpdateProductItemWriter.java
@@ -0,0 +1,39 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.batch.item.demo.internal;
+
+import com.liferay.commerce.batch.engine.api.item.ItemWriter;
+import com.liferay.commerce.batch.item.demo.internal.model.ProductDTO;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+
+/**
+ * @author Ivica Cardic
+ */
+@Component(
+	property = {"operation=update", "type=product", "version=v1.0"},
+	scope = ServiceScope.PROTOTYPE, service = ItemWriter.class
+)
+public class UpdateProductItemWriter implements ItemWriter<ProductDTO> {
+
+	@Override
+	public void write(List<? extends ProductDTO> items) throws Exception {
+		System.out.println(items);
+	}
+
+}
