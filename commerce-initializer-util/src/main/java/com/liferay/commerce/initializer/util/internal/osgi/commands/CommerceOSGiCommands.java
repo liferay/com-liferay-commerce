c0228e0bdb032b4d7fb3b107dd52b45200d57dea
Alessio Rendina <asilcomando@gmail.com>
2019-03-15 18:39:08 +0100

    COMMERCE-1171 Added osgi command to generate orders and shipments

---
+++ commerce-initializer-util/src/main/java/com/liferay/commerce/initializer/util/internal/osgi/commands/CommerceOSGiCommands.java
@@ -0,0 +1,118 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.initializer.util.internal.osgi.commands;
+
+import com.liferay.commerce.initializer.util.CommerceOrderGenerator;
+import com.liferay.commerce.initializer.util.CommerceShipmentGenerator;
+import com.liferay.commerce.product.util.CPRulesThreadLocal;
+import com.liferay.portal.kernel.model.Company;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Role;
+import com.liferay.portal.kernel.model.RoleConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.auth.PrincipalThreadLocal;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionCheckerFactory;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.site.initializer.SiteInitializer;
+import com.liferay.site.initializer.SiteInitializerRegistry;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	immediate = true,
+	property = {
+		"osgi.command.function=generateOrders",
+		"osgi.command.function=generateShipments",
+		"osgi.command.function=initializeSite", "osgi.command.scope=commerce"
+	},
+	service = CommerceOSGiCommands.class
+)
+public class CommerceOSGiCommands {
+
+	public void generateOrders(long groupId, int ordersCount) {
+		_commerceOrderGenerator.generate(groupId, ordersCount);
+	}
+
+	public void generateShipments(long groupId, int shipmentsCount)
+		throws Exception {
+
+		_commerceShipmentGenerator.generate(groupId, shipmentsCount);
+	}
+
+	public void initializeSite(long groupId, String key) throws Exception {
+		Group group = _groupLocalService.getGroup(groupId);
+
+		Company company = _companyLocalService.getCompanyById(
+			group.getCompanyId());
+
+		Role role = _roleLocalService.fetchRole(
+			company.getCompanyId(), RoleConstants.ADMINISTRATOR);
+
+		List<User> roleUsers = _userLocalService.getRoleUsers(role.getRoleId());
+
+		User user = roleUsers.get(0);
+
+		CPRulesThreadLocal.setCPRules(new ArrayList<>());
+
+		PermissionChecker permissionChecker = _permissionCheckerFactory.create(
+			user);
+
+		PrincipalThreadLocal.setName(user.getUserId());
+
+		PermissionThreadLocal.setPermissionChecker(permissionChecker);
+
+		SiteInitializer siteInitializer =
+			_siteInitializerRegistry.getSiteInitializer(key);
+
+		siteInitializer.initialize(groupId);
+	}
+
+	@Reference
+	private CommerceOrderGenerator _commerceOrderGenerator;
+
+	@Reference
+	private CommerceShipmentGenerator _commerceShipmentGenerator;
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private PermissionCheckerFactory _permissionCheckerFactory;
+
+	@Reference
+	private RoleLocalService _roleLocalService;
+
+	@Reference
+	private SiteInitializerRegistry _siteInitializerRegistry;
+
+	@Reference
+	private UserLocalService _userLocalService;
+
+}
