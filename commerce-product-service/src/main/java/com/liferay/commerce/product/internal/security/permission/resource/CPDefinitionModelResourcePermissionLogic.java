71ccd88730d1c2e52cbccee3ca689862abd97f1f
Ethan Bustad <ethan.bustad@liferay.com>
2019-03-20 16:09:58 -0700

    COMMERCE-1183 Apply CPRule configuration to permission check

--- commerce-product-service/src/main/java/com/liferay/commerce/product/internal/security/permission/resource/CPDefinitionModelResourcePermissionLogic.java
+++ commerce-product-service/src/main/java/com/liferay/commerce/product/internal/security/permission/resource/CPDefinitionModelResourcePermissionLogic.java
@@ -16,15 +16,19 @@ package com.liferay.commerce.product.internal.security.permission.resource;
 
 import com.liferay.commerce.product.catalog.rule.CPRuleType;
 import com.liferay.commerce.product.catalog.rule.CPRuleTypeRegistry;
+import com.liferay.commerce.product.configuration.CPRuleGroupServiceConfiguration;
 import com.liferay.commerce.product.constants.CPActionKeys;
+import com.liferay.commerce.product.constants.CPRuleConstants;
 import com.liferay.commerce.product.model.CPDefinition;
 import com.liferay.commerce.product.model.CPRule;
 import com.liferay.commerce.product.util.CPRulesThreadLocal;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.PermissionChecker;
 import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionLogic;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.settings.GroupServiceSettingsLocator;
 import com.liferay.portal.kernel.util.ListUtil;
 
 import java.util.List;
@@ -32,16 +36,19 @@ import java.util.List;
 /**
  * @author Alessio Antonio Rendina
  * @author Andrea Di Giorgi
+ * @author Ethan Bustad
  */
 public class CPDefinitionModelResourcePermissionLogic
 	implements ModelResourcePermissionLogic<CPDefinition> {
 
 	public CPDefinitionModelResourcePermissionLogic(
 		CPRuleTypeRegistry cpRuleTypeRegistry,
-		PortletResourcePermission portletResourcePermission) {
+		PortletResourcePermission portletResourcePermission,
+		ConfigurationProvider configurationProvider) {
 
 		_cpRuleTypeRegistry = cpRuleTypeRegistry;
 		_portletResourcePermission = portletResourcePermission;
+		_configurationProvider = configurationProvider;
 	}
 
 	@Override
@@ -71,18 +78,39 @@ public class CPDefinitionModelResourcePermissionLogic
 			return false;
 		}
 
+		boolean matchAll = true;
+
+		CPRuleGroupServiceConfiguration cpRuleGroupServiceConfiguration =
+			_configurationProvider.getConfiguration(
+				CPRuleGroupServiceConfiguration.class,
+				new GroupServiceSettingsLocator(
+					cpDefinition.getGroupId(), CPRuleConstants.SERVICE_NAME));
+
+		if ((cpRuleGroupServiceConfiguration != null) &&
+			(cpRuleGroupServiceConfiguration.catalogRuleApplicationType() ==
+				CPRuleConstants.APPLICATION_TYPE_ANY)) {
+
+			matchAll = false;
+		}
+
 		for (CPRule cpRule : cpRules) {
 			CPRuleType cpRuleType = _cpRuleTypeRegistry.getCPRuleType(
 				cpRule.getType());
 
-			if (!cpRuleType.isSatisfied(cpDefinition, cpRule)) {
+			boolean satisfied = cpRuleType.isSatisfied(cpDefinition, cpRule);
+
+			if (!satisfied && matchAll) {
 				return false;
+			}
+			else if (satisfied && !matchAll) {
+				return true;
 			}
 		}
 
-		return true;
+		return matchAll;
 	}
 
+	private final ConfigurationProvider _configurationProvider;
 	private final CPRuleTypeRegistry _cpRuleTypeRegistry;
 	private final PortletResourcePermission _portletResourcePermission;
 
