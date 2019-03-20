88381abba300e96cd432cca083b360ebf7978c23
Ethan Bustad <ethan.bustad@liferay.com>
2019-03-20 15:33:36 -0700

    COMMERCE-1183 Add configuration for cp rule application type

---
+++ commerce-admin-web/src/main/java/com/liferay/commerce/admin/web/internal/portlet/EditCPRuleSettingsMVCActionCommand.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.admin.web.internal.portlet;
+
+import com.liferay.commerce.admin.constants.CommerceAdminPortletKeys;
+import com.liferay.commerce.product.constants.CPRuleConstants;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCActionCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCActionCommand;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.servlet.SessionErrors;
+import com.liferay.portal.kernel.settings.GroupServiceSettingsLocator;
+import com.liferay.portal.kernel.settings.ModifiableSettings;
+import com.liferay.portal.kernel.settings.Settings;
+import com.liferay.portal.kernel.settings.SettingsFactory;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.PropertiesParamUtil;
+
+import java.util.Map;
+
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Ethan Bustad
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + CommerceAdminPortletKeys.COMMERCE_ADMIN,
+		"mvc.command.name=editCPRuleSettings"
+	},
+	service = MVCActionCommand.class
+)
+public class EditCPRuleSettingsMVCActionCommand extends BaseMVCActionCommand {
+
+	@Override
+	protected void doProcessAction(
+			ActionRequest actionRequest, ActionResponse actionResponse)
+		throws Exception {
+
+		try {
+			Map<String, String> parameterMap =
+				PropertiesParamUtil.getProperties(actionRequest, "settings--");
+
+			Settings settings = _settingsFactory.getSettings(
+				new GroupServiceSettingsLocator(
+					_portal.getScopeGroupId(actionRequest),
+					CPRuleConstants.SERVICE_NAME));
+
+			ModifiableSettings modifiableSettings =
+				settings.getModifiableSettings();
+
+			for (Map.Entry<String, String> entry : parameterMap.entrySet()) {
+				modifiableSettings.setValue(entry.getKey(), entry.getValue());
+			}
+
+			modifiableSettings.store();
+		}
+		catch (PrincipalException pe) {
+			hideDefaultErrorMessage(actionRequest);
+			hideDefaultSuccessMessage(actionRequest);
+
+			SessionErrors.add(actionRequest, pe.getClass());
+
+			String redirect = ParamUtil.getString(actionRequest, "redirect");
+
+			sendRedirect(actionRequest, actionResponse, redirect);
+		}
+	}
+
+	@Reference
+	private Portal _portal;
+
+	@Reference
+	private SettingsFactory _settingsFactory;
+
+}
