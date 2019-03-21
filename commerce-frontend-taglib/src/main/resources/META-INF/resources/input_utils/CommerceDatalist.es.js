f7d295e68c87c550bbed07328d3eb3878ddb7980
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-03-21 18:43:08 +0100

    COMMERCE-1181 multiselect on datalist styled and working

---
+++ commerce-frontend-taglib/src/main/resources/META-INF/resources/input_utils/CommerceDatalist.es.js
@@ -0,0 +1,276 @@
+'use strict';
+
+import template from './CommerceDatalist.soy';
+import Component from 'metal-component';
+import Soy, {Config} from 'metal-soy';
+import {debounce} from 'metal-debounce';
+
+import '../autocomplete_item/AutocompleteItem.es';
+import 'clay-icon';
+
+const fakeData = [
+    {
+        value: 'mercedes',
+        label: 'Mercedes'
+    },
+    {
+        value: 'audi',
+        label: 'Audi'
+    },
+    {
+        value: 'ferrari',
+        label: 'Ferrari'
+    },
+    {
+        value: 'alfa-romeo',
+        label: 'Alfa Romeo'
+    },
+    {
+        value: 'renault',
+        label: 'Renault'
+    }
+]
+
+class CommerceDatalist extends Component {
+    
+	attached() {
+        if(this.queryUrl) {
+            this._getDataFromApi = debounce(
+                this._getDataFromApi.bind(this),
+                this.apiDebounceTime
+            );
+        }
+        this._handleClickOutside = this._handleClickOutside.bind(this);
+	}
+
+    openDropdown(){
+		this.dropdownVisibilityState = 'expanding';
+
+		return setTimeout(
+			() => {
+				this.dropdownVisibilityState = 'expanded';
+				window.addEventListener('click', this._handleClickOutside);
+			},
+			100
+		);
+    }
+
+    closeDropdown(){
+		this.dropdownVisibilityState = 'collapsing';
+
+		return setTimeout(
+			() => {
+				this.dropdownVisibilityState = 'collapsed';
+				window.removeEventListener('click', this._handleClickOutside);
+			},
+			100
+		);
+    }
+
+    willReceiveState() {
+        this.data = fakeData;
+    }
+
+	_handleClickOutside(e) {
+		if ( !this.element.contains(e.target) ) {
+            this.closeDropdown();
+        }
+        return false
+	}
+
+    _handleInputWrapperClick(e){
+        if(this.dropdownVisibilityState === 'collapsed') {
+            this.openDropdown();
+        }
+    }
+
+    _handleIconClick(e) {
+        e.preventDefault();
+        return this.toggleDropdown();
+    }
+
+    toggleDropdown() {
+        return this.dropdownVisibilityState === 'collapsed' 
+            ? this.openDropdown()
+            : this.closeDropdown()
+    }
+
+    _handleDeleteElement(e) {
+        const elementValue = this._getDatasetValue(e.target);
+        const element = this._getDataElementFromValue(elementValue);
+        return element && this.removeElement(element);
+    }
+
+    _handleSelectData(e) {
+        const elementValue = this._getDatasetValue(e.target);
+        const element = this._getDataElementFromValue(elementValue);
+        if (this.multiSelect) {
+            return element && e.target.checked ? this.addElement(element) : this.removeElement(element);
+        } else {
+            return element && this.addElement(element);
+        }
+    }
+
+    _getDatasetValue(target) {
+        return target.closest('[data-value]').dataset.value
+    }
+
+    _handleInputKeyup(e) {
+        if(e.keyCode === 13) {
+            return this.emit('submitForm');
+        }
+        if (e.keyCode === 8 && !this.query.length && this.multiSelect && this.value.length) {
+			this.value = this.value.slice(0, -1);
+		}
+        if(e.target.value !== this.query) {
+            this.query = e.target.value
+        }
+        if(e.selectionStart === 0 && e.keyCode == 37) {
+            this.refs.scrollTo(500, 0);
+        }
+    }
+
+    _filterDataByQuery(query) {
+        if(!query) {
+            this._filteredValues = null;
+        }
+        if(query && this.data) {
+            this._filteredValues = this.data
+                .filter(
+                    el => el.label.toLowerCase().indexOf(query.toLowerCase()) > -1
+                ).map(
+                    el => el.value
+                );
+        }
+        return this._filteredValues;
+    }
+
+    _getDataFromApi(query) {
+        return fetch(
+            this.queryUrl + '/' + query,
+            {
+                type: 'GET'
+            }
+        )
+        .then(response => response.json())
+        .then(
+            jsonResponse => {
+                console.log(jsonResponse)
+                //this.data = jsonResponse.data
+            }
+        )
+    }
+
+    _getDataElementFromValue(value) {
+        const element = this.data.reduce(
+            (elementFound, el) => elementFound || el.value == value && el,
+            false
+        )
+        return element;
+    }
+
+    syncValue(value) {
+        return this.emit(
+            'valueUpdated',
+            {
+                name: this.selectName,
+                value
+            }
+        );
+    }
+
+    syncData(data) {
+        this.emit(
+            'dataUpdated',
+            {
+                name: this.selectName,
+                data
+            }
+        )
+    }
+
+    syncQuery(query) {
+        if(this.queryUrl) {
+            this._getDataFromApi(query)
+        } else {
+            this._filterDataByQuery(query)
+        }
+    }
+
+    addElement(element) {
+        if(this.multiSelect) {
+            this.value = !!this.value 
+                ? [ ...this.value, element ] 
+                : [ element ];
+            this.refs.inputText.focus();
+            this.refs.queryWrapper.scrollTo(this.refs.queryWrapper.scrollWidth, 0);
+        } else {
+            this.value = element;
+            this.closeDropdown();
+        }
+        return this.value;
+    }
+
+    removeElement(element) {
+        this.value = this.multiSelect 
+            ? this.value.filter(
+                (el) => el.value !== element.value
+            )
+            : null
+    }
+}
+
+Soy.register(CommerceDatalist, template);
+
+const elementMock = Config.shapeOf(
+    {
+        label: Config.oneOfType(
+            [
+                Config.string(),
+                Config.number()
+            ]
+        ),
+        value: Config.oneOfType(
+            [
+                Config.string(),
+                Config.number()
+            ]
+        )
+    }
+);
+
+CommerceDatalist.STATE = {
+    _filteredValues: Config.array(
+        Config.string()
+    ).internal(),
+    apiDebounceTime: Config.number().value(200),
+    data: Config.array(
+        elementMock
+    ),
+    dropdownVisibilityState: Config.oneOf(
+        [
+            'expanded',
+            'expanding',
+            'collapsing',
+            'collapsed'
+        ]
+    ).value('collapsed'),
+    formId: Config.string(),
+    query: Config.string().value(''),
+	label: Config.string(),
+    multiSelect: Config.bool(),
+    value: Config.oneOfType(
+        [
+            elementMock,
+            Config.array(
+                elementMock
+            )
+        ]
+    ),
+    selectName: Config.string().required(),
+    spritemap: Config.string().required(),
+    queryUrl: Config.string()
+};
+
+export {CommerceDatalist};
+export default CommerceDatalist;
