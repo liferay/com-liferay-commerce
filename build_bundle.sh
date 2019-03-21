486bb5f5a64d0c133fe84d232175ef818891abb3
Ethan Bustad <ethan.bustad@liferay.com>
2019-03-21 14:47:30 -0700

    EMP-18 Fail if JVM opts aren't reset to prevent insufficient memory

--- build_bundle.sh
+++ build_bundle.sh
@@ -25,8 +25,17 @@ function fix_tomcat_setenv {
 	# This is only for 7.1.10.
 	#
 
-	sed -i "s/-Xmx1024m -XX:MaxMetaspaceSize=512m/-Xms2560m -Xmx2560m -XX:MaxNewSize=1536m -XX:MaxMetaspaceSize=512m -XX:MetaspaceSize=512m -XX:NewSize=1536m -XX:SurvivorRatio=7/" ${1}/bin/setenv.bat
-	sed -i "s/-Xmx1024m -XX:MaxMetaspaceSize=512m/-Xms2560m -Xmx2560m -XX:MaxNewSize=1536m -XX:MaxMetaspaceSize=512m -XX:MetaspaceSize=512m -XX:NewSize=1536m -XX:SurvivorRatio=7/" ${1}/bin/setenv.sh
+	jvm_opts="-Xms2560m -Xmx2560m -XX:MaxNewSize=1536m -XX:MaxMetaspaceSize=512m -XX:MetaspaceSize=512m -XX:NewSize=1536m -XX:SurvivorRatio=7"
+
+	sed -i "s/-Xmx1024m -XX:MaxMetaspaceSize=512m/$jvm_opts/" ${1}/bin/setenv.bat
+	sed -i "s/-Xmx1024m -XX:MaxMetaspaceSize=512m/$jvm_opts/" ${1}/bin/setenv.sh
+
+	if $(! grep -q -e "$jvm_opts" ${1}/bin/setenv.bat) || $(! grep -q -e "$jvm_opts" ${1}/bin/setenv.sh)
+	then
+		echo "Unable to set JVM options."
+
+		exit 1
+	fi
 }
 
 function get_tomcat_version {
