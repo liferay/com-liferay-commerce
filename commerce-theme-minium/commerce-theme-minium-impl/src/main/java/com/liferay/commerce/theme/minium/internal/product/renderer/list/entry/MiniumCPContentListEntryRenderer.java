487f1e6f9afb50874107e2916abf2aa2716ef2f9
Marco Leo <marco.leo@liferay.com>
2019-03-22 23:27:30 +0100

    COMMERCE-981 Update version of taglib used

--- commerce-theme-minium/commerce-theme-minium-impl/src/main/java/com/liferay/commerce/theme/minium/internal/product/renderer/list/entry/MiniumCPContentListEntryRenderer.java
+++ commerce-theme-minium/commerce-theme-minium-impl/src/main/java/com/liferay/commerce/theme/minium/internal/product/renderer/list/entry/MiniumCPContentListEntryRenderer.java
@@ -201,11 +201,11 @@ public class MiniumCPContentListEntryRenderer
 		Set<String> dependencies = new HashSet<>();
 
 		dependencies.add(
-			"commerce-frontend-taglib@1.1.3/add_to_cart/AddToCartButton.es");
+			"commerce-frontend-taglib@2.0.0/add_to_cart/AddToCartButton.es");
 		dependencies.add(
-			"commerce-frontend-taglib@1.1.3/compare_checkbox" +
+			"commerce-frontend-taglib@2.0.0/compare_checkbox" +
 				"/CompareCheckbox.es");
-		dependencies.add("commerce-frontend-taglib@1.1.3/price/Price.es");
+		dependencies.add("commerce-frontend-taglib@2.0.0/price/Price.es");
 
 		ComponentDescriptor componentDescriptor = new ComponentDescriptor(
 			"ProductCard.render", moduleName, null, dependencies);
