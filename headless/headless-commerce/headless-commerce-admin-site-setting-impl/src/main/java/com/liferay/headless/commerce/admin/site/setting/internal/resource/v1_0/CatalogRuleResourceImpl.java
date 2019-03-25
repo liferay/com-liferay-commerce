87fb54d5ea9ef5d828e4f2d085166630380aa69d
Zoltán Takács <zoltan.takacs@liferay.com>
2019-03-25 16:12:47 +0100

    COMMERCE-1152 Migrated CatalogRule API

--- headless/headless-commerce/headless-commerce-admin-site-setting-impl/src/main/java/com/liferay/headless/commerce/admin/site/setting/internal/resource/v1_0/CatalogRuleResourceImpl.java
+++ headless/headless-commerce/headless-commerce-admin-site-setting-impl/src/main/java/com/liferay/headless/commerce/admin/site/setting/internal/resource/v1_0/CatalogRuleResourceImpl.java
@@ -17,15 +17,19 @@ package com.liferay.headless.commerce.admin.site.setting.internal.resource.v1_0;
 import com.liferay.headless.commerce.admin.site.setting.dto.v1_0.CatalogRule;
 import com.liferay.headless.commerce.admin.site.setting.dto.v1_0.Category;
 import com.liferay.headless.commerce.admin.site.setting.dto.v1_0.UserSegment;
+import com.liferay.headless.commerce.admin.site.setting.internal.util.v1_0.CatalogRuleHelper;
 import com.liferay.headless.commerce.admin.site.setting.resource.v1_0.CatalogRuleResource;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.vulcan.pagination.Page;
 import com.liferay.portal.vulcan.pagination.Pagination;
 
 import javax.validation.constraints.NotNull;
 
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ServiceScope;
 
 /**
@@ -39,12 +43,16 @@ public class CatalogRuleResourceImpl extends BaseCatalogRuleResourceImpl {
 
 	@Override
 	public Response deleteCatalogRule(@NotNull Long id) throws Exception {
-		return super.deleteCatalogRule(id);
+		_catalogRuleHelper.deleteCatalogRule(id);
+
+		Response.ResponseBuilder responseBuilder = Response.noContent();
+
+		return responseBuilder.build();
 	}
 
 	@Override
 	public CatalogRule getCatalogRule(@NotNull Long id) throws Exception {
-		return super.getCatalogRule(id);
+		return _catalogRuleHelper.getCatalogRule(id);
 	}
 
 	@Override
@@ -52,13 +60,15 @@ public class CatalogRuleResourceImpl extends BaseCatalogRuleResourceImpl {
 			@NotNull Long id, Pagination pagination)
 		throws Exception {
 
-		return super.getCatalogRuleCategories(id, pagination);
+		return _catalogRuleHelper.getCategories(id, pagination);
 	}
 
 	@Override
 	public Page<CatalogRule> getCatalogRules(
 			@NotNull Long groupId, Pagination pagination)
 		throws Exception {
+
+		// TODO: Implement this
 
 		return super.getCatalogRules(groupId, pagination);
 	}
@@ -68,14 +78,18 @@ public class CatalogRuleResourceImpl extends BaseCatalogRuleResourceImpl {
 			@NotNull Long id, Pagination pagination)
 		throws Exception {
 
-		return super.getCatalogRuleUserSegments(id, pagination);
+		return _catalogRuleHelper.getUserSegments(id, pagination);
 	}
 
 	@Override
 	public Response updateCatalogRule(@NotNull Long id, CatalogRule catalogRule)
 		throws Exception {
 
-		return super.updateCatalogRule(id, catalogRule);
+		_catalogRuleHelper.updateCatalogRule(id, catalogRule, _user);
+
+		Response.ResponseBuilder responseBuilder = Response.accepted();
+
+		return responseBuilder.build();
 	}
 
 	@Override
@@ -83,7 +97,15 @@ public class CatalogRuleResourceImpl extends BaseCatalogRuleResourceImpl {
 			@NotNull Long groupId, CatalogRule catalogRule)
 		throws Exception {
 
+		// TODO: Implement this
+
 		return super.upsertCatalogRule(groupId, catalogRule);
 	}
+
+	@Reference
+	private CatalogRuleHelper _catalogRuleHelper;
+
+	@Context
+	private User _user;
 
 }
