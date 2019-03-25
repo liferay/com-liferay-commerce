01afca6782a8a0350f5aa5a29cb4aed1788f29fb
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2019-03-25 17:41:57 +0100

    COMMERCE-1210 commerce-initializer-util Update UsersImporter to support external id mapping

--- commerce-initializer-util/src/main/java/com/liferay/commerce/initializer/util/CommerceUsersImporter.java
+++ commerce-initializer-util/src/main/java/com/liferay/commerce/initializer/util/CommerceUsersImporter.java
@@ -14,6 +14,11 @@
 
 package com.liferay.commerce.initializer.util;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.core.TreeNode;
+import com.fasterxml.jackson.databind.MappingJsonFactory;
+
 import com.liferay.commerce.account.model.CommerceAccount;
 import com.liferay.commerce.account.model.CommerceAccountUserRel;
 import com.liferay.commerce.account.service.CommerceAccountLocalService;
@@ -22,8 +27,11 @@ import com.liferay.commerce.account.service.persistence.CommerceAccountUserRelPK
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Organization;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.User;
@@ -31,16 +39,25 @@ import com.liferay.portal.kernel.service.OrganizationLocalService;
 import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserGroupRoleLocalService;
+import com.liferay.portal.kernel.service.UserIdMapperLocalService;
 import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.CalendarFactoryUtil;
 import com.liferay.portal.kernel.util.FileUtil;
+import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.StringUtil;
 import com.liferay.portal.kernel.util.Validator;
 
-import java.io.FileNotFoundException;
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+
+import java.net.URI;
+import java.net.URLEncoder;
 
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -56,6 +73,49 @@ import org.osgi.service.component.annotations.Reference;
  */
 @Component(service = CommerceUsersImporter.class)
 public class CommerceUsersImporter {
+
+	public void importCommerceUsers(
+			File commerceUsersFile, ClassLoader classLoader,
+			String imageDependenciesPath, long scopeGroupId, long userId)
+		throws Exception {
+
+		ServiceContext serviceContext = getServiceContext(scopeGroupId, userId);
+
+		MappingJsonFactory mappingJsonFactory = new MappingJsonFactory();
+
+		JsonParser jsonFactoryParser = mappingJsonFactory.createParser(
+			commerceUsersFile);
+
+		JsonToken jsonToken = jsonFactoryParser.nextToken();
+
+		if (jsonToken != JsonToken.START_ARRAY) {
+			throw new Exception("JSON Array Expected");
+		}
+
+		int importCount = 0;
+
+		while (jsonFactoryParser.nextToken() != JsonToken.END_ARRAY) {
+			TreeNode treeNode = jsonFactoryParser.readValueAsTree();
+
+			JSONObject jsonObject = JSONFactoryUtil.createJSONObject(
+				treeNode.toString());
+
+			if (_log.isDebugEnabled()) {
+				_log.debug(jsonObject);
+			}
+
+			_importCommerceUser(
+				jsonObject, classLoader, imageDependenciesPath, serviceContext);
+
+			importCount += 1;
+		}
+
+		if (_log.isInfoEnabled()) {
+			_log.info("Imported users count: " + importCount);
+		}
+
+		jsonFactoryParser.close();
+	}
 
 	public void importCommerceUsers(
 			JSONArray jsonArray, ClassLoader classLoader,
@@ -78,6 +138,20 @@ public class CommerceUsersImporter {
 		}
 	}
 
+	protected ServiceContext getServiceContext(long scopeGroupId, long userId)
+		throws PortalException {
+
+		User user = _userLocalService.getUser(userId);
+
+		ServiceContext serviceContext = new ServiceContext();
+
+		serviceContext.setScopeGroupId(scopeGroupId);
+		serviceContext.setUserId(userId);
+		serviceContext.setCompanyId(user.getCompanyId());
+
+		return serviceContext;
+	}
+
 	protected User upsertUser(
 			String password, String userReminderQueryQuestion,
 			String userReminderQueryAnswer, String screenName,
@@ -95,30 +169,62 @@ public class CommerceUsersImporter {
 		User user = _userLocalService.fetchUserByScreenName(
 			serviceContext.getCompanyId(), screenName);
 
-		if (user == null) {
-			user = _userLocalService.addUser(
-				serviceContext.getUserId(), serviceContext.getCompanyId(),
-				Validator.isNull(password), password, password, false,
-				screenName, emailAddress, facebookId, openId, locale, firstName,
-				middleName, lastName, prefixId, suffixId, male, birthdayMonth,
-				birthdayDay, birthdayYear, jobTitle,
-				new long[] {serviceContext.getScopeGroupId()}, organizationIds,
-				roleIds, userGroupIds, false, serviceContext);
+		if (user != null) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"User already exists with screen name: " + screenName);
+			}
+
+			return user;
 		}
 
-		user = _userLocalService.updateUser(
-			user.getUserId(), StringPool.BLANK, StringPool.BLANK,
-			StringPool.BLANK, false, userReminderQueryQuestion,
-			userReminderQueryAnswer, screenName, emailAddress, facebookId,
-			openId, portrait, portraitBytes, LocaleUtil.toLanguageId(locale),
-			timeZoneId, greeting, comments, firstName, middleName, lastName,
-			prefixId, suffixId, male, birthdayMonth, birthdayDay, birthdayYear,
-			smsSn, facebookSn, jabberSn, skypeSn, twitterSn, jobTitle, groupIds,
-			organizationIds, roleIds, null, userGroupIds, serviceContext);
+		user = _userLocalService.fetchUserByEmailAddress(
+			serviceContext.getCompanyId(), emailAddress);
+
+		if (user != null) {
+			if (_log.isWarnEnabled()) {
+				_log.warn(
+					"User already exists with email address: " + emailAddress);
+			}
+
+			return user;
+		}
+
+		long companyId = serviceContext.getCompanyId();
+
+		long creatorUserId = serviceContext.getUserId();
+
+		boolean autoPassword = Validator.isNull(password);
+
+		boolean autoScreenName = Validator.isNull(screenName);
+
+		user = _userLocalService.addUser(
+			creatorUserId, companyId, autoPassword, password, password,
+			autoScreenName, screenName, emailAddress, facebookId, openId,
+			locale, firstName, middleName, lastName, prefixId, suffixId, male,
+			birthdayMonth, birthdayDay, birthdayYear, jobTitle, groupIds,
+			organizationIds, roleIds, userGroupIds, false, serviceContext);
+
+		if (Validator.isNotNull(comments)) {
+			user = _userLocalService.updateUser(
+				user.getUserId(), StringPool.BLANK, StringPool.BLANK,
+				StringPool.BLANK, false, userReminderQueryQuestion,
+				userReminderQueryAnswer, screenName, emailAddress, facebookId,
+				openId, portrait, portraitBytes,
+				LocaleUtil.toLanguageId(locale), timeZoneId, greeting, comments,
+				firstName, middleName, lastName, prefixId, suffixId, male,
+				birthdayMonth, birthdayDay, birthdayYear, smsSn, facebookSn,
+				jabberSn, skypeSn, twitterSn, jobTitle, groupIds,
+				organizationIds, roleIds, null, userGroupIds, serviceContext);
+		}
+		else if (portrait) {
+			_userLocalService.updatePortrait(user.getUserId(), portraitBytes);
+		}
 
 		return user;
 	}
 
+	@SuppressFBWarnings("PATH_TRAVERSAL_IN")
 	private void _importCommerceUser(
 			JSONObject jsonObject, ClassLoader classLoader,
 			String dependenciesPath, ServiceContext serviceContext)
@@ -142,19 +248,35 @@ public class CommerceUsersImporter {
 		String portrait = jsonObject.getString("portrait");
 
 		if (!Validator.isBlank(portrait)) {
-			String filePath = dependenciesPath + "images/" + portrait;
+			InputStream inputStream = null;
 
-			try (InputStream inputStream = classLoader.getResourceAsStream(
-					filePath)) {
+			try {
+				String uriString =
+					dependenciesPath + URLEncoder.encode(portrait, "UTF-8");
 
-				if (inputStream == null) {
-					throw new FileNotFoundException(
-						"No file found at " + filePath);
+				URI uri = new URI(uriString);
+
+				String scheme = uri.getScheme();
+
+				if (StringUtil.equalsIgnoreCase(scheme, "file")) {
+					inputStream = new FileInputStream(uri.getPath());
+				}
+				else {
+					inputStream = classLoader.getResourceAsStream(
+						dependenciesPath + portrait);
 				}
 
-				hasPortrait = true;
-
 				portraitBytes = FileUtil.getBytes(inputStream);
+
+				hasPortrait = true;
+			}
+			catch (Exception e) {
+				_log.error(e, e);
+			}
+			finally {
+				if (inputStream != null) {
+					inputStream.close();
+				}
 			}
 		}
 
@@ -182,6 +304,12 @@ public class CommerceUsersImporter {
 		long suffixId = jsonObject.getLong("suffixId");
 		boolean male = jsonObject.getBoolean("male");
 
+		String gender = jsonObject.getString("gender");
+
+		if (Validator.isNotNull(gender)) {
+			male = "male".equals(gender);
+		}
+
 		Calendar calendar = CalendarFactoryUtil.getCalendar(timeZone);
 
 		int birthdayMonth = calendar.get(
@@ -190,6 +318,18 @@ public class CommerceUsersImporter {
 			jsonObject.getInt("birthdayDay", Calendar.DAY_OF_MONTH));
 		int birthdayYear = calendar.get(
 			jsonObject.getInt("birthdayYear", Calendar.YEAR));
+
+		String birthday = jsonObject.getString("birthday");
+
+		if (Validator.isNotNull(birthday)) {
+			String[] birthdayTokens = birthday.split("-");
+
+			birthdayMonth = GetterUtil.getInteger(birthdayTokens[1]) - 1;
+
+			birthdayDay = GetterUtil.getInteger(birthdayTokens[2]);
+
+			birthdayYear = GetterUtil.getInteger(birthdayTokens[0]);
+		}
 
 		String smsSn = jsonObject.getString("smsSn");
 		String facebookSn = jsonObject.getString("facebookSn");
@@ -263,6 +403,23 @@ public class CommerceUsersImporter {
 			}
 		}
 
+		// Add External ID mapping
+
+		String externalUserId = jsonObject.getString("externalUserId");
+
+		String externalSystemType = jsonObject.getString("externalSystemType");
+
+		String exteralSsystemDescription = jsonObject.getString(
+			"externalSystemDescription", "");
+
+		if (Validator.isNotNull(externalUserId) &&
+			Validator.isNotNull(externalSystemType)) {
+
+			_userIdMapperLocalService.updateUserIdMapper(
+				user.getUserId(), externalSystemType, exteralSsystemDescription,
+				externalUserId);
+		}
+
 		// Add User to Commerce Accounts and Account Roles
 
 		JSONArray accountsJSONArray = jsonObject.getJSONArray("accounts");
@@ -320,6 +477,9 @@ public class CommerceUsersImporter {
 		}
 	}
 
+	private static final Log _log = LogFactoryUtil.getLog(
+		CommerceUsersImporter.class);
+
 	@Reference
 	private CommerceAccountLocalService _commerceAccountLocalService;
 
@@ -335,6 +495,9 @@ public class CommerceUsersImporter {
 
 	@Reference
 	private UserGroupRoleLocalService _userGroupRoleLocalService;
+
+	@Reference
+	private UserIdMapperLocalService _userIdMapperLocalService;
 
 	@Reference
 	private UserLocalService _userLocalService;
