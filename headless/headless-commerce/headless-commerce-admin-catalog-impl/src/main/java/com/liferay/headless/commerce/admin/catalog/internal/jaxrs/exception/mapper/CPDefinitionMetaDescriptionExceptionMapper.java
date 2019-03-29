fc55356c7a0fe0a7124f12036a7191a8978aeda6
Zoltán Takács <zoltan.takacs@liferay.com>
2019-03-29 14:55:45 +0100

    COMMERCE-1152 Migrate Exception Mappers for Catalog APIs

---
+++ headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/jaxrs/exception/mapper/CPDefinitionMetaDescriptionExceptionMapper.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.commerce.admin.catalog.internal.jaxrs.exception.mapper;
+
+import com.liferay.commerce.product.exception.CPDefinitionMetaDescriptionException;
+import com.liferay.headless.commerce.core.exception.mapper.BaseExceptionMapper;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Alessio Antonio Rendina
+ * @author Zoltán Takács
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Commerce.Admin.Catalog)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Commerce.Admin.Catalog.CPDefinitionMetaDescriptionException"
+	},
+	service = ExceptionMapper.class
+)
+@Provider
+public class CPDefinitionMetaDescriptionExceptionMapper
+	extends BaseExceptionMapper<CPDefinitionMetaDescriptionException> {
+
+	@Override
+	public String getErrorDescription() {
+		return "Invalid product meta description.";
+	}
+
+	@Override
+	public Response.Status getStatus() {
+		return Response.Status.BAD_REQUEST;
+	}
+
+}
