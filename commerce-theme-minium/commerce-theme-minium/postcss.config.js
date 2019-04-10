f9880f2f28fb9cbee4c33b7710c9d3e523d89894
Ethan Bustad <ethan.bustad@liferay.com>
2019-04-10 12:08:44 -0700

    COMMERCE-686 Auto SF

--- commerce-theme-minium/commerce-theme-minium/postcss.config.js
+++ commerce-theme-minium/commerce-theme-minium/postcss.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  plugins: {
-    'autoprefixer': {
-      grid: true,
-      browsers: [
-        "last 2 version",
-        "> 1%",
-        "IE 11"
-      ]
-    }
-  }
+plugins: {
+	'autoprefixer': {
+	  grid: true,
+	  browsers: [
+		"last 2 version",
+		"> 1%",
+		"IE 11"
+	  ]
+	}
+}
 }
