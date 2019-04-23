0467e8304d5357c6e741bdeb36d4e8e808925fa0
Alessio Rendina <asilcomando@gmail.com>
2019-04-23 16:44:48 +0200

    COMMERCE-1234 Added dto converter

---
+++ headless/headless-commerce/headless-commerce-admin-catalog-api/src/main/java/com/liferay/headless/commerce/admin/catalog/dto/v1_0/converter/DefaultDTOConverterContext.java
@@ -0,0 +1,59 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.commerce.admin.catalog.dto.v1_0.converter;
+
+import java.util.Locale;
+import java.util.Optional;
+
+import javax.ws.rs.core.UriInfo;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class DefaultDTOConverterContext implements DTOConverterContext {
+
+	public DefaultDTOConverterContext(Locale locale, long resourcePrimKey) {
+		_locale = locale;
+		_resourcePrimKey = resourcePrimKey;
+	}
+
+	public DefaultDTOConverterContext(
+		Locale locale, long resourcePrimKey, UriInfo uriInfo) {
+
+		_locale = locale;
+		_resourcePrimKey = resourcePrimKey;
+		_uriInfo = uriInfo;
+	}
+
+	@Override
+	public Locale getLocale() {
+		return _locale;
+	}
+
+	@Override
+	public long getResourcePrimKey() {
+		return _resourcePrimKey;
+	}
+
+	@Override
+	public Optional<UriInfo> getUriInfoOptional() {
+		return Optional.ofNullable(_uriInfo);
+	}
+
+	private final Locale _locale;
+	private final long _resourcePrimKey;
+	private UriInfo _uriInfo;
+
+}
