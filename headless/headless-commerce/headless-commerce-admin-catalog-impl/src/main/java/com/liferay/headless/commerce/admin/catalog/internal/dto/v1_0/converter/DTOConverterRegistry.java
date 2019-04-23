87577c442499df68fcb7044accf275bfdb726c53
Alessio Rendina <asilcomando@gmail.com>
2019-04-23 16:45:11 +0200

    COMMERCE-1234 Added dto converter registry

---
+++ headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/dto/v1_0/converter/DTOConverterRegistry.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.commerce.admin.catalog.internal.dto.v1_0.converter;
+
+import com.liferay.headless.commerce.admin.catalog.dto.v1_0.converter.DTOConverter;
+import com.liferay.osgi.service.tracker.collections.map.ServiceReferenceMapper;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMap;
+import com.liferay.osgi.service.tracker.collections.map.ServiceTrackerMapFactory;
+
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Deactivate;
+
+/**
+ * @author Alessio Pulido
+ */
+@Component(service = DTOConverterRegistry.class)
+public class DTOConverterRegistry {
+
+	public DTOConverter getDTOConverter(String modelClassName) {
+		return _serviceTrackerMap.getService(modelClassName);
+	}
+
+	@Activate
+	protected void activate(BundleContext bundleContext) {
+		_serviceTrackerMap = ServiceTrackerMapFactory.openSingleValueMap(
+			bundleContext, DTOConverter.class, "(model.class.name=*)",
+			new DTOConverterServiceReferenceMapper());
+	}
+
+	@Deactivate
+	protected void deactivate() {
+		_serviceTrackerMap.close();
+	}
+
+	private ServiceTrackerMap<String, DTOConverter> _serviceTrackerMap;
+
+	private static class DTOConverterServiceReferenceMapper
+		implements ServiceReferenceMapper<String, DTOConverter> {
+
+		@Override
+		public void map(
+			ServiceReference<DTOConverter> serviceReference,
+			Emitter<String> emitter) {
+
+			String assetEntryClassName = (String)serviceReference.getProperty(
+				"model.class.name");
+
+			emitter.emit(assetEntryClassName);
+		}
+
+	}
+
+}
