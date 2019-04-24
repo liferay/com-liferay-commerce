3b563e36ca08723f72adae4a3997a559c052612c
Marco Leo <marco.leo@liferay.com>
2019-04-24 22:35:37 +0200

    COMMERCE-1338 Update discount service definition to use account groups

---
+++ commerce-discount-service/src/main/java/com/liferay/commerce/discount/service/impl/CommerceDiscountCommerceAccountGroupRelServiceImpl.java
@@ -0,0 +1,93 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.discount.service.impl;
+
+import com.liferay.commerce.discount.model.CommerceDiscount;
+import com.liferay.commerce.discount.model.CommerceDiscountCommerceAccountGroupRel;
+import com.liferay.commerce.discount.service.base.CommerceDiscountCommerceAccountGroupRelServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermissionFactory;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
+
+/**
+ * @author Marco Leo
+ */
+public class CommerceDiscountCommerceAccountGroupRelServiceImpl
+	extends CommerceDiscountCommerceAccountGroupRelServiceBaseImpl {
+
+	@Override
+	public CommerceDiscountCommerceAccountGroupRel
+			addCommerceDiscountCommerceAccountGroupRel(
+				long commerceDiscountId, long commerceAccountGroupId,
+				ServiceContext serviceContext)
+		throws PortalException {
+
+		_commerceDiscountResourcePermission.check(
+			getPermissionChecker(), commerceDiscountId, ActionKeys.UPDATE);
+
+		return commerceDiscountCommerceAccountGroupRelLocalService.
+			addCommerceDiscountCommerceAccountGroupRel(
+				commerceDiscountId, commerceAccountGroupId, serviceContext);
+	}
+
+	@Override
+	public void deleteCommerceDiscountCommerceAccountGroupRel(
+			long commerceDiscountCommerceAccountGroupRelId)
+		throws PortalException {
+
+		CommerceDiscountCommerceAccountGroupRel
+			commerceDiscountCommerceAccountGroupRel =
+				commerceDiscountCommerceAccountGroupRelLocalService.
+					getCommerceDiscountCommerceAccountGroupRel(
+						commerceDiscountCommerceAccountGroupRelId);
+
+		_commerceDiscountResourcePermission.check(
+			getPermissionChecker(),
+			commerceDiscountCommerceAccountGroupRel.getCommerceDiscountId(),
+			ActionKeys.UPDATE);
+
+		commerceDiscountCommerceAccountGroupRelLocalService.
+			deleteCommerceDiscountCommerceAccountGroupRel(
+				commerceDiscountCommerceAccountGroupRel);
+	}
+
+	@Override
+	public List<CommerceDiscountCommerceAccountGroupRel>
+			getCommerceDiscountCommerceAccountGroupRels(
+				long commerceDiscountId, int start, int end,
+				OrderByComparator<CommerceDiscountCommerceAccountGroupRel>
+					orderByComparator)
+		throws PortalException {
+
+		_commerceDiscountResourcePermission.check(
+			getPermissionChecker(), commerceDiscountId, ActionKeys.UPDATE);
+
+		return commerceDiscountCommerceAccountGroupRelLocalService.
+			getCommerceDiscountCommerceAccountGroupRels(
+				commerceDiscountId, start, end, orderByComparator);
+	}
+
+	private static volatile ModelResourcePermission<CommerceDiscount>
+		_commerceDiscountResourcePermission =
+			ModelResourcePermissionFactory.getInstance(
+				CommerceDiscountCommerceAccountGroupRelServiceImpl.class,
+				"_commerceDiscountResourcePermission", CommerceDiscount.class);
+
+}
