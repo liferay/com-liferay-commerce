7b08540596509a10be945a556e31c2a45302a026
Alec Sloan <Alec.sloan@Liferay.com>
2019-04-25 13:51:35 -0700

    COMMERCE-1318 Implement Catalog constants

---
+++ commerce-product-api/src/main/java/com/liferay/commerce/product/constants/CommerceCatalogActionKeys.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.constants;
+
+/**
+ * @author Alec Sloan
+ */
+public class CommerceCatalogActionKeys {
+
+	public static final String ADD_CATALOG = "ADD_CATALOG";
+
+	public static final String MANAGE_ALL_CATALOGS = "MANAGE_ALL_CATALOGS";
+
+	public static final String MANAGE_AVAILABLE_CATALOGS =
+		"MANAGE_AVAILABLE_CATALOGS";
+
+	public static final String MANAGE_CATALOG = "MANAGE_CATALOG";
+
+	public static final String MANAGE_CATALOG_USERS = "MANAGE_CATALOG_USERS";
+
+	public static final String VIEW_CATALOG_USERS = "VIEW_CATALOG_USERS";
+
+	public static final String VIEW_CATALOGS = "VIEW_CATALOGS";
+
+}
---
+++ commerce-product-api/src/main/java/com/liferay/commerce/product/constants/CommerceCatalogConstants.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.constants;
+
+/**
+ * @author Alec Sloan
+ */
+public class CommerceCatalogConstants {
+
+	public static final String CATALOG_MANAGER_ROLE_NAME = "Catalog Manager";
+
+	public static final String INVENTORY_MANAGER_ROLE_NAME =
+		"Inventory Manager";
+
+	public static final String ORDER_MANAGER_ROLE_NAME = "Order Manager";
+
+	public static final String PRICING_MANAGER_ROLE_NAME = "Pricing Manager";
+
+	public static final String ROLE_SUBTYPE_CATALOG = "catalog";
+
+	public static final String STORE_ADMINISTRATOR_ROLE_NAME =
+		"Store Administrator";
+
+}
