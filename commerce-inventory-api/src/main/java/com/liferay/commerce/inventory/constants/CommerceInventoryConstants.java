a58773e5d3e8baa0d102775ff1de9c5c3ef6bb23
Luca Pellizzon <luca.pellizzon@liferay.com>
2019-05-13 12:49:19 +0200

    COMMERCE-1403 constants

---
+++ commerce-inventory-api/src/main/java/com/liferay/commerce/inventory/constants/CommerceInventoryConstants.java
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.inventory.constants;
+
+/**
+ * @author Luca Pellizzon
+ */
+public class CommerceInventoryConstants {
+
+	public static final String DEFAULT_METHOD_NAME = "default";
+
+	public static final String DEFAULT_WAREHOUSE_NAME = "default";
+
+	public static final String RESOURCE_NAME = "com.liferay.commerce.inventory";
+
+	public static final String SERVICE_NAME = "com.liferay.commerce.inventory";
+
+}
