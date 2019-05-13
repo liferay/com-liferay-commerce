04d097ec200aaed795cb35e11cc066c798a19195
Luca Pellizzon <luca.pellizzon@liferay.com>
2019-05-13 12:55:54 +0200

    COMMERCE-1403 new system settings category for inventory

---
+++ commerce-service/src/main/java/com/liferay/commerce/internal/configuration/category/InventoryConfigurationCategory.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.internal.configuration.category;
+
+import com.liferay.configuration.admin.category.ConfigurationCategory;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Luca Pellizzon
+ */
+@Component(service = ConfigurationCategory.class)
+public class InventoryConfigurationCategory implements ConfigurationCategory {
+
+	@Override
+	public String getBundleSymbolicName() {
+		return "com.liferay.commerce.service";
+	}
+
+	@Override
+	public String getCategoryKey() {
+		return _CATEGORY_KEY;
+	}
+
+	@Override
+	public String getCategorySection() {
+		return _CATEGORY_SECTION;
+	}
+
+	private static final String _CATEGORY_KEY = "inventory";
+
+	private static final String _CATEGORY_SECTION = "commerce";
+
+}
