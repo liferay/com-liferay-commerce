3da985aa9c87d1625013800ac7fe3f12ba999085
Luca Pellizzon <luca.pellizzon@liferay.com>
2019-05-13 12:48:57 +0200

    COMMERCE-1403 inventory configuration

---
+++ commerce-inventory-api/src/main/java/com/liferay/commerce/inventory/configuration/CommerceInventoryGroupConfiguration.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.inventory.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+
+/**
+ * @author Luca Pellizzon
+ */
+@ExtendedObjectClassDefinition(
+	category = "inventory", scope = ExtendedObjectClassDefinition.Scope.GROUP
+)
+@Meta.OCD(
+	id = "com.liferay.commerce.inventory.configuration.CommerceInventoryGroupConfiguration",
+	localization = "content/Language",
+	name = "commerce-inventory-group-configuration-name"
+)
+public interface CommerceInventoryGroupConfiguration {
+
+	@Meta.AD(
+		deflt = "default", name = "inventory-engine-method", required = false
+	)
+	public String commerceInventoryEngineMethod();
+
+	@Meta.AD(
+		deflt = "false", name = "override-system-configuration",
+		required = false
+	)
+	public boolean overrideSystemConfiguration();
+
+}
