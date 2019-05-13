b7f3e484205512d4a8eedb1406fc865d6149c3bd
Luca Pellizzon <luca.pellizzon@liferay.com>
2019-05-13 12:52:10 +0200

    COMMERCE-1403 inventory util comparators

---
+++ commerce-inventory-api/src/main/java/com/liferay/commerce/inventory/util/comparator/CommerceWarehouseItemQuantityComparator.java
@@ -0,0 +1,77 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.inventory.util.comparator;
+
+import com.liferay.commerce.inventory.model.CommerceInventoryWarehouseItem;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+/**
+ * @author Luca Pellizzon
+ */
+public class CommerceWarehouseItemQuantityComparator
+	extends OrderByComparator<CommerceInventoryWarehouseItem> {
+
+	public static final String ORDER_BY_ASC = "CIWarehouseItem.quantity ASC";
+
+	public static final String ORDER_BY_DESC = "CIWarehouseItem.quantity DESC";
+
+	public static final String[] ORDER_BY_FIELDS = {"quantity"};
+
+	public CommerceWarehouseItemQuantityComparator() {
+		this(false);
+	}
+
+	public CommerceWarehouseItemQuantityComparator(boolean ascending) {
+		_ascending = ascending;
+	}
+
+	@Override
+	public int compare(
+		CommerceInventoryWarehouseItem commerceWarehouseItem1,
+		CommerceInventoryWarehouseItem commerceWarehouseItem2) {
+
+		int value = Integer.compare(
+			commerceWarehouseItem1.getQuantity(),
+			commerceWarehouseItem2.getQuantity());
+
+		if (_ascending) {
+			return value;
+		}
+
+		return Math.negateExact(value);
+	}
+
+	@Override
+	public String getOrderBy() {
+		if (_ascending) {
+			return ORDER_BY_ASC;
+		}
+
+		return ORDER_BY_DESC;
+	}
+
+	@Override
+	public String[] getOrderByFields() {
+		return ORDER_BY_FIELDS;
+	}
+
+	@Override
+	public boolean isAscending() {
+		return _ascending;
+	}
+
+	private final boolean _ascending;
+
+}
