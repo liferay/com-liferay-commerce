36072ea246e46662244fab0e0c8c19519f18da20
Alessio Rendina <asilcomando@gmail.com>
2019-05-15 18:38:28 +0200

    COMMERCE-1237 Added order dto converters

---
+++ headless/headless-commerce/headless-commerce-admin-order-impl/src/main/java/com/liferay/headless/commerce/admin/order/internal/dto/v1_0/converter/BillingAddressDTOConverter.java
@@ -0,0 +1,71 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.commerce.admin.order.internal.dto.v1_0.converter;
+
+import com.liferay.commerce.model.CommerceAddress;
+import com.liferay.commerce.service.CommerceAddressService;
+import com.liferay.headless.commerce.admin.order.dto.v1_0.BillingAddress;
+import com.liferay.headless.commerce.core.dto.v1_0.converter.DTOConverter;
+import com.liferay.headless.commerce.core.dto.v1_0.converter.DTOConverterContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	property = "model.class.name=BillingAddress",
+	service = {BillingAddressDTOConverter.class, DTOConverter.class}
+)
+public class BillingAddressDTOConverter implements DTOConverter {
+
+	@Override
+	public String getContentType() {
+		return BillingAddress.class.getSimpleName();
+	}
+
+	public BillingAddress toDTO(DTOConverterContext dtoConverterContext)
+		throws Exception {
+
+		CommerceAddress commerceAddress =
+			_commerceAddressService.getCommerceAddress(
+				dtoConverterContext.getResourcePrimKey());
+
+		return new BillingAddress() {
+			{
+				city = commerceAddress.getCity();
+				commerceCountryId = commerceAddress.getCommerceCountryId();
+				commerceRegionId = commerceAddress.getCommerceRegionId();
+				defaultBilling = commerceAddress.isDefaultBilling();
+				defaultShipping = commerceAddress.isDefaultShipping();
+				description = commerceAddress.getDescription();
+				id = commerceAddress.getCommerceAddressId();
+				latitude = commerceAddress.getLatitude();
+				longitude = commerceAddress.getLongitude();
+				name = commerceAddress.getName();
+				phoneNumber = commerceAddress.getPhoneNumber();
+				street1 = commerceAddress.getStreet1();
+				street2 = commerceAddress.getStreet2();
+				street3 = commerceAddress.getStreet3();
+				zip = commerceAddress.getZip();
+			}
+		};
+	}
+
+	@Reference
+	private CommerceAddressService _commerceAddressService;
+
+}
