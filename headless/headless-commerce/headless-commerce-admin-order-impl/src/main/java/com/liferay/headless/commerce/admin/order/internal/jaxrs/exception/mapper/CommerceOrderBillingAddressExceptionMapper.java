0f58b8de46b12e077d71d637280c9cd8f2cb2553
Alessio Rendina <asilcomando@gmail.com>
2019-05-15 18:38:49 +0200

    COMMERCE-1237 Added new exception mappers

---
+++ headless/headless-commerce/headless-commerce-admin-order-impl/src/main/java/com/liferay/headless/commerce/admin/order/internal/jaxrs/exception/mapper/CommerceOrderBillingAddressExceptionMapper.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.commerce.admin.order.internal.jaxrs.exception.mapper;
+
+import com.liferay.commerce.exception.CommerceOrderBillingAddressException;
+import com.liferay.headless.commerce.core.exception.mapper.BaseExceptionMapper;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	property = {
+		"osgi.jaxrs.application.select=(osgi.jaxrs.name=Liferay.Headless.Commerce.Admin.Order)",
+		"osgi.jaxrs.extension=true",
+		"osgi.jaxrs.name=Liferay.Headless.Commerce.Admin.Order.CommerceOrderBillingAddressException"
+	},
+	service = ExceptionMapper.class
+)
+@Provider
+public class CommerceOrderBillingAddressExceptionMapper
+	extends BaseExceptionMapper<CommerceOrderBillingAddressException> {
+
+	@Override
+	public String getErrorDescription() {
+		return "Invalid order billing address";
+	}
+
+	@Override
+	public Response.Status getStatus() {
+		return Response.Status.BAD_REQUEST;
+	}
+
+}
