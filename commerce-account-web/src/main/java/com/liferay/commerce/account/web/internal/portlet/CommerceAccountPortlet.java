a6b424df90c3cbd6b38535db387f6332943ff596
Alec Sloan <Alec.sloan@Liferay.com>
2019-05-17 10:17:50 -0700

    COMMERCE-1395 Update widget names

--- commerce-account-web/src/main/java/com/liferay/commerce/account/web/internal/portlet/CommerceAccountPortlet.java
+++ commerce-account-web/src/main/java/com/liferay/commerce/account/web/internal/portlet/CommerceAccountPortlet.java
@@ -70,7 +70,7 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.private-session-attributes=false",
 		"com.liferay.portlet.render-weight=50",
 		"com.liferay.portlet.scopeable=true",
-		"javax.portlet.display-name=Commerce Account Management",
+		"javax.portlet.display-name=Accounts",
 		"javax.portlet.expiration-cache=0",
 		"javax.portlet.init-param.view-template=/view.jsp",
 		"javax.portlet.name=" + CommerceAccountPortletKeys.COMMERCE_ACCOUNT,
