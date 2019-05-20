9c1893fe1a011cb2285304a87131dee699a2e76e
Alessio Rendina <asilcomando@gmail.com>
2019-05-21 00:59:28 +0200

    COMMERCE-1428 Added new method to get order detail friendly url

--- commerce-service/src/main/java/com/liferay/commerce/internal/order/CommerceOrderHelperImpl.java
+++ commerce-service/src/main/java/com/liferay/commerce/internal/order/CommerceOrderHelperImpl.java
@@ -14,11 +14,17 @@
 
 package com.liferay.commerce.internal.order;
 
+import com.liferay.commerce.constants.CommerceOrderConstants;
+import com.liferay.commerce.constants.CommercePortletKeys;
 import com.liferay.commerce.model.CommerceOrder;
 import com.liferay.commerce.order.CommerceOrderHelper;
 import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
 import com.liferay.portal.kernel.util.ObjectValuePair;
+import com.liferay.portal.kernel.util.Portal;
 import com.liferay.portal.kernel.workflow.WorkflowTask;
 import com.liferay.portal.kernel.workflow.WorkflowTaskManager;
 
@@ -30,9 +36,41 @@ import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Andrea Di Giorgi
+ * @author Alessio Antonio Rendina
  */
 @Component(immediate = true, service = CommerceOrderHelper.class)
 public class CommerceOrderHelperImpl implements CommerceOrderHelper {
+
+	@Override
+	public String getFriendlyURL(
+			long commerceOrderId, ThemeDisplay themeDisplay)
+		throws PortalException {
+
+		Layout layout = null;
+
+		if (layout == null) {
+			long plid = _portal.getPlidFromPortletId(
+				themeDisplay.getScopeGroupId(),
+				CommercePortletKeys.COMMERCE_ORDER_DETAIL);
+
+			if (plid > 0) {
+				layout = _layoutLocalService.getLayout(plid);
+			}
+		}
+
+		if (layout == null) {
+			layout = themeDisplay.getLayout();
+		}
+
+		String currentSiteURL = _portal.getGroupFriendlyURL(
+			layout.getLayoutSet(), themeDisplay);
+
+		String productFriendlyURL =
+			currentSiteURL + CommerceOrderConstants.SEPARATOR_ORDER_DETAIL_URL +
+				commerceOrderId;
+
+		return _portal.addPreservedParameters(themeDisplay, productFriendlyURL);
+	}
 
 	@Override
 	public List<ObjectValuePair<Long, String>> getWorkflowTransitions(
@@ -73,6 +111,12 @@ public class CommerceOrderHelperImpl implements CommerceOrderHelper {
 			}
 		}
 	}
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private Portal _portal;
 
 	@Reference
 	private WorkflowTaskManager _workflowTaskManager;
