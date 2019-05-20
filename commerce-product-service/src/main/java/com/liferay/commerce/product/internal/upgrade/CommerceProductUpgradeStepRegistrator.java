526cac2093f5e5802d4f37801cdf1ba917da4010
Alec Sloan <Alec.sloan@Liferay.com>
2019-05-20 10:51:50 -0700

    COMMERCE-1309 Implement Commerce Catalog

--- commerce-product-service/src/main/java/com/liferay/commerce/product/internal/upgrade/CommerceProductUpgradeStepRegistrator.java
+++ commerce-product-service/src/main/java/com/liferay/commerce/product/internal/upgrade/CommerceProductUpgradeStepRegistrator.java
@@ -16,16 +16,20 @@ package com.liferay.commerce.product.internal.upgrade;
 
 import com.liferay.commerce.product.internal.upgrade.v1_1_0.CPRuleUpgradeProcess;
 import com.liferay.commerce.product.internal.upgrade.v1_2_0.ProductSubscriptionUpgradeProcess;
+import com.liferay.commerce.product.internal.upgrade.v1_3_0.CPAttachmentFileEntryUpgradeProcess;
 import com.liferay.commerce.product.internal.upgrade.v1_3_0.CPDefinitionLinkUpgradeProcess;
+import com.liferay.commerce.product.internal.upgrade.v1_3_0.CPDefinitionOptionRelUpgradeProcess;
 import com.liferay.commerce.product.internal.upgrade.v1_3_0.CPDefinitionUpgradeProcess;
 import com.liferay.commerce.product.internal.upgrade.v1_3_0.CPFriendlyURLEntryUpgradeProcess;
 import com.liferay.commerce.product.internal.upgrade.v1_3_0.CPInstanceUpgradeProcess;
 import com.liferay.commerce.product.internal.upgrade.v1_3_0.CProductUpgradeProcess;
+import com.liferay.commerce.product.internal.upgrade.v1_4_0.CPDefinitionSpecificationOptionValueUpgradeProcess;
 import com.liferay.commerce.product.internal.upgrade.v1_5_0.CProductExternalReferenceCodeUpgradeProcess;
+import com.liferay.commerce.product.internal.upgrade.v1_6_0.CommerceCatalogUpgradeProcess;
 import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.service.ClassNameLocalService;
-import com.liferay.portal.kernel.upgrade.DummyUpgradeProcess;
+import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
 
 import org.osgi.service.component.annotations.Component;
@@ -55,18 +59,24 @@ public class CommerceProductUpgradeStepRegistrator
 
 		registry.register(
 			_SCHEMA_VERSION_1_2_0, _SCHEMA_VERSION_1_3_0,
+			new CPAttachmentFileEntryUpgradeProcess(),
 			new CPDefinitionLinkUpgradeProcess(),
+			new CPDefinitionOptionRelUpgradeProcess(),
 			new CPDefinitionUpgradeProcess(), new CProductUpgradeProcess(),
 			new CPFriendlyURLEntryUpgradeProcess(_classNameLocalService),
 			new CPInstanceUpgradeProcess());
 
 		registry.register(
 			_SCHEMA_VERSION_1_3_0, _SCHEMA_VERSION_1_4_0,
-			new DummyUpgradeProcess());
+			new CPDefinitionSpecificationOptionValueUpgradeProcess());
 
 		registry.register(
 			_SCHEMA_VERSION_1_4_0, _SCHEMA_VERSION_1_5_0,
 			new CProductExternalReferenceCodeUpgradeProcess());
+
+		registry.register(
+			_SCHEMA_VERSION_1_5_0, _SCHEMA_VERSION_1_6_0,
+			new CommerceCatalogUpgradeProcess(_groupLocalService));
 
 		if (_log.isInfoEnabled()) {
 			_log.info("COMMERCE PRODUCT UPGRADE STEP REGISTRATOR FINISHED");
@@ -85,10 +95,15 @@ public class CommerceProductUpgradeStepRegistrator
 
 	private static final String _SCHEMA_VERSION_1_5_0 = "1.5.0";
 
+	private static final String _SCHEMA_VERSION_1_6_0 = "1.6.0";
+
 	private static final Log _log = LogFactoryUtil.getLog(
 		CommerceProductUpgradeStepRegistrator.class);
 
 	@Reference
 	private ClassNameLocalService _classNameLocalService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
 
 }
