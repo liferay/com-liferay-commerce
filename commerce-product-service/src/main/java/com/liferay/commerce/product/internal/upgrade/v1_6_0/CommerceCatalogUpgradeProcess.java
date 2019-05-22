175ea0d8ed5ae41422e50edc790bc8b718c4b6cc
Alessio Rendina <asilcomando@gmail.com>
2019-05-23 00:30:36 +0200

    COMMERCE-1309 Added upgrade process

--- commerce-product-service/src/main/java/com/liferay/commerce/product/internal/upgrade/v1_6_0/CommerceCatalogUpgradeProcess.java
+++ commerce-product-service/src/main/java/com/liferay/commerce/product/internal/upgrade/v1_6_0/CommerceCatalogUpgradeProcess.java
@@ -16,26 +16,17 @@ package com.liferay.commerce.product.internal.upgrade.v1_6_0;
 
 import com.liferay.commerce.product.model.CommerceCatalog;
 import com.liferay.commerce.product.model.impl.CommerceCatalogModelImpl;
-import com.liferay.portal.kernel.dao.db.IndexMetadata;
 import com.liferay.portal.kernel.dao.jdbc.AutoBatchPreparedStatementUtil;
-import com.liferay.portal.kernel.dao.jdbc.DataAccess;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Group;
 import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.service.GroupLocalService;
 import com.liferay.portal.kernel.upgrade.UpgradeProcess;
-import com.liferay.portal.kernel.util.ObjectValuePair;
 import com.liferay.portal.kernel.util.StringBundler;
 
-import java.sql.DatabaseMetaData;
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
-import java.util.List;
-import java.util.Objects;
 
 /**
  * @author Alec Sloan
@@ -51,8 +42,6 @@ public class CommerceCatalogUpgradeProcess extends UpgradeProcess {
 		if (!hasTable(CommerceCatalogModelImpl.TABLE_NAME)) {
 			runSQL(CommerceCatalogModelImpl.TABLE_SQL_CREATE);
 		}
-
-		_addIndexes(CommerceCatalogModelImpl.TABLE_NAME);
 
 		String insertCommerceCatalogSQL = StringBundler.concat(
 			"insert into CommerceCatalog (commerceCatalogId, companyId, ",
@@ -153,62 +142,6 @@ public class CommerceCatalogUpgradeProcess extends UpgradeProcess {
 			ps2.executeBatch();
 		}
 	}
-
-	private void _addIndexes(String tableName) throws Exception {
-		Class<?> clazz = getClass();
-
-		List<ObjectValuePair<String, IndexMetadata>> indexesSQL = getIndexesSQL(
-			clazz.getClassLoader(), tableName);
-
-		for (ObjectValuePair<String, IndexMetadata> indexSQL : indexesSQL) {
-			IndexMetadata indexMetadata = indexSQL.getValue();
-
-			if (_log.isInfoEnabled()) {
-				_log.info(
-					String.format(
-						"Adding index %s to table %s",
-						indexMetadata.getIndexName(), tableName));
-			}
-
-			if (!_tableHasIndex(tableName, indexMetadata.getIndexName())) {
-				runSQL(indexMetadata.getCreateSQL(null));
-			}
-			else if (_log.isInfoEnabled()) {
-				_log.info(
-					String.format(
-						"Index %s already exists on table %s",
-						indexMetadata.getIndexName(), tableName));
-			}
-		}
-	}
-
-	private boolean _tableHasIndex(String tableName, String indexName)
-		throws Exception {
-
-		ResultSet rs = null;
-
-		try {
-			DatabaseMetaData metadata = connection.getMetaData();
-
-			rs = metadata.getIndexInfo(null, null, tableName, false, false);
-
-			while (rs.next()) {
-				String curIndexName = rs.getString("index_name");
-
-				if (Objects.equals(indexName, curIndexName)) {
-					return true;
-				}
-			}
-		}
-		finally {
-			DataAccess.cleanUp(rs);
-		}
-
-		return false;
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		CommerceCatalogUpgradeProcess.class);
 
 	private final GroupLocalService _groupLocalService;
 
