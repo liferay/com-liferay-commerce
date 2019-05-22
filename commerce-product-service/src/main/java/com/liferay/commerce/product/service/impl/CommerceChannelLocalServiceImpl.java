d4eb2c93ef8edcb534e5d849980e658652c121f0
Alessio Rendina <asilcomando@gmail.com>
2019-05-23 00:10:32 +0200

    COMMERCE-1309 Refactor commerce channel local and remote service implementation

--- commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CommerceChannelLocalServiceImpl.java
+++ commerce-product-service/src/main/java/com/liferay/commerce/product/service/impl/CommerceChannelLocalServiceImpl.java
@@ -19,78 +19,102 @@ import com.liferay.commerce.product.service.base.CommerceChannelLocalServiceBase
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.model.User;
-import com.liferay.portal.kernel.search.Indexable;
-import com.liferay.portal.kernel.search.IndexableType;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.util.UnicodeProperties;
 
-import java.util.Date;
+import java.util.List;
 
 /**
  * @author Alec Sloan
+ * @author Alessio Antonio Rendina
  */
 public class CommerceChannelLocalServiceImpl
 	extends CommerceChannelLocalServiceBaseImpl {
 
-	@Indexable(type = IndexableType.REINDEX)
 	@Override
 	public CommerceChannel addCommerceChannel(
-			String name, String filterType, String type,
-			UnicodeProperties typeSettingsProperties,
-			ServiceContext serviceContext)
+			String name, String type, UnicodeProperties typeSettingsProperties,
+			String externalReferenceCode, ServiceContext serviceContext)
 		throws PortalException {
+
+		User user = userLocalService.getUser(serviceContext.getUserId());
 
 		long commerceChannelId = counterLocalService.increment();
 
 		CommerceChannel commerceChannel = commerceChannelPersistence.create(
 			commerceChannelId);
 
-		User user = userLocalService.getUser(serviceContext.getUserId());
-
 		commerceChannel.setCompanyId(user.getCompanyId());
 		commerceChannel.setUserId(user.getUserId());
 		commerceChannel.setUserName(user.getFullName());
-
-		Date now = new Date();
-
-		commerceChannel.setCreateDate(now);
-		commerceChannel.setModifiedDate(now);
-
 		commerceChannel.setName(name);
-		commerceChannel.setFilterType(filterType);
 		commerceChannel.setType(type);
 		commerceChannel.setTypeSettingsProperties(typeSettingsProperties);
+		commerceChannel.setExternalReferenceCode(externalReferenceCode);
 
-		return commerceChannelPersistence.update(commerceChannel);
+		commerceChannelPersistence.update(commerceChannel);
+
+		return commerceChannel;
 	}
 
-	@Indexable(type = IndexableType.REINDEX)
 	@Override
 	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public CommerceChannel deleteCommerceChannel(
+		CommerceChannel commerceChannel) {
+
+		// Commerce channel rel
+
+		commerceChannelRelLocalService.deleteCommerceChannelRels(
+			commerceChannel.getCommerceChannelId());
+
+		// Commerce channel
+
+		return commerceChannelPersistence.remove(commerceChannel);
+	}
+
+	@Override
 	public CommerceChannel deleteCommerceChannel(long commerceChannelId)
 		throws PortalException {
 
-		return commerceChannelPersistence.remove(commerceChannelId);
+		CommerceChannel commerceChannel =
+			commerceChannelPersistence.findByPrimaryKey(commerceChannelId);
+
+		return commerceChannelLocalService.deleteCommerceChannel(
+			commerceChannel);
 	}
 
-	@Indexable(type = IndexableType.REINDEX)
+	@Override
+	public void deleteCommerceChannels(long companyId) {
+		List<CommerceChannel> commerceChannels =
+			commerceChannelPersistence.findByCompanyId(companyId);
+
+		for (CommerceChannel commerceChannel : commerceChannels) {
+			commerceChannelLocalService.deleteCommerceChannel(commerceChannel);
+		}
+	}
+
+	@Override
+	public List<CommerceChannel> getCommerceChannels(long companyId) {
+		return commerceChannelPersistence.findByCompanyId(companyId);
+	}
+
 	@Override
 	public CommerceChannel updateCommerceChannel(
-			long commerceChannelId, String name, String filterType, String type,
-			UnicodeProperties typeSettingsProperties,
-			ServiceContext serviceContext)
+			long commerceChannelId, String name, String type,
+			UnicodeProperties typeSettingsProperties)
 		throws PortalException {
 
 		CommerceChannel commerceChannel =
 			commerceChannelPersistence.findByPrimaryKey(commerceChannelId);
 
 		commerceChannel.setName(name);
-		commerceChannel.setFilterType(filterType);
 		commerceChannel.setType(type);
 		commerceChannel.setTypeSettingsProperties(typeSettingsProperties);
 
-		return commerceChannelPersistence.update(commerceChannel);
+		commerceChannelPersistence.update(commerceChannel);
+
+		return commerceChannel;
 	}
 
 }
