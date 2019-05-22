98513f4e899e799c440b5d82d83e4e13eaad422a
Alessio Rendina <asilcomando@gmail.com>
2019-05-23 00:25:33 +0200

    COMMERCE-1309 Added new display context

---
+++ commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/display/context/CPDefinitionChannelDisplayContext.java
@@ -0,0 +1,86 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.definitions.web.internal.display.context;
+
+import com.liferay.commerce.product.definitions.web.portlet.action.ActionHelper;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.product.model.CommerceChannelRel;
+import com.liferay.commerce.product.service.CPDefinitionService;
+import com.liferay.commerce.product.service.CommerceCatalogService;
+import com.liferay.commerce.product.service.CommerceChannelRelService;
+import com.liferay.commerce.product.service.CommerceChannelService;
+import com.liferay.commerce.product.util.CPDefinitionHelper;
+import com.liferay.item.selector.ItemSelector;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CPDefinitionChannelDisplayContext
+	extends CPDefinitionsDisplayContext {
+
+	public CPDefinitionChannelDisplayContext(
+		ActionHelper actionHelper, HttpServletRequest httpServletRequest,
+		CommerceCatalogService commerceCatalogService,
+		CPDefinitionHelper cpDefinitionHelper,
+		ModelResourcePermission<CPDefinition>
+			cpDefinitionModelResourcePermission,
+		CPDefinitionService cpDefinitionService, ItemSelector itemSelector,
+		PortletResourcePermission portletResourcePermission,
+		CommerceChannelRelService commerceChannelRelService,
+		CommerceChannelService commerceChannelService) {
+
+		super(
+			actionHelper, httpServletRequest, commerceCatalogService,
+			cpDefinitionHelper, cpDefinitionModelResourcePermission,
+			cpDefinitionService, itemSelector, portletResourcePermission);
+
+		_commerceChannelRelService = commerceChannelRelService;
+		_commerceChannelService = commerceChannelService;
+	}
+
+	public long[] getCommerceChannelRelCommerceChannelIds()
+		throws PortalException {
+
+		List<CommerceChannelRel> commerceChannelRels =
+			_commerceChannelRelService.getCommerceChannelRels(
+				CPDefinition.class.getName(), getCPDefinitionId(),
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
+
+		Stream<CommerceChannelRel> stream = commerceChannelRels.stream();
+
+		return stream.mapToLong(
+			CommerceChannelRel::getCommerceChannelId
+		).toArray();
+	}
+
+	public List<CommerceChannel> getCommerceChannels() throws PortalException {
+		return _commerceChannelService.getCommerceChannels(
+			cpRequestHelper.getCompanyId());
+	}
+
+	private final CommerceChannelRelService _commerceChannelRelService;
+	private final CommerceChannelService _commerceChannelService;
+
+}
