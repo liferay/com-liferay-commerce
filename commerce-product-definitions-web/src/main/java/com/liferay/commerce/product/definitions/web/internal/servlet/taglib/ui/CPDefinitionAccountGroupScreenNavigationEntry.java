2a950cee1a855bbb650d3ea9751572806ab9d337
Alessio Rendina <asilcomando@gmail.com>
2019-05-24 01:08:26 +0200

    COMMERCE-1338 Added new product definition configuration screen navigation entry

---
+++ commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/servlet/taglib/ui/CPDefinitionAccountGroupScreenNavigationEntry.java
@@ -0,0 +1,191 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.definitions.web.internal.servlet.taglib.ui;
+
+import com.liferay.commerce.account.model.CommerceAccountGroup;
+import com.liferay.commerce.account.service.CommerceAccountGroupRelService;
+import com.liferay.commerce.account.service.CommerceAccountGroupService;
+import com.liferay.commerce.product.constants.CPConstants;
+import com.liferay.commerce.product.definitions.web.internal.display.context.CPDefinitionAccountGroupDisplayContext;
+import com.liferay.commerce.product.definitions.web.portlet.action.ActionHelper;
+import com.liferay.commerce.product.definitions.web.servlet.taglib.ui.CPDefinitionScreenNavigationConstants;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.service.CPDefinitionService;
+import com.liferay.commerce.product.service.CommerceCatalogService;
+import com.liferay.commerce.product.util.CPDefinitionHelper;
+import com.liferay.frontend.taglib.servlet.taglib.ScreenNavigationEntry;
+import com.liferay.frontend.taglib.servlet.taglib.util.JSPRenderer;
+import com.liferay.item.selector.ItemSelector;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.security.permission.PermissionThreadLocal;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.util.ResourceBundleUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.io.IOException;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	property = "screen.navigation.entry.order:Integer=40",
+	service = ScreenNavigationEntry.class
+)
+public class CPDefinitionAccountGroupScreenNavigationEntry
+	implements ScreenNavigationEntry<CPDefinition> {
+
+	@Override
+	public String getCategoryKey() {
+		return CPDefinitionScreenNavigationConstants.CATEGORY_KEY_CONFIGURATION;
+	}
+
+	@Override
+	public String getEntryKey() {
+		return CPDefinitionScreenNavigationConstants.ENTRY_KEY_ACCOUNT_GROUPS;
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
+			"content.Language", locale, getClass());
+
+		return LanguageUtil.get(
+			resourceBundle,
+			CPDefinitionScreenNavigationConstants.ENTRY_KEY_ACCOUNT_GROUPS);
+	}
+
+	@Override
+	public String getScreenNavigationKey() {
+		return CPDefinitionScreenNavigationConstants.
+			SCREEN_NAVIGATION_KEY_CP_DEFINITION_GENERAL;
+	}
+
+	@Override
+	public boolean isVisible(User user, CPDefinition cpDefinition) {
+		if (cpDefinition == null) {
+			return false;
+		}
+
+		PermissionChecker permissionChecker =
+			PermissionThreadLocal.getPermissionChecker();
+
+		boolean hasViewCPDefinitionPermission = false;
+
+		try {
+			List<CommerceAccountGroup> commerceAccountGroups =
+				_commerceAccountGroupService.getCommerceAccountGroups(
+					user.getCompanyId(), QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+					null);
+
+			if (commerceAccountGroups.isEmpty()) {
+				return false;
+			}
+
+			hasViewCPDefinitionPermission =
+				_cpDefinitionModelResourcePermission.contains(
+					permissionChecker, cpDefinition, ActionKeys.VIEW);
+		}
+		catch (PortalException pe) {
+			_log.error(pe, pe);
+		}
+
+		return hasViewCPDefinitionPermission;
+	}
+
+	@Override
+	public void render(
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse)
+		throws IOException {
+
+		CPDefinitionAccountGroupDisplayContext
+			cpDefinitionAccountGroupDisplayContext =
+				new CPDefinitionAccountGroupDisplayContext(
+					_actionHelper, httpServletRequest, _commerceCatalogService,
+					_cpDefinitionHelper, _cpDefinitionModelResourcePermission,
+					_cpDefinitionService, _itemSelector,
+					_portletResourcePermission, _commerceAccountGroupRelService,
+					_commerceAccountGroupService);
+
+		httpServletRequest.setAttribute(
+			WebKeys.PORTLET_DISPLAY_CONTEXT,
+			cpDefinitionAccountGroupDisplayContext);
+
+		_jspRenderer.renderJSP(
+			_setServletContext, httpServletRequest, httpServletResponse,
+			"/edit_definition_account_groups.jsp");
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CPDefinitionAccountGroupScreenNavigationEntry.class);
+
+	@Reference
+	private ActionHelper _actionHelper;
+
+	@Reference
+	private CommerceAccountGroupRelService _commerceAccountGroupRelService;
+
+	@Reference
+	private CommerceAccountGroupService _commerceAccountGroupService;
+
+	@Reference
+	private CommerceCatalogService _commerceCatalogService;
+
+	@Reference
+	private CPDefinitionHelper _cpDefinitionHelper;
+
+	@Reference(
+		target = "(model.class.name=com.liferay.commerce.product.model.CPDefinition)"
+	)
+	private ModelResourcePermission<CPDefinition>
+		_cpDefinitionModelResourcePermission;
+
+	@Reference
+	private CPDefinitionService _cpDefinitionService;
+
+	@Reference
+	private ItemSelector _itemSelector;
+
+	@Reference
+	private JSPRenderer _jspRenderer;
+
+	@Reference(target = "(resource.name=" + CPConstants.RESOURCE_NAME + ")")
+	private PortletResourcePermission _portletResourcePermission;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.commerce.product.definitions.web)"
+	)
+	private ServletContext _setServletContext;
+
+}
