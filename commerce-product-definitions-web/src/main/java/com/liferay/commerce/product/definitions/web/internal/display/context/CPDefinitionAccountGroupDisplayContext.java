017dd69e8fed488cc38b35d30981d70849d5b7ef
Alessio Rendina <asilcomando@gmail.com>
2019-05-24 01:08:26 +0200

    COMMERCE-1338 Added new product definition configuration screen navigation entry

---
+++ commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/display/context/CPDefinitionAccountGroupDisplayContext.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.definitions.web.internal.display.context;
+
+import com.liferay.commerce.account.model.CommerceAccountGroup;
+import com.liferay.commerce.account.model.CommerceAccountGroupRel;
+import com.liferay.commerce.account.service.CommerceAccountGroupRelService;
+import com.liferay.commerce.account.service.CommerceAccountGroupService;
+import com.liferay.commerce.product.definitions.web.portlet.action.ActionHelper;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.service.CPDefinitionService;
+import com.liferay.commerce.product.service.CommerceCatalogService;
+import com.liferay.commerce.product.util.CPDefinitionHelper;
+import com.liferay.item.selector.ItemSelector;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.security.permission.resource.ModelResourcePermission;
+import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CPDefinitionAccountGroupDisplayContext
+	extends CPDefinitionsDisplayContext {
+
+	public CPDefinitionAccountGroupDisplayContext(
+		ActionHelper actionHelper, HttpServletRequest httpServletRequest,
+		CommerceCatalogService commerceCatalogService,
+		CPDefinitionHelper cpDefinitionHelper,
+		ModelResourcePermission<CPDefinition>
+			cpDefinitionModelResourcePermission,
+		CPDefinitionService cpDefinitionService, ItemSelector itemSelector,
+		PortletResourcePermission portletResourcePermission,
+		CommerceAccountGroupRelService commerceAccountGroupRelService,
+		CommerceAccountGroupService commerceAccountGroupService) {
+
+		super(
+			actionHelper, httpServletRequest, commerceCatalogService,
+			cpDefinitionHelper, cpDefinitionModelResourcePermission,
+			cpDefinitionService, itemSelector, portletResourcePermission);
+
+		_commerceAccountGroupRelService = commerceAccountGroupRelService;
+		_commerceAccountGroupService = commerceAccountGroupService;
+	}
+
+	public long[] getCommerceAccountGroupRelCommerceAccountGroupIds()
+		throws PortalException {
+
+		List<CommerceAccountGroupRel> commerceAccountGroupRels =
+			_commerceAccountGroupRelService.getCommerceAccountGroupRels(
+				CPDefinition.class.getName(), getCPDefinitionId(),
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
+
+		Stream<CommerceAccountGroupRel> stream =
+			commerceAccountGroupRels.stream();
+
+		return stream.mapToLong(
+			CommerceAccountGroupRel::getCommerceAccountGroupId
+		).toArray();
+	}
+
+	public List<CommerceAccountGroup> getCommerceAccountGroups()
+		throws PortalException {
+
+		return _commerceAccountGroupService.getCommerceAccountGroups(
+			cpRequestHelper.getCompanyId(), QueryUtil.ALL_POS,
+			QueryUtil.ALL_POS, null);
+	}
+
+	private final CommerceAccountGroupRelService
+		_commerceAccountGroupRelService;
+	private final CommerceAccountGroupService _commerceAccountGroupService;
+
+}
---
+++ commerce-product-definitions-web/src/main/resources/META-INF/resources/edit_definition_account_groups.jsp
@@ -0,0 +1,57 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+CPDefinitionAccountGroupDisplayContext cpDefinitionAccountGroupDisplayContext = (CPDefinitionAccountGroupDisplayContext)request.getAttribute(WebKeys.PORTLET_DISPLAY_CONTEXT);
+
+long cpDefinitionId = cpDefinitionAccountGroupDisplayContext.getCPDefinitionId();
+List<CommerceAccountGroupRel> commerceAccountGroups = cpDefinitionAccountGroupDisplayContext.getCommerceAccountGroups();
+long[] commerceAccountGroupIds = cpDefinitionAccountGroupDisplayContext.getCommerceAccountGroupRelCommerceAccountGroupIds();
+%>
+
+<portlet:actionURL name="editProductDefinition" var="editProductDefinitionAccountGroupActionURL" />
+
+<aui:form action="<%= editProductDefinitionAccountGroupActionURL %>" cssClass="container-fluid-1280" method="post" name="fm">
+	<aui:input name="<%= Constants.CMD %>" type="hidden" value="updateAccountGroups" />
+	<aui:input name="redirect" type="hidden" value="<%= currentURL %>" />
+	<aui:input name="cpDefinitionId" type="hidden" value="<%= cpDefinitionId %>" />
+
+	<div class="lfr-form-content">
+		<aui:fieldset-group markupView="lexicon">
+			<aui:fieldset>
+
+				<%
+				for (CommerceAccountGroup commerceAccountGroup : commerceAccountGroups) {
+				%>
+
+					<aui:input checked="<%= ArrayUtil.contains(commerceAccountGroupIds, commerceAccountGroup.getCommerceAccountGroupId()) %>" label="<%= commerceAccountGroup.getName() %>" name="commerceAccountGroupIds" type="checkbox" value="<%= commerceAccountGroup.getCommerceAccountGroupId() %>" />
+
+				<%
+				}
+				%>
+
+			</aui:fieldset>
+		</aui:fieldset-group>
+
+		<aui:button-row>
+			<aui:button cssClass="btn-lg" type="submit" />
+
+			<aui:button cssClass="btn-lg" href="<%= catalogURL %>" type="cancel" />
+		</aui:button-row>
+	</div>
+</aui:form>
