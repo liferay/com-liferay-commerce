86fa399c0a987802a60957c61de8b2b1c6055822
Alessio Rendina <asilcomando@gmail.com>
2019-05-24 01:04:07 +0200

    COMMERCE-1338 Added commerce account group indexer

---
+++ commerce-account-service/src/main/java/com/liferay/commerce/account/internal/search/CommerceAccountGroupIndexer.java
@@ -0,0 +1,177 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.account.internal.search;
+
+import com.liferay.commerce.account.model.CommerceAccountGroup;
+import com.liferay.commerce.account.service.CommerceAccountGroupLocalService;
+import com.liferay.portal.kernel.dao.orm.IndexableActionableDynamicQuery;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.search.BaseIndexer;
+import com.liferay.portal.kernel.search.BooleanQuery;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.IndexWriterHelper;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.Summary;
+import com.liferay.portal.kernel.search.filter.BooleanFilter;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import java.util.Locale;
+
+import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(immediate = true, service = Indexer.class)
+public class CommerceAccountGroupIndexer
+	extends BaseIndexer<CommerceAccountGroup> {
+
+	public static final String CLASS_NAME =
+		CommerceAccountGroup.class.getName();
+
+	@Override
+	public String getClassName() {
+		return CLASS_NAME;
+	}
+
+	@Override
+	public void postProcessSearchQuery(
+			BooleanQuery searchQuery, BooleanFilter fullQueryBooleanFilter,
+			SearchContext searchContext)
+		throws Exception {
+
+		addSearchTerm(searchQuery, searchContext, Field.ENTRY_CLASS_PK, false);
+		addSearchTerm(searchQuery, searchContext, Field.NAME, false);
+	}
+
+	@Override
+	protected void doDelete(CommerceAccountGroup commerceAccountGroup)
+		throws Exception {
+
+		deleteDocument(
+			commerceAccountGroup.getCompanyId(),
+			commerceAccountGroup.getCommerceAccountGroupId());
+	}
+
+	@Override
+	protected Document doGetDocument(CommerceAccountGroup commerceAccountGroup)
+		throws Exception {
+
+		if (_log.isDebugEnabled()) {
+			_log.debug(
+				"Indexing commerce account group " + commerceAccountGroup);
+		}
+
+		Document document = getBaseModelDocument(
+			CLASS_NAME, commerceAccountGroup);
+
+		document.addText(Field.NAME, commerceAccountGroup.getName());
+		document.addNumber(Field.TYPE, commerceAccountGroup.getType());
+
+		if (_log.isDebugEnabled()) {
+			_log.debug(
+				"Document " + commerceAccountGroup + " indexed successfully");
+		}
+
+		return document;
+	}
+
+	@Override
+	protected Summary doGetSummary(
+		Document document, Locale locale, String snippet,
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
+		Summary summary = createSummary(
+			document, Field.ENTRY_CLASS_PK, Field.NAME);
+
+		summary.setMaxContentLength(200);
+
+		return summary;
+	}
+
+	@Override
+	protected void doReindex(CommerceAccountGroup commerceAccountGroup)
+		throws Exception {
+
+		Document document = getDocument(commerceAccountGroup);
+
+		_indexWriterHelper.updateDocument(
+			getSearchEngineId(), commerceAccountGroup.getCompanyId(), document,
+			isCommitImmediately());
+	}
+
+	@Override
+	protected void doReindex(String className, long classPK) throws Exception {
+		CommerceAccountGroup commerceAccountGroup =
+			_commerceAccountGroupLocalService.getCommerceAccountGroup(classPK);
+
+		doReindex(commerceAccountGroup);
+	}
+
+	@Override
+	protected void doReindex(String[] ids) throws Exception {
+		long companyId = GetterUtil.getLong(ids[0]);
+
+		reindexCommerceAccountGroups(companyId);
+	}
+
+	protected void reindexCommerceAccountGroups(long companyId)
+		throws PortalException {
+
+		final IndexableActionableDynamicQuery indexableActionableDynamicQuery =
+			_commerceAccountGroupLocalService.
+				getIndexableActionableDynamicQuery();
+
+		indexableActionableDynamicQuery.setCompanyId(companyId);
+		indexableActionableDynamicQuery.setPerformActionMethod(
+			(CommerceAccountGroup commerceAccountGroup) -> {
+				try {
+					Document document = getDocument(commerceAccountGroup);
+
+					indexableActionableDynamicQuery.addDocuments(document);
+				}
+				catch (PortalException pe) {
+					if (_log.isWarnEnabled()) {
+						_log.warn(
+							"Unable to index commerce account group " +
+								commerceAccountGroup.
+									getCommerceAccountGroupId(),
+							pe);
+					}
+				}
+			});
+		indexableActionableDynamicQuery.setSearchEngineId(getSearchEngineId());
+
+		indexableActionableDynamicQuery.performActions();
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CommerceAccountGroupIndexer.class);
+
+	@Reference
+	private CommerceAccountGroupLocalService _commerceAccountGroupLocalService;
+
+	@Reference
+	private IndexWriterHelper _indexWriterHelper;
+
+}
