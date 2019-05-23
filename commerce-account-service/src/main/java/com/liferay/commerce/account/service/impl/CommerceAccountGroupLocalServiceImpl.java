b6104f2e57d289a0446b8e3f56f174c6b03fb3a1
Alessio Rendina <asilcomando@gmail.com>
2019-05-24 00:58:38 +0200

    COMMERCE-1338 Refactor commerce account group local and remote service implementation

--- commerce-account-service/src/main/java/com/liferay/commerce/account/service/impl/CommerceAccountGroupLocalServiceImpl.java
+++ commerce-account-service/src/main/java/com/liferay/commerce/account/service/impl/CommerceAccountGroupLocalServiceImpl.java
@@ -16,19 +16,35 @@ package com.liferay.commerce.account.service.impl;
 
 import com.liferay.commerce.account.model.CommerceAccountGroup;
 import com.liferay.commerce.account.service.base.CommerceAccountGroupLocalServiceBaseImpl;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.GroupConstants;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.SystemEventConstants;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Document;
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.kernel.search.Hits;
 import com.liferay.portal.kernel.search.Indexable;
 import com.liferay.portal.kernel.search.IndexableType;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
+import com.liferay.portal.kernel.search.QueryConfig;
+import com.liferay.portal.kernel.search.SearchContext;
+import com.liferay.portal.kernel.search.SearchException;
+import com.liferay.portal.kernel.search.Sort;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.kernel.util.GetterUtil;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
 
  * @author Marco Leo
+ * @author Alessio Antonio Rendina
  */
 public class CommerceAccountGroupLocalServiceImpl
 	extends CommerceAccountGroupLocalServiceBaseImpl {
@@ -113,10 +129,50 @@ public class CommerceAccountGroupLocalServiceImpl
 			commerceAccountGroup);
 	}
 
+	@Override
+	public List<CommerceAccountGroup> getCommerceAccountGroups(
+		long companyId, int start, int end,
+		OrderByComparator<CommerceAccountGroup> orderByComparator) {
+
+		return commerceAccountGroupPersistence.findByCompanyId(
+			companyId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCommerceAccountGroupsCount(long companyId) {
+		return commerceAccountGroupPersistence.countByCompanyId(companyId);
+	}
+
+	@Override
+	public List<CommerceAccountGroup> searchCommerceAccountGroups(
+			long companyId, String keywords, int start, int end, Sort sort)
+		throws PortalException {
+
+		SearchContext searchContext = buildSearchContext(
+			companyId, start, end, sort);
+
+		searchContext.setKeywords(keywords);
+
+		return searchCommerceAccountGroups(searchContext);
+	}
+
+	@Override
+	public int searchCommerceAccountsGroupCount(long companyId, String keywords)
+		throws PortalException {
+
+		SearchContext searchContext = buildSearchContext(
+			companyId, QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
+
+		searchContext.setKeywords(keywords);
+
+		return searchCommerceAccountGroupsCount(searchContext);
+	}
+
 	@Indexable(type = IndexableType.REINDEX)
 	@Override
 	public CommerceAccountGroup updateCommerceAccountGroup(
-			long commerceAccountGroupId, String name)
+			long commerceAccountGroupId, String name,
+			ServiceContext serviceContext)
 		throws PortalException {
 
 		CommerceAccountGroup commerceAccountGroup =
@@ -124,8 +180,100 @@ public class CommerceAccountGroupLocalServiceImpl
 				commerceAccountGroupId);
 
 		commerceAccountGroup.setName(name);
+		commerceAccountGroup.setExpandoBridgeAttributes(serviceContext);
 
 		return commerceAccountGroupPersistence.update(commerceAccountGroup);
 	}
+
+	protected SearchContext buildSearchContext(
+		long companyId, int start, int end, Sort sort) {
+
+		SearchContext searchContext = new SearchContext();
+
+		searchContext.setCompanyId(companyId);
+		searchContext.setStart(start);
+		searchContext.setEnd(end);
+
+		QueryConfig queryConfig = searchContext.getQueryConfig();
+
+		queryConfig.setHighlightEnabled(false);
+		queryConfig.setScoreEnabled(false);
+
+		if (sort != null) {
+			searchContext.setSorts(sort);
+		}
+
+		return searchContext;
+	}
+
+	protected List<CommerceAccountGroup> getCommerceAccountGroups(Hits hits)
+		throws PortalException {
+
+		List<Document> documents = hits.toList();
+
+		List<CommerceAccountGroup> commerceAccountGroups = new ArrayList<>(
+			documents.size());
+
+		for (Document document : documents) {
+			long commerceAccountGroupId = GetterUtil.getLong(
+				document.get(Field.ENTRY_CLASS_PK));
+
+			CommerceAccountGroup commerceAccountGroup =
+				commerceAccountGroupPersistence.fetchByPrimaryKey(
+					commerceAccountGroupId);
+
+			if (commerceAccountGroup == null) {
+				commerceAccountGroups = null;
+
+				Indexer<CommerceAccountGroup> indexer =
+					IndexerRegistryUtil.getIndexer(CommerceAccountGroup.class);
+
+				long companyId = GetterUtil.getLong(
+					document.get(Field.COMPANY_ID));
+
+				indexer.delete(companyId, document.getUID());
+			}
+			else if (commerceAccountGroup != null) {
+				commerceAccountGroups.add(commerceAccountGroup);
+			}
+		}
+
+		return commerceAccountGroups;
+	}
+
+	protected List<CommerceAccountGroup> searchCommerceAccountGroups(
+			SearchContext searchContext)
+		throws PortalException {
+
+		Indexer<CommerceAccountGroup> indexer =
+			IndexerRegistryUtil.nullSafeGetIndexer(CommerceAccountGroup.class);
+
+		for (int i = 0; i < 10; i++) {
+			Hits hits = indexer.search(searchContext, _SELECTED_FIELD_NAMES);
+
+			List<CommerceAccountGroup> commerceAccountGroups =
+				getCommerceAccountGroups(hits);
+
+			if (commerceAccountGroups != null) {
+				return commerceAccountGroups;
+			}
+		}
+
+		throw new SearchException(
+			"Unable to fix the search index after 10 attempts");
+	}
+
+	protected int searchCommerceAccountGroupsCount(SearchContext searchContext)
+		throws PortalException {
+
+		Indexer<CommerceAccountGroup> indexer =
+			IndexerRegistryUtil.nullSafeGetIndexer(CommerceAccountGroup.class);
+
+		return GetterUtil.getInteger(indexer.searchCount(searchContext));
+	}
+
+	private static final String[] _SELECTED_FIELD_NAMES = {
+		Field.ENTRY_CLASS_PK, Field.COMPANY_ID
+	};
 
 }
--- commerce-account-service/src/main/java/com/liferay/commerce/account/service/impl/CommerceAccountGroupServiceImpl.java
+++ commerce-account-service/src/main/java/com/liferay/commerce/account/service/impl/CommerceAccountGroupServiceImpl.java
@@ -17,12 +17,36 @@ package com.liferay.commerce.account.service.impl;
 import com.liferay.commerce.account.model.CommerceAccountGroup;
 import com.liferay.commerce.account.service.base.CommerceAccountGroupServiceBaseImpl;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.search.Sort;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+import java.util.List;
 
 /**
  * @author Marco Leo
+ * @author Alessio Antonio Rendina
  */
 public class CommerceAccountGroupServiceImpl
 	extends CommerceAccountGroupServiceBaseImpl {
+
+	@Override
+	public CommerceAccountGroup addCommerceAccountGroup(
+			String name, int type, String externalReferenceCode,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		return commerceAccountGroupLocalService.addCommerceAccountGroup(
+			name, type, externalReferenceCode, serviceContext);
+	}
+
+	@Override
+	public void deleteCommerceAccountGroup(long commerceAccountGroupId)
+		throws PortalException {
+
+		commerceAccountGroupLocalService.deleteCommerceAccountGroup(
+			commerceAccountGroupId);
+	}
 
 	@Override
 	public CommerceAccountGroup getCommerceAccountGroup(
@@ -31,6 +55,51 @@ public class CommerceAccountGroupServiceImpl
 
 		return commerceAccountGroupLocalService.getCommerceAccountGroup(
 			commerceAccountGroupId);
+	}
+
+	@Override
+	public List<CommerceAccountGroup> getCommerceAccountGroups(
+			long companyId, int start, int end,
+			OrderByComparator<CommerceAccountGroup> orderByComparator)
+		throws PortalException {
+
+		return commerceAccountGroupLocalService.getCommerceAccountGroups(
+			companyId, start, end, orderByComparator);
+	}
+
+	@Override
+	public int getCommerceAccountGroupsCount(long companyId)
+		throws PortalException {
+
+		return commerceAccountGroupLocalService.getCommerceAccountGroupsCount(
+			companyId);
+	}
+
+	@Override
+	public List<CommerceAccountGroup> searchCommerceAccountGroups(
+			long companyId, String keywords, int start, int end, Sort sort)
+		throws PortalException {
+
+		return commerceAccountGroupLocalService.searchCommerceAccountGroups(
+			companyId, keywords, start, end, sort);
+	}
+
+	@Override
+	public int searchCommerceAccountsGroupCount(long companyId, String keywords)
+		throws PortalException {
+
+		return commerceAccountGroupLocalService.
+			searchCommerceAccountsGroupCount(companyId, keywords);
+	}
+
+	@Override
+	public CommerceAccountGroup updateCommerceAccountGroup(
+			long commerceAccountGroupId, String name,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		return commerceAccountGroupLocalService.updateCommerceAccountGroup(
+			commerceAccountGroupId, name, serviceContext);
 	}
 
 }
