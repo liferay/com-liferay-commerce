ad314ef51d59cffe786bc0ecd692de5ea4a627f5
Alessio Rendina <asilcomando@gmail.com>
2019-05-24 01:05:27 +0200

    COMMERCE-1338 Added commerce account group panel application

---
+++ commerce-account-group-admin-web/src/main/java/com/liferay/commerce/account/group/admin/web/internal/application/list/CommerceAccountGroupPanelApp.java
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.account.group.admin.web.internal.application.list;
+
+import com.liferay.application.list.BasePanelApp;
+import com.liferay.application.list.PanelApp;
+import com.liferay.application.list.constants.PanelCategoryKeys;
+import com.liferay.commerce.account.constants.CommerceAccountPortletKeys;
+import com.liferay.portal.kernel.model.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	immediate = true,
+	property = {
+		"panel.app.order:Integer=250",
+		"panel.category.key=" + PanelCategoryKeys.CONTROL_PANEL_USERS
+	},
+	service = PanelApp.class
+)
+public class CommerceAccountGroupPanelApp extends BasePanelApp {
+
+	@Override
+	public String getPortletId() {
+		return CommerceAccountPortletKeys.COMMERCE_ACCOUNT_GROUP_ADMIN;
+	}
+
+	@Override
+	@Reference(
+		target = "(javax.portlet.name=" + CommerceAccountPortletKeys.COMMERCE_ACCOUNT_GROUP_ADMIN + ")",
+		unbind = "-"
+	)
+	public void setPortlet(Portlet portlet) {
+		super.setPortlet(portlet);
+	}
+
+}
