3e626f626939bc6e4d2b87959978f9a9c5fc01bb
Alec Sloan <Alec.sloan@Liferay.com>
2019-05-28 10:59:55 -0700

    COMMERCE-1309 Change Order Panel's Priority

--- commerce-order-web/src/main/java/com/liferay/commerce/order/web/internal/application/list/CommerceOrderPanelApp.java
+++ commerce-order-web/src/main/java/com/liferay/commerce/order/web/internal/application/list/CommerceOrderPanelApp.java
@@ -29,7 +29,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	immediate = true,
 	property = {
-		"panel.app.order:Integer=200",
+		"panel.app.order:Integer=400",
 		"panel.category.key=" + CommercePanelCategoryKeys.CONTROL_PANEL_COMMERCE
 	},
 	service = PanelApp.class
