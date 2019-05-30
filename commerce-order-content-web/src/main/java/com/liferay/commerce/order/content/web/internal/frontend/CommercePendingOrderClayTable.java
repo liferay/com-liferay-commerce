8d79eef7f174bc11634fc103a8f025201d83a286
Alec Sloan <Alec.sloan@Liferay.com>
2019-05-30 11:58:57 -0700

    COMMERCE-1309 Get pending and placed orders by channel's groupId

--- commerce-order-content-web/src/main/java/com/liferay/commerce/order/content/web/internal/frontend/CommercePendingOrderClayTable.java
+++ commerce-order-content-web/src/main/java/com/liferay/commerce/order/content/web/internal/frontend/CommercePendingOrderClayTable.java
@@ -26,6 +26,7 @@ import com.liferay.commerce.frontend.Pagination;
 import com.liferay.commerce.model.CommerceOrder;
 import com.liferay.commerce.order.content.web.internal.frontend.util.CommerceOrderClayTableUtil;
 import com.liferay.commerce.order.content.web.internal.model.Order;
+import com.liferay.commerce.product.service.CommerceChannelLocalService;
 import com.liferay.commerce.service.CommerceOrderService;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.exception.PortalException;
@@ -104,9 +105,12 @@ public class CommercePendingOrderClayTable
 
 		OrderFilterImpl orderFilter = (OrderFilterImpl)filter;
 
+		long channelGroupId =
+			_commerceChannelLocalService.getCommerceChannelGroupIdBySiteGroupId(
+				themeDisplay.getScopeGroupId());
+
 		return _commerceOrderService.getPendingCommerceOrdersCount(
-			themeDisplay.getScopeGroupId(), orderFilter.getAccountId(),
-			filter.getKeywords());
+			channelGroupId, orderFilter.getAccountId(), filter.getKeywords());
 	}
 
 	@Override
@@ -146,9 +150,13 @@ public class CommercePendingOrderClayTable
 
 		OrderFilterImpl orderFilter = (OrderFilterImpl)filter;
 
+		long channelGroupId =
+			_commerceChannelLocalService.getCommerceChannelGroupIdBySiteGroupId(
+				themeDisplay.getScopeGroupId());
+
 		List<CommerceOrder> commerceOrders =
 			_commerceOrderService.getPendingCommerceOrders(
-				themeDisplay.getScopeGroupId(), orderFilter.getAccountId(),
+				channelGroupId, orderFilter.getAccountId(),
 				filter.getKeywords(), pagination.getStartPosition(),
 				pagination.getEndPosition());
 
@@ -163,6 +171,9 @@ public class CommercePendingOrderClayTable
 
 	@Reference
 	private ClayTableSchemaBuilderFactory _clayTableSchemaBuilderFactory;
+
+	@Reference
+	private CommerceChannelLocalService _commerceChannelLocalService;
 
 	@Reference
 	private CommerceOrderService _commerceOrderService;
