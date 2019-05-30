415a2fe952a724c3e22eacfd3e6701f579e228d7
Alessio Rendina <asilcomando@gmail.com>
2019-05-30 11:05:14 +0200

    COMMERCE-1402 Refactor commerce order item upgrade process

--- commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v4_0_0/CommerceOrderItemUpgradeProcess.java
+++ commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v4_0_0/CommerceOrderItemUpgradeProcess.java
@@ -14,58 +14,20 @@
 
 package com.liferay.commerce.internal.upgrade.v4_0_0;
 
+import com.liferay.commerce.internal.upgrade.base.BaseCommerceServiceUpgradeProcess;
 import com.liferay.commerce.model.impl.CommerceOrderItemModelImpl;
-import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 
 /**
  * @author Luca Pellizzon
  */
-public class CommerceOrderItemUpgradeProcess extends UpgradeProcess {
+public class CommerceOrderItemUpgradeProcess
+	extends BaseCommerceServiceUpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
-		if (hasTable(CommerceOrderItemModelImpl.TABLE_NAME) &&
-			!hasColumn(
-				CommerceOrderItemModelImpl.TABLE_NAME, "bookedQuantityId")) {
-
-			_addColumn(
-				CommerceOrderItemModelImpl.class,
-				CommerceOrderItemModelImpl.TABLE_NAME, "bookedQuantityId",
-				"LONG");
-		}
+		addColumn(
+			CommerceOrderItemModelImpl.class,
+			CommerceOrderItemModelImpl.TABLE_NAME, "bookedQuantityId", "LONG");
 	}
-
-	private void _addColumn(
-			Class<?> entityClass, String tableName, String columnName,
-			String columnType)
-		throws Exception {
-
-		if (_log.isInfoEnabled()) {
-			_log.info(
-				String.format(
-					"Adding column %s to table %s", columnName, tableName));
-		}
-
-		if (!hasColumn(tableName, columnName)) {
-			alter(
-				entityClass,
-				new AlterTableAddColumn(
-					columnName + StringPool.SPACE + columnType));
-		}
-		else {
-			if (_log.isInfoEnabled()) {
-				_log.info(
-					String.format(
-						"Column %s already exists on table %s", columnName,
-						tableName));
-			}
-		}
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		CommerceOrderItemUpgradeProcess.class);
 
 }
