56329d8e7779dbd8aaa221e4f14f0ade5e794a1b
Ethan Bustad <ethan.bustad@liferay.com>
2019-05-31 15:53:14 -0700

    COMMERCE-1469 Move product indexers to internal package

--- commerce-product-service/src/main/java/com/liferay/commerce/product/internal/search/CPDefinitionIndexer.java
+++ commerce-product-service/src/main/java/com/liferay/commerce/product/internal/search/CPDefinitionIndexer.java
@@ -12,7 +12,7 @@
  * details.
  */
 
-package com.liferay.commerce.product.search;
+package com.liferay.commerce.product.internal.search;
 
 import com.liferay.commerce.account.model.CommerceAccountGroupRel;
 import com.liferay.commerce.account.service.CommerceAccountGroupRelService;
