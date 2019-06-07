191481df394e9f18eb47137a3c5642073e3e3133
Luca Pellizzon <luca.pellizzon@liferay.com>
2019-06-07 23:29:46 +0200

    COMMERCE-1309 fix payment method for using the proper groupId

--- commerce-payment-method-paypal/src/main/java/com/liferay/commerce/payment/method/paypal/internal/PayPalCommercePaymentMethod.java
+++ commerce-payment-method-paypal/src/main/java/com/liferay/commerce/payment/method/paypal/internal/PayPalCommercePaymentMethod.java
@@ -34,6 +34,8 @@ import com.liferay.commerce.product.constants.CPConstants;
 import com.liferay.commerce.product.model.CPDefinition;
 import com.liferay.commerce.product.model.CPInstance;
 import com.liferay.commerce.product.model.CPSubscriptionInfo;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.product.service.CommerceChannelLocalService;
 import com.liferay.commerce.service.CommerceOrderLocalService;
 import com.liferay.petra.string.CharPool;
 import com.liferay.petra.string.StringPool;
@@ -815,8 +817,13 @@ public class PayPalCommercePaymentMethod implements CommercePaymentMethod {
 	}
 
 	private APIContext _getAPIContext(long groupId) throws PortalException {
+		CommerceChannel commerceChannel =
+			_commerceChannelLocalService.getCommerceChannelByOrderGroupId(
+				groupId);
+
 		PayPalGroupServiceConfiguration payPalGroupServiceConfiguration =
-			_getPayPalGroupServiceConfiguration(groupId);
+			_getPayPalGroupServiceConfiguration(
+				commerceChannel.getSiteGroupId());
 
 		return new APIContext(
 			payPalGroupServiceConfiguration.clientId(),
@@ -1049,8 +1056,13 @@ public class PayPalCommercePaymentMethod implements CommercePaymentMethod {
 			PayPalCommercePaymentMethodConstants.INITIAL_FAIL_AMOUNT_ACTION);
 		merchantPreferences.setReturnUrl(commercePaymentRequest.getReturnUrl());
 
+		CommerceChannel commerceChannel =
+			_commerceChannelLocalService.getCommerceChannelByOrderGroupId(
+				commerceOrder.getGroupId());
+
 		PayPalGroupServiceConfiguration payPalGroupServiceConfiguration =
-			_getPayPalGroupServiceConfiguration(commerceOrder.getGroupId());
+			_getPayPalGroupServiceConfiguration(
+				commerceChannel.getSiteGroupId());
 
 		String attemptsMaxCount =
 			payPalGroupServiceConfiguration.paymentAttemptsMaxCount();
@@ -1195,6 +1207,9 @@ public class PayPalCommercePaymentMethod implements CommercePaymentMethod {
 	private static final DecimalFormat _payPalDecimalFormat = new DecimalFormat(
 		"#,###.##");
 	private static final TimeZone _utc = TimeZone.getTimeZone("UTC");
+
+	@Reference
+	private CommerceChannelLocalService _commerceChannelLocalService;
 
 	@Reference
 	private CommerceOrderLocalService _commerceOrderLocalService;
