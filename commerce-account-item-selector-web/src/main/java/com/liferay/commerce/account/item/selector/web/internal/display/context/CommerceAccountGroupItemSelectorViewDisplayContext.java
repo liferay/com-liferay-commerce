3f9877b8bd9d92d9b976a1e83b68237077493d78
Alessio Rendina <asilcomando@gmail.com>
2019-06-07 22:09:02 +0200

    COMMERCE-1309 Added missing account group item selector

---
+++ commerce-account-item-selector-web/src/main/java/com/liferay/commerce/account/item/selector/web/internal/display/context/CommerceAccountGroupItemSelectorViewDisplayContext.java
@@ -0,0 +1,136 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.account.item.selector.web.internal.display.context;
+
+import com.liferay.commerce.account.item.selector.web.internal.display.context.util.CommerceAccountItemSelectorRequestHelper;
+import com.liferay.commerce.account.item.selector.web.internal.search.CommerceAccountGroupItemSelectorChecker;
+import com.liferay.commerce.account.model.CommerceAccountGroup;
+import com.liferay.commerce.account.service.CommerceAccountGroupService;
+import com.liferay.portal.kernel.dao.search.RowChecker;
+import com.liferay.portal.kernel.dao.search.SearchContainer;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.util.ParamUtil;
+
+import java.util.List;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+public class CommerceAccountGroupItemSelectorViewDisplayContext {
+
+	public CommerceAccountGroupItemSelectorViewDisplayContext(
+		CommerceAccountGroupService commerceAccountGroupService,
+		HttpServletRequest httpServletRequest, PortletURL portletURL,
+		String itemSelectedEventName) {
+
+		_commerceAccountGroupService = commerceAccountGroupService;
+		_portletURL = portletURL;
+		_itemSelectedEventName = itemSelectedEventName;
+
+		_commerceAccountItemSelectorRequestHelper =
+			new CommerceAccountItemSelectorRequestHelper(httpServletRequest);
+	}
+
+	public String getItemSelectedEventName() {
+		return _itemSelectedEventName;
+	}
+
+	public String getOrderByCol() {
+		return ParamUtil.getString(
+			_commerceAccountItemSelectorRequestHelper.getRenderRequest(),
+			SearchContainer.DEFAULT_ORDER_BY_COL_PARAM, "createDate_sortable");
+	}
+
+	public String getOrderByType() {
+		return ParamUtil.getString(
+			_commerceAccountItemSelectorRequestHelper.getRenderRequest(),
+			SearchContainer.DEFAULT_ORDER_BY_TYPE_PARAM, "desc");
+	}
+
+	public PortletURL getPortletURL() {
+		return _portletURL;
+	}
+
+	public SearchContainer<CommerceAccountGroup> getSearchContainer()
+		throws PortalException {
+
+		if (_searchContainer != null) {
+			return _searchContainer;
+		}
+
+		_searchContainer = new SearchContainer<>(
+			_commerceAccountItemSelectorRequestHelper.
+				getLiferayPortletRequest(),
+			getPortletURL(), null, null);
+
+		_searchContainer.setEmptyResultsMessage("there-are-no-account-groups");
+
+		_searchContainer.setOrderByCol(getOrderByCol());
+		_searchContainer.setOrderByType(getOrderByType());
+
+		RowChecker rowChecker = new CommerceAccountGroupItemSelectorChecker(
+			_commerceAccountItemSelectorRequestHelper.getRenderResponse(),
+			getCheckedCommerceAccountGroupIds());
+
+		_searchContainer.setRowChecker(rowChecker);
+
+		int total =
+			_commerceAccountGroupService.searchCommerceAccountsGroupCount(
+				_commerceAccountItemSelectorRequestHelper.getCompanyId(),
+				getKeywords());
+
+		List<CommerceAccountGroup> results =
+			_commerceAccountGroupService.searchCommerceAccountGroups(
+				_commerceAccountItemSelectorRequestHelper.getCompanyId(),
+				getKeywords(), _searchContainer.getStart(),
+				_searchContainer.getEnd(), null);
+
+		_searchContainer.setTotal(total);
+		_searchContainer.setResults(results);
+
+		return _searchContainer;
+	}
+
+	protected long[] getCheckedCommerceAccountGroupIds() {
+		return ParamUtil.getLongValues(
+			_commerceAccountItemSelectorRequestHelper.getRenderRequest(),
+			"checkedCommerceAccountGroupIds");
+	}
+
+	protected String getKeywords() {
+		if (_keywords != null) {
+			return _keywords;
+		}
+
+		_keywords = ParamUtil.getString(
+			_commerceAccountItemSelectorRequestHelper.getRenderRequest(),
+			"keywords");
+
+		return _keywords;
+	}
+
+	private final CommerceAccountGroupService _commerceAccountGroupService;
+	private final CommerceAccountItemSelectorRequestHelper
+		_commerceAccountItemSelectorRequestHelper;
+	private final String _itemSelectedEventName;
+	private String _keywords;
+	private final PortletURL _portletURL;
+	private SearchContainer<CommerceAccountGroup> _searchContainer;
+
+}
---
+++ commerce-account-item-selector-web/src/main/resources/META-INF/resources/account_group_item_selector.jsp
@@ -0,0 +1,128 @@
+<%--
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+--%>
+
+<%@ include file="/init.jsp" %>
+
+<%
+CommerceAccountGroupItemSelectorViewDisplayContext commerceAccountGroupItemSelectorViewDisplayContext = (CommerceAccountGroupItemSelectorViewDisplayContext)request.getAttribute(WebKeys.PORTLET_DISPLAY_CONTEXT);
+
+SearchContainer commerceAccountGroupSearchContainer = commerceAccountGroupItemSelectorViewDisplayContext.getSearchContainer();
+String itemSelectedEventName = commerceAccountGroupItemSelectorViewDisplayContext.getItemSelectedEventName();
+PortletURL portletURL = commerceAccountGroupItemSelectorViewDisplayContext.getPortletURL();
+%>
+
+<liferay-frontend:management-bar
+	includeCheckBox="<%= true %>"
+	searchContainerId="commerceAccountGroups"
+>
+	<liferay-frontend:management-bar-buttons>
+		<liferay-frontend:management-bar-display-buttons
+			displayViews='<%= new String[] {"list"} %>'
+			portletURL="<%= portletURL %>"
+			selectedDisplayStyle="list"
+		/>
+
+		<li>
+			<aui:form action="<%= String.valueOf(portletURL) %>" name="searchFm">
+				<liferay-ui:input-search
+					markupView="lexicon"
+				/>
+			</aui:form>
+		</li>
+	</liferay-frontend:management-bar-buttons>
+
+	<liferay-frontend:management-bar-filters>
+		<liferay-frontend:management-bar-navigation
+			navigationKeys='<%= new String[] {"all"} %>'
+			portletURL="<%= portletURL %>"
+		/>
+
+		<liferay-frontend:management-bar-sort
+			orderByCol="<%= commerceAccountGroupItemSelectorViewDisplayContext.getOrderByCol() %>"
+			orderByType="<%= commerceAccountGroupItemSelectorViewDisplayContext.getOrderByType() %>"
+			orderColumns="<%= new String[0] %>"
+			portletURL="<%= portletURL %>"
+		/>
+	</liferay-frontend:management-bar-filters>
+</liferay-frontend:management-bar>
+
+<div class="container-fluid-1280" id="<portlet:namespace />commerceAccountGroupSelectorWrapper">
+	<liferay-ui:search-container
+		id="commerceAccountGroups"
+		searchContainer="<%= commerceAccountGroupSearchContainer %>"
+	>
+		<liferay-ui:search-container-row
+			className="com.liferay.commerce.account.model.CommerceAccountGroup"
+			cssClass="commerce-account-row"
+			keyProperty="commerceAccountGroupId"
+			modelVar="commerceAccountGroup"
+		>
+
+			<%
+			Map<String, Object> data = new HashMap<>();
+
+			data.put("commerce-account-group-id", commerceAccountGroup.getCommerceAccountGroupId());
+			data.put("name", commerceAccountGroup.getName());
+
+			row.setData(data);
+			%>
+
+			<liferay-ui:search-container-column-text
+				cssClass="table-cell-content"
+				property="name"
+			/>
+
+			<liferay-ui:search-container-column-date
+				cssClass="table-cell-content"
+				property="createDate"
+			/>
+		</liferay-ui:search-container-row>
+
+		<liferay-ui:search-iterator
+			markupView="lexicon"
+		/>
+	</liferay-ui:search-container>
+</div>
+
+<aui:script use="liferay-search-container">
+	var searchContainer = Liferay.SearchContainer.get('<portlet:namespace />commerceAccountGroups');
+
+	searchContainer.on(
+		'rowToggled',
+		function(event) {
+
+			var allSelectedElements = event.elements.allSelectedElements
+			var arr = [];
+
+			allSelectedElements.each(
+				function() {
+					var row = this.ancestor('tr');
+
+					var data = row.getDOM().dataset;
+
+					arr.push({commerceAccountGroupId : data.commerceAccountGroupId, name : data.name});
+				}
+			);
+
+			Liferay.Util.getOpener().Liferay.fire(
+				'<%= HtmlUtil.escapeJS(itemSelectedEventName) %>',
+				{
+					data: arr
+				}
+			);
+		}
+	);
+</aui:script>
