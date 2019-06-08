fa1105dfba7a940cc357021926a19e57ba3a8f36
Luca Pellizzon <luca.pellizzon@liferay.com>
2019-06-08 15:24:32 +0200

    COMMERCE-1496 CommercePriceList upgrade process

--- commerce-price-list-service/src/main/java/com/liferay/commerce/price/list/internal/upgrade/CommercePriceListUpgradeStepRegistrator.java
+++ commerce-price-list-service/src/main/java/com/liferay/commerce/price/list/internal/upgrade/CommercePriceListUpgradeStepRegistrator.java
@@ -18,6 +18,7 @@ import com.liferay.commerce.price.list.internal.upgrade.v1_1_0.CommercePriceEntr
 import com.liferay.commerce.price.list.internal.upgrade.v1_2_0.CommercePriceListAccountRelUpgradeProcess;
 import com.liferay.commerce.price.list.internal.upgrade.v2_0_0.CommercePriceListCommerceAccountGroupRelUpgradeProcess;
 import com.liferay.commerce.price.list.internal.upgrade.v2_0_0.CommerceTierPriceEntryUpgradeProcess;
+import com.liferay.commerce.price.list.internal.upgrade.v3_0_0.CommercePriceListUpgradeProcess;
 import com.liferay.commerce.product.service.CPDefinitionLocalService;
 import com.liferay.commerce.product.service.CPInstanceLocalService;
 import com.liferay.portal.kernel.log.Log;
@@ -59,6 +60,10 @@ public class CommercePriceListUpgradeStepRegistrator
 			new CommercePriceListCommerceAccountGroupRelUpgradeProcess(),
 			new CommerceTierPriceEntryUpgradeProcess());
 
+		registry.register(
+			_SCHEMA_VERSION_2_0_0, _SCHEMA_VERSION_3_0_0,
+			new CommercePriceListUpgradeProcess());
+
 		if (_log.isInfoEnabled()) {
 			_log.info("COMMERCE PRICE LIST UPGRADE STEP REGISTRATOR FINISHED");
 		}
@@ -71,6 +76,8 @@ public class CommercePriceListUpgradeStepRegistrator
 	private static final String _SCHEMA_VERSION_1_2_0 = "1.2.0";
 
 	private static final String _SCHEMA_VERSION_2_0_0 = "2.0.0";
+
+	private static final String _SCHEMA_VERSION_3_0_0 = "3.0.0";
 
 	private static final Log _log = LogFactoryUtil.getLog(
 		CommercePriceListUpgradeStepRegistrator.class);
