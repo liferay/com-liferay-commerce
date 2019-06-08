e0687cc21c9930f66b79735c9a47ed50ef195f2e
Marco Leo <marco.leo@liferay.com>
2019-06-08 19:36:15 +0200

    COMMERCE-1039 Use permision

--- commerce-product-service/src/main/java/com/liferay/commerce/product/search/CPSpecificationOptionIndexer.java
+++ commerce-product-service/src/main/java/com/liferay/commerce/product/search/CPSpecificationOptionIndexer.java
@@ -70,8 +70,9 @@ public class CPSpecificationOptionIndexer
 	public CPSpecificationOptionIndexer() {
 		setDefaultSelectedFieldNames(
 			Field.COMPANY_ID, Field.ENTRY_CLASS_NAME, Field.ENTRY_CLASS_PK,
-			Field.GROUP_ID, Field.MODIFIED_DATE, Field.SCOPE_GROUP_ID,
-			Field.TITLE, Field.UID, FIELD_KEY);
+			Field.MODIFIED_DATE, Field.TITLE, Field.UID, FIELD_KEY);
+		setFilterSearch(true);
+		setPermissionAware(true);
 	}
 
 	@Override
