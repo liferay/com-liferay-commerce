78a90b062ca4085475c35fc3c9ff2bb25dbdd473
Alessio Rendina <asilcomando@gmail.com>
2019-06-10 16:26:13 +0200

    COMMERCE-1309 Refactor commerce order list display context

--- commerce-order-web/src/main/java/com/liferay/commerce/order/web/internal/portlet/CommerceOrderPortlet.java
+++ commerce-order-web/src/main/java/com/liferay/commerce/order/web/internal/portlet/CommerceOrderPortlet.java
@@ -17,7 +17,7 @@ package com.liferay.commerce.order.web.internal.portlet;
 import com.liferay.commerce.constants.CommercePortletKeys;
 import com.liferay.commerce.order.web.internal.display.context.CommerceOrderListDisplayContext;
 import com.liferay.commerce.price.CommerceOrderPriceCalculation;
-import com.liferay.commerce.product.service.CommerceChannelLocalService;
+import com.liferay.commerce.product.service.CommerceChannelService;
 import com.liferay.commerce.service.CommerceOrderLocalService;
 import com.liferay.commerce.service.CommerceOrderNoteService;
 import com.liferay.portal.kernel.json.JSONFactory;
@@ -97,8 +97,7 @@ public class CommerceOrderPortlet extends MVCPortlet {
 				CommerceOrderListDisplayContext
 					commerceOrderListDisplayContext =
 						new CommerceOrderListDisplayContext(
-							_commerceChannelLocalService,
-							_commerceOrderLocalService,
+							_commerceChannelService, _commerceOrderLocalService,
 							_commerceOrderNoteService,
 							_commerceOrderPriceCalculation, _groupLocalService,
 							_jsonFactory, renderRequest);
@@ -116,7 +115,7 @@ public class CommerceOrderPortlet extends MVCPortlet {
 	}
 
 	@Reference
-	private CommerceChannelLocalService _commerceChannelLocalService;
+	private CommerceChannelService _commerceChannelService;
 
 	@Reference
 	private CommerceOrderLocalService _commerceOrderLocalService;
