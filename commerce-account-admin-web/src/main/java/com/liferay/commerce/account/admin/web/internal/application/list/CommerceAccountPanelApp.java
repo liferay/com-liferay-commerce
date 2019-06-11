09ce6feea4048adc0f0abb8e19f3debd528386cb
Alec Sloan <Alec.sloan@Liferay.com>
2019-06-11 09:59:29 -0700

    COMMERCE-1502 Reorder Users panel apps

--- commerce-account-admin-web/src/main/java/com/liferay/commerce/account/admin/web/internal/application/list/CommerceAccountPanelApp.java
+++ commerce-account-admin-web/src/main/java/com/liferay/commerce/account/admin/web/internal/application/list/CommerceAccountPanelApp.java
@@ -29,7 +29,7 @@ import org.osgi.service.component.annotations.Reference;
 @Component(
 	immediate = true,
 	property = {
-		"panel.app.order:Integer=200",
+		"panel.app.order:Integer=225",
 		"panel.category.key=" + PanelCategoryKeys.CONTROL_PANEL_USERS
 	},
 	service = PanelApp.class
