cbfdb36289dfb2dd73c0890a7fcf01e456299143
Alec Sloan <Alec.sloan@Liferay.com>
2019-06-13 15:16:40 -0700

    COMMERCE-1309 C119 Remove groupId from searchContext

--- commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/portlet/action/CPDefinitionsFacetsMVCResourceCommand.java
+++ commerce-product-definitions-web/src/main/java/com/liferay/commerce/product/definitions/web/internal/portlet/action/CPDefinitionsFacetsMVCResourceCommand.java
@@ -80,12 +80,9 @@ public class CPDefinitionsFacetsMVCResourceCommand
 		ThemeDisplay themeDisplay = (ThemeDisplay)resourceRequest.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
-		long[] groupIds = {themeDisplay.getScopeGroupId()};
-
 		SearchContext searchContext = new SearchContext();
 
 		searchContext.setCompanyId(themeDisplay.getCompanyId());
-		searchContext.setGroupIds(groupIds);
 		searchContext.setLayout(themeDisplay.getLayout());
 		searchContext.setLocale(themeDisplay.getLocale());
 		searchContext.setTimeZone(themeDisplay.getTimeZone());
