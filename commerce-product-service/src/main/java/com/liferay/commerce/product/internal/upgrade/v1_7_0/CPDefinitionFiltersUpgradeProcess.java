29fdbf60acf03f292b4320cb6540baa1daa22414
Marco Leo <marco.leo@liferay.com>
2019-06-16 19:50:34 +0200

    COMMERCE-1309 Add new upgrade process

---
+++ commerce-product-service/src/main/java/com/liferay/commerce/product/internal/upgrade/v1_7_0/CPDefinitionFiltersUpgradeProcess.java
@@ -0,0 +1,69 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.internal.upgrade.v1_7_0;
+
+import com.liferay.commerce.product.model.impl.CPDefinitionModelImpl;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.upgrade.UpgradeProcess;
+
+/**
+ * @author Alec Sloan
+ */
+public class CPDefinitionFiltersUpgradeProcess extends UpgradeProcess {
+
+	@Override
+	protected void doUpgrade() throws Exception {
+		_addColumn(
+			CPDefinitionModelImpl.class, CPDefinitionModelImpl.TABLE_NAME,
+			"accountGroupFilterEnabled", "BOOLEAN");
+
+		_addColumn(
+			CPDefinitionModelImpl.class, CPDefinitionModelImpl.TABLE_NAME,
+			"channelFilterEnabled", "BOOLEAN");
+	}
+
+	private void _addColumn(
+			Class<?> entityClass, String tableName, String columnName,
+			String columnType)
+		throws Exception {
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				String.format(
+					"Adding column %s to table %s", columnName, tableName));
+		}
+
+		if (!hasColumn(tableName, columnName)) {
+			alter(
+				entityClass,
+				new AlterTableAddColumn(
+					columnName + StringPool.SPACE + columnType));
+		}
+		else {
+			if (_log.isInfoEnabled()) {
+				_log.info(
+					String.format(
+						"Column %s already exists on table %s", columnName,
+						tableName));
+			}
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CPDefinitionFiltersUpgradeProcess.class);
+
+}
