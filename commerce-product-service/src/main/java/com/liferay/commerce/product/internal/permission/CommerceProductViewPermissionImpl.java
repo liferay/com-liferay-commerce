450f15bcd55663e2b28e29e776d2615d6347d91f
Marco Leo <marco.leo@liferay.com>
2019-06-17 00:25:13 +0200

    COMMERCE-1309 Add Product permission util

---
+++ commerce-product-service/src/main/java/com/liferay/commerce/product/internal/permission/CommerceProductViewPermissionImpl.java
@@ -0,0 +1,198 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.internal.permission;
+
+import com.liferay.commerce.account.model.CommerceAccountGroupRel;
+import com.liferay.commerce.account.service.CommerceAccountGroupRelService;
+import com.liferay.commerce.account.util.CommerceAccountHelper;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CommerceCatalog;
+import com.liferay.commerce.product.model.CommerceChannel;
+import com.liferay.commerce.product.model.CommerceChannelRel;
+import com.liferay.commerce.product.permission.CommerceCatalogPermission;
+import com.liferay.commerce.product.permission.CommerceProductViewPermission;
+import com.liferay.commerce.product.service.CPDefinitionLocalService;
+import com.liferay.commerce.product.service.CommerceChannelLocalService;
+import com.liferay.commerce.product.service.CommerceChannelRelLocalService;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.security.auth.PrincipalException;
+import com.liferay.portal.kernel.security.permission.ActionKeys;
+import com.liferay.portal.kernel.security.permission.PermissionChecker;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.util.ArrayUtil;
+
+import java.util.List;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Marco Leo
+ */
+@Component(immediate = true, service = CommerceProductViewPermission.class)
+public class CommerceProductViewPermissionImpl
+	implements CommerceProductViewPermission {
+
+	@Override
+	public void check(
+			PermissionChecker permissionChecker, long groupId,
+			long commerceAccountId, long cpDefinitionId)
+		throws PortalException {
+
+		if (contains(
+				permissionChecker, groupId, commerceAccountId,
+				cpDefinitionId)) {
+
+			return;
+		}
+
+		throw new PrincipalException.MustHavePermission(
+			permissionChecker.getUserId(), CPDefinition.class.getName(),
+			cpDefinitionId, ActionKeys.VIEW);
+	}
+
+	@Override
+	public boolean contains(
+			PermissionChecker permissionChecker, long groupId,
+			long commerceAccountId, long cpDefinitionId)
+		throws PortalException {
+
+		CPDefinition cpDefinition = _cpDefinitionLocalService.getCPDefinition(
+			cpDefinitionId);
+
+		if (!_channelEnabled(groupId, cpDefinition)) {
+			return false;
+		}
+
+		if (_viewCatalog(
+				permissionChecker, cpDefinition.getCommerceCatalog())) {
+
+			return true;
+		}
+
+		if (_accountEnabled(commerceAccountId, cpDefinition)) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private boolean _accountEnabled(
+			long commerceAccountId, CPDefinition cpDefinition)
+		throws PortalException {
+
+		if (!cpDefinition.isAccountGroupFilterEnabled()) {
+			return true;
+		}
+
+		List<CommerceAccountGroupRel> commerceAccountGroupRels =
+			_commerceAccountGroupRelService.getCommerceAccountGroupRels(
+				CPDefinition.class.getName(), cpDefinition.getCPDefinitionId(),
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
+
+		long[] comerceAccountGroupIds =
+			_commerceAccountHelper.getCommerceAccountGroupIds(
+				commerceAccountId);
+
+		for (CommerceAccountGroupRel commerceAccountGroupRel :
+				commerceAccountGroupRels) {
+
+			if (ArrayUtil.contains(
+					comerceAccountGroupIds,
+					commerceAccountGroupRel.getCommerceAccountGroupId())) {
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private boolean _channelEnabled(long groupId, CPDefinition cpDefinition) {
+		if (!cpDefinition.isChannelFilterEnabled()) {
+			return true;
+		}
+
+		List<CommerceChannelRel> commerceChannelRels =
+			_commerceChannelRelLocalService.getCommerceChannelRels(
+				CPDefinition.class.getName(), cpDefinition.getCPDefinitionId(),
+				QueryUtil.ALL_POS, QueryUtil.ALL_POS, null);
+
+		long commerceChannelId = _getCommerceChannelId(groupId);
+
+		for (CommerceChannelRel commerceChannelRel : commerceChannelRels) {
+			if (commerceChannelRel.getCommerceChannelId() ==
+					commerceChannelId) {
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	private long _getCommerceChannelId(long groupId) {
+		Group group = _groupLocalService.fetchGroup(groupId);
+
+		String className = group.getClassName();
+
+		if (className.equals(CommerceChannel.class.getName())) {
+			return group.getClassPK();
+		}
+
+		CommerceChannel commerceChannel =
+			_commerceChannelLocalService.fetchCommerceChannelBySiteGroupId(
+				groupId);
+
+		if (commerceChannel != null) {
+			return commerceChannel.getCommerceChannelId();
+		}
+
+		return 0;
+	}
+
+	private boolean _viewCatalog(
+			PermissionChecker permissionChecker,
+			CommerceCatalog commerceCatalog)
+		throws PortalException {
+
+		return _commerceCatalogPermission.contains(
+			permissionChecker, commerceCatalog, ActionKeys.VIEW);
+	}
+
+	@Reference
+	private CommerceAccountGroupRelService _commerceAccountGroupRelService;
+
+	@Reference
+	private CommerceAccountHelper _commerceAccountHelper;
+
+	@Reference
+	private CommerceCatalogPermission _commerceCatalogPermission;
+
+	@Reference
+	private CommerceChannelLocalService _commerceChannelLocalService;
+
+	@Reference
+	private CommerceChannelRelLocalService _commerceChannelRelLocalService;
+
+	@Reference
+	private CPDefinitionLocalService _cpDefinitionLocalService;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+}
