ee2e32535ea32ede16a48dd4afe72d1ba636bc7a
Marco Leo <marco.leo@liferay.com>
2019-06-17 19:35:53 +0200

    COMMERCE-1309 Check for empty mail

--- headless/headless-commerce/headless-commerce-admin-account-impl/src/main/java/com/liferay/headless/commerce/admin/account/internal/resource/v1_0/AccountResourceImpl.java
+++ headless/headless-commerce/headless-commerce-admin-account-impl/src/main/java/com/liferay/headless/commerce/admin/account/internal/resource/v1_0/AccountResourceImpl.java
@@ -36,7 +36,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.ws.rs.ClientErrorException;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 
@@ -254,9 +253,7 @@ public class AccountResourceImpl extends BaseAccountResourceImpl {
 		}
 
 		if (commerceAccount == null) {
-			throw new ClientErrorException(
-				"Email address should be specified in the request body",
-				Response.Status.CONFLICT);
+			return "";
 		}
 
 		return commerceAccount.getEmail();
