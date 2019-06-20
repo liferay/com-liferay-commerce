7bfb25366ae028bc6894c6553046d2dc4f0c53be
Marco Leo <marco.leo@liferay.com>
2019-06-20 15:42:53 +0200

    COMMERCE-1309 Add upgrade process to add AccoungGroupRel Table

---
+++ commerce-notification-service/src/main/java/com/liferay/commerce/notification/internal/upgrade/CommerceNotificationUpgradeStepRegistrator.java
@@ -0,0 +1,61 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.notification.internal.upgrade;
+
+import com.liferay.commerce.notification.internal.upgrade.v2_0_0.AccountGroupRelUpgradeProcess;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.upgrade.DummyUpgradeProcess;
+import com.liferay.portal.upgrade.registry.UpgradeStepRegistrator;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Marco Leo
+ */
+@Component(immediate = true, service = UpgradeStepRegistrator.class)
+public class CommerceNotificationUpgradeStepRegistrator
+	implements UpgradeStepRegistrator {
+
+	@Override
+	public void register(Registry registry) {
+		if (_log.isInfoEnabled()) {
+			_log.info("COMMERCE NOTIFICATION UPGRADE STEP REGISTRATOR STARTED");
+		}
+
+		registry.register(
+			_SCHEMA_VERSION_1_0_0, _SCHEMA_VERSION_1_1_0,
+			new DummyUpgradeProcess());
+
+		registry.register(
+			_SCHEMA_VERSION_1_1_0, _SCHEMA_VERSION_2_0_0,
+			new AccountGroupRelUpgradeProcess());
+
+		if (_log.isInfoEnabled()) {
+			_log.info(
+				"COMMERCE NOTIFICATION UPGRADE STEP REGISTRATOR FINISHED");
+		}
+	}
+
+	private static final String _SCHEMA_VERSION_1_0_0 = "1.0.0";
+
+	private static final String _SCHEMA_VERSION_1_1_0 = "1.1.0";
+
+	private static final String _SCHEMA_VERSION_2_0_0 = "2.0.0";
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		CommerceNotificationUpgradeStepRegistrator.class);
+
+}
