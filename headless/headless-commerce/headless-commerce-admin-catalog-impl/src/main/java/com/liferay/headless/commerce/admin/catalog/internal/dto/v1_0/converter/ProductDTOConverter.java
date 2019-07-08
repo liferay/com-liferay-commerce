fd520208a58a7411b1343a929fb77c455cfcf150
Alessio Rendina <asilcomando@gmail.com>
2019-07-08 15:45:29 +0200

    COMMERCE-1505 Fixed product dto converter

--- headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/dto/v1_0/converter/ProductDTOConverter.java
+++ headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/dto/v1_0/converter/ProductDTOConverter.java
@@ -14,6 +14,10 @@
 
 package com.liferay.headless.commerce.admin.catalog.internal.dto.v1_0.converter;
 
+import com.liferay.asset.kernel.model.AssetCategory;
+import com.liferay.asset.kernel.model.AssetTag;
+import com.liferay.asset.kernel.service.AssetCategoryService;
+import com.liferay.asset.kernel.service.AssetTagService;
 import com.liferay.commerce.model.CPDefinitionInventory;
 import com.liferay.commerce.product.model.CPAttachmentFileEntry;
 import com.liferay.commerce.product.model.CPAttachmentFileEntryConstants;
@@ -24,9 +28,9 @@ import com.liferay.commerce.product.model.CProduct;
 import com.liferay.commerce.product.model.CommerceCatalog;
 import com.liferay.commerce.product.service.CPDefinitionLinkService;
 import com.liferay.commerce.product.service.CPDefinitionService;
-import com.liferay.commerce.product.service.CommerceCatalogService;
 import com.liferay.expando.kernel.model.ExpandoBridge;
 import com.liferay.headless.commerce.admin.catalog.dto.v1_0.Attachment;
+import com.liferay.headless.commerce.admin.catalog.dto.v1_0.Category;
 import com.liferay.headless.commerce.admin.catalog.dto.v1_0.Product;
 import com.liferay.headless.commerce.admin.catalog.dto.v1_0.ProductConfiguration;
 import com.liferay.headless.commerce.admin.catalog.dto.v1_0.ProductOption;
@@ -92,6 +96,7 @@ public class ProductDTOConverter implements DTOConverter {
 					cpDefinition, CPAttachmentFileEntryConstants.TYPE_OTHER,
 					dtoConverterContext);
 				catalogId = _getCommerceCatalogId(cpDefinition);
+				categories = _getCategories(cpDefinition, dtoConverterContext);
 				configuration =
 					(ProductConfiguration)
 						productConfigurationDTOConverter.toDTO(
@@ -131,6 +136,7 @@ public class ProductDTOConverter implements DTOConverter {
 					(ProductTaxConfiguration)
 						productTaxConfigurationDTOConverter.toDTO(
 							dtoConverterContext);
+				tags = _getTags(cpDefinition);
 			}
 		};
 	}
@@ -160,6 +166,34 @@ public class ProductDTOConverter implements DTOConverter {
 		Stream<Attachment> stream = attachments.stream();
 
 		return stream.toArray(Attachment[]::new);
+	}
+
+	private Category[] _getCategories(
+			CPDefinition cpDefinition, DTOConverterContext dtoConverterContext)
+		throws Exception {
+
+		List<Category> categories = new ArrayList<>();
+
+		List<AssetCategory> assetCategories =
+			_assetCategoryService.getCategories(
+				cpDefinition.getModelClassName(),
+				cpDefinition.getCPDefinitionId());
+
+		DTOConverter categoryDTOConverter =
+			_dtoConverterRegistry.getDTOConverter(
+				AssetCategory.class.getName());
+
+		for (AssetCategory assetCategory : assetCategories) {
+			categories.add(
+				(Category)categoryDTOConverter.toDTO(
+					new DefaultDTOConverterContext(
+						dtoConverterContext.getLocale(),
+						assetCategory.getCategoryId())));
+		}
+
+		Stream<Category> stream = categories.stream();
+
+		return stream.toArray(Category[]::new);
 	}
 
 	private long _getCommerceCatalogId(CPDefinition cpDefinition) {
@@ -225,8 +259,22 @@ public class ProductDTOConverter implements DTOConverter {
 		return stream.toArray(RelatedProduct[]::new);
 	}
 
+	private String[] _getTags(CPDefinition cpDefinition) {
+		List<AssetTag> assetEntryAssetTags = _assetTagService.getTags(
+			cpDefinition.getModelClassName(), cpDefinition.getCPDefinitionId());
+
+		Stream<AssetTag> stream = assetEntryAssetTags.stream();
+
+		return (String[])stream.map(
+			AssetTag::getName
+		).toArray();
+	}
+
 	@Reference
-	private CommerceCatalogService _commerceCatalogService;
+	private AssetCategoryService _assetCategoryService;
+
+	@Reference
+	private AssetTagService _assetTagService;
 
 	@Reference
 	private CPDefinitionLinkService _cpDefinitionLinkService;
