cbaef4e06defd81939587fd25537afbae5904154
Alec Sloan <Alec.sloan@Liferay.com>
2019-07-09 16:51:53 -0700

    MKTG-6916 Add permission importing or default to view for Site Members

--- commerce-initializer-util/src/main/java/com/liferay/commerce/initializer/util/DLImporter.java
+++ commerce-initializer-util/src/main/java/com/liferay/commerce/initializer/util/DLImporter.java
@@ -21,13 +21,19 @@ import com.liferay.document.library.kernel.model.DLFolderConstants;
 import com.liferay.document.library.kernel.service.DLFileEntryLocalService;
 import com.liferay.document.library.kernel.service.DLFolderLocalService;
 import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
 import com.liferay.portal.kernel.json.JSONObject;
 import com.liferay.portal.kernel.model.Repository;
+import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.RepositoryLocalService;
+import com.liferay.portal.kernel.service.ResourcePermissionLocalService;
+import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.FileUtil;
 import com.liferay.portal.kernel.util.MimeTypesUtil;
 import com.liferay.portal.kernel.util.Validator;
@@ -64,6 +70,41 @@ public class DLImporter {
 				jsonArray.getJSONObject(i), classLoader,
 				documentsDependenciesPath, userId, scopeGroupId,
 				serviceContext);
+		}
+	}
+
+	protected void updatePermissions(
+			long companyId, String name, String primKey, JSONArray jsonArray)
+		throws PortalException {
+
+		if (jsonArray == null) {
+			jsonArray = JSONFactoryUtil.createJSONArray(
+				"[{\"actionIds\": [\"VIEW\"], \"roleName\": \"Site Member\"," +
+					"\"scope\": 4}]");
+		}
+
+		for (int i = 0; i < jsonArray.length(); i++) {
+			JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+			int scope = jsonObject.getInt("scope");
+
+			String roleName = jsonObject.getString("roleName");
+
+			Role role = _roleLocalService.getRole(companyId, roleName);
+
+			String[] actionIds = new String[0];
+
+			JSONArray actionIdsJSONArray = jsonObject.getJSONArray("actionIds");
+
+			if (actionIdsJSONArray != null) {
+				for (int j = 0; j < actionIdsJSONArray.length(); j++) {
+					actionIds = ArrayUtil.append(
+						actionIds, actionIdsJSONArray.getString(j));
+				}
+			}
+
+			_resourcePermissionLocalService.setResourcePermissions(
+				companyId, name, scope, primKey, role.getRoleId(), actionIds);
 		}
 	}
 
@@ -117,6 +158,25 @@ public class DLImporter {
 				StringPool.BLANK,
 				DLFileEntryTypeConstants.FILE_ENTRY_TYPE_ID_BASIC_DOCUMENT,
 				null, file, inputStream, file.length(), serviceContext);
+			JSONArray permissionsJSONArray = jsonObject.getJSONArray(
+				"permissions");
+
+			if ((permissionsJSONArray != null) &&
+				(permissionsJSONArray.length() > 0)) {
+
+				updatePermissions(
+					dlFileEntry.getCompanyId(), dlFileEntry.getModelClassName(),
+					String.valueOf(dlFileEntry.getFileEntryId()),
+					permissionsJSONArray);
+			}
+			else {
+
+				// Give site members view permissions
+
+				updatePermissions(
+					dlFileEntry.getCompanyId(), dlFileEntry.getModelClassName(),
+					String.valueOf(dlFileEntry.getPrimaryKey()), null);
+			}
 		}
 
 		return dlFileEntry;
@@ -137,10 +197,30 @@ public class DLImporter {
 
 		Repository repository = repositories.get(0);
 
-		return _dlFolderLocalService.addFolder(
+		DLFolder dlFolder = _dlFolderLocalService.addFolder(
 			userId, scopeGroupId, repository.getRepositoryId(), false,
 			DLFolderConstants.DEFAULT_PARENT_FOLDER_ID, name, description,
 			false, serviceContext);
+
+		JSONArray permissionsJSONArray = jsonObject.getJSONArray("permissions");
+
+		if ((permissionsJSONArray != null) &&
+			(permissionsJSONArray.length() > 0)) {
+
+			updatePermissions(
+				dlFolder.getCompanyId(), dlFolder.getModelClassName(),
+				String.valueOf(dlFolder.getPrimaryKey()), permissionsJSONArray);
+		}
+		else {
+
+			// Give site members view permissions
+
+			updatePermissions(
+				dlFolder.getCompanyId(), dlFolder.getModelClassName(),
+				String.valueOf(dlFolder.getPrimaryKey()), null);
+		}
+
+		return dlFolder;
 	}
 
 	@Reference
@@ -151,6 +231,12 @@ public class DLImporter {
 
 	@Reference
 	private RepositoryLocalService _repositoryLocalService;
+
+	@Reference
+	private ResourcePermissionLocalService _resourcePermissionLocalService;
+
+	@Reference
+	private RoleLocalService _roleLocalService;
 
 	@Reference
 	private UserLocalService _userLocalService;
