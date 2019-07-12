714c5fed8adcf1b6688bcc18541f86829053147b
Alessio Rendina <asilcomando@gmail.com>
2019-07-12 15:54:21 +0200

    COMMERCE-1056 Refactor commerce regions starters

--- commerce-service/src/main/java/com/liferay/commerce/internal/starter/ItalyCommerceRegionsStarter.java
+++ commerce-service/src/main/java/com/liferay/commerce/internal/starter/ItalyCommerceRegionsStarter.java
@@ -15,6 +15,9 @@
 package com.liferay.commerce.internal.starter;
 
 import com.liferay.commerce.starter.CommerceRegionsStarter;
+import com.liferay.portal.kernel.language.LanguageUtil;
+
+import java.util.Locale;
 
 import org.osgi.service.component.annotations.Component;
 
@@ -29,6 +32,16 @@ import org.osgi.service.component.annotations.Component;
 public class ItalyCommerceRegionsStarter extends BaseCommerceRegionsStarter {
 
 	public static final int ITALY_NUMERIC_ISO_CODE = 380;
+
+	@Override
+	public String getKey() {
+		return String.valueOf(ITALY_NUMERIC_ISO_CODE);
+	}
+
+	@Override
+	public String getLabel(Locale locale) {
+		return LanguageUtil.get(locale, "italy");
+	}
 
 	@Override
 	protected int getCountryIsoCode() {
