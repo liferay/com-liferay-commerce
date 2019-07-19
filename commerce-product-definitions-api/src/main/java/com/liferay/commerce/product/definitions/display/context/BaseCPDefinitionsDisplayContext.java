8715b826965e2c603e56a39346bcc78bc7429310
Luca Pellizzon <luca.pellizzon@liferay.com>
2019-07-19 09:43:24 +0200

    COMMERCE-1453 moved utilities and base classes to product-definitions-api module

---
+++ commerce-product-definitions-api/src/main/java/com/liferay/commerce/product/definitions/display/context/BaseCPDefinitionsDisplayContext.java
@@ -0,0 +1,195 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.definitions.display.context;
+
+import com.liferay.commerce.product.definitions.display.context.util.CPDefinitionRequestHelper;
+import com.liferay.commerce.product.definitions.portlet.action.ActionHelper;
+import com.liferay.commerce.product.definitions.servlet.taglib.ui.CPDefinitionScreenNavigationConstants;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.model.CPInstance;
+import com.liferay.commerce.product.model.CommerceCatalog;
+import com.liferay.commerce.product.type.CPType;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.portlet.LiferayPortletRequest;
+import com.liferay.portal.kernel.portlet.LiferayPortletResponse;
+import com.liferay.portal.kernel.portlet.PortalPreferences;
+import com.liferay.portal.kernel.portlet.PortletPreferencesFactoryUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Validator;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.List;
+import java.util.Locale;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * @author Alessio Antonio Rendina
+ * @author Marco Leo
+ * @author Luca Pellizzon
+ */
+public abstract class BaseCPDefinitionsDisplayContext {
+
+	public BaseCPDefinitionsDisplayContext(
+		ActionHelper actionHelper, HttpServletRequest httpServletRequest) {
+
+		this.actionHelper = actionHelper;
+		this.httpServletRequest = httpServletRequest;
+
+		portalPreferences = PortletPreferencesFactoryUtil.getPortalPreferences(
+			this.httpServletRequest);
+
+		requestHelper = new CPDefinitionRequestHelper(httpServletRequest);
+
+		liferayPortletRequest = requestHelper.getLiferayPortletRequest();
+		liferayPortletResponse = requestHelper.getLiferayPortletResponse();
+	}
+
+	public CommerceCatalog getCommerceCatalog() throws PortalException {
+		if (_commerceCatalog != null) {
+			return _commerceCatalog;
+		}
+
+		CPDefinition cpDefinition = getCPDefinition();
+
+		if (cpDefinition == null) {
+			return null;
+		}
+
+		_commerceCatalog = _cpDefinition.getCommerceCatalog();
+
+		return _commerceCatalog;
+	}
+
+	public CPDefinition getCPDefinition() throws PortalException {
+		if (_cpDefinition != null) {
+			return _cpDefinition;
+		}
+
+		_cpDefinition = actionHelper.getCPDefinition(
+			requestHelper.getRenderRequest());
+
+		return _cpDefinition;
+	}
+
+	public long getCPDefinitionId() throws PortalException {
+		CPDefinition cpDefinition = getCPDefinition();
+
+		if (cpDefinition == null) {
+			return 0;
+		}
+
+		return cpDefinition.getCPDefinitionId();
+	}
+
+	public CPType getCPType() throws PortalException {
+		CPDefinition cpDefinition = getCPDefinition();
+
+		if (cpDefinition == null) {
+			return actionHelper.getCPType(
+				ParamUtil.getString(httpServletRequest, "productTypeName"));
+		}
+
+		return actionHelper.getCPType(cpDefinition.getProductTypeName());
+	}
+
+	public CPType getCPType(String name) {
+		return actionHelper.getCPType(name);
+	}
+
+	public List<CPType> getCPTypes() {
+		return actionHelper.getCPTypes();
+	}
+
+	public PortletURL getEditProductDefinitionURL() throws PortalException {
+		PortletURL portletURL = liferayPortletResponse.createRenderURL();
+
+		portletURL.setParameter(
+			"mvcRenderCommandName", "editProductDefinition");
+		portletURL.setParameter(
+			"cpDefinitionId", String.valueOf(getCPDefinitionId()));
+
+		return portletURL;
+	}
+
+	public PortletURL getPortletURL() throws PortalException {
+		PortletURL portletURL = liferayPortletResponse.createRenderURL();
+
+		String redirect = ParamUtil.getString(httpServletRequest, "redirect");
+
+		if (Validator.isNotNull(redirect)) {
+			portletURL.setParameter("redirect", redirect);
+		}
+
+		CPDefinition cpDefinition = getCPDefinition();
+
+		if (cpDefinition != null) {
+			portletURL.setParameter(
+				"cpDefinitionId", String.valueOf(getCPDefinitionId()));
+		}
+
+		return portletURL;
+	}
+
+	public long getScopeGroupId() {
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		return themeDisplay.getScopeGroupId();
+	}
+
+	public String getScreenNavigationCategoryKey() {
+		return CPDefinitionScreenNavigationConstants.CATEGORY_KEY_DETAILS;
+	}
+
+	public String getSelectedScreenNavigationCategoryKey() {
+		return ParamUtil.getString(
+			httpServletRequest, "screenNavigationCategoryKey",
+			getScreenNavigationCategoryKey());
+	}
+
+	public String getSku(CPDefinition cpDefinition, Locale locale) {
+		List<CPInstance> cpInstances = cpDefinition.getCPInstances();
+
+		if (cpInstances.isEmpty()) {
+			return StringPool.BLANK;
+		}
+
+		if (cpInstances.size() > 1) {
+			return LanguageUtil.get(locale, "multiple-skus");
+		}
+
+		CPInstance cpInstance = cpInstances.get(0);
+
+		return cpInstance.getSku();
+	}
+
+	protected final ActionHelper actionHelper;
+	protected final HttpServletRequest httpServletRequest;
+	protected final LiferayPortletRequest liferayPortletRequest;
+	protected final LiferayPortletResponse liferayPortletResponse;
+	protected final PortalPreferences portalPreferences;
+	protected final CPDefinitionRequestHelper requestHelper;
+
+	private CommerceCatalog _commerceCatalog;
+	private CPDefinition _cpDefinition;
+
+}
---
+++ commerce-product-definitions-api/src/main/java/com/liferay/commerce/product/definitions/servlet/taglib/ui/CPInstanceScreenNavigationConstants.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.product.definitions.servlet.taglib.ui;
+
+/**
+ * @author Alessio Antonio Rendina
+ * @author Luca Pellizzon
+ */
+public class CPInstanceScreenNavigationConstants {
+
+	public static final String CATEGORY_KEY_DETAILS = "details";
+
+	public static final String ENTRY_KEY_PRICING = "pricing";
+
+	public static final String ENTRY_KEY_SHIPPING_OVERRIDE =
+		"shipping-override";
+
+	public static final String ENTRY_KEY_SUBSCRIPTION_OVERRIDE =
+		"subscription-override";
+
+	public static final String SCREEN_NAVIGATION_KEY_CP_INSTANCE_GENERAL =
+		"cp.instance.general";
+
+}
