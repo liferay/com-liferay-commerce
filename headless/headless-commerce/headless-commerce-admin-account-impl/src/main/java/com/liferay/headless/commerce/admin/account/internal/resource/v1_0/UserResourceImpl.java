d2bafed4ef9a9220ed27e7d6e1800d503dbbfaf0
Alessio Rendina <asilcomando@gmail.com>
2019-07-23 11:06:42 +0200

    COMMERCE-1622 Refactor user resource implementation

--- headless/headless-commerce/headless-commerce-admin-account-impl/src/main/java/com/liferay/headless/commerce/admin/account/internal/resource/v1_0/UserResourceImpl.java
+++ headless/headless-commerce/headless-commerce-admin-account-impl/src/main/java/com/liferay/headless/commerce/admin/account/internal/resource/v1_0/UserResourceImpl.java
@@ -27,10 +27,15 @@ import com.liferay.headless.commerce.core.util.ServiceContextHelper;
 import com.liferay.portal.kernel.model.Role;
 import com.liferay.portal.kernel.service.RoleLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.UserLocalService;
 import com.liferay.portal.kernel.service.UserService;
 import com.liferay.portal.kernel.util.ArrayUtil;
+import com.liferay.portal.kernel.util.CalendarFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+
+import java.util.Calendar;
+import java.util.Date;
 
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
@@ -63,14 +68,48 @@ public class UserResourceImpl extends BaseUserResourceImpl {
 		ServiceContext serviceContext = _serviceContextHelper.getServiceContext(
 			commerceAccount.getCommerceAccountGroupId());
 
-		com.liferay.portal.kernel.model.User invitedUser = _userService.addUser(
-			contextCompany.getCompanyId(), true, null, null, true, null,
-			user.getEmail(), 0L, null, LocaleUtil.getDefault(),
-			user.getFirstName(), user.getMiddleName(), user.getLastName(), 0L,
-			0L, GetterUtil.getBoolean(user.getMale(), true), 1, 1, 1970,
-			user.getJobTitle(),
-			new long[] {commerceAccount.getCommerceAccountGroupId()}, null,
-			null, null, false, serviceContext);
+		com.liferay.portal.kernel.model.User invitedUser =
+			_userLocalService.fetchUserByReferenceCode(
+				contextCompany.getCompanyId(), user.getExternalReferenceCode());
+
+		if (invitedUser == null) {
+			invitedUser = _userService.addUser(
+				contextCompany.getCompanyId(), true, null, null, true, null,
+				user.getEmail(), 0L, null, LocaleUtil.getDefault(),
+				user.getFirstName(), user.getMiddleName(), user.getLastName(),
+				0L, 0L, user.getMale(), 1, 1, 1970, user.getJobTitle(),
+				new long[] {commerceAccount.getCommerceAccountGroupId()}, null,
+				null, null, false, serviceContext);
+		}
+		else {
+			Date birthday = invitedUser.getBirthday();
+
+			Calendar birthdayCalendar = CalendarFactoryUtil.getCalendar(
+				birthday.getTime(), invitedUser.getTimeZone());
+
+			invitedUser = _userService.updateUser(
+				invitedUser.getUserId(), null, null, null, false,
+				invitedUser.getReminderQueryQuestion(),
+				invitedUser.getReminderQueryAnswer(),
+				invitedUser.getScreenName(),
+				GetterUtil.get(user.getEmail(), invitedUser.getEmailAddress()),
+				invitedUser.getFacebookId(), invitedUser.getOpenId(),
+				invitedUser.getLanguageId(), invitedUser.getTimeZoneId(),
+				invitedUser.getGreeting(), invitedUser.getComments(),
+				user.getFirstName(),
+				GetterUtil.get(
+					user.getMiddleName(), invitedUser.getMiddleName()),
+				user.getLastName(), 0L, 0L,
+				GetterUtil.get(user.getMale(), invitedUser.isMale()),
+				birthdayCalendar.get(Calendar.MONTH),
+				birthdayCalendar.get(Calendar.DAY_OF_MONTH),
+				birthdayCalendar.get(Calendar.YEAR), null, null, null, null,
+				null,
+				GetterUtil.get(user.getJobTitle(), invitedUser.getJobTitle()),
+				invitedUser.getGroupIds(), invitedUser.getOrganizationIds(),
+				invitedUser.getRoleIds(), null, invitedUser.getUserGroupIds(),
+				serviceContext);
+		}
 
 		// Account rel
 
@@ -114,6 +153,9 @@ public class UserResourceImpl extends BaseUserResourceImpl {
 
 	@Reference
 	private ServiceContextHelper _serviceContextHelper;
+
+	@Reference
+	private UserLocalService _userLocalService;
 
 	@Reference
 	private UserService _userService;
