f649ffa74ed4bac13f3b87c06a8eb8845bf7bd1b
Alessio Rendina <asilcomando@gmail.com>
2019-07-24 21:29:52 +0200

    COMMERCE-1622 Regen rest module

---
+++ headless/headless-commerce/headless-commerce-admin-inventory-impl/src/main/java/com/liferay/headless/commerce/admin/inventory/internal/graphql/mutation/v1_0/Mutation.java
@@ -0,0 +1,235 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.commerce.admin.inventory.internal.graphql.mutation.v1_0;
+
+import com.liferay.headless.commerce.admin.inventory.dto.v1_0.Warehouse;
+import com.liferay.headless.commerce.admin.inventory.dto.v1_0.WarehouseItem;
+import com.liferay.headless.commerce.admin.inventory.resource.v1_0.WarehouseItemResource;
+import com.liferay.headless.commerce.admin.inventory.resource.v1_0.WarehouseResource;
+import com.liferay.petra.function.UnsafeConsumer;
+import com.liferay.petra.function.UnsafeFunction;
+import com.liferay.portal.kernel.security.auth.CompanyThreadLocal;
+import com.liferay.portal.kernel.service.CompanyLocalServiceUtil;
+
+import graphql.annotations.annotationTypes.GraphQLField;
+import graphql.annotations.annotationTypes.GraphQLInvokeDetached;
+import graphql.annotations.annotationTypes.GraphQLName;
+
+import javax.annotation.Generated;
+
+import javax.ws.rs.core.Response;
+
+import org.osgi.service.component.ComponentServiceObjects;
+
+/**
+ * @author Alessio Antonio Rendina
+ * @generated
+ */
+@Generated("")
+public class Mutation {
+
+	public static void setWarehouseResourceComponentServiceObjects(
+		ComponentServiceObjects<WarehouseResource>
+			warehouseResourceComponentServiceObjects) {
+
+		_warehouseResourceComponentServiceObjects =
+			warehouseResourceComponentServiceObjects;
+	}
+
+	public static void setWarehouseItemResourceComponentServiceObjects(
+		ComponentServiceObjects<WarehouseItemResource>
+			warehouseItemResourceComponentServiceObjects) {
+
+		_warehouseItemResourceComponentServiceObjects =
+			warehouseItemResourceComponentServiceObjects;
+	}
+
+	@GraphQLInvokeDetached
+	public Response deleteWarehousId(@GraphQLName("id") Long id)
+		throws Exception {
+
+		return _applyComponentServiceObjects(
+			_warehouseResourceComponentServiceObjects,
+			this::_populateResourceContext,
+			warehouseResource -> warehouseResource.deleteWarehousId(id));
+	}
+
+	@GraphQLInvokeDetached
+	public Response patchWarehousId(
+			@GraphQLName("id") Long id,
+			@GraphQLName("warehouse") Warehouse warehouse)
+		throws Exception {
+
+		return _applyComponentServiceObjects(
+			_warehouseResourceComponentServiceObjects,
+			this::_populateResourceContext,
+			warehouseResource -> warehouseResource.patchWarehousId(
+				id, warehouse));
+	}
+
+	@GraphQLInvokeDetached
+	public Response deleteWarehousByExternalReferenceCode(
+			@GraphQLName("externalReferenceCode") String externalReferenceCode)
+		throws Exception {
+
+		return _applyComponentServiceObjects(
+			_warehouseResourceComponentServiceObjects,
+			this::_populateResourceContext,
+			warehouseResource ->
+				warehouseResource.deleteWarehousByExternalReferenceCode(
+					externalReferenceCode));
+	}
+
+	@GraphQLInvokeDetached
+	public Response patchWarehousByExternalReferenceCode(
+			@GraphQLName("externalReferenceCode") String externalReferenceCode,
+			@GraphQLName("warehouse") Warehouse warehouse)
+		throws Exception {
+
+		return _applyComponentServiceObjects(
+			_warehouseResourceComponentServiceObjects,
+			this::_populateResourceContext,
+			warehouseResource ->
+				warehouseResource.patchWarehousByExternalReferenceCode(
+					externalReferenceCode, warehouse));
+	}
+
+	@GraphQLField
+	@GraphQLInvokeDetached
+	public Warehouse postWarehous(@GraphQLName("warehouse") Warehouse warehouse)
+		throws Exception {
+
+		return _applyComponentServiceObjects(
+			_warehouseResourceComponentServiceObjects,
+			this::_populateResourceContext,
+			warehouseResource -> warehouseResource.postWarehous(warehouse));
+	}
+
+	@GraphQLField
+	@GraphQLInvokeDetached
+	public WarehouseItem postWarehousIdWarehouseItem(
+			@GraphQLName("id") Long id,
+			@GraphQLName("warehouseItem") WarehouseItem warehouseItem)
+		throws Exception {
+
+		return _applyComponentServiceObjects(
+			_warehouseItemResourceComponentServiceObjects,
+			this::_populateResourceContext,
+			warehouseItemResource ->
+				warehouseItemResource.postWarehousIdWarehouseItem(
+					id, warehouseItem));
+	}
+
+	@GraphQLField
+	@GraphQLInvokeDetached
+	public WarehouseItem postWarehousByExternalReferenceCodeWarehouseItem(
+			@GraphQLName("externalReferenceCode") String externalReferenceCode,
+			@GraphQLName("warehouseItem") WarehouseItem warehouseItem)
+		throws Exception {
+
+		return _applyComponentServiceObjects(
+			_warehouseItemResourceComponentServiceObjects,
+			this::_populateResourceContext,
+			warehouseItemResource ->
+				warehouseItemResource.
+					postWarehousByExternalReferenceCodeWarehouseItem(
+						externalReferenceCode, warehouseItem));
+	}
+
+	@GraphQLInvokeDetached
+	public Response deleteWarehouseItem(@GraphQLName("id") Long id)
+		throws Exception {
+
+		return _applyComponentServiceObjects(
+			_warehouseItemResourceComponentServiceObjects,
+			this::_populateResourceContext,
+			warehouseItemResource -> warehouseItemResource.deleteWarehouseItem(
+				id));
+	}
+
+	@GraphQLInvokeDetached
+	public Response patchWarehouseItem(
+			@GraphQLName("id") Long id,
+			@GraphQLName("warehouseItem") WarehouseItem warehouseItem)
+		throws Exception {
+
+		return _applyComponentServiceObjects(
+			_warehouseItemResourceComponentServiceObjects,
+			this::_populateResourceContext,
+			warehouseItemResource -> warehouseItemResource.patchWarehouseItem(
+				id, warehouseItem));
+	}
+
+	private <T, R, E1 extends Throwable, E2 extends Throwable> R
+			_applyComponentServiceObjects(
+				ComponentServiceObjects<T> componentServiceObjects,
+				UnsafeConsumer<T, E1> unsafeConsumer,
+				UnsafeFunction<T, R, E2> unsafeFunction)
+		throws E1, E2 {
+
+		T resource = componentServiceObjects.getService();
+
+		try {
+			unsafeConsumer.accept(resource);
+
+			return unsafeFunction.apply(resource);
+		}
+		finally {
+			componentServiceObjects.ungetService(resource);
+		}
+	}
+
+	private <T, E1 extends Throwable, E2 extends Throwable> void
+			_applyVoidComponentServiceObjects(
+				ComponentServiceObjects<T> componentServiceObjects,
+				UnsafeConsumer<T, E1> unsafeConsumer,
+				UnsafeConsumer<T, E2> unsafeFunction)
+		throws E1, E2 {
+
+		T resource = componentServiceObjects.getService();
+
+		try {
+			unsafeConsumer.accept(resource);
+
+			unsafeFunction.accept(resource);
+		}
+		finally {
+			componentServiceObjects.ungetService(resource);
+		}
+	}
+
+	private void _populateResourceContext(WarehouseResource warehouseResource)
+		throws Exception {
+
+		warehouseResource.setContextCompany(
+			CompanyLocalServiceUtil.getCompany(
+				CompanyThreadLocal.getCompanyId()));
+	}
+
+	private void _populateResourceContext(
+			WarehouseItemResource warehouseItemResource)
+		throws Exception {
+
+		warehouseItemResource.setContextCompany(
+			CompanyLocalServiceUtil.getCompany(
+				CompanyThreadLocal.getCompanyId()));
+	}
+
+	private static ComponentServiceObjects<WarehouseResource>
+		_warehouseResourceComponentServiceObjects;
+	private static ComponentServiceObjects<WarehouseItemResource>
+		_warehouseItemResourceComponentServiceObjects;
+
+}
