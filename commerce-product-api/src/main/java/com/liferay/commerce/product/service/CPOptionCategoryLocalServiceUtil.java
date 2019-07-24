8909146f1f0cb44c181473133523cf4ea9f7fa81
Igor Beslic <igor.beslic@liferay.com>
2019-07-24 18:17:35 +0200

    COMMERCE-1632 commerce - product service - Autogenerated

--- commerce-product-api/src/main/java/com/liferay/commerce/product/service/CPOptionCategoryLocalServiceUtil.java
+++ commerce-product-api/src/main/java/com/liferay/commerce/product/service/CPOptionCategoryLocalServiceUtil.java
@@ -375,13 +375,11 @@ public class CPOptionCategoryLocalServiceUtil {
 				long cpOptionCategoryId,
 				java.util.Map<java.util.Locale, String> titleMap,
 				java.util.Map<java.util.Locale, String> descriptionMap,
-				double priority, String key,
-				com.liferay.portal.kernel.service.ServiceContext serviceContext)
+				double priority, String key)
 		throws com.liferay.portal.kernel.exception.PortalException {
 
 		return getService().updateCPOptionCategory(
-			cpOptionCategoryId, titleMap, descriptionMap, priority, key,
-			serviceContext);
+			cpOptionCategoryId, titleMap, descriptionMap, priority, key);
 	}
 
 	public static CPOptionCategoryLocalService getService() {
