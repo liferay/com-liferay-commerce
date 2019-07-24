ee3a8d19b4ee281c782da3ee6530b2e15734257a
Alessio Rendina <asilcomando@gmail.com>
2019-07-24 16:35:22 +0200

    COMMERCE-1622 Set user external reference code

--- headless/headless-commerce/headless-commerce-admin-account-impl/src/main/java/com/liferay/headless/commerce/admin/account/internal/resource/v1_0/UserResourceImpl.java
+++ headless/headless-commerce/headless-commerce-admin-account-impl/src/main/java/com/liferay/headless/commerce/admin/account/internal/resource/v1_0/UserResourceImpl.java
@@ -33,6 +33,7 @@ import com.liferay.portal.kernel.util.ArrayUtil;
 import com.liferay.portal.kernel.util.CalendarFactoryUtil;
 import com.liferay.portal.kernel.util.GetterUtil;
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.Validator;
 
 import java.util.Calendar;
 import java.util.Date;
@@ -109,6 +110,15 @@ public class UserResourceImpl extends BaseUserResourceImpl {
 				invitedUser.getGroupIds(), invitedUser.getOrganizationIds(),
 				invitedUser.getRoleIds(), null, invitedUser.getUserGroupIds(),
 				serviceContext);
+		}
+
+		// External reference code
+
+		if (Validator.isNotNull(user.getExternalReferenceCode())) {
+			invitedUser.setExternalReferenceCode(
+				user.getExternalReferenceCode());
+
+			_userLocalService.updateUser(invitedUser);
 		}
 
 		// Account rel
