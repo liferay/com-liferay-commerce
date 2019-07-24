f9ca015ce2a2e04ac124e8a9dcf03f31028fc728
Igor Beslic <igor.beslic@liferay.com>
2019-07-24 18:14:51 +0200

    COMMERCE-1632 commerce - service - cp definition inventory service impl - avoid serviceContext if possible

--- commerce-service/src/main/java/com/liferay/commerce/service/impl/CPDefinitionInventoryServiceImpl.java
+++ commerce-service/src/main/java/com/liferay/commerce/service/impl/CPDefinitionInventoryServiceImpl.java
@@ -17,11 +17,14 @@ package com.liferay.commerce.service.impl;
 import com.liferay.commerce.constants.CommerceActionKeys;
 import com.liferay.commerce.constants.CommerceConstants;
 import com.liferay.commerce.model.CPDefinitionInventory;
+import com.liferay.commerce.product.model.CPDefinition;
+import com.liferay.commerce.product.service.CPDefinitionLocalService;
 import com.liferay.commerce.service.base.CPDefinitionInventoryServiceBaseImpl;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermissionFactory;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.spring.extender.service.ServiceReference;
 
 /**
  * @author Alessio Antonio Rendina
@@ -29,6 +32,51 @@ import com.liferay.portal.kernel.service.ServiceContext;
 public class CPDefinitionInventoryServiceImpl
 	extends CPDefinitionInventoryServiceBaseImpl {
 
+	@Override
+	public CPDefinitionInventory addCPDefinitionInventory(
+			long cpDefinitionId, String cpDefinitionInventoryEngine,
+			String lowStockActivity, boolean displayAvailability,
+			boolean displayStockQuantity, int minStockQuantity,
+			boolean backOrders, int minOrderQuantity, int maxOrderQuantity,
+			String allowedOrderQuantities, int multipleOrderQuantity)
+		throws PortalException {
+
+		CPDefinition cpDefinition = _cpDefinitionLocalService.getCPDefinition(
+			cpDefinitionId);
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), cpDefinition.getGroupId(),
+			CommerceActionKeys.MANAGE_COMMERCE_PRODUCT_DEFINITION_INVENTORY);
+
+		return cpDefinitionInventoryLocalService.addCPDefinitionInventory(
+			cpDefinitionId, cpDefinitionInventoryEngine, lowStockActivity,
+			displayAvailability, displayStockQuantity, minStockQuantity,
+			backOrders, minOrderQuantity, maxOrderQuantity,
+			allowedOrderQuantities, multipleOrderQuantity);
+	}
+
+	/**
+	 * Adds new CP definition inventory.
+	 *
+	 * @param      cpDefinitionId
+	 * @param      cpDefinitionInventoryEngine
+	 * @param      lowStockActivity
+	 * @param      displayAvailability
+	 * @param      displayStockQuantity
+	 * @param      minStockQuantity
+	 * @param      backOrders
+	 * @param      minOrderQuantity
+	 * @param      maxOrderQuantity
+	 * @param      allowedOrderQuantities
+	 * @param      multipleOrderQuantity
+	 * @param      serviceContext
+	 * @return
+	 * @throws     PortalException
+	 * @deprecated As of Mueller (7.2.x), see {@link
+	 *             #addCPDefinitionInventory(long, String, String, boolean,
+	 *             boolean, int, boolean, int, int, String, int)}
+	 */
+	@Deprecated
 	@Override
 	public CPDefinitionInventory addCPDefinitionInventory(
 			long cpDefinitionId, String cpDefinitionInventoryEngine,
@@ -43,11 +91,11 @@ public class CPDefinitionInventoryServiceImpl
 			getPermissionChecker(), serviceContext.getScopeGroupId(),
 			CommerceActionKeys.MANAGE_COMMERCE_PRODUCT_DEFINITION_INVENTORY);
 
-		return cpDefinitionInventoryLocalService.addCPDefinitionInventory(
+		return cpDefinitionInventoryService.addCPDefinitionInventory(
 			cpDefinitionId, cpDefinitionInventoryEngine, lowStockActivity,
 			displayAvailability, displayStockQuantity, minStockQuantity,
 			backOrders, minOrderQuantity, maxOrderQuantity,
-			allowedOrderQuantities, multipleOrderQuantity, serviceContext);
+			allowedOrderQuantities, multipleOrderQuantity);
 	}
 
 	@Override
@@ -87,6 +135,49 @@ public class CPDefinitionInventoryServiceImpl
 
 	@Override
 	public CPDefinitionInventory updateCPDefinitionInventory(
+			long groupId, long cpDefinitionInventoryId,
+			String cpDefinitionInventoryEngine, String lowStockActivity,
+			boolean displayAvailability, boolean displayStockQuantity,
+			int minStockQuantity, boolean backOrders, int minOrderQuantity,
+			int maxOrderQuantity, String allowedOrderQuantities,
+			int multipleOrderQuantity)
+		throws PortalException {
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), groupId,
+			CommerceActionKeys.MANAGE_COMMERCE_PRODUCT_DEFINITION_INVENTORY);
+
+		return cpDefinitionInventoryLocalService.updateCPDefinitionInventory(
+			cpDefinitionInventoryId, cpDefinitionInventoryEngine,
+			lowStockActivity, displayAvailability, displayStockQuantity,
+			minStockQuantity, backOrders, minOrderQuantity, maxOrderQuantity,
+			allowedOrderQuantities, multipleOrderQuantity);
+	}
+
+	/**
+	 * Updates CP definition inventory
+	 *
+	 * @param      cpDefinitionInventoryId
+	 * @param      cpDefinitionInventoryEngine
+	 * @param      lowStockActivity
+	 * @param      displayAvailability
+	 * @param      displayStockQuantity
+	 * @param      minStockQuantity
+	 * @param      backOrders
+	 * @param      minOrderQuantity
+	 * @param      maxOrderQuantity
+	 * @param      allowedOrderQuantities
+	 * @param      multipleOrderQuantity
+	 * @param      serviceContext
+	 * @return
+	 * @throws     PortalException
+	 * @deprecated As of Mueller (7.2.x), see {@link
+	 *             #updateCPDefinitionInventory(long, long, String, String,
+	 *             boolean, boolean, int, boolean, int, int, String, int)}
+	 */
+	@Deprecated
+	@Override
+	public CPDefinitionInventory updateCPDefinitionInventory(
 			long cpDefinitionInventoryId, String cpDefinitionInventoryEngine,
 			String lowStockActivity, boolean displayAvailability,
 			boolean displayStockQuantity, int minStockQuantity,
@@ -95,15 +186,12 @@ public class CPDefinitionInventoryServiceImpl
 			ServiceContext serviceContext)
 		throws PortalException {
 
-		_portletResourcePermission.check(
-			getPermissionChecker(), serviceContext.getScopeGroupId(),
-			CommerceActionKeys.MANAGE_COMMERCE_PRODUCT_DEFINITION_INVENTORY);
-
-		return cpDefinitionInventoryLocalService.updateCPDefinitionInventory(
-			cpDefinitionInventoryId, cpDefinitionInventoryEngine,
-			lowStockActivity, displayAvailability, displayStockQuantity,
-			minStockQuantity, backOrders, minOrderQuantity, maxOrderQuantity,
-			allowedOrderQuantities, multipleOrderQuantity, serviceContext);
+		return cpDefinitionInventoryService.updateCPDefinitionInventory(
+			serviceContext.getScopeGroupId(), cpDefinitionInventoryId,
+			cpDefinitionInventoryEngine, lowStockActivity, displayAvailability,
+			displayStockQuantity, minStockQuantity, backOrders,
+			minOrderQuantity, maxOrderQuantity, allowedOrderQuantities,
+			multipleOrderQuantity);
 	}
 
 	private static volatile PortletResourcePermission
@@ -111,5 +199,8 @@ public class CPDefinitionInventoryServiceImpl
 			PortletResourcePermissionFactory.getInstance(
 				CPDefinitionInventoryServiceImpl.class,
 				"_portletResourcePermission", CommerceConstants.RESOURCE_NAME);
+
+	@ServiceReference(type = CPDefinitionLocalService.class)
+	private CPDefinitionLocalService _cpDefinitionLocalService;
 
 }
