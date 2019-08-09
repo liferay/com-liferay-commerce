9fecfe1b3e9d188c0dc93a6e621ab2248df55da2
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-08-09 11:10:43 +0200

    COMMERCE-1655 Added support to main input text search

--- commerce-frontend-react/src/main/resources/META-INF/resources/js/table_toolbar/reducers/index.tsx
+++ commerce-frontend-react/src/main/resources/META-INF/resources/js/table_toolbar/reducers/index.tsx
@@ -1,8 +1,10 @@
 import filtersReducer from './filters';
+import appReducer from './app';
 import { combineReducers } from 'redux';
 
 const combinedReducers = combineReducers({
 	filters: filtersReducer,
+	app: appReducer,
 });
 
 export default combinedReducers;
