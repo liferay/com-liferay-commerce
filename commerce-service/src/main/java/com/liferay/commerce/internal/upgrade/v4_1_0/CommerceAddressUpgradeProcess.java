901401b3015684319c284ccc22a444c80d74c4e0
Alec Sloan <Alec.sloan@Liferay.com>
2019-08-22 11:02:18 -0700

    COMMERCE-1711 Refactor Address Upgrade to handle transfer of columns from Address to Account entity

--- commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v4_1_0/CommerceAddressUpgradeProcess.java
+++ commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v4_1_0/CommerceAddressUpgradeProcess.java
@@ -14,9 +14,16 @@
 
 package com.liferay.commerce.internal.upgrade.v4_1_0;
 
+import com.liferay.commerce.account.model.CommerceAccount;
+import com.liferay.commerce.constants.CommerceAddressConstants;
 import com.liferay.commerce.internal.upgrade.base.BaseCommerceServiceUpgradeProcess;
 import com.liferay.commerce.model.impl.CommerceAddressImpl;
 import com.liferay.commerce.model.impl.CommerceAddressModelImpl;
+import com.liferay.portal.kernel.service.ClassNameLocalService;
+import com.liferay.portal.kernel.upgrade.UpgradeException;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 /**
  * @author Alec Sloan
@@ -24,11 +31,124 @@ import com.liferay.commerce.model.impl.CommerceAddressModelImpl;
 public class CommerceAddressUpgradeProcess
 	extends BaseCommerceServiceUpgradeProcess {
 
+	public CommerceAddressUpgradeProcess(
+		ClassNameLocalService classNameLocalService) {
+
+		_classNameLocalService = classNameLocalService;
+	}
+
 	@Override
 	protected void doUpgrade() throws Exception {
+		if (!hasColumn("CommerceAccount", "defaultBillingAddressId") ||
+			!hasColumn("CommerceAccount", "defaultShippingAddressId")) {
+
+			throw new UpgradeException(
+				"Upgrade fails as CommerceAccount does not have both " +
+					"columns: 'defaultBillingAddressId' and" +
+						"'defaultShippingAddressId'");
+		}
+
+		addColumn(
+			CommerceAddressImpl.class, CommerceAddressImpl.TABLE_NAME, "type",
+			"INTEGER");
+
+		PreparedStatement ps = null;
+
 		if (hasColumn(CommerceAddressModelImpl.TABLE_NAME, "groupId")) {
 			dropColumn(CommerceAddressImpl.TABLE_NAME, "groupId");
 		}
+
+		if (hasColumn(CommerceAddressModelImpl.TABLE_NAME, "defaultBilling")) {
+			ps = connection.prepareStatement(
+				"update CommerceAccount set defaultBillingAddressId = ? " +
+					"where commerceAccountId = ?");
+
+			updateCommerceAccountAndSetType(
+				ps, getCommerceAddressResultSet("defaultBilling"));
+
+			dropColumn(CommerceAddressImpl.TABLE_NAME, "defaultBilling");
+		}
+
+		if (hasColumn(CommerceAddressModelImpl.TABLE_NAME, "defaultShipping")) {
+			ps = connection.prepareStatement(
+				"update CommerceAccount set defaultShippingAddressId = ? " +
+					"where commerceAccountId = ?");
+
+			updateCommerceAccountAndSetType(
+				ps, getCommerceAddressResultSet("defaultShipping"));
+
+			dropColumn(CommerceAddressImpl.TABLE_NAME, "defaultShipping");
+		}
 	}
+
+	protected ResultSet getCommerceAddressResultSet(String type)
+		throws Exception {
+
+		long commerceAccountClassNameId = _classNameLocalService.getClassNameId(
+			CommerceAccount.class);
+
+		PreparedStatement ps = null;
+
+		if (type.equals("defaultBilling")) {
+			ps = connection.prepareStatement(
+				"select commerceAddressId, classPK, defaultBilling, " +
+					"defaultShipping from CommerceAddress where classNameId =" +
+						"? and defaultBilling = 1");
+		}
+		else {
+			ps = connection.prepareStatement(
+				"select commerceAddressId, classPK, defaultBilling, " +
+					"defaultShipping from CommerceAddress where classNameId =" +
+						"? and defaultShipping = 1");
+		}
+
+		ps.setLong(1, commerceAccountClassNameId);
+
+		return ps.executeQuery();
+	}
+
+	protected void setType(
+			boolean defaultBilling, boolean defaultShipping,
+			long commerceAddressId)
+		throws Exception {
+
+		PreparedStatement ps = connection.prepareStatement(
+			"update CommerceAddress set type = ? where commerceAddressId = ?");
+
+		int type = CommerceAddressConstants.TYPE_SHIPPING;
+
+		if (defaultBilling && !defaultShipping) {
+			type = CommerceAddressConstants.ADDRESS_TYPE_BILLING;
+		}
+		else if (!defaultBilling && defaultShipping) {
+			type = CommerceAddressConstants.ADDRESS_TYPE_SHIPPING;
+		}
+
+		ps.setInt(1, type);
+		ps.setLong(2, commerceAddressId);
+
+		ps.executeUpdate();
+	}
+
+	protected void updateCommerceAccountAndSetType(
+			PreparedStatement ps, ResultSet rs)
+		throws Exception {
+
+		while (rs.next()) {
+			long commerceAddressId = rs.getLong("commerceAddressId");
+
+			ps.setLong(1, commerceAddressId);
+
+			ps.setLong(2, rs.getLong("classPK"));
+
+			setType(
+				rs.getBoolean("defaultBilling"),
+				rs.getBoolean("defaultShipping"), commerceAddressId);
+
+			ps.executeUpdate();
+		}
+	}
+
+	private final ClassNameLocalService _classNameLocalService;
 
 }
