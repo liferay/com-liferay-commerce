52c423f80f80d5c4c194cccbf2fe94a4f762df8d
Luca Pellizzon <luca.pellizzon@liferay.com>
2019-08-28 20:15:38 +0200

    COMMERCE-1786 new finder for account groups

---
+++ commerce-account-service/src/main/java/com/liferay/commerce/account/service/persistence/impl/CommerceAccountGroupFinderImpl.java
@@ -0,0 +1,85 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.account.service.persistence.impl;
+
+import com.liferay.commerce.account.service.persistence.CommerceAccountGroupFinder;
+import com.liferay.petra.string.StringBundler;
+import com.liferay.portal.dao.orm.custom.sql.CustomSQL;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.dao.orm.SQLQuery;
+import com.liferay.portal.kernel.dao.orm.Session;
+import com.liferay.portal.kernel.exception.SystemException;
+import com.liferay.portal.kernel.util.StringUtil;
+import com.liferay.portal.spring.extender.service.ServiceReference;
+
+import java.util.List;
+
+/**
+ * @author Luca Pellizzon
+ */
+public class CommerceAccountGroupFinderImpl
+	extends CommerceAccountGroupFinderBaseImpl
+	implements CommerceAccountGroupFinder {
+
+	public static final String FIND_ACCOUNT_USER_IDS_FROM_ACCOUNT_GROUP_IDS =
+		CommerceAccountGroupFinder.class.getName() +
+			".findAccountUserIdsFromAccountGroupIds";
+
+	@Override
+	public List<Long> findAccountUserIdsFromAccountGroupIds(
+		long[] commerceAccountGroupIds, int start, int end) {
+
+		Session session = null;
+
+		try {
+			session = openSession();
+
+			String sql = _customSQL.get(
+				getClass(), FIND_ACCOUNT_USER_IDS_FROM_ACCOUNT_GROUP_IDS);
+
+			sql = replaceAccountGroupIds(sql, commerceAccountGroupIds);
+
+			SQLQuery q = session.createSynchronizedSQLQuery(sql);
+
+			return (List<Long>)QueryUtil.list(q, getDialect(), start, end);
+		}
+		catch (Exception e) {
+			throw new SystemException(e);
+		}
+		finally {
+			closeSession(session);
+		}
+	}
+
+	protected String replaceAccountGroupIds(
+		String sql, long[] accountGroupIds) {
+
+		StringBundler sb = new StringBundler(accountGroupIds.length);
+
+		for (int i = 0; i < accountGroupIds.length; i++) {
+			sb.append(accountGroupIds[i]);
+
+			if (i != (accountGroupIds.length - 1)) {
+				sb.append(", ");
+			}
+		}
+
+		return StringUtil.replace(sql, "[$ACCOUNT_GROUP_IDS$]", sb.toString());
+	}
+
+	@ServiceReference(type = CustomSQL.class)
+	private CustomSQL _customSQL;
+
+}
