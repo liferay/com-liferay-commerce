712d564d00dcf3bb9275bdc9b44c38a304aa84e6
Alessio Rendina <asilcomando@gmail.com>
2019-08-29 16:35:24 +0200

    COMMERCE-1791 Fixed dto converters

--- headless/headless-commerce/headless-commerce-admin-account-impl/src/main/java/com/liferay/headless/commerce/admin/account/internal/dto/v1_0/converter/AccountDTOConverter.java
+++ headless/headless-commerce/headless-commerce-admin-account-impl/src/main/java/com/liferay/headless/commerce/admin/account/internal/dto/v1_0/converter/AccountDTOConverter.java
@@ -18,14 +18,18 @@ import com.liferay.commerce.account.model.CommerceAccount;
 import com.liferay.commerce.account.model.CommerceAccountOrganizationRel;
 import com.liferay.commerce.account.model.CommerceAccountUserRel;
 import com.liferay.commerce.account.service.CommerceAccountService;
+import com.liferay.commerce.model.CommerceAddress;
+import com.liferay.commerce.service.CommerceAddressService;
 import com.liferay.expando.kernel.model.ExpandoBridge;
 import com.liferay.headless.commerce.admin.account.dto.v1_0.Account;
+import com.liferay.headless.commerce.admin.account.dto.v1_0.AccountAddress;
 import com.liferay.headless.commerce.admin.account.dto.v1_0.AccountMember;
 import com.liferay.headless.commerce.admin.account.dto.v1_0.AccountOrganization;
 import com.liferay.headless.commerce.core.dto.v1_0.converter.DTOConverter;
 import com.liferay.headless.commerce.core.dto.v1_0.converter.DTOConverterContext;
 import com.liferay.headless.commerce.core.dto.v1_0.converter.DTOConverterRegistry;
 import com.liferay.headless.commerce.core.dto.v1_0.converter.DefaultDTOConverterContext;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -59,6 +63,7 @@ public class AccountDTOConverter implements DTOConverter {
 
 		return new Account() {
 			{
+				addresses = _getAddresses(commerceAccount, dtoConverterContext);
 				customFields = expandoBridge.getAttributes();
 				emailAddresses = new String[] {commerceAccount.getEmail()};
 				externalReferenceCode =
@@ -74,6 +79,36 @@ public class AccountDTOConverter implements DTOConverter {
 				users = _getUsers(commerceAccount, dtoConverterContext);
 			}
 		};
+	}
+
+	private AccountAddress[] _getAddresses(
+			CommerceAccount commerceAccount,
+			DTOConverterContext dtoConverterContext)
+		throws Exception {
+
+		List<AccountAddress> accountAddresses = new ArrayList<>();
+
+		DTOConverter accountAddressDTOConverter =
+			_dtoConverterRegistry.getDTOConverter(
+				CommerceAddress.class.getName());
+
+		List<CommerceAddress> commerceAddresses =
+			_commerceAddressService.getCommerceAddresses(
+				commerceAccount.getModelClassName(),
+				commerceAccount.getCommerceAccountId(), QueryUtil.ALL_POS,
+				QueryUtil.ALL_POS, null);
+
+		for (CommerceAddress commerceAddress : commerceAddresses) {
+			accountAddresses.add(
+				(AccountAddress)accountAddressDTOConverter.toDTO(
+					new DefaultDTOConverterContext(
+						dtoConverterContext.getLocale(),
+						commerceAddress.getCommerceAddressId())));
+		}
+
+		Stream<AccountAddress> stream = accountAddresses.stream();
+
+		return stream.toArray(AccountAddress[]::new);
 	}
 
 	private AccountOrganization[] _getOrganizations(
@@ -130,6 +165,9 @@ public class AccountDTOConverter implements DTOConverter {
 
 	@Reference
 	private CommerceAccountService _commerceAccountService;
+
+	@Reference
+	private CommerceAddressService _commerceAddressService;
 
 	@Reference
 	private DTOConverterRegistry _dtoConverterRegistry;
