a6d20a4ec338aac42c14e3aa9ea04573ff37ddbf
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-09-04 15:49:52 +0200

    COMMERCE-1655 new functional modals. Using modal to submit forms now works

--- commerce-frontend-react/src/main/resources/META-INF/resources/js/modal/Modal.es.js
+++ commerce-frontend-react/src/main/resources/META-INF/resources/js/modal/Modal.es.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ModalBase from './ModalBase.es';
-import ClayModal from '@clayui/modal';
+import React, { useState, useRef, useEffect } from 'react';
+import ClayModal, {useModal} from '@clayui/modal';
 // import { Status, Size } from '@clayui/modal/src/types';
 import ClayButton from '@clayui/button';
 
 /*
 export interface ModalProps {
+    id: string
     url?: string,
     spritemap?: string,
     status?: Status,
@@ -14,7 +14,6 @@ export interface ModalProps {
     showSubmit: boolean,
     submitLabel: string,
     closeOnSubmit: boolean,
-    submitAvailableAtLoading: boolean,
     showDelete: boolean,
     deleteLabel: string
     showCancel: boolean,
@@ -26,159 +25,197 @@ interface ModalState {
 }
 */
 
-class Modal extends React.Component {
-    constructor(props) {
-        super(props);
-        this.iframeRef = React.createRef();
-        this.state = {
-            visible: false,
-            submitActive: true,
-            listenersInitialized: false,
-        };
+const Modal = (props) => {
+
+    const [ visible, setVisible ] = useState(false);
+
+    function open() {
+        setVisible(true);
     }
 
-    attachListenerToIframe(){
-        const iframeDocument = this.iframeRef.current.contentDocument;
-        if (
-            (
-                this.props.showSubmit ||
-                this.props.submitLabel
-            ) &&
-            iframeDocument && 
-            !this.state.listenersInitialized
-        ) {
-            this.iframeForm = iframeDocument.querySelector('form');
-
-            this.iframeForm.addEventListener('submitAvailable', () => this.enableSubmit());
-            this.iframeForm.addEventListener('submitUnavailable', () => this.disableSubmit());
-
-            this.setState({
-                listenersInitialized: true,
-            })
-        }
-    }
-
-    setSubmitAvailability(active) {
-        this.setState({
-            submitActive: active
-        });
-    }
-
-    enableSubmit() {
-        this.setSubmitAvailability(true);
-    }
-
-    disableSubmit() {
-        this.setSubmitAvailability(false);
-    }
-
-    open() {
-        this.setState({
-            visible: true
-        });
-    }
-
-    close() {
-        this.setState({
-            visible: false,
-            listenersInitialized: false,
-        });
-    }
-
-    _submit(e, close) {
-        e.preventDefault();
-        try {
-            this.iframeForm.submit();
-
-            if(this.props.closeOnSubmit) {
-                close();
-            }
-        } catch (error) {
-            throw new Error('Form not available')            
-        }
-    }
-
-    render() { 
-        
-        const { 
-            title,
-            url,
-            showSubmit,
-            submitLabel,
-            showCancel,
-            cancelLabel,
-            spritemap,
-            size,
-            status
-        } = this.props;
-
-        return (
-            <ModalBase 
-                visible={this.state.visible}
-                onClose={() => this.close()}
-                spritemap={spritemap}
-                size={size}
-                status={status}
-            >
-                { onClose => (
-                    <>
-                        {
-                            title && <ClayModal.Header>{title}</ClayModal.Header>
-                        }
-                        <div className="modal-body modal-body-iframe">
-                            <iframe 
-                                src={url} 
-                                title={title} 
-                                ref={this.iframeRef} 
-                                onLoad={() => this.attachListenerToIframe()}
-                            />
-                        </div>
-                        {
-                            (
-                                showSubmit ||
-                                submitLabel ||
-                                showCancel ||
-                                cancelLabel 
-                            ) && (
-                                <ClayModal.Footer
-                                    last={
-                                        (
-                                            showSubmit || submitLabel ||
-                                            showCancel || cancelLabel 
-                                        ) && (
-                                            <ClayButton.Group spaced>
-                                                {(
-                                                    showCancel || cancelLabel
-                                                ) && (
-                                                    <ClayButton 
-                                                        displayType="secondary"
-                                                        onClick={onClose}
-                                                    >
-                                                        {cancelLabel || Liferay.Language.get('cancel')}
-                                                    </ClayButton>
-                                                )}
-                                                {(
-                                                    showSubmit || submitLabel
-                                                ) && (
-                                                    <ClayButton 
-                                                        displayType="primary"
-                                                        disabled={!this.state.submitActive}
-                                                        onClick={(e) => this._submit(e, onClose)}
-                                                    >
-                                                        {submitLabel || Liferay.Language.get('submit')}
-                                                    </ClayButton>
-                                                )}
-                                            </ClayButton.Group>
-                                        )
-                                    }
-                                />
-                            )
-                        }
-                    </>
-                )}
-            </ModalBase>
+    function reset() {
+        setVisible(false);
+        setIframeLoadingCounter(0);
+        setSubmitActive(
+            typeof props.submitActiveAtLoading === 'boolean' 
+                ? props.submitActiveAtLoading 
+                : true
         )
     }
+
+    const [ iframeLoadingCounter, setIframeLoadingCounter ] = useState(0);
+    const [ submitActive, setSubmitActive ] = useState(
+        typeof props.submitActiveAtLoading === 'boolean' 
+            ? props.submitActiveAtLoading 
+            : true
+    )
+    const iframeRef = useRef(null);
+    const { observer, onClose } = useModal({onClose: reset});
+
+    function cleanUpListeners(e) {
+        if(e.portletId === props.portletId) {
+            Liferay.detach(`${props.id}-open`, open);
+            Liferay.detach('destroyPortlet', cleanUpListeners);
+        }
+    }
+
+    useEffect( () => {
+        if(Liferay.on){
+            Liferay.on(`${props.id}-open`, open);
+            Liferay.on('destroyPortlet', cleanUpListeners);
+        } else {
+            window.addEventListener(`${props.id}-open`, open);
+        }
+
+    }, [ props.id ] )
+
+    function _handleIframeLoad(){
+        setIframeLoadingCounter(iframeLoadingCounter + 1);
+    }
+
+    useEffect(() => {
+        switch (true) {
+            case iframeLoadingCounter > 1:
+                return _handleFormSubmit();
+            case iframeLoadingCounter === 1:
+                return _handleIframeFirstLoad();
+            default:
+                break;
+        }
+    }, [iframeLoadingCounter])
+
+    function _handleFormSubmit() {
+        setSubmitActive(false);
+
+        if(props.closeOnSubmit) {
+            setTimeout(() => {
+                onClose();
+            }, 1000);
+        }
+    }
+
+    function _handleIframeFirstLoad() {
+
+        const iframeDocument = iframeRef.current.contentDocument;
+        const iframeWindow = iframeRef.current.contentWindow;
+
+        if (
+            (
+                props.showSubmit ||
+                props.submitLabel
+            ) &&
+            iframeDocument 
+        ) {
+            const iframeForm = iframeDocument.querySelector('form');
+
+            iframeForm.addEventListener('submitAvailable', () => enableSubmit());
+            iframeForm.addEventListener('submitUnavailable', () => disableSubmit());
+            
+            if(iframeWindow.Liferay.on) {
+                iframeWindow.Liferay.on(
+                    'endNavigate',
+                    (e) => {
+                        e.preventDefault();
+                        _handleIframeLoad();
+                    }
+                )
+            }
+        }
+    }
+
+    function setSubmitAvailability(state) {
+        setSubmitActive(state);
+    }
+
+    function enableSubmit() {
+        setSubmitAvailability(true);
+    }
+
+    function disableSubmit() {
+        setSubmitAvailability(false);
+    }
+
+    function _submit(e) {
+        e.preventDefault();
+        try {
+            const iframeForm = iframeRef.current.contentDocument.querySelector('form');
+            const iframeSubmitButton = iframeForm.querySelector('[type="submit"]');
+
+            if(iframeSubmitButton) {
+                iframeSubmitButton.click();
+            } else {
+                iframeForm.submit();
+            }
+
+        } catch (error) {
+            throw new Error('Form not available')  ;          
+        }
+    }
+
+    return (
+        visible ? (
+            <ClayModal
+                observer={observer}
+                spritemap={props.spritemap}
+                size={props.size}
+                status={props.status}
+            >
+                {
+                    props.title && <ClayModal.Header>{props.title}</ClayModal.Header>
+                }
+                <div className="modal-body modal-body-iframe">
+                    <iframe 
+                        src={props.url} 
+                        title={props.title} 
+                        ref={iframeRef} 
+                        onLoad={_handleIframeLoad}
+                    />
+                </div>
+                {
+                    (
+                        props.showSubmit ||
+                        props.submitLabel ||
+                        props.showCancel ||
+                        props.cancelLabel 
+                    ) && (
+                        <ClayModal.Footer
+                            last={
+                                (
+                                    props.showSubmit || props.submitLabel ||
+                                    props.showCancel || props.cancelLabel 
+                                ) && (
+                                    <ClayButton.Group spaced>
+                                        {(
+                                            props.showCancel || props.cancelLabel
+                                        ) && (
+                                            <ClayButton 
+                                                displayType="secondary"
+                                                onClick={onClose}
+                                            >
+                                                {props.cancelLabel || Liferay.Language.get('cancel')}
+                                            </ClayButton>
+                                        )}
+                                        {(
+                                            props.showSubmit || props.submitLabel
+                                        ) && (
+                                            <ClayButton 
+                                                displayType="primary"
+                                                disabled={!submitActive}
+                                                onClick={_submit}
+                                            >
+                                                {props.submitLabel || Liferay.Language.get('submit')}
+                                            </ClayButton>
+                                        )}
+                                    </ClayButton.Group>
+                                )
+                            }
+                        />
+                    )
+                }
+            </ClayModal>
+        )
+        : null
+    )
 }
 
 export default Modal;
