695fada913ca9287d271d15d48760f572f03c540
Luca Pellizzon <luca.pellizzon@liferay.com>
2019-09-10 16:23:18 +0200

    COMMERCE-1726 new order comparator base on modified date

---
+++ commerce-service/src/main/java/com/liferay/commerce/internal/order/comparator/CommerceOrderModifiedDateComparator.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.internal.order.comparator;
+
+import com.liferay.commerce.model.CommerceOrder;
+import com.liferay.portal.kernel.util.DateUtil;
+import com.liferay.portal.kernel.util.OrderByComparator;
+
+/**
+ * @author Luca Pellizzon
+ */
+public class CommerceOrderModifiedDateComparator
+	extends OrderByComparator<CommerceOrder> {
+
+	public static final String ORDER_BY_ASC = "createDate ASC";
+
+	public static final String ORDER_BY_DESC = "createDate DESC";
+
+	public static final String[] ORDER_BY_FIELDS = {"modifiedDate"};
+
+	public CommerceOrderModifiedDateComparator() {
+		this(false);
+	}
+
+	public CommerceOrderModifiedDateComparator(boolean ascending) {
+		_ascending = ascending;
+	}
+
+	@Override
+	public int compare(CommerceOrder o1, CommerceOrder o2) {
+		int value = DateUtil.compareTo(
+			o1.getModifiedDate(), o2.getModifiedDate());
+
+		if (_ascending) {
+			return value;
+		}
+
+		return Math.negateExact(value);
+	}
+
+	@Override
+	public String getOrderBy() {
+		if (_ascending) {
+			return ORDER_BY_ASC;
+		}
+
+		return ORDER_BY_DESC;
+	}
+
+	@Override
+	public String[] getOrderByFields() {
+		return ORDER_BY_FIELDS;
+	}
+
+	@Override
+	public boolean isAscending() {
+		return _ascending;
+	}
+
+	private final boolean _ascending;
+
+}
