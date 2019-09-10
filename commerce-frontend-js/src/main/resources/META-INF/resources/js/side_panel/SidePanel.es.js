78f1036ea1549a99cbc37ec18c9b6800f5bb98fa
Giacomo Zinetti <giacomo.zinetti@gmail.com>
2019-09-10 16:29:01 +0200

    COMMERCE-1830 up side-panel

--- commerce-frontend-js/src/main/resources/META-INF/resources/js/side_panel/SidePanel.es.js
+++ commerce-frontend-js/src/main/resources/META-INF/resources/js/side_panel/SidePanel.es.js
@@ -1,62 +1,93 @@
 import React from 'react';
 import Tabs from './Tabs.es';
-import { globalEval } from 'metal-dom';
+import ClayLoadingIndicator from '@clayui/loading-indicator';
 
 export default class SidePanel extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			show: !!props.show,
-			content: '',
-			pages: []
+			animating: false,
+			loading: !!props.pages,
+			loaded: !!props.pages,
+			pages: this.formatPages(props.pages),
+			currentTab: this.getFirstTab(props.pages),
+			size: props.size || 'medium',
 		}
-		this.selectPane = this.selectPane.bind(this);
-		this.content = React.createRef();
+		this.selectTab = this.selectTab.bind(this);
+		this.contentLoaded = this.contentLoaded.bind(this);
+		this.panel = React.createRef();
 	}
 
-	componentDidMount() {
-		this.load(this.props.pages);		
-	}
-
-	load(pages) {
+	load(pages, size = this.state.size) {
 		this.setState({
-			pages: typeof pages === 'string' ? [{url: pages}] : pages,
-		}, () => {
-			this.setState({
-				currentTab: this.state.pages[0].url,
-			});
+			loading: true,
+			loaded: false,
+			pages: this.formatPages(pages),
+			currentTab: this.getFirstTab(pages),
+			size,
 		});
 	}
 
-	toggle() {
-		this.setState({show: !this.state.show});
+	setSize(size = 'medium') {
+		this.setState({size});
 	}
 
-	open(content) {
-		content && this.load(content);
-		this.setState({show: true});
+	getFirstTab(pages = '') {
+		return typeof pages === 'string' ? pages : pages[0].url
+	}
+
+	formatPages(pages = []) {
+		return typeof pages === 'string' ? [{url: pages}] : pages;
+	}
+
+	open(content = false, size) {
+		content && this.load(content, size);
+		this.toggle(true);
 	}
 
 	close() {
-		this.setState({show: false});
+		this.toggle(false);
 	}
 
-	selectPane(url) {
+	toggle(status = !this.state.show) {
+		this.setState({show: status, animating: true});
+		this.panel.current.addEventListener('transitionend', () => {
+			this.setState({animating: false});
+		}, {once: true});
+	}
+
+	selectTab(url) {
 		const currentTab = this.state.pages.find(el => el.url === url).url;
-		this.setState({ currentTab });
+		this.setState({
+			loading: true,
+			loaded: false,
+			currentTab
+		});
+	}
+
+	contentLoaded() {
+		this.setState({
+			loading: false,
+			loaded: true,
+		});
+	}
+
+	showIframe() {
+		return this.state.loaded || (this.state.show ^ this.state.animating);
 	}
 
 	render() {
 		const visibility = this.state.show ? 'is-visible' : 'is-hidden';
+		const loading = this.state.loading ? 'is-loading' : '';
 
 		return (
-			<div className={`side-panel side-panel--${this.props.size} ${visibility}`}>
-				{this.state.pages.length > 1 && <Tabs tabs={this.state.pages} onChange={this.selectPane} current={this.state.currentTab} />}
+			<div className={`side-panel side-panel--${this.state.size} ${visibility} ${loading}`} ref={this.panel}>
+				{this.state.pages.length > 1 && <Tabs tabs={this.state.pages} onChange={this.selectTab} current={this.state.currentTab} />}
 				<div className="tab-content">
-					<div className="active fade show tab-pane" role="tabpanel" ref={this.content}>
-						{this.state.show &&
-							<iframe src={this.state.currentTab} frameBorder="0"></iframe>
-						}
+					<div className="side-panel__loader"><ClayLoadingIndicator /></div>
+					<div className="active fade show tab-pane" role="tabpanel">
+						{this.showIframe() && <iframe src={this.state.currentTab} frameBorder="0" onLoad={this.contentLoaded}></iframe>}
 					</div>
 				</div>
 			</div>
