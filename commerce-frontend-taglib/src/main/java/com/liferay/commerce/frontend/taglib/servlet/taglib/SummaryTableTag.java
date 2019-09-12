0aac65c1fe1a5c403c8675be22d59088cf4b90f7
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-09-12 18:20:07 +0200

    COMMERCE-1952 populated changelogs and emails pages

---
+++ commerce-frontend-taglib/src/main/java/com/liferay/commerce/frontend/taglib/servlet/taglib/SummaryTableTag.java
@@ -0,0 +1,67 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.frontend.taglib.servlet.taglib;
+
+import com.liferay.commerce.frontend.model.SummaryElement;
+import com.liferay.commerce.frontend.taglib.internal.servlet.ServletContextUtil;
+import com.liferay.taglib.util.IncludeTag;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.PageContext;
+
+/**
+ * @author Fabio Diego Mastrorilli
+ */
+public class SummaryTableTag extends IncludeTag {
+
+	public List<SummaryElement> getData() {
+		return _data;
+	}
+
+	public void setData(List<SummaryElement> data) {
+		_data = data;
+	}
+
+	@Override
+	public void setPageContext(PageContext pageContext) {
+		super.setPageContext(pageContext);
+
+		servletContext = ServletContextUtil.getServletContext();
+	}
+
+	@Override
+	protected void cleanUp() {
+		super.cleanUp();
+
+		_data = null;
+	}
+
+	@Override
+	protected String getPage() {
+		return _PAGE;
+	}
+
+	@Override
+	protected void setAttributes(HttpServletRequest httpServletRequest) {
+		request.setAttribute("liferay-commerce:summary-table:data", _data);
+	}
+
+	private static final String _PAGE = "/summary_table/page.jsp";
+
+	private List<SummaryElement> _data;
+
+}
