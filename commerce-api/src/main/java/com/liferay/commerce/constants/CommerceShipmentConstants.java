18d44fda71e14756ec45d8ee19d92f2da9a60656
Alessio Rendina <asilcomando@gmail.com>
2019-09-13 14:39:26 +0200

    COMMERCE-609 Refactor constants

--- commerce-api/src/main/java/com/liferay/commerce/constants/CommerceShipmentConstants.java
+++ commerce-api/src/main/java/com/liferay/commerce/constants/CommerceShipmentConstants.java
@@ -20,12 +20,10 @@ package com.liferay.commerce.constants;
 public class CommerceShipmentConstants {
 
 	public static final int[] ALLOWED_ORDER_STATUSES = {
-		CommerceOrderConstants.ORDER_STATUS_AWAITING_FULFILLMENT,
-		CommerceOrderConstants.ORDER_STATUS_AWAITING_SHIPMENT,
 		CommerceOrderConstants.ORDER_STATUS_DISPUTED,
+		CommerceOrderConstants.ORDER_STATUS_TO_FULFILL,
 		CommerceOrderConstants.ORDER_STATUS_PARTIALLY_REFUNDED,
-		CommerceOrderConstants.ORDER_STATUS_PARTIALLY_SHIPPED,
-		CommerceOrderConstants.ORDER_STATUS_TRANSMITTED
+		CommerceOrderConstants.ORDER_STATUS_PARTIALLY_SHIPPED
 	};
 
 	public static final int SHIPMENT_STATUS_DELIVERED = 3;
