ae1ede40b7253b8efae4c42483ead6332f8e446e
Gianmarco Brunialti <gianmarco.brunialti@liferay.com>
2019-09-16 15:44:35 +0200

    COMMERCE-1641 review

---
+++ commerce-theme-speedwell/commerce-theme-speedwell/src/js/features/searchBar.js
@@ -0,0 +1,32 @@
+!!Speedwell && (Speedwell.features.searchBar = (function(w) {
+
+    const searchToggles = w.document.querySelectorAll('.js-toggle-search'),
+        HAS_SEARCH_CLASS = 'has-search',
+        IS_OPEN_CLASS = 'is-open',
+        IS_ACTIVE_CLASS = 'is-active',
+        SEARCHBAR_SELECTOR = '.speedwell-search';
+
+    let searchBarElement;
+
+    return {
+        initialize: function() {
+            const searchBar = w.Liferay.component('search-bar');
+
+            if (searchBar) {
+                searchBarElement = w.document.querySelector(SEARCHBAR_SELECTOR);
+
+                searchBar.on('toggled', function(status) {
+                    searchToggles.forEach(function (el) {
+                        el.classList.toggle(IS_ACTIVE_CLASS, status);
+                    });
+
+                    Speedwell.getContainer()
+                        .classList.toggle(HAS_SEARCH_CLASS, status);
+
+                    !!searchBarElement && searchBarElement
+                        .classList.toggle(IS_OPEN_CLASS, status);
+                });
+            }
+        }
+    }
+})(window));
