a00bb0b1ba4f5613a4dcef91b4b89a908a01c726
Alessio Rendina <asilcomando@gmail.com>
2019-09-17 16:52:38 +0200

    COMMERCE-609 Auto SF

--- commerce-frontend-js/src/main/java/com/liferay/commerce/frontend/js/servlet/taglib/CommerceFrontendJsDynamicInclude.java
+++ commerce-frontend-js/src/main/java/com/liferay/commerce/frontend/js/servlet/taglib/CommerceFrontendJsDynamicInclude.java
@@ -14,22 +14,16 @@
 
 package com.liferay.commerce.frontend.js.servlet.taglib;
 
-import com.liferay.portal.kernel.servlet.BrowserSniffer;
 import com.liferay.portal.kernel.servlet.taglib.BaseDynamicInclude;
 import com.liferay.portal.kernel.servlet.taglib.DynamicInclude;
-import com.liferay.portal.kernel.util.Portal;
-import com.liferay.portal.url.builder.AbsolutePortalURLBuilder;
-import com.liferay.portal.url.builder.AbsolutePortalURLBuilderFactory;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.osgi.service.component.annotations.Component;
-import org.osgi.service.component.annotations.Reference;
 
 /**
  * @author Fabio Mastrorilli
@@ -42,14 +36,15 @@ public class CommerceFrontendJsDynamicInclude extends BaseDynamicInclude {
 
 	@Override
 	public void include(
-			HttpServletRequest request, HttpServletResponse response,
-			String key)
+			HttpServletRequest httpServletRequest,
+			HttpServletResponse httpServletResponse, String key)
 		throws IOException {
 
+		PrintWriter printWriter = httpServletResponse.getWriter();
 
-			PrintWriter printWriter = response.getWriter();
-			printWriter.print(
-				"<link rel=\"stylesheet\" type=\"text/css\" href=\"/o/commerce-frontend-js/css/main.css\" >");
+		printWriter.print(
+			"<link rel=\"stylesheet\" type=\"text/css\" href=\"/o" +
+				"/commerce-frontend-js/css/main.css\" >");
 	}
 
 	@Override
@@ -57,20 +52,5 @@ public class CommerceFrontendJsDynamicInclude extends BaseDynamicInclude {
 		dynamicIncludeRegistry.register(
 			"/html/common/themes/top_head.jsp#post");
 	}
-
-	@Reference
-	private AbsolutePortalURLBuilderFactory _absolutePortalURLBuilderFactory;
-
-	@Reference
-	private BrowserSniffer _browserSniffer;
-
-	@Reference
-	private Portal _portal;
-
-	@Reference(
-		target = "(osgi.web.symbolicname=com.liferay.commerce.frontend.js)",
-		unbind = "-"
-	)
-	private ServletContext _servletContext;
 
 }
