f78fc8d70c58b4335f3195a7b0e2d286712641e5
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-09-20 13:22:41 +0200

    COMMERCE-1831 side panel bugs solved

--- commerce-frontend-js/src/main/resources/META-INF/resources/js/side_panel/SidePanel.es.js
+++ commerce-frontend-js/src/main/resources/META-INF/resources/js/side_panel/SidePanel.es.js
@@ -3,23 +3,46 @@ import ReactDOM from 'react-dom';
 import ClayLoadingIndicator from '@clayui/loading-indicator';
 import ClayIcon from '@clayui/icon';
 import ClayButton from '@clayui/button';
-import {ClayPortal} from '@clayui/shared';
+import {debounce} from '../utilities/entry';
 export default class SidePanel extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
 			visible: !!props.visible,
-			animating: false,
-			loading: !!props.pages,
-			loaded: !!props.pages,
+			moving: false,
+			loading: true,
 			tabs: props.tabs,
-			currentUrl: props.tabs || null,
+			currentUrl: props.currentUrl || null,
 			size: props.size || 'md',
+			topDistance: 0
 		}
 		this.selectTab = this.selectTab.bind(this);
-		this.contentLoaded = this.contentLoaded.bind(this);
+		this.handleContentLoaded = this.handleContentLoaded.bind(this);
 		this.close = this.close.bind(this);
+		this.updateTop = this.updateTop.bind(this);
+		this.debouncedUpdateTop = debounce(this.updateTop, 250);
 		this.panel = React.createRef();
+		this.iframeRef = React.createRef();
+	}
+
+	componentDidMount() {
+		if(this.props.topAnchor) {
+			window.addEventListener('resize', this.debouncedUpdateTop);
+			this.updateTop();
+		}
+	}
+
+	componentWillUnmount() {
+		if(this.props.topAnchor) {
+			window.removeEventListener('resize', this.debouncedUpdateTop);
+		}
+	}
+
+	updateTop() {
+		const {top, height} = this.props.topAnchor.getBoundingClientRect()
+		this.setState({
+			topDistance: top + height + 'px'
+		})
 	}
 
 	setTabs(tabs = []) {
@@ -31,8 +54,14 @@ export default class SidePanel extends React.Component {
 	load(url) {
 		this.setState({
 			loading: true,
-			loaded: false,
 			currentUrl: url
+		}, () => {
+			if(
+				this.iframeRef.current && 
+				this.iframeRef.current.contentWindow
+			) {
+				this.iframeRef.current.contentWindow.location = this.state.currentUrl;
+			}
 		});
 	}
 
@@ -40,73 +69,75 @@ export default class SidePanel extends React.Component {
 		this.setState({size});
 	}
 
-	open(url) {
-		url && this.load(url);
-		this.toggle(true);
+	open(url = this.state.currentUrl) {
+		if(!this.state.visible) {
+			this.toggle(true)
+				.then(() => {
+					this.load(url);
+				});
+		} else {
+			this.load(url)
+		}
 	}
 
-	close(destroy = false) {
-		this.toggle(false);
-		destroy && this.panel.current.addEventListener(
-			'transitionend',
-			() => {
+	close() {
+		this.toggle(false)
+			.then(() => {
 				this.setState({
-					loading: true,
-					loaded: false,
-				});
-			},
-			{
-				once: true
-			}
-		);
+					currentUrl: null,
+					loading: true
+				})
+			})
 	}
 
 	toggle(status = !this.state.visible) {
-		this.setState({visible: status, animating: true});
-		this.panel.current.addEventListener(
-			'transitionend',
-			() => {
-				this.setState({animating: false});
-			},
-			{
-				once: true
-			}
-		);
+		return new Promise((resolve) => {
+			this.setState({visible: status, moving: true});
+			this.panel.current.addEventListener(
+				'transitionend',
+				() => {
+					this.setState(
+						{moving: false},
+						() => resolve(status)
+					);
+				},
+				{
+					once: true
+				}
+			);
+		})
 	}
 
 	selectTab(url) {
 		const currentUrl = this.state.pages.find(el => el.url === url).url;
 		this.setState({
 			loading: true,
-			loaded: false,
 			currentUrl
 		});
 	}
 
-	contentLoaded() {
+	handleContentLoaded() {
 		this.setState({
 			loading: false,
-			loaded: true,
 		});
-	}
-
-	showIframe() {
-		return this.state.loaded || (this.state.visible ^ this.state.animating);
+		const iframeBody = this.iframeRef.current.contentDocument.querySelector('body');
+		iframeBody.classList.add('within-commerce-iframe')
 	}
 
 	render() {
 		const visibility = this.state.visible ? 'is-visible' : 'is-hidden';
-		const loading = this.state.loading ? 'is-loading' : '';
+		const loading = this.state.loading || (this.state.moving && this.state.visible) ? 'is-loading' : '';
 		
 		return ReactDOM.createPortal(
 			<div 
-				className={`side-panel side-panel-${this.state.size} ${visibility} ${loading}`} 
+				className={`side-panel side-panel-${this.state.size} ${visibility} ${loading}`}
+				style={{top: this.state.topDistance}}
 				ref={this.panel}
 			>
 				<ClayButton 
 					displayType="monospaced"
 					className="btn-close"
-					onClick={this.close}
+					onClick={() => this.close()}
 				>
 					<ClayIcon symbol="times" spritemap={this.props.spritemap} />
 				</ClayButton>
@@ -117,11 +148,12 @@ export default class SidePanel extends React.Component {
 					</div>
 					<div className="active fade show tab-pane" role="tabpanel">
 						{
-							this.showIframe() && 
+							!(this.state.moving && this.state.visible) &&
 							<iframe 
 								src={this.state.currentUrl}
-								frameBorder="0" 
-								onLoad={this.contentLoaded}
+								frameBorder="0"
+								ref={this.iframeRef}
+								onLoad={this.handleContentLoaded}
 							></iframe>
 						}
 					</div>
