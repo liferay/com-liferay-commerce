9cb3250ec8505e83ea99deb1af87ffd9e6a62883
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-09-24 17:32:25 +0200

    COMMERCE-2012 removed new sf

--- commerce-frontend-js/src/main/resources/META-INF/resources/js/summary/entry.dev.es.js
+++ commerce-frontend-js/src/main/resources/META-INF/resources/js/summary/entry.dev.es.js
@@ -4,41 +4,41 @@ window.Summary = launcher('summary', 'summary', {
 	items: [
 		{
 			label: 'Items Subtotal',
-			value: '$ 2,208.50',
+			value: '$ 2,208.50'
 		},
 		{
 			label: 'Order Discount',
-			value: '$ 0.00',
+			value: '$ 0.00'
 		},
 		{
 			label: 'Promotion codes',
-			value: '--',
+			value: '--'
 		},
 		{
 			label: 'Estimated Tax',
-			value: '$ 0.00',
+			value: '$ 0.00'
 		},
 		{
 			label: 'Shipping & Handing',
-			value: '$ 50.00',
+			value: '$ 50.00'
 		},
 		{
 			label: 'Grand Total',
 			value: '$ 2,258.50',
-			style: 'big',
+			style: 'big'
 		},
 		{
-			style: 'divider',
+			style: 'divider'
 		},
 		{
 			label: 'Total return refounds',
 			style: 'danger',
-			value: '$ 0.00',
+			value: '$ 0.00'
 		},
 		{
 			label: 'Total appeasement refounds',
 			style: 'danger',
-			value: '$ 0.00',
-		},
-	],
+			value: '$ 0.00'
+		}
+	]
 });
