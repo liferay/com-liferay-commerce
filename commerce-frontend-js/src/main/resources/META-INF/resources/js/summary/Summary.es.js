f9ca23d82dfc0140e0a2ddff4084213595f75032
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-09-24 17:32:25 +0200

    COMMERCE-2012 removed new sf

--- commerce-frontend-js/src/main/resources/META-INF/resources/js/summary/Summary.es.js
+++ commerce-frontend-js/src/main/resources/META-INF/resources/js/summary/Summary.es.js
@@ -3,22 +3,28 @@ import PropTypes from 'prop-types';
 
 function SummaryItemDividerVariant() {
 	return (
-		<div className="col-12"><hr /></div>
-	)
+		<div className="col-12">
+			<hr />
+		</div>
+	);
 }
 
 const baseItemDefaultProps = {
 	label: PropTypes.string,
 	value: PropTypes.string
-}
+};
 
 function SummaryItemBase(props) {
 	return (
 		<>
-			<div className="col-6 col-md-9"><p className="m-0">{props.label}</p></div>
-			<div className="col-6 col-md-3"><p className="m-0">{props.value}</p></div>
+			<div className="col-6 col-md-9">
+				<p className="m-0">{props.label}</p>
+			</div>
+			<div className="col-6 col-md-3">
+				<p className="m-0">{props.value}</p>
+			</div>
 		</>
-	)
+	);
 }
 
 SummaryItemBase.propTypes = baseItemDefaultProps;
@@ -26,10 +32,14 @@ SummaryItemBase.propTypes = baseItemDefaultProps;
 function SummaryItemBigVariant(props) {
 	return (
 		<>
-			<div className="col-6 col-md-9"><h3 className="my-2">{props.label}</h3></div>
-			<div className="col-6 col-md-3"><h3 className="my-2">{props.value}</h3></div>
+			<div className="col-6 col-md-9">
+				<h3 className="my-2">{props.label}</h3>
+			</div>
+			<div className="col-6 col-md-3">
+				<h3 className="my-2">{props.value}</h3>
+			</div>
 		</>
-	)
+	);
 }
 
 SummaryItemBigVariant.propTypes = baseItemDefaultProps;
@@ -37,36 +47,39 @@ SummaryItemBigVariant.propTypes = baseItemDefaultProps;
 function SummaryItemDangerVariant(props) {
 	return (
 		<>
-			<div className="col-6 col-md-9 text-danger"><p className="m-0">{props.label}</p></div>
-			<div className="col-6 col-md-3 text-danger"><p className="m-0">{props.value}</p></div>
+			<div className="col-6 col-md-9 text-danger">
+				<p className="m-0">{props.label}</p>
+			</div>
+			<div className="col-6 col-md-3 text-danger">
+				<p className="m-0">{props.value}</p>
+			</div>
 		</>
-	)
+	);
 }
 
 SummaryItemDangerVariant.propTypes = baseItemDefaultProps;
 
 function SummaryItem(props) {
-	const {style, ...itemProps} = props; 
+	const {style, ...itemProps} = props;
 
-	let ItemVariant
+	let ItemVariant;
+
 	switch (props.style) {
 		case 'big':
-			ItemVariant = SummaryItemBigVariant
+			ItemVariant = SummaryItemBigVariant;
 			break;
 		case 'divider':
-			ItemVariant = SummaryItemDividerVariant
+			ItemVariant = SummaryItemDividerVariant;
 			break;
 		case 'danger':
-			ItemVariant = SummaryItemDangerVariant
+			ItemVariant = SummaryItemDangerVariant;
 			break;
 		default:
-			ItemVariant = SummaryItemBase
+			ItemVariant = SummaryItemBase;
 			break;
 	}
 
-	return (
-		<ItemVariant {...itemProps} />
-	)
+	return <ItemVariant {...itemProps} />;
 }
 
 SummaryItem.propTypes = {
@@ -76,9 +89,11 @@ SummaryItem.propTypes = {
 function Summary(props) {
 	return (
 		<div className="row summary-table text-right">
-			{props.items.map((item, i) => <SummaryItem key={i} {...item} />)}
+			{props.items.map((item, i) => (
+				<SummaryItem key={i} {...item} />
+			))}
 		</div>
-	)
+	);
 }
 
 Summary.propTypes = {
