817ba30c8e96ecdd82b6fe3ba3e76dab8ab9f029
Ethan Bustad <ethan.bustad@liferay.com>
2019-09-26 12:03:57 -0700

    COMMERCE-1621 Add missing instance of p_auth in account selector

--- commerce-frontend-taglib/src/main/resources/META-INF/resources/account_selector/AccountSelector.es.js
+++ commerce-frontend-taglib/src/main/resources/META-INF/resources/account_selector/AccountSelector.es.js
@@ -115,7 +115,7 @@ class AccountSelector extends Component {
 		formData.append('accountId', this.currentAccount.accountId);
 
 		fetch(
-			this.accountsAPI + 'set-current-account?groupId=' + themeDisplay.getScopeGroupId(),
+			this.accountsAPI + 'set-current-account?groupId=' + themeDisplay.getScopeGroupId() + `&p_auth=${window.Liferay.authToken}`,
 			{
 				body: formData,
 				method: 'POST'
@@ -153,7 +153,7 @@ class AccountSelector extends Component {
 
 	_fetchAccounts(query = '') {
 		return fetch(
-			this.accountsAPI + 'search-accounts?groupId=' + themeDisplay.getScopeGroupId() + '&page=1&pageSize=10&q=' + query,
+			this.accountsAPI + 'search-accounts?groupId=' + themeDisplay.getScopeGroupId() + `&p_auth=${window.Liferay.authToken}&page=1&pageSize=10&q=${query}`,
 			{
 				method: 'GET'
 			}
@@ -171,7 +171,7 @@ class AccountSelector extends Component {
 
 	_getOrders(query = '') {
 		return fetch(
-			this.accountsAPI + 'search-accounts/' + this.currentAccount.accountId + '/orders?groupId=' + themeDisplay.getScopeGroupId() + '&page=1&pageSize=10&q=' + query,
+			this.accountsAPI + 'search-accounts/' + this.currentAccount.accountId + '/orders?groupId=' + themeDisplay.getScopeGroupId() + `&p_auth=${window.Liferay.authToken}&page=1&pageSize=10&q=${query}`,
 			{
 				method: 'GET'
 			}
