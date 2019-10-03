38dfb8a448e6b4cd69af82a204e5027bb1ae38b0
Andrea Sbarra <andrea.ale.sbarra@gmail.com>
2019-10-03 15:20:34 +0200

    COMMERCE-2035 autogenerated code (gradle buildREST)

--- headless/headless-commerce/headless-commerce-admin-inventory-impl/src/main/java/com/liferay/headless/commerce/admin/inventory/internal/resource/v1_0/BaseWarehouseItemResourceImpl.java
+++ headless/headless-commerce/headless-commerce-admin-inventory-impl/src/main/java/com/liferay/headless/commerce/admin/inventory/internal/resource/v1_0/BaseWarehouseItemResourceImpl.java
@@ -59,38 +59,48 @@ public abstract class BaseWarehouseItemResourceImpl
 	implements WarehouseItemResource {
 
 	@Override
-	@GET
-	@Parameters(
-		value = {
-			@Parameter(in = ParameterIn.PATH, name = "id"),
-			@Parameter(in = ParameterIn.QUERY, name = "page"),
-			@Parameter(in = ParameterIn.QUERY, name = "pageSize")
-		}
-	)
-	@Path("/warehouses/{id}/warehouseItems/")
+	@DELETE
+	@Parameters(value = {@Parameter(in = ParameterIn.PATH, name = "id")})
+	@Path("/warehouseItems/{id}")
 	@Produces({"application/json", "application/xml"})
 	@Tags(value = {@Tag(name = "WarehouseItem")})
-	public Page<WarehouseItem> getWarehousIdWarehouseItemsPage(
-			@NotNull @Parameter(hidden = true) @PathParam("id") Long id,
-			@Context Pagination pagination)
+	public Response deleteWarehouseItem(
+			@NotNull @Parameter(hidden = true) @PathParam("id") Long id)
 		throws Exception {
 
-		return Page.of(Collections.emptyList());
+		Response.ResponseBuilder responseBuilder = Response.ok();
+
+		return responseBuilder.build();
+	}
+
+	@Override
+	@GET
+	@Parameters(value = {@Parameter(in = ParameterIn.PATH, name = "id")})
+	@Path("/warehouseItems/{id}")
+	@Produces({"application/json", "application/xml"})
+	@Tags(value = {@Tag(name = "WarehouseItem")})
+	public WarehouseItem getWarehouseItem(
+			@NotNull @Parameter(hidden = true) @PathParam("id") Long id)
+		throws Exception {
+
+		return new WarehouseItem();
 	}
 
 	@Override
 	@Consumes({"application/json", "application/xml"})
-	@POST
+	@PATCH
 	@Parameters(value = {@Parameter(in = ParameterIn.PATH, name = "id")})
-	@Path("/warehouses/{id}/warehouseItems/")
+	@Path("/warehouseItems/{id}")
 	@Produces({"application/json", "application/xml"})
 	@Tags(value = {@Tag(name = "WarehouseItem")})
-	public WarehouseItem postWarehousIdWarehouseItem(
+	public Response patchWarehouseItem(
 			@NotNull @Parameter(hidden = true) @PathParam("id") Long id,
 			WarehouseItem warehouseItem)
 		throws Exception {
 
-		return new WarehouseItem();
+		Response.ResponseBuilder responseBuilder = Response.ok();
+
+		return responseBuilder.build();
 	}
 
 	@Override
@@ -141,48 +151,38 @@ public abstract class BaseWarehouseItemResourceImpl
 	}
 
 	@Override
-	@DELETE
-	@Parameters(value = {@Parameter(in = ParameterIn.PATH, name = "id")})
-	@Path("/warehouseItems/{id}")
-	@Produces({"application/json", "application/xml"})
-	@Tags(value = {@Tag(name = "WarehouseItem")})
-	public Response deleteWarehouseItem(
-			@NotNull @Parameter(hidden = true) @PathParam("id") Long id)
-		throws Exception {
-
-		Response.ResponseBuilder responseBuilder = Response.ok();
-
-		return responseBuilder.build();
-	}
-
-	@Override
 	@GET
-	@Parameters(value = {@Parameter(in = ParameterIn.PATH, name = "id")})
-	@Path("/warehouseItems/{id}")
+	@Parameters(
+		value = {
+			@Parameter(in = ParameterIn.PATH, name = "id"),
+			@Parameter(in = ParameterIn.QUERY, name = "page"),
+			@Parameter(in = ParameterIn.QUERY, name = "pageSize")
+		}
+	)
+	@Path("/warehouses/{id}/warehouseItems/")
 	@Produces({"application/json", "application/xml"})
 	@Tags(value = {@Tag(name = "WarehouseItem")})
-	public WarehouseItem getWarehouseItem(
-			@NotNull @Parameter(hidden = true) @PathParam("id") Long id)
+	public Page<WarehouseItem> getWarehousIdWarehouseItemsPage(
+			@NotNull @Parameter(hidden = true) @PathParam("id") Long id,
+			@Context Pagination pagination)
 		throws Exception {
 
-		return new WarehouseItem();
+		return Page.of(Collections.emptyList());
 	}
 
 	@Override
 	@Consumes({"application/json", "application/xml"})
-	@PATCH
+	@POST
 	@Parameters(value = {@Parameter(in = ParameterIn.PATH, name = "id")})
-	@Path("/warehouseItems/{id}")
+	@Path("/warehouses/{id}/warehouseItems/")
 	@Produces({"application/json", "application/xml"})
 	@Tags(value = {@Tag(name = "WarehouseItem")})
-	public Response patchWarehouseItem(
+	public WarehouseItem postWarehousIdWarehouseItem(
 			@NotNull @Parameter(hidden = true) @PathParam("id") Long id,
 			WarehouseItem warehouseItem)
 		throws Exception {
 
-		Response.ResponseBuilder responseBuilder = Response.ok();
-
-		return responseBuilder.build();
+		return new WarehouseItem();
 	}
 
 	public void setContextCompany(Company contextCompany) {
