b7ce290df759c1e8b64f95482dd50b50b9f3e955
Andrea Sbarra <andrea.ale.sbarra@gmail.com>
2019-10-09 16:16:21 +0200

    COMMERCE-2361 account groups an account endpoints now support filter and sort parmeters

---
+++ headless/headless-commerce/headless-commerce-admin-account-impl/src/main/java/com/liferay/headless/commerce/admin/account/internal/odata/entity/v1_0/AccountGroupEntityModel.java
@@ -0,0 +1,48 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.headless.commerce.admin.account.internal.odata.entity.v1_0;
+
+import com.liferay.portal.kernel.search.Field;
+import com.liferay.portal.odata.entity.EntityField;
+import com.liferay.portal.odata.entity.EntityModel;
+import com.liferay.portal.odata.entity.StringEntityField;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author Andrea Sbarra
+ */
+public class AccountGroupEntityModel implements EntityModel {
+
+	public AccountGroupEntityModel() {
+		_entityFieldsMap = Stream.of(
+			new StringEntityField(
+				"name", locale -> Field.getSortableFieldName(Field.NAME))
+		).collect(
+			Collectors.toMap(EntityField::getName, Function.identity())
+		);
+	}
+
+	@Override
+	public Map<String, EntityField> getEntityFieldsMap() {
+		return _entityFieldsMap;
+	}
+
+	private final Map<String, EntityField> _entityFieldsMap;
+
+}
