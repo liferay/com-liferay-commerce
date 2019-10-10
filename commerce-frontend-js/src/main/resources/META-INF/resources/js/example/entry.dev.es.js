8c7bd133bc4788982e8ac3a7e9370b7fcae0d1e9
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-10-10 11:14:22 +0200

    COMMERCE-2252 commerce frontend js cleaned to be moved into 7.1.x-next

---
+++ commerce-frontend-js/src/main/resources/META-INF/resources/js/example/entry.dev.es.js
@@ -0,0 +1,13 @@
+import launcher from './entry.es';
+
+import './_example.scss';
+
+const props = {
+	spritemap: './assets/icons.svg',
+}
+
+launcher(
+	'exampleId',
+	'example-root-id',
+	props
+);
---
+++ commerce-frontend-js/webpack.config.dev.js
@@ -0,0 +1,82 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const {defineServerResponses} = require('./dev/fakeServerUtilities');
+
+const outputPath = path.resolve(__dirname, './dev/public');
+
+const getComponentPath = (component, entry) => path.join(
+	__dirname,
+	'src',
+	'main',
+	'resources',
+	'META-INF',
+	'resources',
+	'js',
+	component,
+	entry
+);
+
+// eslint-disable-next-line no-undef
+module.exports = {
+	devServer: {
+		before(app) {
+			defineServerResponses(app);
+		},
+		compress: false,
+		contentBase: './dev/public',
+		open: true,
+		openPage: 'index.html',
+		port: 9000,
+		proxy: {
+			'/o': {
+				target: 'http://localhost:8080/'
+			}
+		},
+		publicPath: '/',
+	},
+	devtool: 'inline-source-map',
+	entry: {
+		example: getComponentPath('example', 'entry.dev.es.js'),
+	},
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				exclude: /node_modules/,
+				test: /\.(js|jsx)$/,
+				use: [
+					{
+						loader: 'babel-loader',
+					},
+				],
+			},
+			{
+				test: /\.(scss|css)$/,
+				use: [
+					{loader: 'style-loader'},
+					{loader: 'css-loader'},
+					{loader: 'sass-loader'},
+				],
+			},
+			{
+				exclude: /node_modules/,
+				test: /\.tsx?$/,
+				use: 'ts-loader',
+			},
+		],
+	},
+	output: {
+		filename: '[name].js',
+		path: outputPath,
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			inject: false,
+			template: path.resolve(__dirname, './dev/public/index.html'),
+		}),
+	],
+	resolve: {
+		extensions: ['.js', '.jsx', '.ts', '.tsx'],
+	},
+};
