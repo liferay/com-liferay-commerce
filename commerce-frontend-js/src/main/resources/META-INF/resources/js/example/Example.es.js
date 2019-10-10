f52a89feb892f83392aa23cc0d45469c4af70f93
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-10-10 11:19:43 +0200

    COMMERCE-2252 auto SF

--- commerce-frontend-js/src/main/resources/META-INF/resources/js/example/Example.es.js
+++ commerce-frontend-js/src/main/resources/META-INF/resources/js/example/Example.es.js
@@ -2,9 +2,7 @@ import ClayIcon from '@clayui/icon';
 import React from 'react';
 
 function Example(props) {
-	return (
-		<ClayIcon spritemap={props.spritemap} symbol="plus" />
-	);
-};
+	return <ClayIcon spritemap={props.spritemap} symbol="plus" />;
+}
 
 export default Example;
