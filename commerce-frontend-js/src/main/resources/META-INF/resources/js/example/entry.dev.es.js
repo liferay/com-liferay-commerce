b6a1da8c296ae25736c890bafa0bedd2a82a65dc
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-10-10 11:19:43 +0200

    COMMERCE-2252 auto SF

--- commerce-frontend-js/src/main/resources/META-INF/resources/js/example/entry.dev.es.js
+++ commerce-frontend-js/src/main/resources/META-INF/resources/js/example/entry.dev.es.js
@@ -3,11 +3,7 @@ import launcher from './entry.es';
 import './_example.scss';
 
 const props = {
-	spritemap: './assets/icons.svg',
-}
+	spritemap: './assets/icons.svg'
+};
 
-launcher(
-	'exampleId',
-	'example-root-id',
-	props
-);
+launcher('exampleId', 'example-root-id', props);
