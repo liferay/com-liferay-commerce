773b50eb74a50fa84442863dde154a7cbb2571a6
Alec Sloan <Alec.sloan@Liferay.com>
2019-10-15 17:54:10 -0700

    COMMERCE-2014 Add finder for orders by groupid and paymentEngineKey

--- commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceOrderLocalServiceImpl.java
+++ commerce-service/src/main/java/com/liferay/commerce/service/impl/CommerceOrderLocalServiceImpl.java
@@ -625,6 +625,14 @@ public class CommerceOrderLocalServiceImpl
 	}
 
 	@Override
+	public List<CommerceOrder> getCommerceOrders(
+		long groupId, String commercePaymentMethodKey) {
+
+		return commerceOrderPersistence.findByG_CP(
+			groupId, commercePaymentMethodKey);
+	}
+
+	@Override
 	public List<CommerceOrder> getCommerceOrdersByBillingAddress(
 		long billingAddressId) {
 
