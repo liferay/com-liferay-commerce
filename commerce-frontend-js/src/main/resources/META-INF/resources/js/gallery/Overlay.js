91a1034943950e40836ee3f2aa9e513a9515a592
Giacomo Zinetti <giacomo.zinetti@gmail.com>
2019-10-16 14:44:31 +0200

    COMMERCE-2262 Customizable background color

--- commerce-frontend-js/src/main/resources/META-INF/resources/js/gallery/Overlay.js
+++ commerce-frontend-js/src/main/resources/META-INF/resources/js/gallery/Overlay.js
@@ -2,9 +2,9 @@ import React from "react";
 import PropTypes from 'prop-types';
 import Arrows from './Arrows';
 
-export default function Overlay({ onClose, onNext, onPrev, src, title }) {
+export default function Overlay({ background, onClose, onNext, onPrev, src, title }) {
   return (
-    <div className="gallery-overlay" onClick={onClose}>
+    <div className="gallery-overlay" onClick={onClose} style={{background}}>
       <img alt={title} src={src} />
       <Arrows onNext={onNext} onPrev={onPrev} />
     </div>
