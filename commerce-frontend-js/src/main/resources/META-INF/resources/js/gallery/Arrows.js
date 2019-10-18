68cac5c7a9c0cbde1b9ae06b6b4908203143f4cf
Giacomo Zinetti <giacomo.zinetti@gmail.com>
2019-10-18 11:04:32 +0200

    COMMERCE-2262 auto sf

--- commerce-frontend-js/src/main/resources/META-INF/resources/js/gallery/Arrows.js
+++ commerce-frontend-js/src/main/resources/META-INF/resources/js/gallery/Arrows.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Arrows({ onNext, onPrev }) {
-  return (
-    <>
-      {onPrev && <div className="arrow prev" onClick={onPrev} />}
-      {onNext && <div className="arrow next" onClick={onNext} />}
-    </>
-  );
+export default function Arrows({onNext, onPrev}) {
+	return (
+		<>
+			{onPrev && <div className="arrow prev" onClick={onPrev} />}
+			{onNext && <div className="arrow next" onClick={onNext} />}
+		</>
+	);
 }
 
 Arrows.propTypes = {
-  onNext: PropTypes.func,
-  onPrev: PropTypes.func
-}
+	onNext: PropTypes.func,
+	onPrev: PropTypes.func
+};
