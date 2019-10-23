61e4116fd9626acd98306f6dc46a535948b4d200
Alessio Rendina <asilcomando@gmail.com>
2019-10-23 18:28:37 +0200

    COMMERCE-2367 Added new item selector view

---
+++ commerce-item-selector-web/src/main/java/com/liferay/commerce/item/selector/web/internal/CommerceProductInstanceItemSelectorView.java
@@ -0,0 +1,138 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.item.selector.web.internal;
+
+import com.liferay.commerce.item.selector.criterion.CommerceProductInstanceItemSelectorCriterion;
+import com.liferay.commerce.item.selector.web.internal.display.context.CommerceProductInstanceItemSelectorViewDisplayContext;
+import com.liferay.commerce.price.list.service.CommercePriceEntryLocalService;
+import com.liferay.commerce.price.list.service.CommercePriceListService;
+import com.liferay.commerce.product.service.CPInstanceService;
+import com.liferay.item.selector.ItemSelectorReturnType;
+import com.liferay.item.selector.ItemSelectorView;
+import com.liferay.item.selector.criteria.UUIDItemSelectorReturnType;
+import com.liferay.portal.kernel.language.LanguageUtil;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.ListUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.io.IOException;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import javax.portlet.PortletURL;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(immediate = true, service = ItemSelectorView.class)
+public class CommerceProductInstanceItemSelectorView
+	implements ItemSelectorView<CommerceProductInstanceItemSelectorCriterion> {
+
+	@Override
+	public Class<CommerceProductInstanceItemSelectorCriterion>
+		getItemSelectorCriterionClass() {
+
+		return CommerceProductInstanceItemSelectorCriterion.class;
+	}
+
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Override
+	public List<ItemSelectorReturnType> getSupportedItemSelectorReturnTypes() {
+		return _supportedItemSelectorReturnTypes;
+	}
+
+	@Override
+	public String getTitle(Locale locale) {
+		return LanguageUtil.get(locale, "products");
+	}
+
+	@Override
+	public boolean isShowSearch() {
+		return true;
+	}
+
+	@Override
+	public boolean isVisible(ThemeDisplay themeDisplay) {
+		return true;
+	}
+
+	@Override
+	public void renderHTML(
+			ServletRequest servletRequest, ServletResponse servletResponse,
+			CommerceProductInstanceItemSelectorCriterion
+				commerceProductInstanceItemSelectorCriterion,
+			PortletURL portletURL, String itemSelectedEventName, boolean search)
+		throws IOException, ServletException {
+
+		HttpServletRequest httpServletRequest =
+			(HttpServletRequest)servletRequest;
+
+		CommerceProductInstanceItemSelectorViewDisplayContext
+			commerceProductInstanceItemSelectorViewDisplayContext =
+				new CommerceProductInstanceItemSelectorViewDisplayContext(
+					httpServletRequest, portletURL, itemSelectedEventName,
+					_commercePriceEntryLocalService, _commercePriceListService,
+					_cpInstanceService);
+
+		httpServletRequest.setAttribute(
+			WebKeys.PORTLET_DISPLAY_CONTEXT,
+			commerceProductInstanceItemSelectorViewDisplayContext);
+
+		ServletContext servletContext = getServletContext();
+
+		RequestDispatcher requestDispatcher =
+			servletContext.getRequestDispatcher(
+				"/product_instance_item_selector.jsp");
+
+		requestDispatcher.include(servletRequest, servletResponse);
+	}
+
+	private static final List<ItemSelectorReturnType>
+		_supportedItemSelectorReturnTypes = Collections.unmodifiableList(
+			ListUtil.fromArray(
+				new ItemSelectorReturnType[] {
+					new UUIDItemSelectorReturnType()
+				}));
+
+	@Reference
+	private CommercePriceEntryLocalService _commercePriceEntryLocalService;
+
+	@Reference
+	private CommercePriceListService _commercePriceListService;
+
+	@Reference
+	private CPInstanceService _cpInstanceService;
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.commerce.item.selector.web)"
+	)
+	private ServletContext _servletContext;
+
+}
