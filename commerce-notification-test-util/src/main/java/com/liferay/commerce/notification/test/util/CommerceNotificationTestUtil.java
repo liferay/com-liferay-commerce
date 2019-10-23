76ec7803cc7d010afde821829b56faabb8d7181d
Luca Pellizzon <luca.pellizzon@liferay.com>
2019-10-23 17:05:49 +0200

    COMMERCE-2106 new test util class for notifications

---
+++ commerce-notification-test-util/src/main/java/com/liferay/commerce/notification/test/util/CommerceNotificationTestUtil.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.notification.test.util;
+
+import com.liferay.commerce.notification.model.CommerceNotificationTemplate;
+import com.liferay.commerce.notification.service.CommerceNotificationTemplateLocalServiceUtil;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+
+/**
+ * @author Luca Pellizzon
+ */
+public class CommerceNotificationTestUtil {
+
+	public static CommerceNotificationTemplate addCommerceNotificationTemplate(
+			String name, String description, String from, String to,
+			String type, ServiceContext serviceContext)
+		throws PortalException {
+
+		return CommerceNotificationTemplateLocalServiceUtil.
+			addCommerceNotificationTemplate(
+				name, description, from, RandomTestUtil.randomLocaleStringMap(),
+				to, null, null, type, true,
+				RandomTestUtil.randomLocaleStringMap(),
+				RandomTestUtil.randomLocaleStringMap(), serviceContext);
+	}
+
+}
