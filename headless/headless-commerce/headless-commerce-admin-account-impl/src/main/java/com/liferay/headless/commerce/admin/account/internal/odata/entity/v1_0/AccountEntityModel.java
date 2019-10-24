09674ad682479bb29206fb687bfa9eae21077f6d
Andrea Sbarra <andrea.ale.sbarra@gmail.com>
2019-10-24 17:58:19 +0200

    COMMERCE-2361 Auto SF (gradle formatSource)

--- headless/headless-commerce/headless-commerce-admin-account-impl/src/main/java/com/liferay/headless/commerce/admin/account/internal/odata/entity/v1_0/AccountEntityModel.java
+++ headless/headless-commerce/headless-commerce-admin-account-impl/src/main/java/com/liferay/headless/commerce/admin/account/internal/odata/entity/v1_0/AccountEntityModel.java
@@ -14,15 +14,15 @@
 
 package com.liferay.headless.commerce.admin.account.internal.odata.entity.v1_0;
 
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
 import com.liferay.portal.kernel.search.Field;
 import com.liferay.portal.odata.entity.EntityField;
 import com.liferay.portal.odata.entity.EntityModel;
 import com.liferay.portal.odata.entity.StringEntityField;
+
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author Andrea Sbarra
