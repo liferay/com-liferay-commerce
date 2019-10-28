00b509f930747d71c0147727177dc73eefc9adc4
Andrea Sbarra <andrea.ale.sbarra@gmail.com>
2019-10-28 10:20:12 +0100

    COMMERCE-2349 PR change request applied

--- headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/odata/entity/v1_0/SpecificationEntityModel.java
+++ headless/headless-commerce/headless-commerce-admin-catalog-impl/src/main/java/com/liferay/headless/commerce/admin/catalog/internal/odata/entity/v1_0/SpecificationEntityModel.java
@@ -15,6 +15,7 @@
 package com.liferay.headless.commerce.admin.catalog.internal.odata.entity.v1_0;
 
 import com.liferay.portal.kernel.util.LocaleUtil;
+import com.liferay.portal.kernel.util.LocalizationUtil;
 import com.liferay.portal.odata.entity.EntityField;
 import com.liferay.portal.odata.entity.EntityModel;
 import com.liferay.portal.odata.entity.StringEntityField;
@@ -34,7 +35,8 @@ public class SpecificationEntityModel implements EntityModel {
 			new StringEntityField("key", locale -> "key"),
 			new StringEntityField(
 				"title",
-				locale -> "title_".concat(LocaleUtil.toLanguageId(locale)))
+				locale -> LocalizationUtil.getLocalizedName(
+					"title", LocaleUtil.toLanguageId(locale)))
 		).collect(
 			Collectors.toMap(EntityField::getName, Function.identity())
 		);
