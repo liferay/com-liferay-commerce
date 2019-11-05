a8a7e8ec6f958f8caa52c22c1343ea312ea88b69
Alessio Rendina <asilcomando@gmail.com>
2019-11-05 19:46:15 +0100

    COMMERCE-2401 Added login post action

---
+++ commerce-account-service/src/main/java/com/liferay/commerce/account/internal/events/LoginPostAction.java
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.account.internal.events;
+
+import com.liferay.commerce.account.configuration.CommerceAccountServiceConfiguration;
+import com.liferay.commerce.account.service.CommerceAccountUserRelLocalService;
+import com.liferay.portal.kernel.events.Action;
+import com.liferay.portal.kernel.events.LifecycleAction;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.module.configuration.ConfigurationProvider;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.service.ServiceContextFactory;
+import com.liferay.portal.kernel.util.Portal;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Alessio Antonio Rendina
+ */
+@Component(
+	immediate = true, property = "key=login.events.post",
+	service = LifecycleAction.class
+)
+public class LoginPostAction extends Action {
+
+	@Override
+	public void run(
+		HttpServletRequest httpServletRequest,
+		HttpServletResponse httpServletResponse) {
+
+		try {
+			CommerceAccountServiceConfiguration
+				commerceAccountServiceConfiguration =
+					_configurationProvider.getSystemConfiguration(
+						CommerceAccountServiceConfiguration.class);
+
+			if (commerceAccountServiceConfiguration.
+					applyDefaultRoleToExistingUsers()) {
+
+				long userId = _portal.getUserId(httpServletRequest);
+
+				ServiceContext serviceContext =
+					ServiceContextFactory.getInstance(httpServletRequest);
+
+				_commerceAccountUserRelLocalService.addDefaultRole(
+					userId, serviceContext);
+			}
+		}
+		catch (Exception e) {
+			_log.error(e, e);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LoginPostAction.class);
+
+	@Reference
+	private CommerceAccountUserRelLocalService
+		_commerceAccountUserRelLocalService;
+
+	@Reference
+	private ConfigurationProvider _configurationProvider;
+
+	@Reference
+	private Portal _portal;
+
+}
