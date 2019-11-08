82a25a63f28b36941141273e911ab8801f13f773
Alec Sloan <Alec.sloan@Liferay.com>
2019-11-07 16:10:15 -0800

    COMMERCE-2290 Change Interface param to reflect impl

--- commerce-account-api/src/main/java/com/liferay/commerce/account/util/CommerceAccountHelper.java
+++ commerce-account-api/src/main/java/com/liferay/commerce/account/util/CommerceAccountHelper.java
@@ -42,7 +42,7 @@ public interface CommerceAccountHelper {
 			long groupId, HttpServletRequest httpServletRequest)
 		throws PortalException;
 
-	public long[] getUserCommerceAccountIds(long userId, long siteGroupId)
+	public long[] getUserCommerceAccountIds(long userId, long channelGroupId)
 		throws PortalException;
 
 	public void setCurrentCommerceAccount(
