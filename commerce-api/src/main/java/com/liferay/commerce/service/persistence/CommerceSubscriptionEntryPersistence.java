8bed5ac98f732e2d795fdf94174773080d30201f
Luca Pellizzon <luca.pellizzon@liferay.com>
2019-11-12 10:41:49 +0100

    COMMERCE-2433 buildService

--- commerce-api/src/main/java/com/liferay/commerce/service/persistence/CommerceSubscriptionEntryPersistence.java
+++ commerce-api/src/main/java/com/liferay/commerce/service/persistence/CommerceSubscriptionEntryPersistence.java
@@ -398,6 +398,150 @@ public interface CommerceSubscriptionEntryPersistence
 	public int countByUuid_C(String uuid, long companyId);
 
 	/**
+	 * Returns all the commerce subscription entries where groupId = &#63;.
+	 *
+	 * @param groupId the group ID
+	 * @return the matching commerce subscription entries
+	 */
+	public java.util.List<CommerceSubscriptionEntry> findByGroupId(
+		long groupId);
+
+	/**
+	 * Returns a range of all the commerce subscription entries where groupId = &#63;.
+	 *
+	 * <p>
+	 * Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to <code>QueryUtil#ALL_POS</code> will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent, then the query will include the default ORDER BY logic from <code>CommerceSubscriptionEntryModelImpl</code>.
+	 * </p>
+	 *
+	 * @param groupId the group ID
+	 * @param start the lower bound of the range of commerce subscription entries
+	 * @param end the upper bound of the range of commerce subscription entries (not inclusive)
+	 * @return the range of matching commerce subscription entries
+	 */
+	public java.util.List<CommerceSubscriptionEntry> findByGroupId(
+		long groupId, int start, int end);
+
+	/**
+	 * Returns an ordered range of all the commerce subscription entries where groupId = &#63;.
+	 *
+	 * <p>
+	 * Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to <code>QueryUtil#ALL_POS</code> will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent, then the query will include the default ORDER BY logic from <code>CommerceSubscriptionEntryModelImpl</code>.
+	 * </p>
+	 *
+	 * @param groupId the group ID
+	 * @param start the lower bound of the range of commerce subscription entries
+	 * @param end the upper bound of the range of commerce subscription entries (not inclusive)
+	 * @param orderByComparator the comparator to order the results by (optionally <code>null</code>)
+	 * @return the ordered range of matching commerce subscription entries
+	 */
+	public java.util.List<CommerceSubscriptionEntry> findByGroupId(
+		long groupId, int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator
+			<CommerceSubscriptionEntry> orderByComparator);
+
+	/**
+	 * Returns an ordered range of all the commerce subscription entries where groupId = &#63;.
+	 *
+	 * <p>
+	 * Useful when paginating results. Returns a maximum of <code>end - start</code> instances. <code>start</code> and <code>end</code> are not primary keys, they are indexes in the result set. Thus, <code>0</code> refers to the first result in the set. Setting both <code>start</code> and <code>end</code> to <code>QueryUtil#ALL_POS</code> will return the full result set. If <code>orderByComparator</code> is specified, then the query will include the given ORDER BY logic. If <code>orderByComparator</code> is absent, then the query will include the default ORDER BY logic from <code>CommerceSubscriptionEntryModelImpl</code>.
+	 * </p>
+	 *
+	 * @param groupId the group ID
+	 * @param start the lower bound of the range of commerce subscription entries
+	 * @param end the upper bound of the range of commerce subscription entries (not inclusive)
+	 * @param orderByComparator the comparator to order the results by (optionally <code>null</code>)
+	 * @param useFinderCache whether to use the finder cache
+	 * @return the ordered range of matching commerce subscription entries
+	 */
+	public java.util.List<CommerceSubscriptionEntry> findByGroupId(
+		long groupId, int start, int end,
+		com.liferay.portal.kernel.util.OrderByComparator
+			<CommerceSubscriptionEntry> orderByComparator,
+		boolean useFinderCache);
+
+	/**
+	 * Returns the first commerce subscription entry in the ordered set where groupId = &#63;.
+	 *
+	 * @param groupId the group ID
+	 * @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	 * @return the first matching commerce subscription entry
+	 * @throws NoSuchSubscriptionEntryException if a matching commerce subscription entry could not be found
+	 */
+	public CommerceSubscriptionEntry findByGroupId_First(
+			long groupId,
+			com.liferay.portal.kernel.util.OrderByComparator
+				<CommerceSubscriptionEntry> orderByComparator)
+		throws NoSuchSubscriptionEntryException;
+
+	/**
+	 * Returns the first commerce subscription entry in the ordered set where groupId = &#63;.
+	 *
+	 * @param groupId the group ID
+	 * @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	 * @return the first matching commerce subscription entry, or <code>null</code> if a matching commerce subscription entry could not be found
+	 */
+	public CommerceSubscriptionEntry fetchByGroupId_First(
+		long groupId,
+		com.liferay.portal.kernel.util.OrderByComparator
+			<CommerceSubscriptionEntry> orderByComparator);
+
+	/**
+	 * Returns the last commerce subscription entry in the ordered set where groupId = &#63;.
+	 *
+	 * @param groupId the group ID
+	 * @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	 * @return the last matching commerce subscription entry
+	 * @throws NoSuchSubscriptionEntryException if a matching commerce subscription entry could not be found
+	 */
+	public CommerceSubscriptionEntry findByGroupId_Last(
+			long groupId,
+			com.liferay.portal.kernel.util.OrderByComparator
+				<CommerceSubscriptionEntry> orderByComparator)
+		throws NoSuchSubscriptionEntryException;
+
+	/**
+	 * Returns the last commerce subscription entry in the ordered set where groupId = &#63;.
+	 *
+	 * @param groupId the group ID
+	 * @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	 * @return the last matching commerce subscription entry, or <code>null</code> if a matching commerce subscription entry could not be found
+	 */
+	public CommerceSubscriptionEntry fetchByGroupId_Last(
+		long groupId,
+		com.liferay.portal.kernel.util.OrderByComparator
+			<CommerceSubscriptionEntry> orderByComparator);
+
+	/**
+	 * Returns the commerce subscription entries before and after the current commerce subscription entry in the ordered set where groupId = &#63;.
+	 *
+	 * @param commerceSubscriptionEntryId the primary key of the current commerce subscription entry
+	 * @param groupId the group ID
+	 * @param orderByComparator the comparator to order the set by (optionally <code>null</code>)
+	 * @return the previous, current, and next commerce subscription entry
+	 * @throws NoSuchSubscriptionEntryException if a commerce subscription entry with the primary key could not be found
+	 */
+	public CommerceSubscriptionEntry[] findByGroupId_PrevAndNext(
+			long commerceSubscriptionEntryId, long groupId,
+			com.liferay.portal.kernel.util.OrderByComparator
+				<CommerceSubscriptionEntry> orderByComparator)
+		throws NoSuchSubscriptionEntryException;
+
+	/**
+	 * Removes all the commerce subscription entries where groupId = &#63; from the database.
+	 *
+	 * @param groupId the group ID
+	 */
+	public void removeByGroupId(long groupId);
+
+	/**
+	 * Returns the number of commerce subscription entries where groupId = &#63;.
+	 *
+	 * @param groupId the group ID
+	 * @return the number of matching commerce subscription entries
+	 */
+	public int countByGroupId(long groupId);
+
+	/**
 	 * Returns all the commerce subscription entries where companyId = &#63;.
 	 *
 	 * @param companyId the company ID
