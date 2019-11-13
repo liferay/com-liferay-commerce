94a4f6ced14d1f1a836486b441fc75eaceb58d93
Alec Sloan <Alec.sloan@Liferay.com>
2019-11-13 12:31:08 -0800

    COMMERCE-1618 Reindex definitions when a new channel is linked to it

--- commerce-initializer-util/src/main/java/com/liferay/commerce/initializer/util/CPDefinitionsImporter.java
+++ commerce-initializer-util/src/main/java/com/liferay/commerce/initializer/util/CPDefinitionsImporter.java
@@ -65,6 +65,8 @@ import com.liferay.portal.kernel.log.Log;
 import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.liferay.portal.kernel.model.Company;
 import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.search.Indexer;
+import com.liferay.portal.kernel.search.IndexerRegistryUtil;
 import com.liferay.portal.kernel.service.CompanyLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.service.UserLocalService;
@@ -350,6 +352,11 @@ public class CPDefinitionsImporter {
 			_commerceChannelRelLocalService.addCommerceChannelRel(
 				CPDefinition.class.getName(), cpDefinition.getCPDefinitionId(),
 				commerceChannelId, serviceContext);
+
+			Indexer<CPDefinition> indexer =
+				IndexerRegistryUtil.nullSafeGetIndexer(CPDefinition.class);
+
+			indexer.reindex(cpDefinition);
 
 			return cpDefinition;
 		}
