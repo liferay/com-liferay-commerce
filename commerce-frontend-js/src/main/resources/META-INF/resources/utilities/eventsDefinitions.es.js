f68d9f1ff9fa92e91e686be054ac1f946c37bda7
Ethan Bustad <ethan.bustad@liferay.com>
2019-11-22 15:20:52 -0800

    COMMERCE-2294 order details forms fixed on 7.2

--- commerce-frontend-js/src/main/resources/META-INF/resources/utilities/eventsDefinitions.es.js
+++ commerce-frontend-js/src/main/resources/META-INF/resources/utilities/eventsDefinitions.es.js
@@ -1,9 +1,11 @@
 export const OPEN_SIDE_PANEL = 'open-side-panel';
 export const OPEN_MODAL = 'open-modal';
 export const OPEN = 'open-component';
+export const CURRENT_PRODUCT_UPDATED = 'current-product-updated';
 
 export default {
 	OPEN,
 	OPEN_MODAL,
-	OPEN_SIDE_PANEL
+	OPEN_SIDE_PANEL,
+	CURRENT_PRODUCT_UPDATED
 };
