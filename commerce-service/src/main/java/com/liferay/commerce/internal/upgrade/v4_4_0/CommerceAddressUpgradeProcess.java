f78c633c76499606af76c2e5cf43133119fb4506
Andrea Sbarra <andrea.ale.sbarra@gmail.com>
2019-11-25 15:20:21 +0100

    COMMERCE-2392 PR's change requests applied

--- commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v4_4_0/CommerceAddressUpgradeProcess.java
+++ commerce-service/src/main/java/com/liferay/commerce/internal/upgrade/v4_4_0/CommerceAddressUpgradeProcess.java
@@ -14,51 +14,18 @@
 
 package com.liferay.commerce.internal.upgrade.v4_4_0;
 
+import com.liferay.commerce.internal.upgrade.base.BaseCommerceServiceUpgradeProcess;
 import com.liferay.commerce.model.impl.CommerceAddressImpl;
-import com.liferay.petra.string.StringPool;
-import com.liferay.portal.kernel.log.Log;
-import com.liferay.portal.kernel.log.LogFactoryUtil;
-import com.liferay.portal.kernel.upgrade.UpgradeProcess;
 
 /**
  * @author Andrea Sbarra
  */
-public class CommerceAddressUpgradeProcess extends UpgradeProcess {
+public class CommerceAddressUpgradeProcess extends BaseCommerceServiceUpgradeProcess {
 
 	@Override
 	protected void doUpgrade() throws Exception {
-		_addColumn();
+		addColumn(CommerceAddressImpl.class, CommerceAddressImpl.TABLE_NAME, "externalReferenceCode", "VARCHAR(75)");
 	}
 
-	private void _addColumn() throws Exception {
-		String columnName = "externalReferenceCode";
-		String columnType = "VARCHAR(75)";
-		String tableName = CommerceAddressImpl.TABLE_NAME;
-		Class<?> entityClass = CommerceAddressImpl.class;
-
-		if (_log.isInfoEnabled()) {
-			_log.info(
-				String.format(
-					"Adding column %s to table %s", columnName, tableName));
-		}
-
-		if (!hasColumn(tableName, columnName)) {
-			alter(
-				entityClass,
-				new AlterTableAddColumn(
-					columnName + StringPool.SPACE + columnType));
-		}
-		else {
-			if (_log.isInfoEnabled()) {
-				_log.info(
-					String.format(
-						"Column %s already exists on table %s", columnName,
-						tableName));
-			}
-		}
-	}
-
-	private static final Log _log = LogFactoryUtil.getLog(
-		CommerceAddressUpgradeProcess.class);
 
 }
