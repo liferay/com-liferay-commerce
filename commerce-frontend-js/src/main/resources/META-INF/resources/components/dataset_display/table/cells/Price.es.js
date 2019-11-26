7c2d9e789a445fe80c8929cad10973995ad2fe2a
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-11-26 17:53:02 +0100

    COMMERCE-2294 prices now compatible with clay

--- commerce-frontend-js/src/main/resources/META-INF/resources/components/dataset_display/table/cells/Price.es.js
+++ commerce-frontend-js/src/main/resources/META-INF/resources/components/dataset_display/table/cells/Price.es.js
@@ -2,14 +2,14 @@ import React from 'react';
 
 function Price(props) {
 	return props.data.promoPrice ? (
-		<span className="price">
-			<b className="text-danger ">{props.data.promoPrice}</b>
+		<span className="price-container">
+			<b className="text-danger">{props.data.promoPrice}</b>
 			<span className="ml-2 text-secondary">
 				<del>{props.value}</del>
 			</span>
 		</span>
 	) : (
-		<span className="price">
+		<span className="price-container">
 			<b>{props.value}</b>
 		</span>
 	);
