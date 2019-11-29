f64d02ff7d2a3b28ff3ba8615eecc6c0e7cabeca
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-11-29 10:13:50 +0100

    COMMERCE-2491 fixed UI bugs on minium

--- commerce-frontend-js/src/main/resources/META-INF/resources/utilities/eventsDefinitions.es.js
+++ commerce-frontend-js/src/main/resources/META-INF/resources/utilities/eventsDefinitions.es.js
@@ -2,10 +2,14 @@ export const OPEN_SIDE_PANEL = 'open-side-panel';
 export const OPEN_MODAL = 'open-modal';
 export const OPEN = 'open-component';
 export const CURRENT_PRODUCT_UPDATED = 'current-product-updated';
+export const PRODUCT_OPTIONS_CHANGED = 'product-options-changed';
+export const CURRENT_PRODUCT_STATUS_CHANGED = 'current-product-status-changed'
 
 export default {
 	OPEN,
 	OPEN_MODAL,
 	OPEN_SIDE_PANEL,
-	CURRENT_PRODUCT_UPDATED
+	CURRENT_PRODUCT_UPDATED,
+	PRODUCT_OPTIONS_CHANGED,
+	CURRENT_PRODUCT_STATUS_CHANGED
 };
