e70a72498e250ef95f401d5fd2b6feaca2bedd0e
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-12-04 19:36:02 +0100

    COMMERCE-2514 unloaded contents in modals will be automatically hidden by a loading indicator

--- commerce-frontend-js/src/main/resources/META-INF/resources/components/side_panel/entry.dev.es.js
+++ commerce-frontend-js/src/main/resources/META-INF/resources/components/side_panel/entry.dev.es.js
@@ -3,8 +3,9 @@ import launcher from './entry.es';
 import '../../styles/main.scss';
 
 window.SidePanel = launcher('sidePanel', 'side-panel', {
+	containerSelector: '.smart-table-wrapper',
 	id: 'sidePanelTestId',
 	size: 'md',
 	spritemap: './assets/icons.svg',
-	topAnchor: document.querySelector('.top-anchor')
+	topAnchorSelector: '.top-anchor',
 });
