3043ae9171bcefd890223e652cedea6253708362
Gianmarco Brunialti <gianmarco.brunialti@liferay.com>
2019-12-05 09:59:36 +0100

    COMMERCE-2501 Secured speedwell sliders cb removal

--- commerce-theme-speedwell/commerce-theme-speedwell/src/js/main.js
+++ commerce-theme-speedwell/commerce-theme-speedwell/src/js/main.js
@@ -1,12 +1,9 @@
 AUI().ready(
 	function() {
-		console.log('%c[ AUI ready ]', 'background-color: #000; color: #00FFFF');
-
 		if (!!Speedwell && !!Speedwell.features) {
 			Speedwell.features.init.initializeFeatures();
 
 			Speedwell.features.sliders = [];
-			Speedwell.features.sliderCallbacks = [];
 
 			if ('sliderCallbacks' in Speedwell.features &&
 				Speedwell.features.sliderCallbacks.length) {
@@ -19,19 +16,9 @@ AUI().ready(
 							.sliders.push(cb(componentReady));
 					}
 				});
+
+				Speedwell.features.sliderCallbacks = [];
 			}
 		}
-	}
-);
-
-Liferay.Portlet.ready(
-	function(portletId, node) {}
-);
-
-Liferay.on(
-	'allPortletsReady',
-
-	function() {
-		console.log('%c[ All portlets ready ]', 'background-color: #000; color: #FFFF00');
 	}
 );
