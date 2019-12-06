340639566098ffbafe0beecd61d98b42b706c5b8
Fabio Diego mastrorilli <fabio.mastrorilli@liferay.com>
2019-12-06 11:56:48 +0100

    COMMERCE-2514 static env improved for side panel component

--- commerce-frontend-js/src/main/resources/META-INF/resources/components/dataset_display/entry.dev.es.js
+++ commerce-frontend-js/src/main/resources/META-INF/resources/components/dataset_display/entry.dev.es.js
@@ -2,7 +2,6 @@ import launcher from './entry.es';
 
 import '../../styles/main.scss';
 import { OPEN_SIDE_PANEL } from '../../utilities/eventsDefinitions.es';
-import Sidepanel from '../side_panel/entry.es';
 
 launcher('dataset-display', 'dataset-display', {
 	apiUrl: '/o/headless-commerce-admin-order/v1.0/orders/37174/orderItems',
@@ -13,7 +12,7 @@ launcher('dataset-display', 'dataset-display', {
 					id: 'sidePanelTestId',
 					options: {
 						slug: 'edit',
-						url: `/edit-content?ids=${ids.join(',')}`,
+						url: `/side-panel/edit.html?ids=${ids.join(',')}`,
 					}
 				})
 			},
